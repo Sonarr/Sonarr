f5cf44c2b39210ad0c2dc2268a2351a990bf5492
markus101 <markus.mcd5@gmail.com>
2010-09-25 09:26:31 -0700

    New Web theme
    Added IDownloadClientController & SabController (WIP)

---
+++ NzbDrone.Core/Controllers/IDownloadClientController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Controllers
+{
+    public interface IDownloadClientController
+    {
+        string AddByUrl(string url); //Should accept something other than string (NzbInfo?) returns result if applicable
+        bool IsInQueue(string goodName);//Should accept something other than string (Episode?) returns bool
+    }
+}
---
+++ NzbDrone.Core/Controllers/SabController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Controllers
+{
+    public class SabController : IDownloadClientController
+    {
+        private readonly IConfigController _config;
+
+        public SabController(IConfigController config)
+        {
+            _config = config;
+        }
+
+        public string AddByUrl(string url)
+        {
+             
+
+            return "";
+        }
+
+        public string AddByPath(string path)
+        {
+
+            return "";
+        }
+
+
+        public bool IsInQueue(string goodName)
+        {
+
+            return false;
+        }
+    }
+}
