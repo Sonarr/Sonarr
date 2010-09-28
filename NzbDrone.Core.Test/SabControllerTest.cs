a2967f46588c73dd7a3458ee9dbbd2b96bbc850c
markus101 <markus.mcd5@gmail.com>
2010-09-27 17:49:34 -0700

    IHttpController/HttpController Added (So we can Mock SABnzbd requests)
    More unit tests for SabController

--- NzbDrone.Core.Test/SabControllerTest.cs
+++ NzbDrone.Core.Test/SabControllerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Gallio.Framework;
 using log4net;
@@ -16,7 +17,7 @@ namespace NzbDrone.Core.Test
     public class SabControllerTest
     {
         [Test]
-        public void AddByUrl()
+        public void AddByUrlSuccess()
         {
             //Setup
             string sabnzbdInfo = "192.168.5.55:2222";
@@ -32,7 +33,10 @@ namespace NzbDrone.Core.Test
             config.Setup(c => c.GetValue("Password", String.Empty, false)).Returns(password);
             config.Setup(c => c.GetValue("Priority", String.Empty, false)).Returns(priority);
 
-            var target = new SabController(config.Object, new Mock<ILog>().Object);
+            var http = new Mock<IHttpController>();
+            http.Setup(s => s.GetRequest("http://192.168.5.55:2222/sabnzbd/api?mode=addurl&name=http://www.nzbclub.com/nzb_download.aspx?mid=1950232&priority=0&cat=tv&nzbname=This+is+an+Nzb&apikey=5c770e3197e4fe763423ee7c392c25d1&ma_username=admin&ma_password=pass")).Returns("ok");
+
+            var target = new SabController(config.Object, new Mock<ILog>().Object, http.Object);
 
             ItemInfo nzb = new ItemInfo();
             nzb.Link = new Uri("http://www.nzbclub.com/nzb_download.aspx?mid=1950232");
@@ -46,7 +50,7 @@ namespace NzbDrone.Core.Test
         }
 
         [Test]
-        public void IsInQueue()
+        public void AddByUrlError()
         {
             //Setup
             string sabnzbdInfo = "192.168.5.55:2222";
@@ -62,11 +66,127 @@ namespace NzbDrone.Core.Test
             config.Setup(c => c.GetValue("Password", String.Empty, false)).Returns(password);
             config.Setup(c => c.GetValue("Priority", String.Empty, false)).Returns(priority);
 
-            var target = new SabController(config.Object, new Mock<ILog>().Object);
+            var http = new Mock<IHttpController>();
+            http.Setup(s => s.GetRequest("http://192.168.5.55:2222/sabnzbd/api?mode=addurl&name=http://www.nzbclub.com/nzb_download.aspx?mid=1950232&priority=0&cat=tv&nzbname=This+is+an+Nzb&apikey=5c770e3197e4fe763423ee7c392c25d1&ma_username=admin&ma_password=pass")).Returns("error");
+
+            var target = new SabController(config.Object, new Mock<ILog>().Object, http.Object);
+
+            ItemInfo nzb = new ItemInfo();
+            nzb.Link = new Uri("http://www.nzbclub.com/nzb_download.aspx?mid=1950232");
+            nzb.Title = "This is an Nzb";
+
+            //Act
+            bool result = target.AddByUrl(nzb);
+
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void IsInQueue_True()
+        {
+            //Setup
+            string sabnzbdInfo = "192.168.5.55:2222";
+            string apikey = "5c770e3197e4fe763423ee7c392c25d1";
+            string username = "admin";
+            string password = "pass";
+            string priority = "0";
+
+            var config = new Mock<IConfigController>();
+            config.Setup(c => c.GetValue("SabnzbdInfo", String.Empty, false)).Returns(sabnzbdInfo);
+            config.Setup(c => c.GetValue("ApiKey", String.Empty, false)).Returns(apikey);
+            config.Setup(c => c.GetValue("Username", String.Empty, false)).Returns(username);
+            config.Setup(c => c.GetValue("Password", String.Empty, false)).Returns(password);
+            config.Setup(c => c.GetValue("Priority", String.Empty, false)).Returns(priority);
+
+            var http = new Mock<IHttpController>();
+            http.Setup(
+                s =>
+                s.GetRequest(
+                    "http://192.168.5.55:2222/sabnzbd/api?mode=queue&output=xml&apikey=5c770e3197e4fe763423ee7c392c25d1&ma_username=admin&ma_password=pass"))
+                .Returns(new StreamReader(@".\Files\Queue.xml").ReadToEnd());
+
+            var target = new SabController(config.Object, new Mock<ILog>().Object, http.Object);
 
             Episode episode = new Episode();
             FeedItem item = new FeedItem();
-            item.TitleFix = "This is my fixed title";
+            item.TitleFix = "Ubuntu Test";
+            episode.FeedItem = item;
+
+            //Act
+            bool result = target.IsInQueue(episode);
+
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void IsInQueue_False_Empty()
+        {
+            //Setup
+            string sabnzbdInfo = "192.168.5.55:2222";
+            string apikey = "5c770e3197e4fe763423ee7c392c25d1";
+            string username = "admin";
+            string password = "pass";
+            string priority = "0";
+
+            var config = new Mock<IConfigController>();
+            config.Setup(c => c.GetValue("SabnzbdInfo", String.Empty, false)).Returns(sabnzbdInfo);
+            config.Setup(c => c.GetValue("ApiKey", String.Empty, false)).Returns(apikey);
+            config.Setup(c => c.GetValue("Username", String.Empty, false)).Returns(username);
+            config.Setup(c => c.GetValue("Password", String.Empty, false)).Returns(password);
+            config.Setup(c => c.GetValue("Priority", String.Empty, false)).Returns(priority);
+
+            var http = new Mock<IHttpController>();
+            http.Setup(
+                s =>
+                s.GetRequest(
+                    "http://192.168.5.55:2222/sabnzbd/api?mode=queue&output=xml&apikey=5c770e3197e4fe763423ee7c392c25d1&ma_username=admin&ma_password=pass"))
+                .Returns(new StreamReader(@".\Files\QueueEmpty.xml").ReadToEnd());
+
+            var target = new SabController(config.Object, new Mock<ILog>().Object, http.Object);
+
+            Episode episode = new Episode();
+            FeedItem item = new FeedItem();
+            item.TitleFix = "Ubuntu Test";
+            episode.FeedItem = item;
+
+            //Act
+            bool result = target.IsInQueue(episode);
+
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void IsInQueue_False_Error()
+        {
+            //Setup
+            string sabnzbdInfo = "192.168.5.55:2222";
+            string apikey = "5c770e3197e4fe763423ee7c392c25d1";
+            string username = "admin";
+            string password = "pass";
+            string priority = "0";
+
+            var config = new Mock<IConfigController>();
+            config.Setup(c => c.GetValue("SabnzbdInfo", String.Empty, false)).Returns(sabnzbdInfo);
+            config.Setup(c => c.GetValue("ApiKey", String.Empty, false)).Returns(apikey);
+            config.Setup(c => c.GetValue("Username", String.Empty, false)).Returns(username);
+            config.Setup(c => c.GetValue("Password", String.Empty, false)).Returns(password);
+            config.Setup(c => c.GetValue("Priority", String.Empty, false)).Returns(priority);
+
+            var http = new Mock<IHttpController>();
+            http.Setup(
+                s =>
+                s.GetRequest(
+                    "http://192.168.5.55:2222/sabnzbd/api?mode=queue&output=xml&apikey=5c770e3197e4fe763423ee7c392c25d1&ma_username=admin&ma_password=pass"))
+                .Returns(new StreamReader(@".\Files\QueueError.xml").ReadToEnd());
+
+            var target = new SabController(config.Object, new Mock<ILog>().Object, http.Object);
+
+            Episode episode = new Episode();
+            FeedItem item = new FeedItem();
+            item.TitleFix = "Ubuntu Test";
             episode.FeedItem = item;
 
             //Act
--- NzbDrone.Core/Controllers/SabController.cs
+++ NzbDrone.Core/Controllers/SabController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -15,11 +16,13 @@ namespace NzbDrone.Core.Controllers
     {
         private readonly IConfigController _config;
         private readonly ILog _logger;
+        private readonly IHttpController _http;
 
-        public SabController(IConfigController config, ILog logger)
+        public SabController(IConfigController config, ILog logger, IHttpController http)
         {
             _config = config;
             _logger = logger;
+            _http = http;
         }
 
         #region IDownloadClientController Members
@@ -28,15 +31,19 @@ namespace NzbDrone.Core.Controllers
         {
             const string mode = "addurl";
             const string cat = "tv";
+            string priority = _config.GetValue("Priority", String.Empty, false);
             string name = nzb.Link.ToString().Replace("&", "%26");
             string nzbName = HttpUtility.UrlEncode(nzb.Title);
 
-            string action = string.Format("mode={0}&name={1}&cat={2}&nzbname={3}", mode, name, cat, nzbName);
+            string action = string.Format("mode={0}&name={1}&priority={2}&cat={3}&nzbname={4}", mode, name, priority, cat, nzbName);
             string request = GetSabRequest(action);
 
             _logger.DebugFormat("Adding report [{0}] to the queue.", nzbName);
 
-            if (SendRequest(request) == "ok")
+            string response = _http.GetRequest(request).Replace("\n", String.Empty);
+            _logger.DebugFormat("Queue Repsonse: [{0}]", response);
+
+            if (response == "ok")
                 return true;
 
             return false;
@@ -45,18 +52,20 @@ namespace NzbDrone.Core.Controllers
         public bool IsInQueue(Episode epsiode)
         {
             string action = "mode=queue&output=xml";
+            string request = GetSabRequest(action);
+            string response = _http.GetRequest(request);
 
-            XDocument xDoc = XDocument.Load(GetSabRequest(action));
+            XDocument xDoc = XDocument.Parse(response);
 
-            //If an Error Occurred, retuyrn
+            //If an Error Occurred, retuyrn)
             if (xDoc.Descendants("error").Count() != 0)
                 return false;
 
             if (xDoc.Descendants("queue").Count() == 0)
                 return false;
 
-            //Get the Count of Items in Queue where 'filename' is Equal to goodName, if not zero, return true (isInQueue)
-            if ((from s in xDoc.Descendants("slot") where s.Element("filename").ToString().Equals(epsiode.FeedItem.TitleFix, StringComparison.InvariantCultureIgnoreCase) select s).Count() != 0)
+            //Get the Count of Items in Queue where 'filename' is Equal to goodName, if not zero, return true (isInQueue)))
+            if ((from s in xDoc.Descendants("slot") where s.Element("filename").Value.Equals(epsiode.FeedItem.TitleFix, StringComparison.InvariantCultureIgnoreCase) select s).Count() != 0)
             {
                 _logger.DebugFormat("Episode in queue - '{0}'", epsiode.FeedItem.TitleFix);
 
@@ -74,19 +83,10 @@ namespace NzbDrone.Core.Controllers
             string username = _config.GetValue("Username", String.Empty, false);
             string password = _config.GetValue("Password", String.Empty, false);
             string apiKey = _config.GetValue("ApiKey", String.Empty, false);
-            string priority = _config.GetValue("Priority", String.Empty, false);
 
             return string.Format(
-                @"http://{0}/sabnzbd/api?$Action&priority={1}&apikey={2}&ma_username={3}&ma_password={4}",
-                sabnzbdInfo, priority, apiKey, username, password).Replace("$Action", action);
-        }
-
-        private string SendRequest(string request)
-        {
-            var webClient = new WebClient();
-            string response = webClient.DownloadString(request).Replace("\n", String.Empty);
-            _logger.DebugFormat("Queue Repsonse: [{0}]", response);
-            return response;
+                @"http://{0}/sabnzbd/api?$Action&apikey={1}&ma_username={2}&ma_password={3}",
+                sabnzbdInfo, apiKey, username, password).Replace("$Action", action);
         }
     }
 }
