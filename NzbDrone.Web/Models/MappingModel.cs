519e2df560d14d0af17dbd9ef6a596a348808c2e
Keivan <kay.one@gmail.com>
2011-02-03 17:00:55 -0800

    added missing file

---
+++ NzbDrone.Web/Models/MappingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NzbDrone.Core.Repository.Quality;
+
+namespace NzbDrone.Web.Models
+{
+    public class MappingModel
+    {
+        public int Id { get; set; }
+        public String Path { get; set; }
+    }
+}
