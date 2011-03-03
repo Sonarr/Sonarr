1de903e4da0d5fc6c77cc67718ff350361f10476
markus101 <markus.mcd5@gmail.com>
2011-03-02 17:16:48 -0800

    DisplayName added for EpisodeSortingModel (Used on UI).

--- NzbDrone.Web/Models/EpisodeSortingModel.cs
+++ NzbDrone.Web/Models/EpisodeSortingModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +11,38 @@ namespace NzbDrone.Web.Models
 {
     public class EpisodeSortingModel
     {
+        [DisplayName("Show Name")]
         public bool ShowName { get; set; }
+
+        [DisplayName("Episode Name")]
         public bool EpisodeName { get; set; }
+
+        [DisplayName("Replace Spaces")]
         public bool ReplaceSpaces { get; set; }
+
+        [DisplayName("Append Quality")]
         public bool AppendQuality { get; set; }
+
+        [DisplayName("Use Air By Date")]
         public bool UseAirByDate { get; set; }
+
+        [DisplayName("Use Season Folders")]
         public bool SeasonFolders { get; set; }
+
+        [DisplayName("Season Folder Format")]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [Display(Description = "This is a Description")]
         public string SeasonFolderFormat { get; set; }
+
+        [DisplayName("Separator Style")]
         public int SeparatorStyle { get; set; }
+
+        [DisplayName("Numbering Style")]
         public int NumberStyle { get; set; }
+
+        [DisplayName("Multi-Episode Style")]
         public int MultiEpisodeStyle { get; set; }
+
         public SelectList SeparatorStyles { get; set; }
         public SelectList NumberStyles { get; set; }
         public SelectList MultiEpisodeStyles { get; set; }
