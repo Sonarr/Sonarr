975d8bc67905f756a228775a5c9e41063e04bd09
Mark McDowall <markus.mcd5@gmail.com>
2011-04-26 16:43:27 -0700

    New Helper (Html Extension) for displaying the DescriptionAttribute.

---
+++ NzbDrone.Web/Helpers/DescriptionExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NzbDrone.Web.Helpers
+{
+    public static class DescriptionExtension
+    {
+        public static MvcHtmlString DescriptionFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+        {
+            var memberEx = expression.Body as MemberExpression;
+
+            if (memberEx == null)
+                throw new ArgumentException("Body not a member-expression.");
+
+            string name = memberEx.Member.Name;
+
+            var attributes = TypeDescriptor.GetProperties(typeof(TModel))[name].Attributes;
+            var desc = (DescriptionAttribute)attributes[typeof(DescriptionAttribute)];
+
+            return new MvcHtmlString(desc.Description);
+        }
+    }
+}
