9ee74d68ebfa3cb9232e128d373097577426a2b1
Mark McDowall <markus.mcd5@gmail.com>
2011-06-01 16:50:36 -0700

    Moved episodeSearch to it's own javascript file.

---
+++ NzbDrone.Web/Scripts/episodeSearch.js
@@ -0,0 +1,13 @@
+﻿var searchUrl = '../Episode/Search';
+
+function searchForEpisode(id) {
+    $.ajax({
+        type: "POST",
+        url: searchUrl,
+        data: jQuery.param({ episodeId: id }),
+        error: function (req, status, error) {
+            alert("Sorry! We could search for " + id + " at this time. " + error);
+        }
+    });
+}
+   
