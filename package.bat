3665856d791c0ceb519717201f8bff7382e6c843
kay.one <kay.one@gmail.com>
2011-06-01 19:41:24 -0700

    Updated deploy file to remove some unneeded files

--- package.bat
+++ package.bat
@@ -1,15 +1,20 @@
-SET TARGET=_deploy
+SET PACKAGEROOT=_rawPackage
+SET TARGET=%PACKAGEROOT%\NzbDrone
 
-rmdir /s "%TARGET%"
-xcopy IISExpress %TARGET%\IISExpress /E /V /I /F /Y
-xcopy NzbDrone\bin\Release\*.*  %TARGET%\ /E /V /I /F /Y
+xcopy IISExpress %TARGET%\IISExpress /E /V /I /Y
+xcopy NzbDrone\bin\Release\*.*  %TARGET%\ /E /V /I /Y
 
-xcopy NzbDrone.Web\bin\*.*  %TARGET%\NzbDrone.Web\bin\ /E /V /I /F /Y
-xcopy NzbDrone.Web\App_GlobalResources\*.*  %TARGET%\NzbDrone.Web\App_GlobalResources\ /E /V /I /F /Y
-xcopy NzbDrone.Web\Content\*.*  %TARGET%\NzbDrone.Web\Content\ /E /V /I /F /Y
-xcopy NzbDrone.Web\Scripts\*.*  %TARGET%\NzbDrone.Web\Scripts\ /E /V /I /F /Y
-xcopy NzbDrone.Web\Views\*.*  %TARGET%\NzbDrone.Web\Views\ /E /V /I /F /Y
+del %TARGET%\nlog.xml
+del %TARGET%\*.vshost.exe.*
 
+xcopy NzbDrone.Web\bin\*.*  %TARGET%\NzbDrone.Web\bin\ /E /V /I /Y
+xcopy NzbDrone.Web\App_GlobalResources\*.*  %TARGET%\NzbDrone.Web\App_GlobalResources\ /E /V /I /Y
+xcopy NzbDrone.Web\Content\*.*  %TARGET%\NzbDrone.Web\Content\ /E /V /I /Y
+xcopy NzbDrone.Web\Scripts\*.*  %TARGET%\NzbDrone.Web\Scripts\ /E /V /I /Y
+xcopy NzbDrone.Web\Views\*.*  %TARGET%\NzbDrone.Web\Views\ /E /V /I /Y
+
+del %TARGET%\NzbDrone.Web\bin\*.xml /q
+del %TARGET%\NzbDrone.Web\bin\ninject*.pdb /q
 
 xcopy NzbDrone.Web\log.config  %TARGET%\NzbDrone.Web\
 xcopy NzbDrone.Web\Global.asax  %TARGET%\NzbDrone.Web\
@@ -17,7 +22,8 @@ xcopy NzbDrone.Web\favicon.ico  %TARGET%\NzbDrone.Web\
 xcopy NzbDrone.Web\web.config  %TARGET%\NzbDrone.Web\ 
 
 
-CD "%TARGET%"
-..\Libraries\7zip\7za.exe a -tzip   NzbDrone.zip *
-..\Libraries\7zip\7za.exe t NzbDrone.zip
-cD ..
+CD %PACKAGEROOT%
+..\Libraries\7zip\7za.exe a -tzip ..\NzbDrone.zip *
+
+CD ..
+Pause
