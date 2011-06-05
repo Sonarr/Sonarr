4812cb461132129585343f5686ca5186d942c23d
kay.one <kay.one@gmail.com>
2011-06-05 13:01:28 -0700

    Increased sqlite cache size to 30mb

---
+++ NzbDrone.Core/Datastore/SqliteProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Migrator.Framework;
+using Migrator.Providers.SQLite;
+
+namespace NzbDrone.Core.Datastore
+{
+    class SqliteProvider
+    {
+        private readonly ITransformationProvider _dataBase;
+
+        public SqliteProvider(string connectionString)
+        {
+            _dataBase = new SQLiteTransformationProvider(new SQLiteDialect(), connectionString);
+        }
+
+
+        public int GetPageSize()
+        {
+            return Convert.ToInt32(_dataBase.ExecuteScalar("PRAGMA cache_size"));
+        }
+
+
+
+    }
+}
