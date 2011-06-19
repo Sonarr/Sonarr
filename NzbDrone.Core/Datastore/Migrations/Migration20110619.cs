ec949be7890dc5e77cc98df86c0d0b9603bd2e9a
kay.one <kay.one@gmail.com>
2011-06-19 11:28:18 -0700

    Removing "Histories" on migration.

---
+++ NzbDrone.Core/Datastore/Migrations/Migration20110619.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using Migrator.Framework;
+
+namespace NzbDrone.Core.Datastore.Migrations
+{
+
+    [Migration(20110619)]
+    public class Migration20110619 : Migration
+    {
+        public override void Up()
+        {
+            if (Database.TableExists("Histories"))
+            {
+                Database.RemoveTable("Histories");
+            }
+        }
+        
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
