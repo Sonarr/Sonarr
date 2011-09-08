f8be43a486a1e36ee21498d1d94f2c47d0e855d7
Mark McDowall <markus.mcd5@gmail.com>
2011-09-07 18:56:00 -0700

    Upgraded to MiniProfiler 1.9.
    Updated to latest WebActivator.

--- NzbDrone.Core/Datastore/PetaDbProviderFactory.cs
+++ NzbDrone.Core/Datastore/PetaDbProviderFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlServerCe;
 using System.Text;
+using MvcMiniProfiler;
 using MvcMiniProfiler.Data;
 
 namespace NzbDrone.Core.Datastore
@@ -18,7 +19,7 @@ namespace NzbDrone.Core.Datastore
 
             if (IsProfiled)
             {
-                connection = ProfiledDbConnection.Get(sqliteConnection);
+                connection = new ProfiledDbConnection(sqliteConnection, MiniProfiler.Current);
             }
 
             return connection;
