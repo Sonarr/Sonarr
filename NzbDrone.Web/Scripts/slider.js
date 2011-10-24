4c6287827c2b414138a8691995e88a17c5bf40f1
Mark McDowall <markus.mcd5@gmail.com>
2011-10-24 00:00:25 -0700

    Fixed top slider, it will now work for multiple sliders.

--- NzbDrone.Web/Scripts/slider.js
+++ NzbDrone.Web/Scripts/slider.js
@@ -1,13 +1,36 @@
 ﻿$(document).ready(function () {
-    $(".sliderButtonContainer").live('click', function () {
-        sliderToggle();
+    $(".sliderButton").live('click', function () {
+        sliderToggle(this);
     });
 });
 
-function sliderToggle() {
-    $('.sliderContent').slideToggle('slow');
+function sliderToggle(sliderButton) {
+    //Get sliderContent
+    var sliderContent = $(sliderButton).siblings('.sliderContent');
+    
+    //Open the slider
+    sliderContent.slideToggle('slow');
+    
+    //Change the slider Image
     $(".sliderButtonContainer").children('.sliderImage').toggleClass('sliderOpened sliderClosed');
+
+    //Focus in the search box
+    $(sliderContent).children('.localSeriesLookup').focus();
+
+    //Hide the sliders
+    hideSliders(sliderContent);
 
     //Prevent the Address Bar from changing
     return false;
+}
+
+function hideSliders(newlyOpenedSlider) {
+    $('.sliderContent').each(function (index, value) {
+        var newlyOpenedSliderId = $(newlyOpenedSlider).parent('.top-slider').attr('id');
+        var id = $(this).parent('.top-slider').attr('id');
+
+        //If the ID's of the top-sliders don't match then hide it
+        if (id != newlyOpenedSliderId)
+            $(this).slideUp();
+    });
 }
