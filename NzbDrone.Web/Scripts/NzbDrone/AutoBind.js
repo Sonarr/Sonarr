82e2cb2d1e71e0c44ecd32a9b088faeaabc3af90
kay.one <kay.one@gmail.com>
2011-11-30 22:39:11 -0800

    Settings forms are fixed.

---
+++ NzbDrone.Web/Scripts/NzbDrone/AutoBind.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    //All forms are ajax forms
+    $("form").livequery(function () {
+
+        var options = {
+            type: 'post',
+            resetForm: false
+        };
+
+        $(this).ajaxForm(options);
+
+    });
+
+    $('Form button').livequery(function () {
+        $(this).removeAttr('disabled');
+    });
+
+    
+    //All buttons are jQueryUI buttons
+    $('button, input[type="button"], input[type="submit"], input[type="reset"]').livequery(function () {
+        $(this).button();
+    });
+
+});
