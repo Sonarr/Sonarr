9c2637e23b9899fee2bf6052d8570ff4ab63ebaa
Mark McDowall <markus.mcd5@gmail.com>
2011-12-09 08:18:20 -0800

    Hotkeys won't fire when typing in a password field (now the same as text boxes).

--- NzbDrone.Web/Scripts/jquery.hotkeys.js
+++ NzbDrone.Web/Scripts/jquery.hotkeys.js
@@ -44,7 +44,7 @@
 		handleObj.handler = function( event ) {
 			// Don't fire in text-accepting inputs that we didn't directly bind to
 			if ( this !== event.target && (/textarea|select/i.test( event.target.nodeName ) ||
-				 event.target.type === "text") ) {
+				 event.target.type === "text" || event.target.type === "password") ) {
 				return;
 			}
 			
