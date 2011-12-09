9f07d7c27abf597d81991e9ccfe75b94d94a12ac
kay.one <kay.one@gmail.com>
2011-12-08 20:57:23 -0800

    Full static content (css,js) versioning.

---
+++ NzbDrone.Web/Helpers/HtmlIncludeExtentions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using NzbDrone.Common;
+
+namespace NzbDrone.Web.Helpers
+{
+    public static class HtmlIncludeExtentions
+    {
+        private static string _versionString;
+        private static bool _isProduction;
+
+        static HtmlIncludeExtentions()
+        {
+            _versionString = new EnviromentProvider().Version.ToString().Replace('.', '_');
+            _isProduction = EnviromentProvider.IsProduction;
+        }
+
+        public static MvcHtmlString IncludeScript(this HtmlHelper helper, string filename)
+        {
+            var relativePath = "/Scripts/" + filename;
+            VerifyFile(helper, relativePath);
+            return MvcHtmlString.Create(String.Format("<script type='text/javascript' src='{0}?{1}'></script>", relativePath, _versionString));
+        }
+        
+        public static MvcHtmlString IncludeCss(this HtmlHelper helper, string filename)
+        {
+            var relativePath = "/Content/" + filename;
+            VerifyFile(helper, relativePath);
+            return MvcHtmlString.Create(String.Format("<link type='text/css' rel='stylesheet' href='{0}?{1}'/>", relativePath, _versionString));
+        }
+
+        private static void VerifyFile(HtmlHelper helper, string filename)
+        {
+            if (!_isProduction)
+            {
+                var path = helper.ViewContext.RequestContext.HttpContext.Server.MapPath(filename);
+
+                if (!File.Exists(path))
+                {
+                    throw new FileNotFoundException("static file not found " + path, path);
+                }
+            }
+        }
+    }
+}
