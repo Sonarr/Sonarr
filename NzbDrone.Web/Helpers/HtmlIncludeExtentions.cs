847b743a16bab949e286eb1a0653ef0a156212fe
kay.one <kay.one@gmail.com>
2011-12-08 23:59:34 -0800

    cleanup in HtmlIncludeExtentions

--- NzbDrone.Web/Helpers/HtmlIncludeExtentions.cs
+++ NzbDrone.Web/Helpers/HtmlIncludeExtentions.cs
@@ -8,40 +8,41 @@ namespace NzbDrone.Web.Helpers
 {
     public static class HtmlIncludeExtentions
     {
-        private static string _versionString;
-        private static bool _isProduction;
+        private static readonly string versionString;
+        private static readonly bool isProduction;
 
         static HtmlIncludeExtentions()
         {
-            _versionString = new EnviromentProvider().Version.ToString().Replace('.', '_');
-            _isProduction = EnviromentProvider.IsProduction;
+            versionString = new EnviromentProvider().Version.ToString().Replace('.', '_');
+            isProduction = EnviromentProvider.IsProduction;
         }
 
         public static MvcHtmlString IncludeScript(this HtmlHelper helper, string filename)
         {
             var relativePath = "/Scripts/" + filename;
             VerifyFile(helper, relativePath);
-            return MvcHtmlString.Create(String.Format("<script type='text/javascript' src='{0}?{1}'></script>", relativePath, _versionString));
+            return MvcHtmlString.Create(String.Format("<script type='text/javascript' src='{0}?{1}'></script>", relativePath, versionString));
         }
-        
+
         public static MvcHtmlString IncludeCss(this HtmlHelper helper, string filename)
         {
             var relativePath = "/Content/" + filename;
             VerifyFile(helper, relativePath);
-            return MvcHtmlString.Create(String.Format("<link type='text/css' rel='stylesheet' href='{0}?{1}'/>", relativePath, _versionString));
+            return MvcHtmlString.Create(String.Format("<link type='text/css' rel='stylesheet' href='{0}?{1}'/>", relativePath, versionString));
         }
 
         private static void VerifyFile(HtmlHelper helper, string filename)
         {
-            if (!_isProduction)
-            {
-                var path = helper.ViewContext.RequestContext.HttpContext.Server.MapPath(filename);
+            if (isProduction)
+                return;
 
-                if (!File.Exists(path))
-                {
-                    throw new FileNotFoundException("static file not found " + path, path);
-                }
+            var path = helper.ViewContext.RequestContext.HttpContext.Server.MapPath(filename);
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Static file not found " + path, path);
             }
+
         }
     }
 }
