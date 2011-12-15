af328c9e55eb6e099acc656eb011ad6275ac245c
Mark McDowall <markus.mcd5@gmail.com>
2011-12-15 00:47:32 -0800

    History, Series/Details and Missing use ImageLink when possible for actions.

--- NzbDrone.Web/Scripts/NzbDrone/seriesDetails.js
+++ NzbDrone.Web/Scripts/NzbDrone/seriesDetails.js
@@ -7,9 +7,6 @@ var downloadingImage = '../../Content/Images/Downloading.png';
 var seriesId = 0;
 var saveSeasonIgnoreUrl = '../Series/SaveSeasonIgnore';
 var saveEpisodeIgnoreUrl = '../Series/SaveEpisodeIgnore';
-var renameEpisodeUrl = '../Episode/Rename';
-var renameSeasonUrl = '../Episode/RenameSeason';
-var searchSeasonUrl = '../Episode/SearchSeason';
 
 //Episode Ignore Functions
 $(".ignoreEpisode").live("click", function () {
@@ -154,41 +151,6 @@ function saveEpisodeIgnore(episodeId, ignored) {
         data: jQuery.param({ episodeId: episodeId, ignored: ignored }),
         error: function (req, status, error) {
             alert("Sorry! We could save the ignore settings for Episode: " + episodeId + " at this time. " + error);
-        }
-    });
-}
-
-//Episode Renaming
-function renameEpisode(id) {
-    $.ajax({
-        type: "POST",
-        url: renameEpisodeUrl,
-        data: jQuery.param({ episodeFileId: id }),
-        error: function (req, status, error) {
-            alert("Sorry! We could rename " + id + " at this time. " + error);
-        }
-    });
-}
-
-function renameSeason(seriesId, seasonNumber) {
-    $.ajax({
-        type: "POST",
-        url: renameSeasonUrl,
-        data: jQuery.param({ seriesId: seriesId, seasonNumber: seasonNumber }),
-        error: function (req, status, error) {
-            alert("Sorry! We could rename series: " + seriesId + " season: " + seasonNumber + " at this time. " + error);
-        }
-    });
-}
-
-//Season Search
-function searchSeason(seriesId, seasonNumber) {
-    $.ajax({
-        type: "POST",
-        url: searchSeasonUrl,
-        data: jQuery.param({ seriesId: seriesId, seasonNumber: seasonNumber }),
-        error: function (req, status, error) {
-            alert("Sorry! We could search for series: " + seriesId + " season: " + seasonNumber + " at this time. " + error);
         }
     });
 }
