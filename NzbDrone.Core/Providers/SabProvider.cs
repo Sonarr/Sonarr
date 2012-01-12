5f0c03d69dfefe525165fc724fc04ff35b62f3b5
Mark McDowall <markus.mcd5@gmail.com>
2012-01-12 12:05:24 -0800

    Bug: Fixed ND-9, no longer strip out * from SABnzbd categories, * is Default.

--- NzbDrone.Core/Providers/SabProvider.cs
+++ NzbDrone.Core/Providers/SabProvider.cs
@@ -179,8 +179,6 @@ namespace NzbDrone.Core.Providers
 
             var deserialized =  new JavaScriptSerializer().Deserialize<SabnzbdCategoryModel>(response);
 
-            deserialized.categories.Remove("*");
-
             return deserialized;
         }
 
