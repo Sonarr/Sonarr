a93846d2d83b463fc90cfe7dbaa9294667dd5808
kay.one <kay.one@gmail.com>
2012-01-18 21:12:30 -0800

    Deleting any series with seriesid =0 during migration.

---
+++ NzbDrone.Core/Datastore/Migrations/Migration20120118.cs
@@ -0,0 +1,14 @@
+﻿using Migrator.Framework;
+
+namespace NzbDrone.Core.Datastore.Migrations
+{
+
+    [Migration(20120118)]
+    public class Migration20120118 : NzbDroneMigration
+    {
+        protected override void MainDbUpgrade()
+        {
+            Database.ExecuteNonQuery("DELETE FROM SERIES WHERE  SeriesID = 0");
+        }
+    }
+}
