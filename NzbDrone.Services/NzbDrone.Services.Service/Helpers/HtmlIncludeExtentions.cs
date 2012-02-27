f342b3127115203316f66ccb8fcb220527f2b80d
kay.one <kay.one@gmail.com>
2012-02-26 19:26:52 -0800

    Fixed some namespaces in NzbDrone.Services

--- NzbDrone.Services/NzbDrone.Services.Service/Helpers/HtmlIncludeExtentions.cs
+++ NzbDrone.Services/NzbDrone.Services.Service/Helpers/HtmlIncludeExtentions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web.Mvc;
 using NzbDrone.Common;
 
-namespace NzbDrone.Web.Helpers
+namespace NzbDrone.Services.Service.Helpers
 {
     public static class HtmlIncludeExtentions
     {
