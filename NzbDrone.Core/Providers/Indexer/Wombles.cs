e56477abb057a327552fe3d95d46349153566fe1
Mark McDowall <markus.mcd5@gmail.com>
2012-04-13 16:44:23 -0700

    New: Womble's Index

---
+++ NzbDrone.Core/Providers/Indexer/Wombles.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using System.Text.RegularExpressions;
+using Ninject;
+using NzbDrone.Common;
+using NzbDrone.Core.Model;
+using NzbDrone.Core.Providers.Core;
+
+namespace NzbDrone.Core.Providers.Indexer
+{
+    public class Wombles : IndexerBase
+    {
+        [Inject]
+        public Wombles(HttpProvider httpProvider, ConfigProvider configProvider) : base(httpProvider, configProvider)
+        {
+        }
+
+        protected override string[] Urls
+        {
+            get
+            {
+                return new[]
+                           {
+                               string.Format("http://nzb.isasecret.com/rss")
+                           };
+            }
+        }
+
+        public override bool IsConfigured
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override string Name
+        {
+            get { return "Wombles"; }
+        }
+
+        protected override string NzbDownloadUrl(SyndicationItem item)
+        {
+            return item.Links[0].Uri.ToString();
+        }
+
+
+        protected override IList<string> GetEpisodeSearchUrls(string seriesTitle, int seasonNumber, int episodeNumber)
+        {
+            return new List<string>();
+        }
+
+        protected override IList<string> GetSeasonSearchUrls(string seriesTitle, int seasonNumber)
+        {
+            return new List<string>();
+        }
+
+        protected override IList<string> GetDailyEpisodeSearchUrls(string seriesTitle, DateTime date)
+        {
+            return new List<string>();
+        }
+
+        protected override IList<string> GetPartialSeasonSearchUrls(string seriesTitle, int seasonNumber, int episodeWildcard)
+        {
+            return new List<string>();
+        }
+
+        protected override EpisodeParseResult CustomParser(SyndicationItem item, EpisodeParseResult currentResult)
+        {
+            if (currentResult != null)
+            {
+                currentResult.Size = 0;
+            }
+
+            return currentResult;
+        }
+    }
+}
