c24cb161278b2807f3798418014f9e7eeb740fe1
Mark McDowall <markus.mcd5@gmail.com>
2012-04-14 16:54:54 -0700

    Registered NzbClub as indexer.

--- NzbDrone.Core/CentralDispatch.cs
+++ NzbDrone.Core/CentralDispatch.cs
@@ -98,6 +98,7 @@ namespace NzbDrone.Core
             Kernel.Bind<IndexerBase>().To<Wombles>();
             Kernel.Bind<IndexerBase>().To<FileSharingTalk>();
             Kernel.Bind<IndexerBase>().To<NzbIndex>();
+            Kernel.Bind<IndexerBase>().To<NzbClub>();
 
             var indexers = Kernel.GetAll<IndexerBase>();
             Kernel.Get<IndexerProvider>().InitializeIndexers(indexers.ToList());
