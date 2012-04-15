06fb34ce6c8902d9de311a84328100735a661274
Mark McDowall <markus.mcd5@gmail.com>
2012-04-14 17:57:15 -0700

    Nzb Url column added.

---
+++ NzbDrone.Core/Datastore/Migrations/Migration20120414.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Migrator.Framework;
+
+namespace NzbDrone.Core.Datastore.Migrations
+{
+
+    [Migration(20120414)]
+    public class Migration20120414 : NzbDroneMigration
+    {
+        protected override void MainDbUpgrade()
+        {
+            Database.AddColumn("History", "Url", DbType.String, ColumnProperty.Null);
+        }
+    }
+}
