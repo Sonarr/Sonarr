1a7911ab828db13be0e2b0b74364f8159d111424
Mark McDowall <markus.mcd5@gmail.com>
2012-07-07 16:34:07 -0700

    Central Dispatch update from last commit

--- NzbDrone.Core/CentralDispatch.cs
+++ NzbDrone.Core/CentralDispatch.cs
@@ -141,6 +141,7 @@ namespace NzbDrone.Core
             Kernel.Bind<IJob>().To<RecentBacklogSearchJob>().InSingletonScope();
             Kernel.Bind<IJob>().To<CheckpointJob>().InSingletonScope();
             Kernel.Bind<IJob>().To<SearchHistoryCleanupJob>().InSingletonScope();
+            Kernel.Bind<IJob>().To<PastWeekBacklogSearchJob>().InSingletonScope();
 
             Kernel.Get<JobProvider>().Initialize();
             Kernel.Get<WebTimer>().StartTimer(30);
