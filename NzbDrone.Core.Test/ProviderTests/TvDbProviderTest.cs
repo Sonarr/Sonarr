7b5d57d2245b252fd0cf2cd7ff2b8c50a2b4add8
Mark McDowall <markus.mcd5@gmail.com>
2012-10-17 17:07:16 -0700

    Removed American Dad fix test

--- NzbDrone.Core.Test/ProviderTests/TvDbProviderTest.cs
+++ NzbDrone.Core.Test/ProviderTests/TvDbProviderTest.cs
@@ -68,58 +68,5 @@ namespace NzbDrone.Core.Test.ProviderTests
                 .Max(e => e.Count()).Should().Be(1);
 
         }
-
-        [Test]
-        public void American_dad_fix()
-        {
-            //act
-            var result = tvDbProvider.GetSeries(73141, true);
-
-            var seasonsNumbers = result.Episodes.Select(e => e.SeasonNumber)
-                .Distinct().ToList();
-
-            var seasons = new Dictionary<int, List<TvdbEpisode>>(seasonsNumbers.Count);
-
-            foreach (var season in seasonsNumbers)
-            {
-                seasons.Add(season, result.Episodes.Where(e => e.SeasonNumber == season).ToList());
-            }
-
-            foreach (var episode in result.Episodes)
-            {
-                Console.WriteLine(episode);
-            }
-
-            //assert
-            seasonsNumbers.Should().HaveCount(9);
-            seasons[1].Should().HaveCount(23);
-            seasons[2].Should().HaveCount(19);
-            seasons[3].Should().HaveCount(16);
-            seasons[4].Should().HaveCount(20);
-            seasons[5].Should().HaveCount(18);
-            seasons[6].Should().HaveCount(19);
-            seasons[7].Should().HaveCount(18);
-
-            foreach (var season in seasons)
-            {
-                season.Value.Should().OnlyHaveUniqueItems("Season {0}", season.Key);
-            }
-
-            //Make sure no episode number is skipped
-            foreach (var season in seasons)
-            {
-                for (int i = 1; i < season.Value.Count; i++)
-                {
-                    //Skip specials, because someone decided 1,3,4,6,7,21 is how you count...
-                    if (season.Key == 0)
-                        continue;
-
-                    season.Value.Should().Contain(c => c.EpisodeNumber == i, "Can't find Episode S{0:00}E{1:00}",
-                                            season.Value[0].SeasonNumber, i);
-                }
-            }
-
-
-        }
     }
 }
