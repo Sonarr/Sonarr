5220de5c66dc7a00b97761d7507b2c5330bdfb57
Mark McDowall <markus.mcd5@gmail.com>
2012-10-28 12:08:37 -0700

    Lazy load exisitng series
    New: Existing series will be lazy loaded on Add Series page

---
+++ NzbDrone.Web/Helpers/RenderActionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace NzbDrone.Web.Helpers
+{
+    public static class RenderActionHelper
+    {
+        public static MvcHtmlString RenderAction(this AjaxHelper helper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions)
+        {
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+
+            var url = urlHelper.Action(actionName, controllerName, routeValues);
+
+            var tagBuilder = new TagBuilder("div");
+            if (ajaxOptions != null) tagBuilder.MergeAttributes<string, object>(ajaxOptions.ToUnobtrusiveHtmlAttributes());
+            tagBuilder.MergeAttribute("data-ajax-action-link", "true");
+            tagBuilder.MergeAttribute("data-href", url);
+
+            return new MvcHtmlString(tagBuilder.ToString(TagRenderMode.Normal));
+        }
+    }
+}
