9f304b7be59bcb4ead67eb25b8edcc00dddd5851
Keivan Beigi <kay.one@gmail.com>
2013-01-18 14:05:38 -0800

    added application basics

---
+++ NzbDrone.Backbone/app.js
@@ -0,0 +1,17 @@
+﻿NzbDrone = new Backbone.Marionette.Application();
+
+NzbDrone.MyRouter = Backbone.Marionette.AppRouter.extend({
+
+    // "someMethod" must exist at controller.someMethod
+    appRoutes: {
+        "add": "AddSeries",
+        "add/new": "AddNewSeries",
+        "add/existing": "AddExistingSeries",
+    }
+
+});
+
+NzbDrone.addInitializer(function (options) {
+    new NzbDrone.MyRouter();
+    Backbone.history.start();
+});
