bf50c5989addfc11b7c5d4835884fc54c7852ff4
kay.one <kay.one@gmail.com>
2013-01-26 13:20:17 -0800

    fixed where year could show up twice in series search.

--- NzbDrone.Web/_backboneApp/AddSeries/SearchResultModel.js
+++ NzbDrone.Web/_backboneApp/AddSeries/SearchResultModel.js
@@ -2,8 +2,14 @@
 NzbDrone.AddSeries.SearchResultModel = Backbone.Model.extend({
     mutators: {
         seriesYear: function () {
-            var date = Date.utc.create(this.get('firstAired'));
-            return date.format('{yyyy}');
+            var date = Date.utc.create(this.get('firstAired')).format('({yyyy})');
+
+            //don't append year, if the series name already has the name appended.
+            if (this.get('seriesName').endsWith(date)) {
+                return "";
+            } else {
+                return date;
+            }
         }
     }
 });
