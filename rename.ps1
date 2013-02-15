be0b0055f1e5c2b54988aa9131c02f5b54b27ad0
Mark McDowall <markus.mcd5@gmail.com>
2013-02-15 17:59:20 -0600

    Rename package for handling <default branch>

---
+++ rename.ps1
@@ -0,0 +1,17 @@
+Param( 
+    [Parameter(Mandatory=$true, Position=0, HelpMessage="A branch name is #requires required")] 
+    [string]$branch, 
+    [Parameter(Mandatory=$true, Position=1, HelpMessage="A version is required")] 
+    [string]$version
+)
+
+if ($branch -eq "<default branch>")
+{
+    $branch = "teamcity";
+}
+
+Write-Host $branch;
+Write-Host $version;
+Write-Host "NzbDrone.$branch.$version.zip";
+
+Rename-Item "nzbdrone.zip" "NzbDrone.$branch.$version.zip"
