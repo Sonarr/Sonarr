911311d1e8fccb8202bad505a9b82c781f911bc3
Keivan Beigi <kay.one@gmail.com>
2013-02-26 18:28:55 -0800

    fixed OID issues.

--- NzbDrone.Core/Datastore/ModelBase.cs
+++ NzbDrone.Core/Datastore/ModelBase.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
 using Newtonsoft.Json;
 
@@ -17,6 +17,6 @@ namespace NzbDrone.Core.Datastore
         }
 
         [JsonProperty(PropertyName = "id")]
-        private int OID;
+        public int OID { get; set; }
     }
 }
