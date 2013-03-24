96fe01c0401522649dd0fd85018d721a92bd81e6
kay.one <kay.one@gmail.com>
2013-03-23 21:58:23 -0700

    cleaned up model base Id property.

--- NzbDrone.Core/Datastore/ModelBase.cs
+++ NzbDrone.Core/Datastore/ModelBase.cs
@@ -1,20 +1,10 @@
 ﻿using System.Diagnostics;
-using System.Linq;
-using Newtonsoft.Json;
 
 namespace NzbDrone.Core.Datastore
 {
     [DebuggerDisplay("{GetType()} ID = {Id}")]
     public abstract class ModelBase
     {
-        [JsonIgnore]
-        public int Id
-        {
-            get { return OID; }
-            set { OID = value; }
-        }
-
-        [JsonProperty(PropertyName = "id")]
-        public int OID { get; set; }
+        public int Id { get; set; }
     }
 }
