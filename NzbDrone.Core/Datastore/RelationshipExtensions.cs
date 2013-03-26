e9b9e63f7ae9680f578de97c900e5f978754ae2c
kay.one <kay.one@gmail.com>
2013-03-26 01:00:46 -0700

    RelationshipExtensions has more type enforcement.

--- NzbDrone.Core/Datastore/RelationshipExtensions.cs
+++ NzbDrone.Core/Datastore/RelationshipExtensions.cs
@@ -8,26 +8,38 @@ namespace NzbDrone.Core.Datastore
 {
     public static class RelationshipExtensions
     {
-        public static RelationshipBuilder<TParent> HasOne<TParent, TChild>(this ColumnMapBuilder<TParent> columnMapBuilder, Expression<Func<TParent, object>> portalExpression, Func<TParent, int> childIdSelector)
+        public static RelationshipBuilder<TParent> HasOne<TParent, TChild>(this ColumnMapBuilder<TParent> columnMapBuilder, Expression<Func<TParent, LazyLoaded<TChild>>> portalExpression, Func<TParent, int> childIdSelector)
             where TParent : ModelBase
             where TChild : ModelBase
         {
             return columnMapBuilder.Relationships.AutoMapComplexTypeProperties<ILazyLoaded>()
-                                   .For(portalExpression)
+                                   .For(portalExpression.GetMemberName())
                                    .LazyLoad((db, parent) => db.Query<TChild>().Single(c => c.Id == childIdSelector(parent)));
 
 
         }
 
-        public static RelationshipBuilder<TParent> HasMany<TParent, TChild>(this ColumnMapBuilder<TParent> columnMapBuilder, Expression<Func<TParent, object>> portalExpression, Func<TParent, int> childIdSelector)
+        public static RelationshipBuilder<TParent> HasMany<TParent, TChild>(this ColumnMapBuilder<TParent> columnMapBuilder, Expression<Func<TParent, LazyList<TChild>>> portalExpression, Func<TParent, int> childIdSelector)
             where TParent : ModelBase
             where TChild : ModelBase
         {
             return columnMapBuilder.Relationships.AutoMapComplexTypeProperties<ILazyLoaded>()
-                                   .For(portalExpression)
+                                   .For(portalExpression.GetMemberName())
                                    .LazyLoad((db, parent) => db.Query<TChild>().Where(c => c.Id == childIdSelector(parent)).ToList());
 
 
+        }
+
+        private static string GetMemberName<T, TMember>(this Expression<Func<T, TMember>> member)
+        {
+            var expression = member.Body as MemberExpression;
+
+            if (expression == null)
+            {
+                expression = (MemberExpression)((UnaryExpression)member.Body).Operand;
+            }
+
+            return expression.Member.Name;
         }
     }
 }
