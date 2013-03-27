c1960525d7bf973e1c4e18b4e6ef1fe0040f2326
kay.one <kay.one@gmail.com>
2013-03-26 19:12:40 -0700

    updated lazylist to make it easy to work with.

--- NzbDrone.Core/Datastore/LazyList.cs
+++ NzbDrone.Core/Datastore/LazyList.cs
@@ -5,6 +5,25 @@ namespace NzbDrone.Core.Datastore
 {
     public class LazyList<T> : LazyLoaded<List<T>>
     {
+        public LazyList()
+        {
 
+        }
+
+        public LazyList(IEnumerable<T> items)
+            : base(new List<T>(items))
+        {
+
+        }
+
+        public static implicit operator LazyList<T>(List<T> val)
+        {
+            return new LazyList<T>(val);
+        }
+
+        public static implicit operator List<T>(LazyList<T> lazy)
+        {
+            return lazy.Value;
+        }
     }
 }
