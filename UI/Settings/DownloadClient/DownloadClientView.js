0779ff63d445a53d0ad6323d9ba43e0f6a628582
kay.one <kay.one@gmail.com>
2013-04-01 00:24:18 -0700

    download client settings only shows config for the selected client.

--- UI/Settings/DownloadClient/DownloadClientView.js
+++ UI/Settings/DownloadClient/DownloadClientView.js
@@ -10,16 +10,76 @@ define([
         className: 'form-horizontal',
 
         ui: {
-            switch   : '.switch',
-            tooltip  : '[class^="help-inline"] i',
-            pathInput: '.x-path'
+            switch              : '.switch',
+            tooltip             : '.help-inline i',
+            pathInput           : '.x-path',
+            sabConfig           : '.x-sab-config',
+            blackholeConfig     : '.x-blackhole-config',
+            pneumaticConfig     : '.x-pneumatic-config',
+            nzbGetConfig        : '.x-nzbget-config',
+            downloadClientSelect: '.x-download-client'
+        },
+
+        events: {
+            'change .x-download-client': 'downloadClientChanged'
         },
 
         onRender: function () {
             NzbDrone.ModelBinder.bind(this.model, this.el);
+
             this.ui.switch.bootstrapSwitch();
             this.ui.tooltip.tooltip({ placement: 'right', html: true });
             this.ui.pathInput.autoComplete('/directories');
+
+            this.refreshUIVisibility(this.model.get('downloadClient'));
+        },
+
+        downloadClientChanged: function () {
+            var clientId = this.ui.downloadClientSelect.val();
+            this.refreshUIVisibility(clientId);
+        },
+
+        refreshUIVisibility: function (clientId) {
+
+            if (!clientId) {
+                clientId = "0";
+            }
+
+            switch (clientId.toString()) {
+                case "0":
+                    this.ui.sabConfig.show();
+                    this.ui.blackholeConfig.hide();
+                    this.ui.pneumaticConfig.hide();
+                    this.ui.nzbGetConfig.hide();
+                    break;
+
+                case "1":
+                    this.ui.sabConfig.hide();
+                    this.ui.blackholeConfig.show();
+                    this.ui.pneumaticConfig.hide();
+                    this.ui.nzbGetConfig.hide();
+                    break;
+
+                case "2":
+                    this.ui.sabConfig.hide();
+                    this.ui.blackholeConfig.hide();
+                    this.ui.pneumaticConfig.show();
+                    this.ui.nzbGetConfig.hide();
+                    break;
+
+                case "3":
+                    this.ui.sabConfig.hide();
+                    this.ui.blackholeConfig.hide();
+                    this.ui.pneumaticConfig.hide();
+                    this.ui.nzbGetConfig.show();
+                    break;
+
+                default :
+                    throw "unknown download client id" + clientId;
+            }
         }
+
+
+
     });
 });
