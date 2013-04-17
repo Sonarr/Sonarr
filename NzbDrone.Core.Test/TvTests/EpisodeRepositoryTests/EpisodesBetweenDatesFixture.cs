0ee175e382838d91f6d83e8aba20ca1bcae6ee85
Mark McDowall <markus.mcd5@gmail.com>
2013-04-16 19:46:07 -0700

    Attempting join of Series to Episodes

---
+++ NzbDrone.Core.Test/TvTests/EpisodeRepositoryTests/EpisodesBetweenDatesFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.TvTests.EpisodeRepositoryTests
+{
+    [TestFixture]
+    public class EpisodesBetweenDatesFixture : DbTest<EpisodeRepository, Episode>
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var series = Builder<Series>.CreateNew()
+                                        .With(s => s.Id = 0)
+                                        .With(s => s.Runtime = 30)
+                                        .Build();
+
+            series.Id = Db.Insert(series).Id;
+
+            var episode = Builder<Episode>.CreateNew()
+                                          .With(e => e.Id = 0)
+                                          .With(e => e.SeriesId = series.Id)
+                                          .Build();
+
+            Db.Insert(episode);
+        }
+
+        [Test]
+        public void should_get_episodes_with_attached_series()
+        {
+            var episodes = Subject.EpisodesBetweenDates(DateTime.Today.AddDays(-1), DateTime.Today.AddDays(3));
+            episodes.Should().HaveCount(1);
+            episodes.First().Series.Should().NotBeNull();
+        }
+    }
+}
