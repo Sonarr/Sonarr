5928ffc693b1ba1478a5e7e6573af601120b2f88
Keivan Beigi <kay.one@gmail.com>
2013-05-14 19:07:14 -0700

    animate I say!

---
+++ UI/Mixins/backbone.marionette.region.mixin.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+(function () {
+
+    var _originalRegionClose = Marionette.Region.prototype.close;
+
+    Marionette.Region.prototype.open = function (view) {
+        var self = this;
+
+        this.$el.fadeOut(200, function () {
+            _originalRegionClose.apply(this, arguments);
+            self.$el.html(view.el);
+            self.$el.fadeIn(150);
+        });
+    };
+
+    Marionette.Region.prototype.close = function () {
+        //do nothing. we close the region as part of open so we can chain the animation
+    };
+}());
