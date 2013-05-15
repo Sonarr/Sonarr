b8102b0d1fd759db4e542199ae3d81959641bb63
Keivan Beigi <kay.one@gmail.com>
2013-05-14 19:20:43 -0700

    for now disabling animation

--- UI/Mixins/backbone.marionette.region.mixin.js
+++ UI/Mixins/backbone.marionette.region.mixin.js
@@ -1,20 +1,26 @@
-﻿"use strict";
+﻿/*
+"use strict";
 
 (function () {
 
     var _originalRegionClose = Marionette.Region.prototype.close;
-
+*/
+/*
     Marionette.Region.prototype.open = function (view) {
         var self = this;
 
-        this.$el.fadeOut(200, function () {
-            _originalRegionClose.apply(this, arguments);
-            self.$el.html(view.el);
-            self.$el.fadeIn(150);
-        });
-    };
+        self.$el.html(view.el);
+
+        //self.$el.fadeIn(100);
+    };*//*
+
 
     Marionette.Region.prototype.close = function () {
-        //do nothing. we close the region as part of open so we can chain the animation
+*/
+/*        this.$el.fadeOut(200, function () {
+            _originalRegionClose.apply(this, arguments);
+        });*//*
+
     };
 }());
+*/
