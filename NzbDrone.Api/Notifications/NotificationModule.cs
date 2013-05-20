8a6a64d104831dbfa0488e84fbc5596e3b694bb5
Mark McDowall <markus.mcd5@gmail.com>
2013-05-19 18:32:05 -0700

    NotificationModule added to API

---
+++ NzbDrone.Api/Notifications/NotificationModule.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NzbDrone.Api.ClientSchema;
+using NzbDrone.Core.Notifications;
+using Omu.ValueInjecter;
+
+namespace NzbDrone.Api.Notifications
+{
+    public class NotificationModule : NzbDroneRestModule<NotificationResource>
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationModule(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+            GetResourceAll = GetAll;
+        }
+
+        private List<NotificationResource> GetAll()
+        {
+            var notifications = _notificationService.All();
+
+            var result = new List<NotificationResource>(notifications.Count);
+
+            foreach (var notification in notifications)
+            {
+                var indexerResource = new NotificationResource();
+                indexerResource.InjectFrom(notification);
+                indexerResource.Fields = SchemaBuilder.GenerateSchema(notification.Settings);
+
+                result.Add(indexerResource);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NzbDrone.Api/Notifications/NotificationResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NzbDrone.Api.ClientSchema;
+using NzbDrone.Api.REST;
+
+namespace NzbDrone.Api.Notifications
+{
+    public class NotificationResource : RestResource
+    {
+        public Boolean Enable { get; set; }
+        public String Name { get; set; }
+        public List<Field> Fields { get; set; }
+    }
+}
