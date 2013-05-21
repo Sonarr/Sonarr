1016edb05b312dabc6ace9dfcefbc9fab2a321bb
kay.one <kay.one@gmail.com>
2013-05-20 19:49:52 -0700

    5 minutes update check interval.

--- NzbDrone.Core/Jobs/TaskManager.cs
+++ NzbDrone.Core/Jobs/TaskManager.cs
@@ -41,7 +41,7 @@ namespace NzbDrone.Core.Jobs
                     new ScheduledTask{ Interval = 12*60, TypeName = typeof(UpdateXemMappings).FullName},
                     new ScheduledTask{ Interval = 6*60, TypeName = typeof(DiskScanCommand).FullName},
                     new ScheduledTask{ Interval = 1, TypeName = typeof(DownloadedEpisodesScanCommand).FullName},
-                    new ScheduledTask{ Interval = 15, TypeName = typeof(ApplicationUpdateCommand).FullName}
+                    new ScheduledTask{ Interval = 5, TypeName = typeof(ApplicationUpdateCommand).FullName}
                 };
 
             var currentTasks = _scheduledTaskRepository.All();
