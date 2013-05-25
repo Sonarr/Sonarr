3b09cc12406b15304c034b8dca984799988ac8b8
Mark McDowall <markus.mcd5@gmail.com>
2013-05-24 23:38:43 -0700

    Notification Schema in UI

---
+++ UI/Settings/Notifications/AddView.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+define([
+    'app',
+    'Settings/Notifications/Model'
+
+], function () {
+
+    NzbDrone.Settings.Notifications.AddItemView = Backbone.Marionette.ItemView.extend({
+        template : 'Settings/Notifications/AddItemTemplate',
+        tagName  : 'button',
+        className: 'btn',
+
+        events: {
+            'click': 'add'
+        },
+
+        add: function () {
+            this.model.set('id', undefined);
+            var view = new NzbDrone.Settings.Notifications.EditView({ model: this.model});
+            NzbDrone.modalRegion.show(view);
+        }
+    });
+
+    NzbDrone.Settings.Notifications.AddView = Backbone.Marionette.CompositeView.extend({
+        itemView                : NzbDrone.Settings.Notifications.AddItemView,
+        itemViewContainer       : '#notifications-to-add',
+        template                : 'Settings/Notifications/AddTemplate'
+    });
+});
--- UI/Settings/Notifications/CollectionView.js
+++ UI/Settings/Notifications/CollectionView.js
@@ -1,8 +1,19 @@
 ﻿'use strict';
-define(['app', 'Settings/Notifications/ItemView'], function () {
+define(['app', 'Settings/Notifications/ItemView', 'Settings/Notifications/AddView'], function () {
     NzbDrone.Settings.Notifications.CollectionView = Backbone.Marionette.CompositeView.extend({
         itemView                : NzbDrone.Settings.Notifications.ItemView,
         itemViewContainer       : 'tbody',
-        template                : 'Settings/Notifications/CollectionTemplate'
+        template                : 'Settings/Notifications/CollectionTemplate',
+
+        events: {
+            'click .x-add': function () {
+                var schema = new NzbDrone.Settings.Notifications.Collection();
+                schema.url = '/api/notification/schema';
+                schema.fetch();
+
+                var view = new NzbDrone.Settings.Notifications.AddView({ collection: schema});
+                NzbDrone.modalRegion.show(view);
+            }
+        }
     });
 });
