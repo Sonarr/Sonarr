bb677259e10f50ba5f477820df3f2419e76296b9
kay.one <kay.one@gmail.com>
2013-06-02 20:22:47 -0700

    added missing file

---
+++ NzbDrone.Core.Test/Datastore/ReflectionStrategyFixture/Benchmarks.cs
@@ -0,0 +1,43 @@
+﻿using Marr.Data.Reflection;
+using NUnit.Framework;
+using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Test.Datastore.ReflectionStrategyFixture
+{
+    [TestFixture]
+    public class Benchmarks
+    {
+        private const int iterations = 5000000;
+        private object _target;
+        private IReflectionStrategy _simpleReflectionStrategy;
+
+        [SetUp]
+        public void Setup()
+        {
+            //  _simpleReflectionStrategy = new DelegateReflectionStrategy();
+        }
+
+        [Test]
+        public void clr_reflection_test()
+        {
+            _target = new Series();
+
+            var del = _simpleReflectionStrategy.BuildSetter(typeof(Series), "Title");
+
+            for (int i = 0; i < iterations; i++)
+            {
+                del(_target, "TestTile");
+                //_simpleReflectionStrategy.SetFieldValue(_target, "Title", "TestTile");
+            }
+        }
+
+
+        private void SetField()
+        {
+
+
+        }
+
+    }
+}
