1809aefea2611b3024899ce5fba8ae5535e4094d
Mark McDowall <markus.mcd5@gmail.com>
2013-06-07 17:31:08 -0700

    Backgrid pagination works again.

--- UI/Mixins/backbone.Backgrid.mixin.js
+++ UI/Mixins/backbone.Backgrid.mixin.js
@@ -119,18 +119,19 @@ Backgrid.NzbDroneHeaderCell = Backgrid.HeaderCell.extend({
 
 Backgrid.NzbDronePaginator = Backgrid.Extension.Paginator.extend({
 
+    template: 'Shared/BackgridPaginatorTemplate',
+
     events: {
-        "click a": "changePage",
-        "click i": "preventLinkClick"
+        "click .pager-btn": "changePage"
     },
 
     windowSize: 1,
 
     fastForwardHandleLabels: {
-        first: '<i class="icon-fast-backward"></i>',
-        prev : '<i class="icon-backward"></i>',
-        next : '<i class="icon-forward"></i>',
-        last : '<i class="icon-fast-forward"></i>'
+        first: 'icon-fast-backward',
+        prev : 'icon-backward',
+        next : 'icon-forward',
+        last : 'icon-fast-forward'
     },
 
     changePage: function (e) {
@@ -142,31 +143,27 @@ Backgrid.NzbDronePaginator = Backgrid.Extension.Paginator.extend({
             return;
         }
 
-        if (!$(target).is('a')) {
-            target = target.parent('a');
-        }
-
-        var label = target.html();
+        var label = target.attr('data-action');
         var ffLabels = this.fastForwardHandleLabels;
 
         var collection = this.collection;
 
         if (ffLabels) {
             switch (label) {
-                case ffLabels.first:
+                case 'first':
                     collection.getFirstPage();
                     return;
-                case ffLabels.prev:
+                case 'prev':
                     if (collection.hasPrevious()) {
                         collection.getPreviousPage();
                     }
                     return;
-                case ffLabels.next:
+                case 'next':
                     if (collection.hasNext()) {
                         collection.getNextPage();
                     }
                     return;
-                case ffLabels.last:
+                case 'last':
                     collection.getLastPage();
                     return;
             }
@@ -177,7 +174,82 @@ Backgrid.NzbDronePaginator = Backgrid.Extension.Paginator.extend({
         collection.getPage(state.firstPage === 0 ? pageIndex - 1 :pageIndex);
     },
 
-    preventLinkClick: function (e) {
-        e.preventDefault();
+    makeHandles: function () {
+
+        var handles = [];
+        var collection = this.collection;
+        var state = collection.state;
+
+        // convert all indices to 0-based here
+        var firstPage = state.firstPage;
+        var lastPage = +state.lastPage;
+        lastPage = Math.max(0, firstPage ? lastPage - 1 : lastPage);
+        var currentPage = Math.max(state.currentPage, state.firstPage);
+        currentPage = firstPage ? currentPage - 1 : currentPage;
+        var windowStart = Math.floor(currentPage / this.windowSize) * this.windowSize;
+        var windowEnd = Math.min(lastPage + 1, windowStart + this.windowSize);
+
+        if (collection.mode !== "infinite") {
+            for (var i = windowStart; i < windowEnd; i++) {
+                handles.push({
+                    label: i + 1,
+                    title: "No. " + (i + 1),
+                    className: currentPage === i ? "active" : undefined,
+                    pageNumber: i + 1
+                });
+            }
+        }
+
+        var ffLabels = this.fastForwardHandleLabels;
+        if (ffLabels) {
+
+            if (ffLabels.prev) {
+                handles.unshift({
+                    label: ffLabels.prev,
+                    className: collection.hasPrevious() ? void 0 : "disabled",
+                    action: 'prev'
+                });
+            }
+
+            if (ffLabels.first) {
+                handles.unshift({
+                    label: ffLabels.first,
+                    className: collection.hasPrevious() ? void 0 : "disabled",
+                    action: 'first'
+                });
+            }
+
+            if (ffLabels.next) {
+                handles.push({
+                    label: ffLabels.next,
+                    className: collection.hasNext() ? void 0 : "disabled",
+                    action: 'next'
+                });
+            }
+
+            if (ffLabels.last) {
+                handles.push({
+                    label: ffLabels.last,
+                    className: collection.hasNext() ? void 0 : "disabled",
+                    action: 'last'
+                });
+            }
+        }
+
+        return handles;
+    },
+
+    render: function () {
+        this.$el.empty();
+
+        var templateFunction = Marionette.TemplateCache.get(this.template);
+
+        this.$el.html(templateFunction({
+            handles: this.makeHandles()
+        }));
+
+        this.delegateEvents();
+
+        return this;
     }
 });
