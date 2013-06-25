f8fee8e3d032e824ac548def3818bc441e732315
kay.one <kay.one@gmail.com>
2013-06-24 23:27:34 -0700

    fixed 404 placeholder image template helper.

--- UI/Handlebars/Helpers/Html.js
+++ UI/Handlebars/Helpers/Html.js
@@ -5,6 +5,6 @@ define(
         'handlebars'
     ], function (Handlebars) {
         Handlebars.registerHelper('defaultImg', function () {
-            return new Handlebars.SafeString('onerror="this.src="/content/images/poster-dark.jpg";"');
+            return new Handlebars.SafeString('onerror=this.src=\'/content/images/poster-dark.jpg\';');
         });
     });
