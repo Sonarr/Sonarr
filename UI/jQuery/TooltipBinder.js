7d7cdfa1487b972a4f5801930ac9e52cc31cf534
kay.one <kay.one@gmail.com>
2013-07-13 23:06:18 -0700

    form tooltips pop from right.

--- UI/jQuery/TooltipBinder.js
+++ UI/jQuery/TooltipBinder.js
@@ -4,6 +4,13 @@ define(
         'bootstrap'
     ], function () {
         $(document).on('mouseenter', '[title]', function () {
-            $(this).tooltip('show');
+
+            var element = $(this);
+
+            if (!element.attr('data-placement') && element.parents('.control-group').length > 0) {
+                element.attr('data-placement', 'right');
+            }
+
+            element.tooltip('show');
         });
     });
