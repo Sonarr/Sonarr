8f0d3e2e3bbf5bbe9e9cbb6ca447b4a90cd51259
Mark McDowall <markus.mcd5@gmail.com>
2013-07-18 08:35:44 -0700

    Episode grid is sorted by episode number descending

--- UI/Series/EpisodeCollection.js
+++ UI/Series/EpisodeCollection.js
@@ -16,6 +16,22 @@ define(
                 var EpisodeCollection = require('Series/EpisodeCollection');
 
                 return new EpisodeCollection(filtered);
+            },
+
+            comparator: function (model1, model2) {
+                var episode1 = model1.get('episodeNumber');
+
+                var episode2 = model2.get('episodeNumber');
+
+                if (episode1 < episode2){
+                    return 1;
+                }
+
+                if (episode1 > episode2){
+                    return -1;
+                }
+
+                return 0;
             }
         });
     });
