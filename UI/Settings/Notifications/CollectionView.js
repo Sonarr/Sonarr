d7c70a9c917397fb5fda3efb5e8461148db3874a
Mark McDowall <markus.mcd5@gmail.com>
2013-07-25 23:53:37 -0700

    Add notification card

--- UI/Settings/Notifications/CollectionView.js
+++ UI/Settings/Notifications/CollectionView.js
@@ -10,12 +10,34 @@ define([
         itemViewContainer: '.notifications',
         template         : 'Settings/Notifications/CollectionTemplate',
 
+        ui: {
+            'addCard': '.x-add-card'
+        },
+
         events: {
-            'click .x-add': '_openSchemaModal'
+            'click .x-add': '_openSchemaModal',
+            'click .x-add-card': '_openSchemaModal'
+        },
+
+        onBeforeItemAdded: function () {
+            this.ui.addCard.remove();
+        },
+
+        onAfterItemAdded: function () {
+            this._appendAddCard();
         },
 
         _openSchemaModal: function () {
             SchemaModal.open(this.collection);
+        },
+
+        _appendAddCard: function () {
+            this.$itemViewContainer.find('.x-add-card').remove();
+
+            this.templateFunction = Marionette.TemplateCache.get('Settings/Notifications/AddCardTemplate');
+            var html = this.templateFunction();
+
+            this.$itemViewContainer.append(html);
         }
     });
 });
