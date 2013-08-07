c7d3a072198bf37e9a408c3476bf9ab90054a552
Mark McDowall <markus.mcd5@gmail.com>
2013-08-06 23:16:28 -0700

    Throw a custom error for trakt so it doesn't die in LINQ

---
+++ NzbDrone.Core/MetadataSource/Trakt/TraktCommunicationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.MetadataSource.Trakt
+{
+   public class TraktCommunicationException : Exception
+    {
+       public TraktCommunicationException(string message, Exception innerException) : base(message, innerException)
+       {
+       }
+    }
+}
