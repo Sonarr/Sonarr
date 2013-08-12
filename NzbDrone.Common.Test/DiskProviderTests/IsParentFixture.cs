5b25f9c7997e989275af9b8fccac93a820205c70
Mark McDowall <markus.mcd5@gmail.com>
2013-08-11 21:57:10 -0700

    OsAgnostic Paths

--- NzbDrone.Common.Test/DiskProviderTests/IsParentFixture.cs
+++ NzbDrone.Common.Test/DiskProviderTests/IsParentFixture.cs
@@ -11,22 +11,30 @@ namespace NzbDrone.Common.Test.DiskProviderTests
     [TestFixture]
     public class IsParentFixture : TestBase<DiskProvider>
     {
+        private string _parent = @"C:\Test".AsOsAgnostic();
+
         [Test]
         public void should_return_false_when_not_a_child()
         {
-            Subject.IsParent(@"C:\Test", @"C:\Another Folder").Should().BeFalse();
+            var path = @"C:\Another Folder".AsOsAgnostic();
+
+            Subject.IsParent(_parent, path).Should().BeFalse();
         }
 
         [Test]
         public void should_return_true_when_folder_is_parent_of_another_folder()
         {
-            Subject.IsParent(@"C:\Test", @"C:\Test\TV").Should().BeTrue();
+            var path = @"C:\Test\TV".AsOsAgnostic();
+
+            Subject.IsParent(_parent, path).Should().BeTrue();
         }
 
         [Test]
         public void should_return_true_when_folder_is_parent_of_a_file()
         {
-            Subject.IsParent(@"C:\Test", @"C:\Test\30.Rock.S01E01.Pilot.avi").Should().BeTrue();
+            var path = @"C:\Test\30.Rock.S01E01.Pilot.avi".AsOsAgnostic();
+
+            Subject.IsParent(_parent, path).Should().BeTrue();
         }
     }
 }
