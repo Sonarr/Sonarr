8d54bb64a77cf6dd1a8aa99a00f20b3fb06b34e6
Mark McDowall <markus.mcd5@gmail.com>
2013-08-11 18:18:35 -0700

    Extending NamingConfigResource instead of rec recreating its properties

--- NzbDrone.Api/Naming/NamingResource.cs
+++ NzbDrone.Api/Naming/NamingResource.cs
@@ -1,18 +1,9 @@
-﻿using NzbDrone.Api.REST;
+﻿using NzbDrone.Api.Config;
 
 namespace NzbDrone.Api.Naming
 {
-    public class NamingResource : RestResource
+    public class NamingResource : NamingConfigResource
     {
-        public bool RenameEpisodes { get; set; }
-        public string Separator { get; set; }
-        public int NumberStyle { get; set; }
-        public bool IncludeSeriesTitle { get; set; }
-        public bool IncludeEpisodeTitle { get; set; }
-        public bool IncludeQuality { get; set; }
-        public int MultiEpisodeStyle { get; set; }
-        public bool ReplaceSpaces { get; set; }
-
         public string SingleEpisodeExample { get; set; }
         public string MultiEpisodeExample { get; set; }
     }
