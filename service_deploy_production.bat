16813327d183886eefd0af66ce74eae557aa88e3
Keivan Beigi <kay.one@gmail.com>
2013-08-16 15:22:21 -0700

    cleaned up some old files

--- service_deploy_production.bat
+++
@@ -1,6 +0,0 @@
-rd C:\inetpub\services /S /Q
-
-xcopy C:\inetpub\services_stage\*.* C:\inetpub\services\ /E /V /I /Y /F /C /o
-xcopy C:\inetpub\services\web.production.config c:\inetpub\services\web.config /o /y
-
-pause
