de145015d870173e4f21c4c3be8ba7c28904158a
kay.one <kay.one@gmail.com>
2013-08-18 16:51:26 -0700

    updated test script

--- tests_mono.sh
+++ tests_mono.sh
@@ -1,6 +1,6 @@
 EXCLUDE="/exclude:Windows"
-TESTDIR=.
-NUNIT=$TESTDIR/NUnit.Runners.2.6.1/tools/nunit-console-x86.exe
+TESTDIR="."
+NUNIT="$TESTDIR/NUnit.Runners.2.6.1/tools/nunit-console-x86.exe"
 
 mono $NUNIT $EXCLUDE  /xml:NzbDrone.Libraries.Result.xml $TESTDIR/NzbDrone.Libraries.Test.dll
 mono $NUNIT $EXCLUDE  /xml:NzbDrone.Libraries.Common.Result.xm $TESTDIR/NzbDrone.Common.Test.dll
