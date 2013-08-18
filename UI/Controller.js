2f061e648e80e7356cb795ae161edf188f626aaa
Mark McDowall <markus.mcd5@gmail.com>
2013-08-17 22:28:00 -0700

    No longer clearing cookies, oops.

--- UI/Controller.js
+++ UI/Controller.js
@@ -109,24 +109,6 @@ define(
                 else {
                     window.document.title = title + ' - NzbDrone';
                 }
-
-                this._clearCookies();
-            },
-
-            _clearCookies: function () {
-
-                if (!document.cookie) {
-                    return;
-                }
-
-                var cookies = document.cookie.split(';');
-
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = cookies[i];
-                    var eqPos = cookie.indexOf('=');
-                    var name = eqPos > -1 ? cookie.substr(0, eqPos) :cookie;
-                    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
-                }
             }
         });
     });
