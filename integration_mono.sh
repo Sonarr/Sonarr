238f1e6815f5104dc4c6e1c1cd77fb2eb22b7268
Keivan Beigi <kay.one@gmail.com>
2013-08-23 18:05:15 -0700

    Integration test script for mono

---
+++ integration_mono.sh
@@ -0,0 +1,7 @@
+EXCLUDE="-exclude:Windows,IntegrationTest"
+TESTDIR="."
+NUNIT="$TESTDIR/NUnit.Runners.2.6.1/tools/nunit-console-x86.exe"
+
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Api.Result.xml $TESTDIR/NzbDrone.Api.Test.dll
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Core.Result.xml $TESTDIR/NzbDrone.Core.Test.dll
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Integration.Result.xml $TESTDIR/NzbDrone.Integration.Test.dll
