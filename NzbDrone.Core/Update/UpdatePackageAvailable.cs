11c49c9260c38b2c61eeee25348e14b9cf66bfe2
Mark McDowall <markus.mcd5@gmail.com>
2013-08-23 19:21:12 -0700

    Using services to get updates now

---
+++ NzbDrone.Core/Update/UpdatePackageAvailable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Update
+{
+    public class UpdatePackageAvailable
+    {
+        public Boolean Available { get; set; }
+        public UpdatePackage UpdatePackage { get; set; }
+    }
+}
