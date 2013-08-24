8c3af7fbbfe8b90308500058e14d0af0e9269a73
Keivan Beigi <kay.one@gmail.com>
2013-08-23 17:51:01 -0700

    updated tests_mono.sh

--- tests_mono.sh
+++ tests_mono.sh
@@ -1,6 +1,9 @@
-EXCLUDE="/exclude:Windows"
+EXCLUDE="-exclude:Windows,IntegrationTest"
 TESTDIR="."
 NUNIT="$TESTDIR/NUnit.Runners.2.6.1/tools/nunit-console-x86.exe"
 
-mono $NUNIT $EXCLUDE  /xml:NzbDrone.Libraries.Result.xml $TESTDIR/NzbDrone.Libraries.Test.dll
-mono $NUNIT $EXCLUDE  /xml:NzbDrone.Libraries.Common.Result.xm $TESTDIR/NzbDrone.Common.Test.dll
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Api.Result.xml $TESTDIR/NzbDrone.Api.Test.dll
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Common.Result.xml $TESTDIR/NzbDrone.Common.Test.dll
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Core.Result.xml $TESTDIR/NzbDrone.Core.Test.dll
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Host.Result.xml $TESTDIR/NzbDrone.Host.Test.dll
+mono $NUNIT $EXCLUDE  -xml:NzbDrone.Libraries.Result.xml $TESTDIR/NzbDrone.Libraries.Test.dll
