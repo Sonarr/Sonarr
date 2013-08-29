9e5353aaccf65f45b5f97729b309da39ed592dbe
Mark McDowall <markus.mcd5@gmail.com>
2013-08-28 18:43:45 -0700

    Cleanup orphaned seasons when deleting episodes

---
+++ NzbDrone.Core/Tv/Events/EpisodeInfoDeletedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NzbDrone.Common.Messaging;
+
+namespace NzbDrone.Core.Tv.Events
+{
+    public class EpisodeInfoDeletedEvent : IEvent
+    {
+        public ReadOnlyCollection<Episode> Episodes { get; private set; }
+
+        public EpisodeInfoDeletedEvent(IList<Episode> episodes)
+        {
+            Episodes = new ReadOnlyCollection<Episode>(episodes);
+        }
+    }
+}
