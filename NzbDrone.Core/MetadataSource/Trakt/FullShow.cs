f72d4dd394cc917c6d6012c03e8cb5b3e3cca019
kay.one <kay.one@gmail.com>
2013-09-02 23:18:54 -0700

    emergency hack to fix breaking trakt api.

--- NzbDrone.Core/MetadataSource/Trakt/FullShow.cs
+++ NzbDrone.Core/MetadataSource/Trakt/FullShow.cs
@@ -29,4 +29,31 @@ namespace NzbDrone.Core.MetadataSource.Trakt
         public List<string> genres { get; set; }
         public List<Season> seasons { get; set; }
     }
+
+    public class SearchShow
+    {
+        public string title { get; set; }
+        public int year { get; set; }
+        public string url { get; set; }
+        public int first_aired { get; set; }
+        public string first_aired_iso { get; set; }
+        public int first_aired_utc { get; set; }
+        public string country { get; set; }
+        public string overview { get; set; }
+        public int runtime { get; set; }
+        public string status { get; set; }
+        public string network { get; set; }
+        public string air_day { get; set; }
+        public string air_day_utc { get; set; }
+        public string air_time { get; set; }
+        public string air_time_utc { get; set; }
+        public string certification { get; set; }
+        public string imdb_id { get; set; }
+        public int tvdb_id { get; set; }
+        public int tvrage_id { get; set; }
+        public int last_updated { get; set; }
+        public string poster { get; set; }
+        public Images images { get; set; }
+        public List<string> genres { get; set; }
+    }
 }
