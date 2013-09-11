89a72a50cf7b23f2a363ed81b325e70c0dcf3119
Mark McDowall <markus.mcd5@gmail.com>
2013-09-11 08:35:58 -0700

    Better error message when port is in use

---
+++ NzbDrone.Host/Owin/PortInUseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NzbDrone.Common.Exceptions;
+
+namespace NzbDrone.Host.Owin
+{
+    public class PortInUseException : NzbDroneException
+    {
+        public PortInUseException(string message, Exception innerException, params object[] args) : base(message, innerException, args)
+        {
+        }
+    }
+}
