94f64567c6e18bff8015c9be90a1a0b78cbfdb05
Mark McDowall <markus.mcd5@gmail.com>
2013-09-11 08:13:41 -0700

    Using regex match instead of endsWith for better browser compatibility

--- UI/Handlebars/Helpers/Series.js
+++ UI/Handlebars/Helpers/Series.js
@@ -64,7 +64,7 @@ define(
         });
 
         Handlebars.registerHelper('titleWithYear', function () {
-            if (this.title.endsWith(' ({0})'.format(this.year))) {
+            if (this.title.match(/\s\(\d{4}\)$/)) {
                 return this.title;
             }
 
