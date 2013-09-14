e2e787261d63190b615c03defa47001fd6553501
kay.one <kay.one@gmail.com>
2013-09-14 12:59:06 -0700

    don't log signalr heartbeat exceptions as EPIC FAIL.

--- NzbDrone.Common/Instrumentation/GlobalExceptionHandlers.cs
+++ NzbDrone.Common/Instrumentation/GlobalExceptionHandlers.cs
@@ -21,6 +21,15 @@ namespace NzbDrone.Common.Instrumentation
 
         private static void AppDomainException(Exception exception)
         {
+            if (exception == null) return;
+
+            if (exception is NullReferenceException &&
+                exception.ToString().Contains("Microsoft.AspNet.SignalR.Transports.TransportHeartbeat.ProcessServerCommand"))
+            {
+                Logger.Warn("SignalR Heartbeat error.");
+                return;
+            }
+
             Console.WriteLine("EPIC FAIL: {0}", exception);
             Logger.FatalException("EPIC FAIL: " + exception.Message, exception);
         }
