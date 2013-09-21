d1a4c7c942420b0541bae7e1d5ef6a7ff4222d34
Keivan Beigi <kay.one@gmail.com>
2013-09-20 17:18:11 -0700

    something!

---
+++ NzbDrone.Core.Test/ThingiProvider/ProviderBaseFixture.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using NzbDrone.Core.Test.Datastore;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.ThingiProvider;
+
+namespace NzbDrone.Core.Test.ThingiProvider
+{
+
+    public class ProviderRepositoryFixture : DbTest<DownloadProviderRepository, DownloadProviderModel>
+    {
+        [Test]
+        public void should_read_write_download_provider()
+        {
+            var model = new DownloadProviderModel();
+
+            model.Config = new DownloadProviderConfig();
+
+            //Subject.Insert(new )
+        }
+
+
+    }
+}
---
+++ NzbDrone.Core/ThingiProvider/ProviderBase.cs
@@ -0,0 +1,49 @@
+﻿
+using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Messaging.Events;
+
+namespace NzbDrone.Core.ThingiProvider
+{
+
+
+    public class DownloadProviderRepository : BasicRepository<DownloadProviderModel>
+    {
+        public DownloadProviderRepository(IDatabase database, IEventAggregator eventAggregator)
+            : base(database, eventAggregator)
+        {
+        }
+    }
+
+    public class DownloadProviderModel : Provider<DownloadProviderConfig>
+    {
+
+    }
+
+    public class DownloadProviderConfig : ProviderSetting
+    {
+
+    }
+
+
+    public abstract class Provider<TSettings> : ModelBase
+        where TSettings : ProviderSetting
+    {
+        public string Implementation { get; set; }
+        public TSettings Config { get; set; }
+    }
+
+    public abstract class ProviderSetting : IEmbeddedDocument
+    {
+
+    }
+
+    public abstract class ProviderBase<TSettings> where TSettings : ProviderSetting
+    {
+        public TSettings Settings { get; private set; }
+
+        public void LoadSettings(TSettings setting)
+        {
+            Settings = setting;
+        }
+    }
+}
