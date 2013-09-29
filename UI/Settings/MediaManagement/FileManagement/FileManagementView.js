0ee82feab7e33b6aece660deeae769285684df5b
Mark McDowall <markus.mcd5@gmail.com>
2013-09-28 21:17:20 -0700

    Recycling bin setting is available on Media Management (advanced)
    New: Optional recycling bin on Media Management (advanced)

--- UI/Settings/MediaManagement/FileManagement/FileManagementView.js
+++ UI/Settings/MediaManagement/FileManagement/FileManagementView.js
@@ -2,11 +2,20 @@
 define(
     [
         'marionette',
-        'Mixins/AsModelBoundView'
+        'Mixins/AsModelBoundView',
+        'Mixins/AutoComplete'
     ], function (Marionette, AsModelBoundView) {
 
         var view = Marionette.ItemView.extend({
-            template: 'Settings/MediaManagement/FileManagement/FileManagementViewTemplate'
+            template: 'Settings/MediaManagement/FileManagement/FileManagementViewTemplate',
+
+            ui: {
+                recyclingBin: '.x-path'
+            },
+
+            onShow: function () {
+                this.ui.recyclingBin.autoComplete('/directories');
+            }
         });
 
         return AsModelBoundView.call(view);
