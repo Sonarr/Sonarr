5bf0e197ec6fc9fe844cacba0f6c67010187a2c0
Mark McDowall <markm@iqmetrix.com>
2013-10-02 18:01:32 -0700

    Moved source code under src folder - massive change

---
+++ src/NzbDrone.Common/Instrumentation/LogEventExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NLog;
+using NzbDrone.Common.Serializer;
+
+namespace NzbDrone.Common.Instrumentation
+{
+    public static class LogEventExtensions
+    {
+        public static string GetHash(this LogEventInfo logEvent)
+        {
+            var stackString = logEvent.StackTrace.ToJson();
+            var hashSeed = String.Concat(logEvent.LoggerName, logEvent.Exception.GetType().ToString(), stackString, logEvent.Level);
+            return HashUtil.CalculateCrc(hashSeed);
+        }
+
+        public static string GetFormattedMessage(this LogEventInfo logEvent)
+        {
+            var message = logEvent.FormattedMessage;
+
+            if (logEvent.Exception != null)
+            {
+                if (logEvent.Exception != null)
+                {
+                    if (String.IsNullOrWhiteSpace(message))
+                    {
+                        message = logEvent.Exception.Message;
+                    }
+                    else
+                    {
+                        message += ": " + logEvent.Exception.Message;
+                    }
+                }
+
+            }
+
+            return message;
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Datastore/MappingExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+using Marr.Data;
+using Marr.Data.Mapping;
+using NzbDrone.Common.Reflection;
+
+namespace NzbDrone.Core.Datastore
+{
+    public static class MappingExtensions
+    {
+
+        public static ColumnMapBuilder<T> MapResultSet<T>(this FluentMappings.MappingsFluentEntity<T> mapBuilder) where T : ResultSet, new()
+        {
+            return mapBuilder
+                .Columns
+                .AutoMapPropertiesWhere(IsMappableProperty);
+        }
+
+
+        public static ColumnMapBuilder<T> RegisterModel<T>(this FluentMappings.MappingsFluentEntity<T> mapBuilder, string tableName = null) where T : ModelBase, new()
+        {
+            return mapBuilder.Table.MapTable(tableName)
+                             .Columns
+                             .AutoMapPropertiesWhere(IsMappableProperty)
+                             .PrefixAltNames(String.Format("{0}_", typeof(T).Name))
+                             .For(c => c.Id)
+                             .SetPrimaryKey()
+                             .SetReturnValue()
+                             .SetAutoIncrement();
+        }
+
+        public static RelationshipBuilder<T> AutoMapChildModels<T>(this ColumnMapBuilder<T> mapBuilder)
+        {
+            return mapBuilder.Relationships.AutoMapPropertiesWhere(m =>
+                    m.MemberType == MemberTypes.Property &&
+                    typeof(ModelBase).IsAssignableFrom(((PropertyInfo) m).PropertyType));
+        }
+
+        public static bool IsMappableProperty(MemberInfo memberInfo)
+        {
+            var propertyInfo = memberInfo as PropertyInfo;
+
+            if (propertyInfo == null) return false;
+
+
+            if (!propertyInfo.IsReadable() || !propertyInfo.IsWritable())
+            {
+                return false;
+            }
+
+            if (propertyInfo.PropertyType.IsSimpleType() || MapRepository.Instance.TypeConverters.ContainsKey(propertyInfo.PropertyType))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
