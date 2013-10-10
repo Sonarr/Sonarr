c9267757197c8dd642f3e5bdb8d496cab80c3d97
Mark McDowall <markus.mcd5@gmail.com>
2013-10-09 23:58:56 -0700

    Cleanup orphaned episode files if the series was deleted
    Fixed: Cleanup orphaned episode files after a series is deleted

---
+++ src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedEpisodeFiles.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using NzbDrone.Core.Datastore;
+
+namespace NzbDrone.Core.Housekeeping.Housekeepers
+{
+    public class CleanupOrphanedEpisodeFiles : IHousekeepingTask
+    {
+        private readonly IDatabase _database;
+        private readonly Logger _logger;
+
+        public CleanupOrphanedEpisodeFiles(IDatabase database, Logger logger)
+        {
+            _database = database;
+            _logger = logger;
+        }
+
+        public void Clean()
+        {
+            _logger.Trace("Running orphaned episode files cleanup");
+
+            var mapper = _database.GetDataMapper();
+
+            mapper.ExecuteNonQuery(@"DELETE FROM EpisodeFiles
+                                     WHERE Id IN (
+                                     SELECT EpisodeFiles.Id FROM EpisodeFiles
+                                     LEFT OUTER JOIN Episodes
+                                     ON EpisodeFiles.Id = Episodes.EpisodeFileId
+                                     WHERE Episodes.Id IS NULL)");
+        }
+    }
+}
