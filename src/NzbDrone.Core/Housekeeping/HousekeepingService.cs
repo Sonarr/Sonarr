8cfc9ef0a37f48745076c7b8cc5358f54de0b322
Mark McDowall <markus.mcd5@gmail.com>
2013-10-10 20:34:06 -0700

    Run cleanup when drone starts
    New: Cleanup jobs will run when drone starts as well as every 24 hours

--- src/NzbDrone.Core/Housekeeping/HousekeepingService.cs
+++ src/NzbDrone.Core/Housekeeping/HousekeepingService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using NLog;
+using NzbDrone.Core.Lifecycle;
 using NzbDrone.Core.Messaging.Commands;
+using NzbDrone.Core.Messaging.Events;
 
 namespace NzbDrone.Core.Housekeeping
 {
- public class HousekeepingService : IExecute<HousekeepingCommand>
+    public class HousekeepingService : IExecute<HousekeepingCommand>, IHandleAsync<ApplicationStartedEvent>
     {
         private readonly IEnumerable<IHousekeepingTask> _housekeepers;
         private readonly Logger _logger;
@@ -16,7 +18,7 @@ namespace NzbDrone.Core.Housekeeping
             _logger = logger;
         }
 
-        public void Execute(HousekeepingCommand message)
+        private void Clean()
         {
             _logger.Info("Running housecleaning tasks");
 
@@ -31,6 +33,16 @@ namespace NzbDrone.Core.Housekeeping
                     _logger.ErrorException("Error running housekeeping task: " + housekeeper.GetType().FullName, ex);
                 }
             }
+        }
+
+        public void Execute(HousekeepingCommand message)
+        {
+            Clean();
+        }
+
+        public void HandleAsync(ApplicationStartedEvent message)
+        {
+            Clean();
         }
     }
 }
