f99573e3342e713ad24afd4c997efc014592eee7
Mark McDowall <markus.mcd5@gmail.com>
2013-10-24 08:34:39 -0700

    Added some blacklist tests

---
+++ src/NzbDrone.Core.Test/Blacklisting/BlacklistRepositoryFixture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using NzbDrone.Core.Blacklisting;
+using NzbDrone.Core.Download;
+using NzbDrone.Core.Qualities;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Test.Blacklisting
+{
+    [TestFixture]
+    public class BlacklistRepositoryFixture : DbTest<BlacklistRepository, Blacklist>
+    {
+        private Blacklist _blacklist;
+
+        [SetUp]
+        public void Setup()
+        {
+            _blacklist = new Blacklist
+                     {
+                         SeriesId = 12345,
+                         EpisodeIds = new List<int> {1},
+                         Quality = new QualityModel(Quality.Bluray720p),
+                         SourceTitle = "series.title.s01e01",
+                         Date = DateTime.UtcNow
+                     };
+        }
+
+        [Test]
+        public void should_be_able_to_write_to_database()
+        {
+            Subject.Insert(_blacklist);
+            Subject.All().Should().HaveCount(1);
+        }
+
+        [Test]
+        public void should_should_have_episode_ids()
+        {
+            Subject.Insert(_blacklist);
+
+            Subject.All().First().EpisodeIds.Should().Contain(_blacklist.EpisodeIds);
+        }
+
+        [Test]
+        public void should_check_for_blacklisted_title_case_insensative()
+        {
+            Subject.Insert(_blacklist);
+
+            Subject.Blacklisted(_blacklist.SourceTitle.ToUpperInvariant()).Should().BeTrue();
+        }
+    }
+}
