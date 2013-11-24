afda9242f725421a712300f41de34e997ebe5b98
kayone <kay.one@gmail.com>
2013-11-23 17:40:20 -0800

    generate msi installer based on _output folder.

--- wix/package.bat
+++ wix/package.bat
@@ -1,4 +1,7 @@
 SET BUILD_NUMBER=1.9.9.9
 
-"bin\candle.exe" -nologo "nzbdrone.wxs" -out "nzbdrone.wixobj"  -ext WixNetFxExtension -ext WixUIExtension
-"bin\light.exe" -nologo "nzbdrone.wixobj" -out "nzbdrone.msi"  -ext WixNetFxExtension -ext WixUIExtension
+bin\heat.exe dir ..\_output\ -out _output.wxs -cg OUTPUT_GROUP -dr BIN_DIR -gg -scom -srd -sfrag -sreg -suid -svb6
+
+"bin\candle.exe" nzbdrone.wxs _output.wxs -ext WixNetFxExtension -ext WixUIExtension
+"bin\light.exe"  nzbdrone.wixobj _output.wixobj -out "nzbdrone.msi"  -ext WixNetFxExtension -ext WixUIExtension -b ..\_output
+
