39bb2ce80a77b8e92fc46228b4ab3fe7cf4f7387
Mark McDowall <markus.mcd5@gmail.com>
2013-11-30 23:24:30 -0800

    Refresh upcoming collection every hour

--- src/UI/Calendar/UpcomingCollectionView.js
+++ src/UI/Calendar/UpcomingCollectionView.js
@@ -2,16 +2,28 @@
 
 define(
     [
+        'underscore',
         'marionette',
         'Calendar/UpcomingCollection',
         'Calendar/UpcomingItemView',
         'Mixins/backbone.signalr.mixin'
-    ], function (Marionette, UpcomingCollection, UpcomingItemView) {
+    ], function (_, Marionette, UpcomingCollection, UpcomingItemView) {
         return Marionette.CollectionView.extend({
             itemView: UpcomingItemView,
 
             initialize: function () {
                 this.collection = new UpcomingCollection().bindSignalR({ updateOnly: true });
+                this.collection.fetch();
+
+                this._fetchCollection = _.bind(this._fetchCollection, this);
+                this.timer = window.setInterval(this._fetchCollection, 60 * 60 * 1000);
+            },
+
+            onClose: function () {
+                window.clearInterval(this.timer);
+            },
+
+            _fetchCollection: function () {
                 this.collection.fetch();
             }
         });
