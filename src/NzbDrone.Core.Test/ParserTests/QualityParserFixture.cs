c0f567633a5ef8e831ea1a71c260353afdf30900
Mark McDowall <markus.mcd5@gmail.com>
2013-12-04 23:16:24 -0800

    Fixed: Incorrect parsing as DVD for releases that contained 'pal' as part of another word

--- src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
@@ -77,6 +77,7 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Two.and.a.Half.Men.S08E05.720p.HDTV.X264-DIMENSION", false)]
         [TestCase("Sonny.With.a.Chance.S02E15.mkv", false)]
         [TestCase(@"E:\Downloads\tv\The.Big.Bang.Theory.S01E01.720p.HDTV\ajifajjjeaeaeqwer_eppj.avi", false)]
+        [TestCase("Gem.Hunt.S01E08.Tourmaline.Nepal.720p.HDTV.x264-DHD", false)]
         public void should_parse_hdtv720p_quality(string title, bool proper)
         {
             ParseAndVerifyQuality(title, Quality.HDTV720p, proper);
