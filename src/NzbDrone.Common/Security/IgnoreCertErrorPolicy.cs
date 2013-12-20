c2a09b70f4c96439f75e3d922f2210718bba5dd7
Mark McDowall <markus.mcd5@gmail.com>
2013-12-19 20:34:59 -0800

    New: SSL certificates will not be checked for validity (to support self-signed certificates)

--- src/NzbDrone.Common/Security/IgnoreCertErrorPolicy.cs
+++ src/NzbDrone.Common/Security/IgnoreCertErrorPolicy.cs
@@ -1,27 +1,18 @@
 ﻿using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using NLog;
-using NzbDrone.Common.EnvironmentInfo;
-using NzbDrone.Common.Instrumentation;
 
 namespace NzbDrone.Common.Security
 {
     public static class IgnoreCertErrorPolicy
     {
-        private static readonly Logger Logger = NzbDroneLogger.GetLogger();
-
         public static void Register()
         {
-            if (OsInfo.IsLinux)
-            {
-                ServicePointManager.ServerCertificateValidationCallback = ValidationCallback;
-            }
+            ServicePointManager.ServerCertificateValidationCallback = ValidationCallback;
         }
 
         private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslpolicyerrors)
         {
-            Logger.Warn("[{0}] {1}", sender.GetType(), sslpolicyerrors);
             return true;
         }
     }
