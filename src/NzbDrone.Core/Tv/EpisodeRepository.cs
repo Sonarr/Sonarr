3c2fca1f731f983af4bbe767bdb6cc95247b2234
Icer Addis <iceraddis@gmail.com>
2014-01-02 21:22:28 -0800

    UnitTests - Fixed IParsingService.ParseTitle mock

--- src/NzbDrone.Core/Tv/EpisodeRepository.cs
+++ src/NzbDrone.Core/Tv/EpisodeRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Marr.Data.QGen;
 using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Messaging.Events;
