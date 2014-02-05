f9b0b2206c809ca317303eeefe64e6d2df0344d0
markus101 <markus.mcd5@gmail.com>
2014-02-04 21:29:57 -0800

    Added RescanSeries command

---
+++ src/NzbDrone.Core/MediaFiles/Commands/RescanSeriesCommand.cs
@@ -0,0 +1,17 @@
+﻿using NzbDrone.Core.Messaging.Commands;
+
+namespace NzbDrone.Core.MediaFiles.Commands
+{
+    public class RescanSeriesCommand : Command
+    {
+        public int SeriesId { get; set; }
+
+        public override bool SendUpdatesToClient
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
