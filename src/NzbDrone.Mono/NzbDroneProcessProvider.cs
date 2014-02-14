7d4a514a68d8490650441009cd18d39b00785319
Mark McDowall <markus.mcd5@gmail.com>
2014-02-13 18:18:11 -0800

    Changed how running a process and waiting for exit is handled

--- src/NzbDrone.Mono/NzbDroneProcessProvider.cs
+++ src/NzbDrone.Mono/NzbDroneProcessProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using NLog;
 using NzbDrone.Common.Model;
 using NzbDrone.Common.Processes;
 
@@ -9,10 +10,12 @@ namespace NzbDrone.Mono
     public class NzbDroneProcessProvider : INzbDroneProcessProvider
     {
         private readonly IProcessProvider _processProvider;
+        private readonly Logger _logger;
 
-        public NzbDroneProcessProvider(IProcessProvider processProvider)
+        public NzbDroneProcessProvider(IProcessProvider processProvider, Logger logger)
         {
             _processProvider = processProvider;
+            _logger = logger;
         }
 
         public List<ProcessInfo> FindNzbDroneProcesses()
@@ -21,10 +24,19 @@ namespace NzbDrone.Mono
 
             return monoProcesses.Where(c =>
             {
-                var processArgs = _processProvider.StartAndCapture("ps", String.Format("-p {0} -o args=", c.Id));
+                try
+                {
+                    var processArgs = _processProvider.StartAndCapture("ps", String.Format("-p {0} -o args=", c.Id));
 
-                return processArgs.Standard.Any(p => p.Contains(ProcessProvider.NZB_DRONE_PROCESS_NAME + ".exe") ||
-                                                     p.Contains(ProcessProvider.NZB_DRONE_CONSOLE_PROCESS_NAME + ".exe"));
+                    return processArgs.Standard.Any(p => p.Contains(ProcessProvider.NZB_DRONE_PROCESS_NAME + ".exe") ||
+                                                         p.Contains(ProcessProvider.NZB_DRONE_CONSOLE_PROCESS_NAME + ".exe"));
+                }
+                catch (InvalidOperationException ex)
+                {
+                    _logger.WarnException("Error getting process arguments", ex);
+                    return false;
+                }
+                
             }).ToList();
         }
     }
