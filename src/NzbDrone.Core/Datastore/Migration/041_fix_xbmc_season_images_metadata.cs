5b2c3b88c0fdf002bb46a45e85c2f5066cc84877
Mark McDowall <markus.mcd5@gmail.com>
2014-02-16 22:41:13 -0800

    Couple XBMC Metadata fixes
    Fixed: Actor URL for XBMC Metadata
    Fixed: Incorrectly storing season images for XBMC metadata

---
+++ src/NzbDrone.Core/Datastore/Migration/041_fix_xbmc_season_images_metadata.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(41)]
+    public class fix_xbmc_season_images_metadata : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Execute.Sql("UPDATE MetadataFiles SET Type = 4 WHERE Consumer = 'XbmcMetadata' AND SeasonNumber IS NOT NULL");
+        }
+    }
+}
