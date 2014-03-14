088028b4261581a672bb0247a44c1a0966c8a710
Mark McDowall <markus.mcd5@gmail.com>
2014-03-14 12:35:34 -0700

    DB will log Info and above now (temp fix)

--- src/NzbDrone.Core/Instrumentation/DatabaseTarget.cs
+++ src/NzbDrone.Core/Instrumentation/DatabaseTarget.cs
@@ -24,7 +24,7 @@ namespace NzbDrone.Core.Instrumentation
         {
             Layout = new SimpleLayout("${callsite:className=false:fileName=false:includeSourcePath=false:methodName=true}");
 
-            Rule = new LoggingRule("*", LogLevel.Debug, this);
+            Rule = new LoggingRule("*", LogLevel.Info, this);
 
             LogManager.Configuration.AddTarget("DbLogger", this);
             LogManager.Configuration.LoggingRules.Add(Rule);
