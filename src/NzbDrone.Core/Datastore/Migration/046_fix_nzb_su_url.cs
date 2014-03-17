87f591dfbd271f4805cf99d9d0defda21465bb24
Mark McDowall <markus.mcd5@gmail.com>
2014-03-16 22:32:39 -0700

    Fixed: nzb.su URL changed to api.nzb.su

---
+++ src/NzbDrone.Core/Datastore/Migration/046_fix_nzb_su_url.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(46)]
+    public class fix_nzb_su_url : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Execute.Sql("UPDATE Indexers SET Settings = replace(Settings, '//nzb.su', '//api.nzb.su')" +
+                        "WHERE Implementation = 'Newznab'" +
+                        "AND Settings LIKE '%//nzb.su%'");
+        }
+    }
+}
