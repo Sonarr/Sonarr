63022626f1f55ad35044ad0f82a9beb16d7130db
Mark McDowall <markus.mcd5@gmail.com>
2014-04-13 13:25:47 -0700

    Fixed: Next airing will only include monitored episodes

--- src/NzbDrone.Core/SeriesStats/SeriesStatisticsRepository.cs
+++ src/NzbDrone.Core/SeriesStats/SeriesStatisticsRepository.cs
@@ -56,7 +56,7 @@ namespace NzbDrone.Core.SeriesStats
                      SeriesId,
                      SUM(CASE WHEN (Monitored = 1 AND AirdateUtc <= @currentDate) OR EpisodeFileId > 0 THEN 1 ELSE 0 END) AS EpisodeCount,
                      SUM(CASE WHEN EpisodeFileId > 0 THEN 1 ELSE 0 END) AS EpisodeFileCount,
-                     MIN(CASE WHEN AirDateUtc < @currentDate OR EpisodeFileId > 0 THEN NULL ELSE AirDateUtc END) AS NextAiringString
+                     MIN(CASE WHEN AirDateUtc < @currentDate OR EpisodeFileId > 0 OR Monitored = 0 THEN NULL ELSE AirDateUtc END) AS NextAiringString
                      FROM Episodes";
         }
 
