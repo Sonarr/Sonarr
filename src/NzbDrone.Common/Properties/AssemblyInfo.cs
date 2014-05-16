a1abcbcd40771a957bb5adb1fa774b41d5b062e2
Mark McDowall <markus.mcd5@gmail.com>
2014-05-16 08:47:53 -0700

    Fixed: Prevent the update button from being double clicked for 5 seconds

--- src/NzbDrone.Common/Properties/AssemblyInfo.cs
+++ src/NzbDrone.Common/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyVersion("10.0.0.*")]
 [assembly: AssemblyFileVersion("10.0.0.*")]
