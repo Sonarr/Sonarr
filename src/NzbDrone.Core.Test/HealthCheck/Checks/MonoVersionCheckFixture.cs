7dd573163365a9aa5c256fa331c43faee6bb53c2
Mark McDowall <markm@iqmetrix.com>
2014-07-04 07:52:24 -0700

    Version check for mono 3.6.1

--- src/NzbDrone.Core.Test/HealthCheck/Checks/MonoVersionCheckFixture.cs
+++ src/NzbDrone.Core.Test/HealthCheck/Checks/MonoVersionCheckFixture.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using FluentAssertions;
 using NUnit.Framework;
 using NzbDrone.Common.Processes;
 using NzbDrone.Core.HealthCheck.Checks;
@@ -75,6 +74,31 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
         public void should_return_ok_when_mono_3_2_1()
         {
             GivenOutput("3.2.1");
+
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_ok_when_mono_3_6_1()
+        {
+            GivenOutput("3.6.1");
+
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_ok_when_mono_3_6_1_with_custom_output()
+        {
+            Mocker.GetMock<IProcessProvider>()
+                  .Setup(s => s.StartAndCapture("mono", "--version"))
+                  .Returns(new ProcessOutput
+                  {
+                      Standard = new List<string>
+                      {
+                          "Mono JIT compiler version 3.6.1 (master/fce3972 Fri Jul 4 01:12:43 CEST 2014)",
+                          "Copyright (C) 2002-2011 Novell, Inc, Xamarin, Inc and Contributors. www.mono-project.com"
+                      }
+                  });
 
             Subject.Check().ShouldBeOk();
         }
