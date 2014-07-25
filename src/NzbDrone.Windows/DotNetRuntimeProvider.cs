0d112075cbd855cff205ec0be0077e39b4d3e94f
Mark McDowall <markus.mcd5@gmail.com>
2014-07-24 23:40:55 -0700

    Added implementation of IRuntimeProvider for Windows

---
+++ src/NzbDrone.Windows/DotNetRuntimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NzbDrone.Common.Processes;
+
+namespace NzbDrone.Windows
+{
+    public class DotNetRuntimeProvider : IRuntimeProvider
+    {
+        public String GetVersion()
+        {
+            return Environment.Version.ToString();
+        }
+    }
+}
