BO45/NzbDrone

New: Enable/Disable RSS Sync/Searching on a per indexer basis

src/NzbDrone.Api/Indexers/IndexerResource.cs
@@ -5,7 +5,9 @@ namespace NzbDrone.Api.Indexers
 {
     public class IndexerResource : ProviderResource
     {
-        public Boolean Enable { get; set; }
+        public Boolean EnableRss { get; set; }
+        public Boolean EnableSearch { get; set; }
+        public Boolean SupportsSearching { get; set; }
         public DownloadProtocol Protocol { get; set; }
     }
 }

src/NzbDrone.Core/Indexers/IndexerDefinition.cs
@@ -1,9 +1,21 @@
-﻿using NzbDrone.Core.ThingiProvider;
+﻿using System;
+using NzbDrone.Core.ThingiProvider;
 
 namespace NzbDrone.Core.Indexers
 {
     public class IndexerDefinition : ProviderDefinition
     {
+        public Boolean EnableRss { get; set; }
+        public Boolean EnableSearch { get; set; }
         public DownloadProtocol Protocol { get; set; }
+        public Boolean SupportsSearching { get; set; }
+
+        public override Boolean Enable
+        {
+            get
+            {
+                return EnableRss || EnableSearch;
+            }
+        }
     }
 }

Sonarr/Sonarr

src/NzbDrone.Api/Indexers/IndexerResource.cs
@@ -5,7 +5,10 @@ namespace NzbDrone.Api.Indexers
 {
     public class IndexerResource : ProviderResource
     {
-        public Boolean Enable { get; set; }
+        public Boolean EnableRss { get; set; }
+        public Boolean EnableSearch { get; set; }
+        public Boolean SupportsRss { get; set; }
+        public Boolean SupportsSearch { get; set; }
         public DownloadProtocol Protocol { get; set; }
     }
 }

src/NzbDrone.Core/Indexers/IndexerDefinition.cs
@@ -1,9 +1,22 @@
-﻿using NzbDrone.Core.ThingiProvider;
+﻿using System;
+using NzbDrone.Core.ThingiProvider;
 
 namespace NzbDrone.Core.Indexers
 {
     public class IndexerDefinition : ProviderDefinition
     {
+        public Boolean EnableRss { get; set; }
+        public Boolean EnableSearch { get; set; }
         public DownloadProtocol Protocol { get; set; }
+        public Boolean SupportsRss { get; set; }
+        public Boolean SupportsSearch { get; set; }
+
+        public override Boolean Enable
+        {
+            get
+            {
+                return EnableRss || EnableSearch;
+            }
+        }
     }
 }