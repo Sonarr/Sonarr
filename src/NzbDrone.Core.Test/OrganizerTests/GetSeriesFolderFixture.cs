044d5ad0a57e552004b5ff642a73e1e3145ae177
Mark McDowall <markus.mcd5@gmail.com>
2014-09-02 21:44:02 -0700

    A couple more tests for periods at start/end of folder name

--- src/NzbDrone.Core.Test/OrganizerTests/GetSeriesFolderFixture.cs
+++ src/NzbDrone.Core.Test/OrganizerTests/GetSeriesFolderFixture.cs
@@ -26,6 +26,8 @@ namespace NzbDrone.Core.Test.OrganizerTests
         [TestCase("30 Rock", "{Series.Title}", "30.Rock")]
         [TestCase("24/7 Road to the NHL Winter Classic", "{Series Title}", "24+7 Road to the NHL Winter Classic")]
         [TestCase("Venture Bros.", "{Series.Title}", "Venture.Bros")]
+        [TestCase(".hack", "{Series.Title}", "hack")]
+        [TestCase("30 Rock", ".{Series.Title}.", "30.Rock")]
         public void should_use_seriesFolderFormat_to_build_folder_name(String seriesTitle, String format, String expected)
         {
             namingConfig.SeriesFolderFormat = format;
