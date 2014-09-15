5081af0446e4ec53378607cd8f113732d0e32177
Taloth Saldono <Taloth@users.noreply.github.com>
2014-09-15 21:35:03 +0200

    New: Added support for BroadcastheNet private provider for normal and daily series.

---
+++ src/NzbDrone.Core/Indexers/BroadcastheNet/BroadcastheNetTorrents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Indexers.BroadcastheNet
+{
+    public class BroadcastheNetTorrents
+    {
+        public Dictionary<Int32, BroadcastheNetTorrent> Torrents { get; set; }
+        public Int32 Results { get; set; }
+    }
+}
