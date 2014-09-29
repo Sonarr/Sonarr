4d649b21d49319066c738262dc8f73eec23394e1
Mark McDowall <markus.mcd5@gmail.com>
2014-09-29 07:09:03 -0700

    UI!

--- src/UI/Mixins/TagInput.js
+++ src/UI/Mixins/TagInput.js
@@ -2,24 +2,80 @@
 define(
     [
         'jquery',
+        'underscore',
         'Tags/TagCollection',
+        'Tags/TagModel',
         'bootstrap.tagsinput'
-    ], function ($, TagCollection) {
+    ], function ($, _, TagCollection, TagModel) {
 
-    $.fn.tagInput = function () {
-        var test = TagCollection.map(function (tag) {
-            return tag.get('label');
-        });
+        $.fn.tagInput = function () {
+            var input = this;
+            var tagValues = $(this).val().replace(' ', '').split(',');
+            var tags = getExistingTags(tagValues);
 
-        $(this).tagsinput({
-            freeInput: true,
-//            itemValue : 'id',
-//            itemText  : 'label',
-//            trimValue : true,
-            typeahead : {
-//                source : test
-                source: ['Amsterdam', 'Washington', 'Sydney', 'Beijing', 'Cairo']
-            }
-        });
-    };
-});
+            $(this).attr('placeholder', 'Enter tag');
+            $(this).val('');
+
+            var tagInput = $(this).tagsinput({
+                freeInput: true,
+                itemValue : 'id',
+                itemText  : 'label',
+                trimValue : true,
+                typeaheadjs : {
+                    name: 'tags',
+                    displayKey: 'label',
+                    source: substringMatcher()
+                }
+            });
+
+            //Override the free input being set to false because we're using objects
+            $(tagInput)[0].options.freeInput = true;
+
+            var proto = $(tagInput)[0].__proto__;
+            var originalAdd = proto.add;
+            proto.add = function (item, dontPushVal) {
+                if (typeof item === 'string') {
+                    //create a new tag
+
+                    var newTag = new TagModel();
+                    newTag.set({ label: item });
+                    TagCollection.add(newTag);
+
+                    //newTag.save();
+                    newTag.set({ id: 5 });
+
+                    item = newTag.toJSON();
+                }
+
+                window.alert('do stuff here');
+                originalAdd.call($(tagInput)[0], item, dontPushVal);
+            };
+
+            $(this).tagsinput('removeAll');
+
+            _.each(tags, function(tag) {
+                $(input).tagsinput('add', tag);
+            });
+        };
+
+        var substringMatcher = function() {
+            return function findMatches(q, cb) {
+                // regex used to determine if a string contains the substring `q`
+                var substrRegex = new RegExp(q, 'i');
+
+                var matches = _.select(TagCollection.toJSON(), function (tag) {
+                    return substrRegex.test(tag.label);
+                });
+
+                cb(matches);
+            };
+        };
+
+        var getExistingTags = function(tagValues) {
+            var tags = _.select(TagCollection.toJSON(), function (tag) {
+                return _.contains(tagValues, tag.id.toString());
+            });
+
+            return tags;
+        };
+    });
