1b6f6845222170af64c6138517f6c9aaafb084bb
Keivan Beigi <me@keivan.io>
2014-10-13 10:36:51 -0700

    New: Added health check to show a warning when MediaInfo is missing

---
+++ src/NzbDrone.Core/HealthCheck/Checks/MediaInfoDllCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using MediaInfoLib;
+
+namespace NzbDrone.Core.HealthCheck.Checks
+{
+    public class MediaInfoDllCheck : HealthCheckBase
+    {
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public override HealthCheck Check()
+        {
+            try
+            {
+                var mediaInfo = new MediaInfo();
+            }
+            catch (Exception e)
+            {
+                return new HealthCheck(GetType(), HealthCheckResult.Warning, "MediaInfo could not be loaded " + e.Message);
+            }
+
+            return new HealthCheck(GetType());
+        }
+
+        public override bool CheckOnConfigChange
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
