a46b542bfd73ee26c6a03d2e2d7bdd3f9a89a9e8
Keivan Beigi <me@keivan.io>
2014-11-26 11:17:58 -0800

    updated MediaInfo to 0.7.71

--- src/NzbDrone.Core.Test/MediaFiles/MediaInfo/VideoFileInfoReaderFixture.cs
+++ src/NzbDrone.Core.Test/MediaFiles/MediaInfo/VideoFileInfoReaderFixture.cs
@@ -2,7 +2,6 @@ using System.IO;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
-using NzbDrone.Common;
 using NzbDrone.Common.Disk;
 using NzbDrone.Core.MediaFiles.MediaInfo;
 using NzbDrone.Core.Test.Framework;
@@ -28,6 +27,32 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             var path = Path.Combine(Directory.GetCurrentDirectory(), "Files", "Media", "H264_sample.mp4");
 
             Subject.GetRunTime(path).Seconds.Should().Be(10);
+
+        }
+
+
+        [Test]
+        public void get_info()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Files", "Media", "H264_sample.mp4");
+
+            var info = Subject.GetMediaInfo(path);
+
+
+            info.AudioBitrate.Should().Be(128000);
+            info.AudioChannels.Should().Be(2);
+            info.AudioFormat.Should().Be("AAC");
+            info.AudioLanguages.Should().Be("English");
+            info.AudioProfile.Should().Be("LC");
+            info.Height.Should().Be(320);
+            info.RunTime.Seconds.Should().Be(10);
+            info.ScanType.Should().Be("Progressive");
+            info.Subtitles.Should().Be("");
+            info.VideoBitrate.Should().Be(193329);
+            info.VideoCodec.Should().Be("AVC");
+            info.VideoFps.Should().Be(24);
+            info.Width.Should().Be(480);
+
         }
     }
 }
