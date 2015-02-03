a7001ab3227b3d4acb62efd0ad4b2d40ab31b663
Keivan Beigi <me@keivan.io>
2015-02-03 15:33:40 -0800

    updated to webpack 1.5.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.exports = {
   entry     : 'main.js',
   resolve : {
-  root : 'src/UI',
+  root : path.join(__dirname, "src", "ui"),
   alias : {
       'templates'               : '/templates',
       'backbone'                : 'Shims/backbone',
