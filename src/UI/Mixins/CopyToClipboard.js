133f31a80c62dfd2bc095b979f2d863efe5c55a4
Taloth Saldono <Taloth@users.noreply.github.com>
2015-02-04 22:55:11 +0100

    Removed wrapper function.

--- src/UI/Mixins/CopyToClipboard.js
+++ src/UI/Mixins/CopyToClipboard.js
@@ -3,22 +3,20 @@ var StatusModel = require('../System/StatusModel');
 var ZeroClipboard = require('zero.clipboard');
 var Messenger = require('../Shared/Messenger');
 
-module.exports = (function(){
-    $.fn.copyToClipboard = function(input){
+$.fn.copyToClipboard = function(input) {
 
-        ZeroClipboard.config({
-            swfPath: StatusModel.get('urlBase') + '/Content/zero.clipboard.swf'
+    ZeroClipboard.config({
+        swfPath: StatusModel.get('urlBase') + '/Content/zero.clipboard.swf'
+    });
+
+    var client = new ZeroClipboard(this);
+
+    client.on('ready', function(e) {
+        client.on('copy', function(e) {
+            e.clipboardData.setData("text/plain", input.val());
         });
-
-        var client = new ZeroClipboard(this);
-
-        client.on('ready', function(e) {
-            client.on('copy', function(e) {
-                e.clipboardData.setData("text/plain", input.val());
-            });
-            client.on('aftercopy', function() {
-                Messenger.show({message : 'Copied text to clipboard'});
-            });
+        client.on('aftercopy', function() {
+            Messenger.show({message : 'Copied text to clipboard'});
         });
-    };
-}).call(this);
+    });
+};
