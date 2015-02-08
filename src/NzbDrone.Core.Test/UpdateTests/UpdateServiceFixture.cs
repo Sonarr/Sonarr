5b54b02d7ebc42564a51392b18c5381e149e18e7
Keivan Beigi <me@keivan.io>
2015-02-07 22:40:20 -0800

    updated download url in UpdateServiceFixture

--- src/NzbDrone.Core.Test/UpdateTests/UpdateServiceFixture.cs
+++ src/NzbDrone.Core.Test/UpdateTests/UpdateServiceFixture.cs
@@ -34,7 +34,7 @@ namespace NzbDrone.Core.Test.UpdateTests
                 _updatePackage = new UpdatePackage
                 {
                     FileName = "NzbDrone.develop.2.0.0.0.tar.gz",
-                    Url = "http://update.nzbdrone.com/v2/develop/mono/NzbDrone.develop.tar.gz",
+                    Url = "http://download.sonarr.tv/v2/develop/mono/NzbDrone.develop.tar.gz",
                     Version = new Version("2.0.0.0")
                 };
             }
@@ -44,7 +44,7 @@ namespace NzbDrone.Core.Test.UpdateTests
                 _updatePackage = new UpdatePackage
                 {
                     FileName = "NzbDrone.develop.2.0.0.0.zip",
-                    Url = "http://update.nzbdrone.com/v2/develop/windows/NzbDrone.develop.zip",
+                    Url = "http://download.sonarr.tv/v2/develop/windows/NzbDrone.develop.zip",
                     Version = new Version("2.0.0.0")
                 };
             }
