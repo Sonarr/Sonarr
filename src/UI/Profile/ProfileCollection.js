860f55996cb854b23fa9c13f518c7c4a31a4feb1
Taloth Saldono <Taloth@users.noreply.github.com>
2015-02-13 22:47:42 +0100

    UI Cleanup - Updated Navbar, Profile, Quality and Release subtrees.

--- src/UI/Profile/ProfileCollection.js
+++ src/UI/Profile/ProfileCollection.js
@@ -1,12 +1,13 @@
 var Backbone = require('backbone');
 var ProfileModel = require('./ProfileModel');
 
-module.exports = (function(){
-    var ProfileCollection = Backbone.Collection.extend({
-        model : ProfileModel,
-        url   : window.NzbDrone.ApiRoot + '/profile'
-    });
-    var profiles = new ProfileCollection();
-    profiles.fetch();
-    return profiles;
-}).call(this);
+var ProfileCollection = Backbone.Collection.extend({
+    model : ProfileModel,
+    url   : window.NzbDrone.ApiRoot + '/profile'
+});
+
+var profiles = new ProfileCollection();
+
+profiles.fetch();
+
+module.exports = profiles;
--- src/UI/Quality/QualityDefinitionModel.js
+++ src/UI/Quality/QualityDefinitionModel.js
@@ -2,10 +2,13 @@ var ModelBase = require('../Settings/SettingsModelBase');
 
 module.exports = ModelBase.extend({
     baseInitialize : ModelBase.prototype.initialize,
-    initialize     : function(){
+
+    initialize : function() {
         var name = this.get('quality').name;
+
         this.successMessage = 'Saved ' + name + ' quality settings';
         this.errorMessage = 'Couldn\'t save ' + name + ' quality settings';
+
         this.baseInitialize.call(this);
     }
 });
