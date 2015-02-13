fb7988edb8b2c6d8098355f234b6196c4455856d
Taloth Saldono <Taloth@users.noreply.github.com>
2015-02-13 22:05:08 +0100

    UI Cleanup - Updated Episode subtree.

--- src/UI/Episode/Activity/EpisodeActivityActionsCell.js
+++ src/UI/Episode/Activity/EpisodeActivityActionsCell.js
@@ -4,18 +4,28 @@ var Marionette = require('marionette');
 var NzbDroneCell = require('../../Cells/NzbDroneCell');
 
 module.exports = NzbDroneCell.extend({
-    className     : 'episode-actions-cell',
-    events        : {"click .x-failed" : '_markAsFailed'},
-    render        : function(){
+    className : 'episode-actions-cell',
+
+    events : {
+        'click .x-failed' : '_markAsFailed'
+    },
+
+    render : function() {
         this.$el.empty();
-        if(this.model.get('eventType') === 'grabbed') {
+
+        if (this.model.get('eventType') === 'grabbed') {
             this.$el.html('<i class="icon-nd-delete x-failed" title="Mark download as failed"></i>');
         }
+
         return this;
     },
-    _markAsFailed : function(){
+
+    _markAsFailed : function() {
         var url = window.NzbDrone.ApiRoot + '/history/failed';
-        var data = {id : this.model.get('id')};
+        var data = {
+            id : this.model.get('id')
+        };
+
         $.ajax({
             url  : url,
             type : 'POST',
--- src/UI/Episode/Search/ManualLayout.js
+++ src/UI/Episode/Search/ManualLayout.js
@@ -11,48 +11,63 @@ var PeersCell = require('../../Release/PeersCell');
 
 module.exports = Marionette.Layout.extend({
     template : 'Episode/Search/ManualLayoutTemplate',
-    regions  : {grid : '#episode-release-grid'},
-    columns  : [{
-        name  : 'protocol',
-        label : 'Source',
-        cell  : ProtocolCell
-    }, {
-        name  : 'age',
-        label : 'Age',
-        cell  : AgeCell
-    }, {
-        name  : 'title',
-        label : 'Title',
-        cell  : ReleaseTitleCell
-    }, {
-        name  : 'indexer',
-        label : 'Indexer',
-        cell  : Backgrid.StringCell
-    }, {
-        name  : 'size',
-        label : 'Size',
-        cell  : FileSizeCell
-    }, {
-        name  : 'seeders',
-        label : 'Peers',
-        cell  : PeersCell
-    }, {
-        name  : 'quality',
-        label : 'Quality',
-        cell  : QualityCell
-    }, {
-        name     : 'rejections',
-        label    : '',
-        cell     : ApprovalStatusCell,
-        sortable : false
-    }, {
-        name     : 'download',
-        label    : '',
-        cell     : DownloadReportCell,
-        sortable : true
-    }],
-    onShow   : function(){
-        if(!this.isClosed) {
+
+    regions : {
+        grid : '#episode-release-grid'
+    },
+
+    columns : [
+        {
+            name  : 'protocol',
+            label : 'Source',
+            cell  : ProtocolCell
+        },
+        {
+            name  : 'age',
+            label : 'Age',
+            cell  : AgeCell
+        },
+        {
+            name  : 'title',
+            label : 'Title',
+            cell  : ReleaseTitleCell
+        },
+        {
+            name  : 'indexer',
+            label : 'Indexer',
+            cell  : Backgrid.StringCell
+        },
+        {
+            name  : 'size',
+            label : 'Size',
+            cell  : FileSizeCell
+        },
+        {
+            name  : 'seeders',
+            label : 'Peers',
+            cell  : PeersCell
+        },
+        {
+            name  : 'quality',
+            label : 'Quality',
+            cell  : QualityCell
+        },
+        {
+            name     : 'rejections',
+            label    : '',
+            cell     : ApprovalStatusCell,
+            sortable : false
+        },
+        {
+            name     : 'download',
+            label    : '',
+            cell     : DownloadReportCell,
+            sortable : true
+        }
+    ],
+
+    onShow : function() {
+        if (!this.isClosed) {
             this.grid.show(new Backgrid.Grid({
                 row        : Backgrid.Row,
                 columns    : this.columns,
