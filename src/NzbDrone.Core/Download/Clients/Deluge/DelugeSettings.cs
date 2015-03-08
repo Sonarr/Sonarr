112cde1cee1bbab2f00370cb2a6ec2fe56b8255a
Taloth Saldono <Taloth@users.noreply.github.com>
2015-03-08 15:37:33 +0100

    New: Added UrlBase to Deluge Settings to facilitate seedbox setups.

--- src/NzbDrone.Core/Download/Clients/Deluge/DelugeSettings.cs
+++ src/NzbDrone.Core/Download/Clients/Deluge/DelugeSettings.cs
@@ -35,19 +35,22 @@ namespace NzbDrone.Core.Download.Clients.Deluge
         [FieldDefinition(1, Label = "Port", Type = FieldType.Textbox)]
         public Int32 Port { get; set; }
 
-        [FieldDefinition(2, Label = "Password", Type = FieldType.Password)]
+        [FieldDefinition(2, Label = "Url Base", Type = FieldType.Textbox, Advanced = true, HelpText = "Adds a prefix to the deluge json url, see http://[host]:[port]/[urlBase]/json")]
+        public String UrlBase { get; set; }
+
+        [FieldDefinition(3, Label = "Password", Type = FieldType.Password)]
         public String Password { get; set; }
 
-        [FieldDefinition(3, Label = "Category", Type = FieldType.Textbox, HelpText = "Adding a category specific to Sonarr avoids conflicts with unrelated downloads, but it's optional")]
+        [FieldDefinition(4, Label = "Category", Type = FieldType.Textbox, HelpText = "Adding a category specific to Sonarr avoids conflicts with unrelated downloads, but it's optional")]
         public String TvCategory { get; set; }
 
-        [FieldDefinition(4, Label = "Recent Priority", Type = FieldType.Select, SelectOptions = typeof(DelugePriority), HelpText = "Priority to use when grabbing episodes that aired within the last 14 days")]
+        [FieldDefinition(5, Label = "Recent Priority", Type = FieldType.Select, SelectOptions = typeof(DelugePriority), HelpText = "Priority to use when grabbing episodes that aired within the last 14 days")]
         public Int32 RecentTvPriority { get; set; }
 
-        [FieldDefinition(5, Label = "Older Priority", Type = FieldType.Select, SelectOptions = typeof(DelugePriority), HelpText = "Priority to use when grabbing episodes that aired over 14 days ago")]
+        [FieldDefinition(6, Label = "Older Priority", Type = FieldType.Select, SelectOptions = typeof(DelugePriority), HelpText = "Priority to use when grabbing episodes that aired over 14 days ago")]
         public Int32 OlderTvPriority { get; set; }
 
-        [FieldDefinition(6, Label = "Use SSL", Type = FieldType.Checkbox)]
+        [FieldDefinition(7, Label = "Use SSL", Type = FieldType.Checkbox)]
         public Boolean UseSsl { get; set; }
 
         public ValidationResult Validate()
