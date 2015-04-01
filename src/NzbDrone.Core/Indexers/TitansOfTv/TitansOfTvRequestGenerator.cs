9fb5b4be1bdbbbe4a392d20e35edc17626791148
AutoDownloader <torrentpirate42@gmail.com>
2015-03-31 21:53:17 -0500

    add initial titans stub

---
+++ src/NzbDrone.Core/Indexers/TitansOfTv/TitansOfTvRequestGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Indexers.TitansOfTv
+{
+    public class TitansOfTvRequestGenerator : IIndexerRequestGenerator
+    {
+        public TitansOfTvSettings Settings { get; set; }
+
+        public IList<IEnumerable<IndexerRequest>> GetRecentRequests()
+        {
+            var pageableRequests = new List<IEnumerable<IndexerRequest>>();
+
+            return pageableRequests;
+        }
+
+        public IList<IEnumerable<IndexerRequest>> GetSearchRequests(IndexerSearch.Definitions.SingleEpisodeSearchCriteria searchCriteria)
+        {
+            return new List<IEnumerable<IndexerRequest>>();
+        }
+
+        public IList<IEnumerable<IndexerRequest>> GetSearchRequests(IndexerSearch.Definitions.SeasonSearchCriteria searchCriteria)
+        {
+            return new List<IEnumerable<IndexerRequest>>();
+        }
+
+        public IList<IEnumerable<IndexerRequest>> GetSearchRequests(IndexerSearch.Definitions.DailyEpisodeSearchCriteria searchCriteria)
+        {
+            return new List<IEnumerable<IndexerRequest>>();
+        }
+
+        public IList<IEnumerable<IndexerRequest>> GetSearchRequests(IndexerSearch.Definitions.AnimeEpisodeSearchCriteria searchCriteria)
+        {
+            return new List<IEnumerable<IndexerRequest>>();
+        }
+
+        public IList<IEnumerable<IndexerRequest>> GetSearchRequests(IndexerSearch.Definitions.SpecialEpisodeSearchCriteria searchCriteria)
+        {
+            return new List<IEnumerable<IndexerRequest>>();
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Indexers/TitansOfTv/TitansOfTvSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentValidation;
+using FluentValidation.Results;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.Indexers.TitansOfTv
+{
+
+       public class TitansOfTvSettingsValidator : AbstractValidator<TitansOfTvSettings>
+    {
+        public TitansOfTvSettingsValidator()
+        {
+            RuleFor(c => c.ApiKey).ValidRootUrl();
+        }
+    }
+
+    public class TitansOfTvSettings : IProviderConfig
+    {
+     private static readonly TitansOfTvSettingsValidator Validator = new TitansOfTvSettingsValidator();
+
+     public TitansOfTvSettings()
+        {
+            ApiKey = "";
+        }
+
+        [FieldDefinition(0, Label = "API key", HelpText = "Enter your ToTV API key. (Profile->API->Site API Key)")]
+        public String ApiKey { get; set; }
+
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
