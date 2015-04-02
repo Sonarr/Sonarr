c15711cfd31c51b39c8dd84c810e66cb15693a14
Victor GIUBILEI <victor.giubilei@epitech.eu>
2015-04-02 18:53:18 +0200

    Fixed case sensitive file

---
+++ src/NzbDrone.Core/Metadata/Files/ImageFileResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NzbDrone.Core.Metadata.Files
+{
+    public class ImageFileResult
+    {
+        public String RelativePath { get; set; }
+        public String Url { get; set; }
+
+        public ImageFileResult(string relativePath, string url)
+        {
+            RelativePath = relativePath;
+            Url = url;
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Metadata/Files/MetadataFileResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NzbDrone.Core.Metadata.Files
+{
+    public class MetadataFileResult
+    {
+        public String RelativePath { get; set; }
+        public String Contents { get; set; }
+
+        public MetadataFileResult(string relativePath, string contents)
+        {
+            RelativePath = relativePath;
+            Contents = contents;
+        }
+    }
+}
