cfefed34fc0056679b327fdf248164c36fb9c0cf
Mark McDowall <markus.mcd5@gmail.com>
2015-04-10 07:30:06 -0700

    API endpoint to parse a release title

---
+++ src/NzbDrone.Api/Parse/ParseResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using NzbDrone.Api.Episodes;
+using NzbDrone.Api.REST;
+using NzbDrone.Api.Series;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Api.Parse
+{
+    public class ParseResource : RestResource
+    {
+        public string Title { get; set; }
+        public ParsedEpisodeInfo ParsedEpisodeInfo { get; set; }
+        public SeriesResource Series { get; set; }
+        public List<EpisodeResource> Episodes { get; set; }
+    }
+}
