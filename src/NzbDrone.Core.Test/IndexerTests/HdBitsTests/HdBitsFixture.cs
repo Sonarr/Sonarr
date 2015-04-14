3ae2883eb587f60fd7bf81edc0a343b884980c34
scherzo <scherz0@users.noreply.github.com>
2015-04-13 21:30:02 -0700

    New: Add support for the HDBits torrent tracker.
    The indexer implementation borrows heavily from the BroadcastTheNet
    implementation as HDBits also provides a JSON API that can be used
    to query both the recent torrents and the catalog.

---
+++ src/NzbDrone.Core.Test/IndexerTests/HdBitsTests/HdBitsFixture.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+using NUnit.Framework;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Indexers.HDBits;
+using NzbDrone.Core.Test.Framework;
+using FluentAssertions;
+using System.Linq;
+using NzbDrone.Core.Parser.Model;
+using System;
+using System.Text;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.IndexerTests.HdBitsTests
+{
+    [TestFixture]
+    public class HdBitsFixture : CoreTest<HdBits>
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Subject.Definition = new IndexerDefinition()
+            {
+                Name = "HdBits",
+                Settings = new HdBitsSettings() { ApiKey = "fakekey" }
+            };
+        }
+
+        [Test]
+        public void TestSimpleResponse()
+        {
+            var responseJson = ReadAllText(@"Files/Indexers/HdBits/RecentFeed.json");
+
+            Mocker.GetMock<IHttpClient>()
+                .Setup(o => o.Execute(It.Is<HttpRequest>(v => v.Method == HttpMethod.POST)))
+                .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), responseJson));
+
+            var torrents = Subject.FetchRecent();
+
+            torrents.Should().HaveCount(2);
+            torrents.First().Should().BeOfType<TorrentInfo>();
+
+            var first = torrents.First() as TorrentInfo;
+
+            first.Guid.Should().Be("HDBits-257142");
+            first.Title.Should().Be("Supernatural S10E17 1080p WEB-DL DD5.1 H.264-ECI");
+            first.DownloadProtocol.Should().Be(DownloadProtocol.Torrent);
+            first.DownloadUrl.Should().Be("https://hdbits.org/download.php?id=257142&passkey=fakekey");
+            first.InfoUrl.Should().Be("https://hdbits.org/details.php?id=257142");
+            first.PublishDate.Should().Be(DateTime.Parse("2015-04-04T20:30:46+0000"));
+            first.Size.Should().Be(1718009717);
+            first.MagnetUrl.Should().BeNullOrEmpty();
+            first.Peers.Should().Be(47);
+            first.Seeders.Should().Be(46);
+        }
+
+        [Test]
+        public void TestBadPasskey()
+        {
+            var responseJson = @"
+{
+    ""status"": 5,
+    ""message"": ""Invalid authentication credentials""
+}";
+            Mocker.GetMock<IHttpClient>()
+                .Setup(v => v.Execute(It.IsAny<HttpRequest>()))
+                .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(),
+                    Encoding.UTF8.GetBytes(responseJson)));
+
+            var torrents = Subject.FetchRecent();
+
+            torrents.Should().BeEmpty();
+
+            ExceptionVerification.ExpectedWarns(1);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Indexers/HDBits/HdBitsSettings.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using FluentValidation;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+using NzbDrone.Core.Annotations;
+
+namespace NzbDrone.Core.Indexers.HDBits
+{
+    public class HdBitsSettingsValidator : AbstractValidator<HdBitsSettings>
+    {
+        public HdBitsSettingsValidator()
+        {
+            RuleFor(c => c.BaseUrl).ValidRootUrl();
+            RuleFor(c => c.ApiKey).NotEmpty();
+        }
+    }
+
+    public class HdBitsSettings : IProviderConfig
+    {
+        private static readonly HdBitsSettingsValidator Validator = new HdBitsSettingsValidator();
+
+        public HdBitsSettings()
+        {
+            BaseUrl = "https://hdbits.org";
+        }
+
+        [FieldDefinition(0, Label = "Username")]
+        public string Username { get; set; }
+
+        [FieldDefinition(1, Label = "API Key")]
+        public string ApiKey { get; set; }
+
+        [FieldDefinition(2, Label = "API URL", Advanced = true, HelpText = "Do not change this unless you know what you're doing. Since your API key will be sent to that host.")]
+        public string BaseUrl { get; set; }
+
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+
+    public enum HdBitsCategory
+    {
+        Movie = 1,
+        Tv = 2,
+        Documentary = 3,
+        Music = 4,
+        Sport = 5,
+        Audio = 6,
+        Xxx = 7,
+        MiscDemo = 8
+    }
+
+    public enum HdBitsCodec
+    {
+        H264 = 1,
+        Mpeg2 = 2,
+        Vc1 = 3,
+        Xvid = 4
+    }
+
+    public enum HdBitsMedium
+    {
+        Bluray = 1,
+        Encode = 3,
+        Capture = 4,
+        Remux = 5,
+        WebDl = 6
+    }
+}
