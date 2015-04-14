c76e3f8748e80dd889957519e3ed7a9bca5f7d64
scherzo <scherz0@users.noreply.github.com>
2015-04-13 21:30:02 -0700

    Add support for the HDBits torrent tracker.
    The indexer implementation borrows heavily from the BroadcastTheNet
    implementation as HDBits also provides a JSON API that can be used
    to query both the recent torrents and the catalog.

---
+++ src/NzbDrone.Core/Indexers/HDBits/HdBits.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Parser;
+using System;
+
+namespace NzbDrone.Core.Indexers.HDBits
+{
+    public class HdBits : HttpIndexerBase<HdBitsSettings>
+    {
+        public override DownloadProtocol Protocol { get { return DownloadProtocol.Torrent; } }
+        public override bool SupportsRss { get { return true; } }
+        public override bool SupportsSearch { get { return true; } }
+        public override int PageSize { get { return 30; } }
+
+        public HdBits(IHttpClient httpClient,
+                      IConfigService configService,
+                      IParsingService parsingService,
+                      Logger logger)
+            : base(httpClient, configService, parsingService, logger)
+        { }
+
+        public override IIndexerRequestGenerator GetRequestGenerator()
+        {
+            return new HdBitsRequestGenerator() { Settings = Settings };
+        }
+
+        public override IParseIndexerResponse GetParser()
+        {
+            return new HdBitsParser(Settings.ApiKey);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Indexers/HDBits/HdBitsParser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using NzbDrone.Core.Parser.Model;
+using System.Net;
+using NzbDrone.Core.Indexers.Exceptions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Web;
+using System.Collections.Specialized;
+
+namespace NzbDrone.Core.Indexers.HDBits
+{
+    public class HdBitsParser : IParseIndexerResponse
+    {
+        private readonly string _apiKey;
+
+        public HdBitsParser(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public IList<ReleaseInfo> ParseResponse(IndexerResponse indexerResponse)
+        {
+            var torrentInfos = new List<ReleaseInfo>();
+
+            if (indexerResponse.HttpResponse.StatusCode != HttpStatusCode.OK)
+            {
+                throw new IndexerException(
+                    indexerResponse,
+                    "Unexpected response status {0} code from API request",
+                    indexerResponse.HttpResponse.StatusCode);
+            }
+
+            var jsonResponse = JsonConvert.DeserializeObject<HdBitsResponse>(indexerResponse.Content);
+
+            if (jsonResponse.Status != StatusCode.Success)
+            {
+                throw new IndexerException(
+                    indexerResponse,
+                    @"HDBits API request returned status code {0} with message ""{1}""",
+                    jsonResponse.Status,
+                    jsonResponse.Message ?? "");
+            }
+
+            var responseData = jsonResponse.Data as JArray;
+            if (responseData == null)
+            {
+                throw new IndexerException(
+                    indexerResponse,
+                    "Indexer API call response missing result data");
+            }
+
+            var queryResults = responseData.ToObject<TorrentQueryResponse[]>();
+
+            foreach (var result in queryResults)
+            {
+                var id = result.Id;
+                torrentInfos.Add(new TorrentInfo()
+                {
+                    Guid = string.Format("HDB-{0}", id),
+                    Title = result.Name,
+                    Size = result.Size,
+                    DownloadUrl = GetDownloadUrl(id),
+                    InfoUrl = GetInfoUrl(id),
+                    Seeders = result.Seeders,
+                    Peers = result.Leechers + result.Seeders,
+                    PublishDate = result.Added
+                });
+            }
+
+            return torrentInfos.ToArray();
+        }
+
+        private string GetDownloadUrl(long torrentId)
+        {
+            var args = new NameValueCollection(2);
+            args["id"] = torrentId.ToString();
+            args["passkey"] = _apiKey;
+
+            return BuildUrl("https://hdbits.org/download.php", args);
+        }
+
+        private string GetInfoUrl(long torrentId)
+        {
+            var args = new NameValueCollection(1);
+            args["id"] = torrentId.ToString();
+
+            return BuildUrl("https://hdbits.org/details.php", args);
+
+        }
+        
+        private string BuildUrl(string urlBase, NameValueCollection args)
+        {
+            var builder = new UriBuilder(urlBase);
+            var queryString = HttpUtility.ParseQueryString("");
+
+            queryString.Add(args);
+
+            builder.Query = queryString.ToString();
+
+            return builder.Uri.ToString();
+        }
+    }
+}
