1fc99fd24e01ff8ad3423b7761870fbc4e5eac4d
Roy Handelsman <royal2000h@gmail.com>
2015-04-22 00:03:42 -0400

    Fixed: Pushbullet settings typo

--- src/NzbDrone.Core/Notifications/PushBullet/PushBulletSettings.cs
+++ src/NzbDrone.Core/Notifications/PushBullet/PushBulletSettings.cs
@@ -21,7 +21,7 @@ namespace NzbDrone.Core.Notifications.PushBullet
         [FieldDefinition(0, Label = "API Key", HelpLink = "https://www.pushbullet.com/")]
         public String ApiKey { get; set; }
 
-        [FieldDefinition(1, Label = "Device ID", HelpText = "device_iden in the device's URL on pubshbullet.com (leave blank to send to all devices)")]
+        [FieldDefinition(1, Label = "Device ID", HelpText = "device_iden in the device's URL on pushbullet.com (leave blank to send to all devices)")]
         public String DeviceId { get; set; }
 
         public bool IsValid
