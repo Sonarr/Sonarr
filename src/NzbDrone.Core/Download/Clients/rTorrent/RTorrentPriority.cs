4b9664d82a734121373aff76281907f62af39037
Your Name <you@example.com>
2015-05-08 20:50:22 +0200

    Download clients: New client rTorrent

---
+++ src/NzbDrone.Core/Download/Clients/rTorrent/RTorrentPriority.cs
@@ -0,0 +1,10 @@
+namespace NzbDrone.Core.Download.Clients.RTorrent
+{
+    public enum RTorrentPriority
+    {
+        DoNotDownload = 0,
+        Low = 1,
+        Normal = 2,
+        High = 3
+    }
+}
---
+++ src/NzbDrone.Core/Download/Clients/rTorrent/RTorrentTorrent.cs
@@ -0,0 +1,17 @@
+﻿namespace NzbDrone.Core.Download.Clients.RTorrent
+{
+    public class RTorrentTorrent
+    {
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public string Path { get; set; }
+        public string Category { get; set; }
+        public long TotalSize { get; set; }
+        public long RemainingSize { get; set; }
+        public long DownRate { get; set; }
+        public long Ratio { get; set; }
+        public bool IsFinished { get; set; }
+        public bool IsOpen { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
