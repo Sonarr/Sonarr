34bb1d1996299bc017f8ba4eaf84e397d8927edf
Your Name <you@example.com>
2015-05-09 05:11:06 +0200

    Profile editor: Integrate cutoff

--- src/UI/Settings/Profile/Edit/EditProfileItemView.js
+++ src/UI/Settings/Profile/Edit/EditProfileItemView.js
@@ -1,5 +1,25 @@
 var Marionette = require('marionette');
 
 module.exports = Marionette.ItemView.extend({
-    template : 'Settings/Profile/Edit/EditProfileItemViewTemplate'
+    template : 'Settings/Profile/Edit/EditProfileItemViewTemplate',
+    ui: {
+        cutoff: ".x-cutoff"
+    },
+
+    onRender: function() {
+        if (this.model.get("quality").id === this.collectionListView.model.get("cutoff").id) {
+            this.ui.cutoff.prop("checked", true);
+        }
+    },
+
+    events : {
+        'click .cutoff': '_onRadioSelect'
+    },
+
+    _onRadioSelect: function(e) {
+        this.collectionListView.model.set("cutoff", this.model.get("quality"));
+
+        // Do not let event bubble, as the parent will register the click as select / deselect
+        e.stopPropagation();
+    }
 });
