ee224cb4228ec954903a6de33483ea067f90b4dc
Mark McDowall <markus.mcd5@gmail.com>
2015-05-12 16:57:30 -0700

    Modal Regions inherit from a common base

--- src/UI/Shared/Modal/ModalRegion.js
+++ src/UI/Shared/Modal/ModalRegion.js
@@ -1,50 +1,7 @@
-var $ = require('jquery');
-var Backbone = require('backbone');
-var Marionette = require('marionette');
-require('bootstrap');
-var region = Marionette.Region.extend({
-    el : '#modal-region',
+var ModalRegionBase = require('./ModalRegionBase');
 
-    constructor : function() {
-        Backbone.Marionette.Region.prototype.constructor.apply(this, arguments);
-        this.on('show', this.showModal, this);
-    },
-
-    getEl : function(selector) {
-        var $el = $(selector);
-        $el.on('hidden', this.close);
-        return $el;
-    },
-
-    showModal : function() {
-        this.$el.addClass('modal fade');
-
-        //need tab index so close on escape works
-        //https://github.com/twitter/bootstrap/issues/4663
-        this.$el.attr('tabindex', '-1');
-        this.$el.modal({
-            show     : true,
-            keyboard : true,
-            backdrop : true
-        });
-
-        this.$el.on('hide.bs.modal', $.proxy(this._closing, this));
-
-        this.currentView.$el.addClass('modal-dialog');
-    },
-
-    closeModal : function() {
-        $(this.el).modal('hide');
-        this.reset();
-    },
-
-    _closing : function() {
-        if (this.$el) {
-            this.$el.off('hide.bs.modal');
-        }
-
-        this.reset();
-    }
+var region = ModalRegionBase.extend({
+    el : '#modal-region'
 });
 
 module.exports = region;
---
+++ src/UI/Shared/Modal/ModalRegionBase.js
@@ -0,0 +1,53 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
+var Marionette = require('marionette');
+require('bootstrap');
+var region = Marionette.Region.extend({
+    el : '#modal-region',
+
+    constructor : function() {
+        Backbone.Marionette.Region.prototype.constructor.apply(this, arguments);
+        this.on('show', this.showModal, this);
+    },
+
+    getEl : function(selector) {
+        var $el = $(selector);
+        $el.on('hidden', this.close);
+        return $el;
+    },
+
+    showModal : function() {
+        this.trigger('modal:beforeShow');
+        this.$el.addClass('modal fade');
+
+        //need tab index so close on escape works
+        //https://github.com/twitter/bootstrap/issues/4663
+        this.$el.attr('tabindex', '-1');
+        this.$el.modal({
+            show     : true,
+            keyboard : true,
+            backdrop : true
+        });
+
+        this.$el.on('hide.bs.modal', $.proxy(this._closing, this));
+
+        this.currentView.$el.addClass('modal-dialog');
+
+        this.trigger('modal:afterShow');
+    },
+
+    closeModal : function() {
+        $(this.el).modal('hide');
+        this.reset();
+    },
+
+    _closing : function() {
+        if (this.$el) {
+            this.$el.off('hide.bs.modal');
+        }
+
+        this.reset();
+    }
+});
+
+module.exports = region;
