9d980a8ac7b4d170499957098aae4782761558e6
Mark McDowall <markus.mcd5@gmail.com>
2015-05-13 08:05:23 -0700

    Manual Import sends progress messages

--- src/NzbDrone.Core/MediaFiles/EpisodeImport/Manual/ManualImportCommand.cs
+++ src/NzbDrone.Core/MediaFiles/EpisodeImport/Manual/ManualImportCommand.cs
@@ -6,5 +6,13 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Manual
     public class ManualImportCommand : Command
     {
         public List<ManualImportFile> Files { get; set; }
+
+        public override bool SendUpdatesToClient
+        {
+            get
+            {
+                return true;
+            }
+        }
     }
 }
