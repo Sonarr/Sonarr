bd222dbd95b19950581ff8b7a38573899b26f1b1
Mark McDowall <markus.mcd5@gmail.com>
2015-06-24 20:58:56 -0700

    Fixed: Updating empty Kodi library

--- src/NzbDrone.Core/Notifications/Xbmc/Model/TvShowResult.cs
+++ src/NzbDrone.Core/Notifications/Xbmc/Model/TvShowResult.cs
@@ -6,5 +6,10 @@ namespace NzbDrone.Core.Notifications.Xbmc.Model
     {
         public Dictionary<string, int> Limits { get; set; }
         public List<TvShow> TvShows;
+
+        public TvShowResult()
+        {
+            TvShows = new List<TvShow>();
+        }
     }
 }
