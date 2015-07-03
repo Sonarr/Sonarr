9c2aee6971b5c3ab109d87c191bbae6f6b24b324
Taloth Saldono <Taloth@users.noreply.github.com>
2015-07-03 23:26:18 +0200

    Added Housekeeping to clear out IndexerStatus from removed Indexers.

---
+++ src/NzbDrone.Core.Test/Housekeeping/Housekeepers/CleanupOrphanedIndexerStatusFixture.cs
@@ -0,0 +1,56 @@
+﻿using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Housekeeping.Housekeepers;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Qualities;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
+{
+    [TestFixture]
+    public class CleanupOrphanedIndexerStatusFixture : DbTest<CleanupOrphanedIndexerStatus, IndexerStatus>
+    {
+        private IndexerDefinition _indexer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _indexer = Builder<IndexerDefinition>.CreateNew()
+                                                 .BuildNew();
+        }
+
+        private void GivenIndexer()
+        {
+            Db.Insert(_indexer);
+        }
+
+        [Test]
+        public void should_delete_orphaned_indexerstatus()
+        {
+            var status = Builder<IndexerStatus>.CreateNew()
+                                               .With(h => h.IndexerId = _indexer.Id)
+                                               .BuildNew();
+            Db.Insert(status);
+
+            Subject.Clean();
+            AllStoredModels.Should().BeEmpty();
+        }
+
+        [Test]
+        public void should_not_delete_unorphaned_indexerstatus()
+        {
+            GivenIndexer();
+
+            var status = Builder<IndexerStatus>.CreateNew()
+                                               .With(h => h.IndexerId = _indexer.Id)
+                                               .BuildNew();
+            Db.Insert(status);
+
+            Subject.Clean();
+            AllStoredModels.Should().HaveCount(1);
+            AllStoredModels.Should().Contain(h => h.IndexerId == _indexer.Id);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedIndexerStatus.cs
@@ -0,0 +1,26 @@
+﻿using NzbDrone.Core.Datastore;
+
+namespace NzbDrone.Core.Housekeeping.Housekeepers
+{
+    public class CleanupOrphanedIndexerStatus : IHousekeepingTask
+    {
+        private readonly IMainDatabase _database;
+
+        public CleanupOrphanedIndexerStatus(IMainDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Clean()
+        {
+            var mapper = _database.GetDataMapper();
+
+            mapper.ExecuteNonQuery(@"DELETE FROM IndexerStatus
+                                     WHERE Id IN (
+                                     SELECT IndexerStatus.Id FROM IndexerStatus
+                                     LEFT OUTER JOIN Indexers
+                                     ON IndexerStatus.IndexerId = Indexers.Id
+                                     WHERE Indexers.Id IS NULL)");
+        }
+    }
+}
