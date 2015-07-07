c5684439f4bd964e0db58578c6770debca5b307f
Taloth Saldono <Taloth@users.noreply.github.com>
2015-07-07 22:13:01 +0200

    Added robots.txt
    Fixes #667

---
+++ src/NzbDrone.Api/Frontend/Mappers/RobotsTxtMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using NLog;
+using NzbDrone.Common.Disk;
+using NzbDrone.Common.EnvironmentInfo;
+
+namespace NzbDrone.Api.Frontend.Mappers
+{
+    public class RobotsTxtMapper : StaticResourceMapperBase
+    {
+        private readonly IAppFolderInfo _appFolderInfo;
+
+        public RobotsTxtMapper(IAppFolderInfo appFolderInfo, IDiskProvider diskProvider, Logger logger)
+            : base(diskProvider, logger)
+        {
+            _appFolderInfo = appFolderInfo;
+        }
+
+        public override string Map(string resourceUrl)
+        {
+            var path = Path.Combine("Content", "robots.txt");
+
+            return Path.Combine(_appFolderInfo.StartUpFolder, "UI", path);
+        }
+
+        public override bool CanHandle(string resourceUrl)
+        {
+            return resourceUrl.Equals("/robots.txt");
+        }
+    }
+}
