9b1915a187f9716c9dfc66e73b1260c9c8c56b28
Rodolphe Stoclin <srodolphe@gmail.com>
2015-07-20 11:18:09 +0200

    New: Boxcar 2 notifications

---
+++ src/NzbDrone.Core/Notifications/Boxcar/BoxcarException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NzbDrone.Common.Exceptions;
+
+namespace NzbDrone.Core.Notifications.Boxcar
+{
+    public class BoxcarException : NzbDroneException
+    {
+        public BoxcarException(string message) : base(message)
+        {
+        }
+
+        public BoxcarException(string message, Exception innerException, params object[] args) : base(message, innerException, args)
+        {
+        }
+    }
+}
