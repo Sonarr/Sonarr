5c1d683e7175e4009a161a371eb3c919fdd23103
Taloth Saldono <Taloth@users.noreply.github.com>
2015-07-22 19:40:36 +0200

    Tooltips are now properly hidden if the element is removed from the dom.

--- src/UI/Shared/Tooltip.js
+++ src/UI/Shared/Tooltip.js
@@ -19,6 +19,24 @@ Tooltip.prototype.getOptions = function(options) {
     return result;
 };
 
+var onElementRemoved = function(event) {
+    event.data.hide();
+};
+
+var origShow = Tooltip.prototype.show;
+Tooltip.prototype.show = function() {
+    origShow.call(this);
+
+    this.$element.on('remove', this, onElementRemoved);
+};
+
+var origHide = Tooltip.prototype.hide;
+Tooltip.prototype.hide = function() {
+    origHide.call(this);
+
+    this.$element.off('remove', onElementRemoved);
+};
+
 module.exports = {
     appInitializer : function() {
 
