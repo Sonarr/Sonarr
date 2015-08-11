5ea954695eb06d320e00a7d769493495fa93b8a6
Taloth Saldono <Taloth@users.noreply.github.com>
2015-08-11 20:52:39 +0200

    Ignore duplicates when using history to identify an existing downloaditem.

--- src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
+++ src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
@@ -78,7 +78,7 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                         parsedEpisodeInfo = Parser.Parser.ParseTitle(firstHistoryItem.SourceTitle);
                         if (parsedEpisodeInfo != null)
                         {
-                            trackedDownload.RemoteEpisode = _parsingService.Map(parsedEpisodeInfo, firstHistoryItem.SeriesId, historyItems.Select(h => h.EpisodeId));
+                            trackedDownload.RemoteEpisode = _parsingService.Map(parsedEpisodeInfo, firstHistoryItem.SeriesId, historyItems.Where(v => v.EventType == HistoryEventType.Grabbed).Select(h => h.EpisodeId).Distinct());
                         }
                     }
                 }
