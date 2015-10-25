d37b24cd0b423d6d5fe826296a806685d43be31a
Mark McDowall <markus.mcd5@gmail.com>
2015-10-25 10:10:57 -0700

    Better UI messaging when searching for all specials in a series
    Fixed: Specials season search UI messaging
    Closes #881

--- src/NzbDrone.Core/IndexerSearch/Definitions/SpecialEpisodeSearchCriteria.cs
+++ src/NzbDrone.Core/IndexerSearch/Definitions/SpecialEpisodeSearchCriteria.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace NzbDrone.Core.IndexerSearch.Definitions
 {
@@ -8,6 +8,13 @@ namespace NzbDrone.Core.IndexerSearch.Definitions
 
         public override string ToString()
         {
+            var episodeTitles = EpisodeQueryTitles.ToList();
+
+            if (episodeTitles.Count > 0)
+            {
+                return string.Format("[{0}] Specials", Series.Title);
+            }
+
             return string.Format("[{0} : {1}]", Series.Title, string.Join(",", EpisodeQueryTitles));
         }
     }
