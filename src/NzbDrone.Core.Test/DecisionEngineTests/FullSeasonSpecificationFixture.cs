7dc386cc24060c355e25731626db8fc7b778c0db
vawen <vawenus@gmail.com>
2015-10-30 14:18:43 +0100

    Fix: Prevent grabbing season packs if full season hasnt aired
    Issue #743

---
+++ src/NzbDrone.Core.Test/DecisionEngineTests/FullSeasonSpecificationFixture.cs
@@ -0,0 +1,79 @@
+﻿
+using NUnit.Framework;
+using NzbDrone.Core.DecisionEngine.Specifications;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+using FizzWare.NBuilder;
+using System.Linq;
+using FluentAssertions;
+using NzbDrone.Core.Tv;
+using Moq;
+using System.Collections.Generic;
+
+namespace NzbDrone.Core.Test.DecisionEngineTests
+{
+    [TestFixture]
+    public class FullSeasonSpecificationFixture : CoreTest<FullSeasonSpecification>
+    {
+        private RemoteEpisode _remoteEpisode;
+
+        [SetUp]
+        public void Setup()
+        {
+            var show = Builder<Series>.CreateNew().With(s => s.Id = 1234).Build();
+            _remoteEpisode = new RemoteEpisode
+            {
+                ParsedEpisodeInfo = new ParsedEpisodeInfo
+                {
+                    FullSeason = true
+                },
+                Episodes = Builder<Episode>.CreateListOfSize(3)
+                                           .All()
+                                           .With(e => e.AirDateUtc = System.DateTime.UtcNow.AddDays(-8))
+                                           .With(s => s.SeriesId = show.Id)
+                                           .BuildList(),
+                Series = show
+            };
+
+            Mocker.GetMock<IEpisodeService>().Setup(s => s.EpisodesBetweenDates(It.IsAny<System.DateTime>(), It.IsAny<System.DateTime>(), false))
+                                             .Returns(new List<Episode>());
+        }
+
+        [Test]
+        public void should_return_true_if_all_episodes_were_emited_over_7_days_ago()
+        {
+            Mocker.Resolve<FullSeasonSpecification>().IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_return_false_if_one_episode_has_not_been_emited()
+        {
+            _remoteEpisode.Episodes.Last().AirDateUtc = System.DateTime.UtcNow.AddDays(+2);
+            Mocker.Resolve<FullSeasonSpecification>().IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_return_false_if_last_episode_was_emited_last_week()
+        {
+            _remoteEpisode.Episodes.Last().AirDateUtc = System.DateTime.UtcNow.AddDays(-5);
+            Mocker.Resolve<FullSeasonSpecification>().IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_return_true_if_is_not_a_full_season()
+        {
+            _remoteEpisode.ParsedEpisodeInfo.FullSeason = false;
+            _remoteEpisode.Episodes.Last().AirDateUtc = System.DateTime.UtcNow.AddDays(+2);
+            Mocker.Resolve<FullSeasonSpecification>().IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_return_false_if_an_episode_was_emited_today()
+        {
+            var episode = Builder<Episode>.CreateNew().With(s => s.SeriesId = _remoteEpisode.Series.Id).Build();
+            Mocker.GetMock<IEpisodeService>().Setup(s => s.EpisodesBetweenDates(It.IsAny<System.DateTime>(), It.IsAny<System.DateTime>(), false))
+                                             .Returns(new List<Episode> { episode });
+            Mocker.Resolve<FullSeasonSpecification>().IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeFalse();
+        }
+    }
+}
---
+++ src/NzbDrone.Core/DecisionEngine/Specifications/FullSeasonSpecification.cs
@@ -0,0 +1,44 @@
+﻿using NLog;
+using NzbDrone.Core.IndexerSearch.Definitions;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Common.Extensions;
+using System.Linq;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.DecisionEngine.Specifications
+{
+    public class FullSeasonSpecification : IDecisionEngineSpecification
+    {
+        private readonly Logger _logger;
+        private readonly IEpisodeService _episodeService;
+
+        public FullSeasonSpecification(Logger logger, IEpisodeService episodeService)
+        {
+            _logger = logger;
+            _episodeService = episodeService;
+        }
+
+        public RejectionType Type { get { return RejectionType.Permanent; } }
+
+        public virtual Decision IsSatisfiedBy(RemoteEpisode subject, SearchCriteriaBase searchCriteria)
+        {
+            if (subject.ParsedEpisodeInfo.FullSeason)
+            {
+                _logger.Debug("Checking if report is a full season. {0}", subject.ParsedEpisodeInfo.SeasonNumber);
+                if (subject.Episodes.Any(e => e.AirDateUtc.HasValue && e.AirDateUtc.Value.After(System.DateTime.UtcNow.AddDays(-7))))
+                {
+                    _logger.Debug("Report Full Season {0} rejected. At least one episode younger than one week.", subject.ParsedEpisodeInfo.SeasonNumber);
+                    return Decision.Reject("Full season {0} have pending episodes to be aired.", subject.ParsedEpisodeInfo.SeasonNumber);
+                }
+
+                if (_episodeService.EpisodesBetweenDates(System.DateTime.UtcNow.AddDays(-1), System.DateTime.UtcNow, false).Any(e => e.SeriesId == subject.Series.Id))
+                {
+                    _logger.Debug("Report Full Season {0} rejected. An episode for that show has been aired in the last 24 hours.", subject.ParsedEpisodeInfo.SeasonNumber);
+                    return Decision.Reject("Not a full season ({0}). Episode aired today.", subject.ParsedEpisodeInfo.SeasonNumber);
+                }
+            }
+
+            return Decision.Accept();
+        }
+    }
+}
