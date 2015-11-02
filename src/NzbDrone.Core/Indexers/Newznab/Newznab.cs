6f7fea3591d9086c4f018ac78e51348ab862c0a6
Mark McDowall <markus.mcd5@gmail.com>
2015-11-01 21:35:44 -0800

    PFMonkey Newznab preset

--- src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
+++ src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
@@ -51,6 +51,7 @@ namespace NzbDrone.Core.Indexers.Newznab
                 yield return GetDefinition("OZnzb.com", GetSettings("https://api.oznzb.com"));
                 yield return GetDefinition("nzbplanet.net", GetSettings("https://nzbplanet.net"));
                 yield return GetDefinition("NZBgeek", GetSettings("https://api.nzbgeek.info"));
+                yield return GetDefinition("PFmonkey", GetSettings("https://www.pfmonkey.com"));
             }
         }
 
