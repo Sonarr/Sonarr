5c22afe415e14a57bb0296d2238ec9b536ab541e
Björn Dahlgren <bjorn@dahlgren.at>
2015-11-10 01:02:33 +0100

    Travis CI

--- integration_mono.sh
+++ integration_mono.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+set -e
+
 EXCLUDE="-exclude:Windows -include:IntegrationTest"
-TESTDIR="."
+TESTDIR="./_tests"
 NUNIT="$TESTDIR/NUnit.Runners.2.6.1/tools/nunit-console-x86.exe"
 
 mono --debug --runtime=v4.0 $NUNIT $EXCLUDE -xml:NzbDrone.Api.Result.xml $TESTDIR/NzbDrone.Api.Test.dll
