d943551a7f1ea0ebff34e3d1fb10f97c0c6880b8
Keivan Beigi <me@keivan.io>
2016-01-16 16:39:45 -0800

    fixed build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ outputFolderOsxApp='./_output_osx_app'
 testPackageFolder='./_tests/'
 testSearchPattern='*.Test/bin/x86/Release'
 sourceFolder='./src'
-slnFile = '$sourceFolder/NzbDrone.sln'
+slnFile=$sourceFolder/NzbDrone.sln
 updateFolder=$outputFolder/NzbDrone.Update
 updateFolderMono=$outputFolderMono/NzbDrone.Update
 
