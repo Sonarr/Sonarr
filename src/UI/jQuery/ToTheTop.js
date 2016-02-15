34d5fb1aa0c44e9b683e92cc52682fbb209ede1b
Taloth Saldono <Taloth@users.noreply.github.com>
2016-02-15 22:30:47 +0100

    Fiddled with the Back to the Top button a bit so it's better visible on the white background, also only on widescreen now.

--- src/UI/jQuery/ToTheTop.js
+++ src/UI/jQuery/ToTheTop.js
@@ -3,13 +3,14 @@ var _ = require('underscore');
 
 $(document).ready(function() {
     var _window = $(window);
-    var _scrollButton = $('#scroll-up');
+    var _scrollContainer = $('#scroll-up');
+    var _scrollButton = $('#scroll-up i');
 
     var _scrollHandler = function() {
-        if (_window.scrollTop() > 100) {
-            _scrollButton.fadeIn();
+        if (_window.scrollTop() > 400) {
+            _scrollContainer.fadeIn();
         } else {
-            _scrollButton.fadeOut();
+            _scrollContainer.fadeOut();
         }
     };
 
