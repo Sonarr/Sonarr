ebbccd1bfeee0d7389d8f9f467bc24622c79df50
Martin Hartvig <martinhartvig@gmail.com>
2016-04-12 18:26:54 +0200

    Removed unnecessary camel case

--- src/NzbDrone.Core/Notifications/Slack/Payloads/Attachment.cs
+++ src/NzbDrone.Core/Notifications/Slack/Payloads/Attachment.cs
@@ -4,19 +4,12 @@ namespace NzbDrone.Core.Notifications.Slack.Payloads
 {
     public class Attachment
     {
-        [JsonProperty("fallback")]
         public string Fallback { get; set; }
 
-        [JsonProperty("title")]
         public string Title { get; set; }
 
-        [JsonProperty("title_link")]
-        public string TitleLink { get; set; }
-
-        [JsonProperty("text")]
         public string Text { get; set; }
 
-        [JsonProperty("color")]
         public string Color { get; set; }
     }
 }
--- src/NzbDrone.Core/Notifications/Slack/Payloads/SlackPayload.cs
+++ src/NzbDrone.Core/Notifications/Slack/Payloads/SlackPayload.cs
@@ -5,16 +5,13 @@ namespace NzbDrone.Core.Notifications.Slack.Payloads
 {
     public class SlackPayload
     {
-        [JsonProperty("text")]
         public string Text { get; set; }
 
-        [JsonProperty("username")]
         public string Username { get; set; }
 
         [JsonProperty("icon_emoji")]
         public string IconEmoji { get; set; }
 
-        [JsonProperty("attachments")]
         public List<Attachment> Attachments { get; set; }
     }
 }
