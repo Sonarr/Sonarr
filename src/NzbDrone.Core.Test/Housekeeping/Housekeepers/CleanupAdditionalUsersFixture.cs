2f6fded7c33ec6112ff0d214450bb2b6d541dba4
Mark McDowall <markus.mcd5@gmail.com>
2016-04-27 16:18:11 -0700

    Fixed: An issue preventing access to settings due to extraneous data in the database

---
+++ src/NzbDrone.Core.Test/Housekeeping/Housekeepers/CleanupAdditionalUsersFixture.cs
@@ -0,0 +1,37 @@
+﻿using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Authentication;
+using NzbDrone.Core.Housekeeping.Housekeepers;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
+{
+    [TestFixture]
+    public class CleanupAdditionalUsersFixture : DbTest<CleanupAdditionalUsers, User>
+    {
+        [Test]
+        public void should_delete_additional_users()
+        {
+            var specs = Builder<User>.CreateListOfSize(5)
+                                             .BuildListOfNew();
+
+            Db.InsertMany(specs);
+
+            Subject.Clean();
+            AllStoredModels.Should().HaveCount(1);
+        }
+
+        [Test]
+        public void should_not_delete_if_only_one_user()
+        {
+            var spec = Builder<User>.CreateNew()
+                                            .BuildNew();
+
+            Db.Insert(spec);
+
+            Subject.Clean();
+            AllStoredModels.Should().HaveCount(1);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupAdditionalUsers.cs
@@ -0,0 +1,24 @@
+﻿using NzbDrone.Core.Datastore;
+
+namespace NzbDrone.Core.Housekeeping.Housekeepers
+{
+    public class CleanupAdditionalUsers : IHousekeepingTask
+    {
+        private readonly IMainDatabase _database;
+
+        public CleanupAdditionalUsers(IMainDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Clean()
+        {
+            var mapper = _database.GetDataMapper();
+
+            mapper.ExecuteNonQuery(@"DELETE FROM Users
+                                     WHERE ID NOT IN (
+                                     SELECT ID FROM Users
+                                     LIMIT 1)");
+        }
+    }
+}
