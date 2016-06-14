d984dd41d6c5e77472d9f33d54979d51be060713
Mark McDowall <markus.mcd5@gmail.com>
2016-06-13 19:57:10 -0700

    Fixed: Parsing WEB releases that use spaces instead of periods
    Closes #1339

--- src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
@@ -162,6 +162,8 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("The.Nightly.Show.2016.03.14.1080p.WEB.x264-spamTV", false)]
         [TestCase("The.Nightly.Show.2016.03.14.1080p.WEB.h264-spamTV", false)]
         [TestCase("Psych.S01.1080p.WEB-DL.AAC2.0.AVC-TrollHD", false)]
+        [TestCase("Series Title S06E08 1080p WEB h264-EXCLUSIVE", false)]
+        [TestCase("Series Title S06E08 No One PROPER 1080p WEB DD5 1 H 264-EXCLUSIVE", true)]
         public void should_parse_webdl1080p_quality(string title, bool proper)
         {
             ParseAndVerifyQuality(title, Quality.WEBDL1080p, proper);
