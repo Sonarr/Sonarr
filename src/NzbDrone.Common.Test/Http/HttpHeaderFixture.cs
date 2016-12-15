7726ba959570929b6f0aafe9fd7250ce1adcfee2
margaale <margaale@gmail.com>
2016-12-15 19:41:35 -0300

    workaround for faulty server sending wrong charset

---
+++ src/NzbDrone.Common.Test/Http/HttpHeaderFixture.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NzbDrone.Test.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NzbDrone.Common.Http;
+using System.Collections.Specialized;
+
+namespace NzbDrone.Common.Test.Http
+{
+    [TestFixture]
+    public class HttpHeaderFixture : TestBase
+    {
+        [TestCase("text/html; charset=\"utf-8\"")]
+        [TestCase("text/html; charset=utf-8")]
+
+        public void should_get_encoding(string contentType)
+        {
+            var headers = new NameValueCollection();
+
+            headers.Add("content-type", contentType);
+
+            var httpheader = new HttpHeader(headers);
+
+            Assert.DoesNotThrow(() => httpheader.GetEncodingFromContentType());
+        }
+    }
+}
