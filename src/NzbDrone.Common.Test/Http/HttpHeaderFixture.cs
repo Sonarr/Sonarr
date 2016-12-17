fba47a639097516a42ef46faf661d3b8cf88a679
margaale <margaale@gmail.com>
2016-12-17 13:52:31 -0300

    Test for inexisting charset

--- src/NzbDrone.Common.Test/Http/HttpHeaderFixture.cs
+++ src/NzbDrone.Common.Test/Http/HttpHeaderFixture.cs
@@ -12,9 +12,9 @@ namespace NzbDrone.Common.Test.Http
     [TestFixture]
     public class HttpHeaderFixture : TestBase
     {
-        [TestCase("text/html; charset=\"utf-8\"")]
-        [TestCase("text/html; charset=utf-8")]
-        public void should_get_encoding(string contentType)
+        [TestCase("text/html; charset=\"utf-8\"", "utf-8")]
+        [TestCase("text/html; charset=utf-8", "utf-8")]
+        public void should_get_encoding_from_content_type_header(string contentType, string charsetExpected)
         {
             var headers = new NameValueCollection();
 
@@ -22,7 +22,19 @@ namespace NzbDrone.Common.Test.Http
 
             var httpheader = new HttpHeader(headers);
 
-            Assert.DoesNotThrow(() => httpheader.GetEncodingFromContentType());
+            Assert.AreEqual(httpheader.GetEncodingFromContentType(), Encoding.GetEncoding(charsetExpected));
+        }
+
+        [TestCase("text/html; charset=asdasd")]
+        public void should_throw_when_invalid_encoding_is_in_content_type_header(string contentType)
+        {
+            var headers = new NameValueCollection();
+
+            headers.Add("Content-Type", contentType);
+
+            var httpheader = new HttpHeader(headers);
+
+            Assert.Throws<ArgumentException>(() => httpheader.GetEncodingFromContentType());
         }
     }
 }
