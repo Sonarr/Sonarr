3fc5ebafb941c16824fbca6732b23dc4499bda05
Keivan Beigi <me@keivan.io>
2017-01-02 13:14:38 -0800

    New: Switched nyaa.se to HTTPS

--- src/NzbDrone.Core/Indexers/Nyaa/NyaaSettings.cs
+++ src/NzbDrone.Core/Indexers/Nyaa/NyaaSettings.cs
@@ -25,10 +25,10 @@ namespace NzbDrone.Core.Indexers.Nyaa
         }
 
         [FieldDefinition(0, Label = "Website URL")]
-        public string BaseUrl { get; set; }
+        public string BaseUrl { get; }
 
         [FieldDefinition(1, Label = "Additional Parameters", Advanced = true, HelpText = "Please note if you change the category you will have to add required/restricted rules about the subgroups to avoid foreign language releases.")]
-        public string AdditionalParameters { get; set; }
+        public string AdditionalParameters { get; }
 
         public NzbDroneValidationResult Validate()
         {
