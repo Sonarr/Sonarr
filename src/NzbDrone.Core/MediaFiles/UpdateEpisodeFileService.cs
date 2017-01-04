4483f680275c6f1960071d36b7829c2d481921a1
Keivan Beigi <me@keivan.io>
2017-01-04 00:19:32 -0800

    RIP

--- src/NzbDrone.Core/MediaFiles/UpdateEpisodeFileService.cs
+++ src/NzbDrone.Core/MediaFiles/UpdateEpisodeFileService.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using NLog;
 using NzbDrone.Common.Disk;
-using NzbDrone.Common.Exceptron;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Instrumentation.Extensions;
 using NzbDrone.Core.Configuration;
