139bb0ea25300c2ab89dde6dae7910194d2ea10a
Keivan Beigi <me@keivan.io>
2017-01-05 14:01:58 -0800

    Added a one hour debounce of reporting the same errors to sentry

---
+++ src/NzbDrone.Common/Instrumentation/Sentry/SentryDebounce.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NzbDrone.Common.Instrumentation.Sentry
+{
+    public class SentryDebounce
+    {
+        private readonly Dictionary<string, DateTime> _dictionary;
+        private static readonly TimeSpan TTL = TimeSpan.FromHours(1);
+
+        public SentryDebounce()
+        {
+            _dictionary = new Dictionary<string, DateTime>();
+        }
+
+        public bool Allowed(IEnumerable<string> fingerPrint)
+        {
+            var key = string.Join("|", fingerPrint);
+
+            DateTime expiry;
+            _dictionary.TryGetValue(key, out expiry);
+
+            if (expiry >= DateTime.Now)
+            {
+                return false;
+            }
+
+            _dictionary[key] = DateTime.Now + TTL;
+            return true;
+        }
+    }
+}
