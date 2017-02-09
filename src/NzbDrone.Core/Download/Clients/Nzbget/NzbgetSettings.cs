52a22877f9c4b315052c64381456005f3f061bae
vertigo235 <jbjonesclt@gmail.com>
2017-02-08 23:36:39 -0500

    New: Add paused option for NZBGet
    Closes #346

--- src/NzbDrone.Core/Download/Clients/Nzbget/NzbgetSettings.cs
+++ src/NzbDrone.Core/Download/Clients/Nzbget/NzbgetSettings.cs
@@ -55,6 +55,9 @@ namespace NzbDrone.Core.Download.Clients.Nzbget
         [FieldDefinition(7, Label = "Use SSL", Type = FieldType.Checkbox)]
         public bool UseSsl { get; set; }
 
+        [FieldDefinition(8, Label = "Add Paused", Type = FieldType.Checkbox, HelpText = "This option requires at least NzbGet version 16.0")]
+        public bool AddPaused { get; set; }
+
         public NzbDroneValidationResult Validate()
         {
             return new NzbDroneValidationResult(Validator.Validate(this));
