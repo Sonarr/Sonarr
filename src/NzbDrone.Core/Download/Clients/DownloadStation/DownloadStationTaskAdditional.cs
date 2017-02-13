c56c83e169a2fa64579d9986b107a9fd89c429ee
margaale <margaale@gmail.com>
2017-02-13 17:18:35 -0300

    New: Added support for nzb downloads in Synology Download Station.

--- src/NzbDrone.Core/Download/Clients/DownloadStation/DownloadStationTaskAdditional.cs
+++ src/NzbDrone.Core/Download/Clients/DownloadStation/DownloadStationTaskAdditional.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace NzbDrone.Core.Download.Clients.DownloadStation
 {
-    public class DownloadStationTorrentAdditional
+    public class DownloadStationTaskAdditional
     {
         public Dictionary<string, string> Detail { get; set; }
 
         public Dictionary<string, string> Transfer { get; set; }
 
         [JsonProperty("File")]
-        public List<DownloadStationTorrentFile> Files { get; set; }
+        public List<DownloadStationTaskFile> Files { get; set; }
     }
 }
