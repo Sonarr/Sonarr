0ce7ff3e606f4570c3e2a47488b1bcd5e4711cb8
Mark McDowall <markus.mcd5@gmail.com>
2017-02-16 09:19:28 -0800

    Fixed: Saving nyaa settings
    Fixes #1687

--- src/NzbDrone.Core/Indexers/Nyaa/NyaaSettings.cs
+++ src/NzbDrone.Core/Indexers/Nyaa/NyaaSettings.cs
@@ -1,4 +1,4 @@
-using FluentValidation;
+﻿using FluentValidation;
 using NzbDrone.Core.Annotations;
 using NzbDrone.Core.ThingiProvider;
 using NzbDrone.Core.Validation;
@@ -25,10 +25,10 @@ namespace NzbDrone.Core.Indexers.Nyaa
         }
 
         [FieldDefinition(0, Label = "Website URL")]
-        public string BaseUrl { get; }
+        public string BaseUrl { get; set; }
 
         [FieldDefinition(1, Label = "Additional Parameters", Advanced = true, HelpText = "Please note if you change the category you will have to add required/restricted rules about the subgroups to avoid foreign language releases.")]
-        public string AdditionalParameters { get; }
+        public string AdditionalParameters { get; set; }
 
         public NzbDroneValidationResult Validate()
         {
