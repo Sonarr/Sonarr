43ff97e85ab972ca1418ff790189320aafaae6d9
margaale <margaale@gmail.com>
2017-02-19 14:47:09 -0300

    removed unused usings

--- src/NzbDrone.Core/Download/Clients/DownloadStation/UsenetDownloadStation.cs
+++ src/NzbDrone.Core/Download/Clients/DownloadStation/UsenetDownloadStation.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Disk;
