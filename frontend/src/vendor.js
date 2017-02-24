50d4d7a19da52519a84043d271bc704e64d0bdcc
Keivan Beigi <me@keivan.io>
2017-02-23 16:56:47 -0800

    removed legacy UI components

--- frontend/src/vendor.js
+++ frontend/src/vendor.js
@@ -3,26 +3,3 @@
 require('lodash');
 require('moment');
 // require('signalR');
-// require('jquery-ui');
-// require('jquery.easypiechart');
-// require('jquery.dotdotdot');
-// require('typeahead');
-// require('zero.clipboard');
-
-/* Bootstrap */
-// require('bootstrap');
-// require('bootstrap.tagsinput');
-
-/* Backbone */
-// require('backbone');
-// require('backbone.deepmodel');
-// require('backbone.paginator');
-
-// require('backbone.modelbinder');
-// require('backbone.collectionview');
-// require('backgrid');
-// require('backgrid.paginator');
-// require('backgrid.selectall');
-
-// require('marionette'); // this brings in a bunch of our code into this chunk because of template helpers.
-// require('vent');
