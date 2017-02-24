692525a48dc44ec1b2b28025bb294282a98e47d6
Keivan Beigi <me@keivan.io>
2017-02-23 16:56:47 -0800

    removed legacy UI components

--- frontend/gulp/stripBom.js
+++ frontend/gulp/stripBom.js
@@ -6,14 +6,6 @@ function stripBom(dest) {
   gulp.src([paths.src.scripts, paths.src.exclude.libs])
     .pipe(stripbom({ showLog: false }))
     .pipe(gulp.dest(dest));
-
-  gulp.src(paths.src.less)
-    .pipe(stripbom({ showLog: false }))
-    .pipe(gulp.dest(dest));
-
-  gulp.src(paths.src.templates)
-    .pipe(stripbom({ showLog: false }))
-    .pipe(gulp.dest(dest));
 }
 
 gulp.task('stripBom', () => {
