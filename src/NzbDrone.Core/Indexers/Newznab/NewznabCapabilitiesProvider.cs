e7331539f0b30a14b80f0350f74e2a1981848211
Taloth Saldono <Taloth@users.noreply.github.com>
2017-03-23 17:12:10 +0100

    Fixed: Newznab default capabilities erroneously cached if indexer is unavailable.

--- src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilitiesProvider.cs
+++ src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilitiesProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Xml;
 using System.Xml.Linq;
 using NLog;
 using NzbDrone.Common.Cache;
@@ -48,15 +50,30 @@ namespace NzbDrone.Core.Indexers.Newznab
 
             var request = new HttpRequest(url, HttpAccept.Rss);
 
+            HttpResponse response;
+
             try
             {
-                var response = _httpClient.Get(request);
-
-                capabilities = ParseCapabilities(response);
+                response = _httpClient.Get(request);
             }
             catch (Exception ex)
             {
-                _logger.Debug(ex, string.Format("Failed to get capabilities from {0}: {1}", indexerSettings.Url, ex.Message));
+                _logger.Debug(ex, "Failed to get newznab api capabilities from {0}", indexerSettings.Url);
+                throw;
+            }
+
+            try
+            {
+                capabilities = ParseCapabilities(response);
+            }
+            catch (XmlException ex)
+            {
+                _logger.Debug(ex, "Failed to parse newznab api capabilities for {0}.", indexerSettings.Url);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Failed to determine newznab api capabilities for {0}, using the defaults instead till Sonarr restarts.", indexerSettings.Url);
             }
 
             return capabilities;
