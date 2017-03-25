b8e853804a55f639e3b53c5254653fe22b1e7957
Mark McDowall <markus.mcd5@gmail.com>
2017-03-25 09:13:28 -0700

    New: Explicit toggle for importing extra files

---
+++ src/NzbDrone.Core/Datastore/Migration/109_import_extra_files.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using FluentMigrator;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(109)]
+    public class import_extra_files : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Execute.WithConnection(ImportExtraFiles);
+        }
+
+        private void ImportExtraFiles(IDbConnection conn, IDbTransaction tran)
+        {
+
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.Transaction = tran;
+                cmd.CommandText = "SELECT Value from Config WHERE Key = 'extrafileextensions'";
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var value = reader.GetString(0);
+
+                        if (value.IsNotNullOrWhiteSpace())
+                        {
+                            using (var insertCmd = conn.CreateCommand())
+                            {
+                                insertCmd.Transaction = tran;
+                                insertCmd.CommandText = "INSERT INTO Config (Key, Value) VALUES('importextrafiles', 1)";
+                                insertCmd.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
