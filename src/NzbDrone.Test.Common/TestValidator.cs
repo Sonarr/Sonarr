41f769790d55b66f5c23fe5b4f7afed2f4685dd2
Mark McDowall <markus.mcd5@gmail.com>
2017-04-11 17:57:29 -0700

    Fix issue adding a series when TitleSlug for another series is null
    Fixed: Adding a series when an existing series is has a null slug
    Closes #1840

---
+++ src/NzbDrone.Test.Common/TestValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FluentValidation;
+
+namespace NzbDrone.Test.Common
+{
+    public class TestValidator<T> : InlineValidator<T>
+    {
+        public TestValidator(params Action<TestValidator<T>>[] actions)
+        {
+            foreach (var action in actions)
+            {
+                action(this);
+            }
+        }
+    }
+}
