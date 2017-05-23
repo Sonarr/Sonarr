41b0591ffba7e518e5a471a99dbaa6304b58bc07
Mark McDowall <mark@mcdowall.ca>
2017-05-22 23:26:56 -0700

    fixup! Store and forward releases when download client is unavailable

--- src/NzbDrone.Core/Download/ProcessDownloadDecisions.cs
+++ src/NzbDrone.Core/Download/ProcessDownloadDecisions.cs
@@ -5,6 +5,7 @@ using NLog;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Download.Clients;
 using NzbDrone.Core.Download.Pending;
+using NzbDrone.Core.Indexers;
 
 namespace NzbDrone.Core.Download
 {
@@ -37,19 +38,17 @@ namespace NzbDrone.Core.Download
             var prioritizedDecisions = _prioritizeDownloadDecision.PrioritizeDecisions(qualifiedReports);
             var grabbed = new List<DownloadDecision>();
             var pending = new List<DownloadDecision>();
+            var storedUsenet = new List<DownloadDecision>();
+            var storedTorrent = new List<DownloadDecision>();
 
             foreach (var report in prioritizedDecisions)
             {
                 var remoteEpisode = report.RemoteEpisode;
-
+                var downloadProtocol = report.RemoteEpisode.Release.DownloadProtocol;
                 var episodeIds = remoteEpisode.Episodes.Select(e => e.Id).ToList();
 
-                //Skip if already grabbed
-                if (grabbed.SelectMany(r => r.RemoteEpisode.Episodes)
-                                .Select(e => e.Id)
-                                .ToList()
-                                .Intersect(episodeIds)
-                                .Any())
+                // Skip if already grabbed
+                if (IsGrabbed(grabbed, report))
                 {
                     continue;
                 }
@@ -70,6 +69,18 @@ namespace NzbDrone.Core.Download
                     continue;
                 }
 
+                if (downloadProtocol == DownloadProtocol.Usenet && storedUsenet.Any())
+                {
+                    storedUsenet.Add(report);
+                    continue;
+                }
+
+                if (downloadProtocol == DownloadProtocol.Torrent && storedTorrent.Any())
+                {
+                    storedTorrent.Add(report);
+                    continue;
+                }
+
                 try
                 {
                     _downloadService.DownloadReport(remoteEpisode);
@@ -78,14 +89,24 @@ namespace NzbDrone.Core.Download
                 catch (DownloadClientUnavailableException e)
                 {
                     _logger.Debug("Failed to send release to download client, storing until later");
-                    _pendingReleaseService.Add(report, PendingReleaseReason.StoreAndForward);
-                    pending.Add(report);
+
+                    if (downloadProtocol == DownloadProtocol.Torrent)
+                    {
+                        storedTorrent.Add(report);
+                    }
+                    else
+                    {
+                        storedUsenet.Add(report);
+                    }
                 }
                 catch (Exception e)
                 {
                     _logger.Warn(e, "Couldn't add report to download queue. " + remoteEpisode);
                 }
             }
+
+            pending.AddRange(ProcessFailedGrabs(grabbed, storedUsenet));
+            pending.AddRange(ProcessFailedGrabs(grabbed, storedTorrent));
 
             return new ProcessedDecisions(grabbed, pending, decisions.Where(d => d.Rejected).ToList());
         }
@@ -94,6 +115,33 @@ namespace NzbDrone.Core.Download
         {
             //Process both approved and temporarily rejected
             return decisions.Where(c => (c.Approved || c.TemporarilyRejected) && c.RemoteEpisode.Episodes.Any()).ToList();
+        }
+
+        private bool IsGrabbed(List<DownloadDecision> grabbed, DownloadDecision report)
+        {
+            var episodeIds = report.RemoteEpisode.Episodes.Select(e => e.Id).ToList();
+
+            return grabbed.SelectMany(r => r.RemoteEpisode.Episodes)
+                          .Select(e => e.Id)
+                          .ToList()
+                          .Intersect(episodeIds)
+                          .Any();
+        }
+
+        private List<DownloadDecision> ProcessFailedGrabs(List<DownloadDecision> grabbed, List<DownloadDecision> failed)
+        {
+            var pending = new List<DownloadDecision>();
+
+            foreach (var report in failed)
+            {
+                if (!IsGrabbed(grabbed, report))
+                {
+                    _pendingReleaseService.Add(report, PendingReleaseReason.StoreAndForward);
+                    pending.Add(report);
+                }
+            }
+
+            return pending;
         }
     }
 }
