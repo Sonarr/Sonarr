e8ee1dde95da7d6e23a971f8c5a044c92533d320
Mark McDowall <mark@mcdowall.ca>
2017-06-10 12:19:36 -0700

    Fixed: Redirect calls missing URL Base

---
+++ src/NzbDrone.Api/Extensions/Pipelines/UrlBasePipeline.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Responses;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Configuration;
+
+namespace NzbDrone.Api.Extensions.Pipelines
+{
+    public class UrlBasePipeline : IRegisterNancyPipeline
+    {
+        private readonly string _urlBase;
+
+        public UrlBasePipeline(IConfigFileProvider configFileProvider)
+        {
+            _urlBase = configFileProvider.UrlBase;
+        }
+
+        public int Order => 99;
+
+        public void Register(IPipelines pipelines)
+        {
+            pipelines.BeforeRequest.AddItemToStartOfPipeline((Func<NancyContext, Response>) Handle);
+        }
+
+        private Response Handle(NancyContext context)
+        {
+
+            if (_urlBase.IsNotNullOrWhiteSpace() && _urlBase != context.Request.Url.BasePath)
+            {
+                return new RedirectResponse($"{_urlBase}{context.Request.Path}{context.Request.Url.Query}");
+            }
+
+            return null;
+        }
+    }
+}
