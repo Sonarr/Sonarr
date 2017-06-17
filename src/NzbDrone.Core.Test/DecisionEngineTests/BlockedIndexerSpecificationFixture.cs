6a6d415625b54a7f2375d96d82a4794383d1eeb5
Taloth Saldono <Taloth@users.noreply.github.com>
2017-06-18 00:06:51 +0200

    Fixed: Pending releases from blocked indexers should not be grabbed.
    ref #1961

---
+++ src/NzbDrone.Core.Test/DecisionEngineTests/BlockedIndexerSpecificationFixture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.DecisionEngine;
+using NzbDrone.Core.DecisionEngine.Specifications;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.DecisionEngineTests
+{
+    [TestFixture]
+
+    public class BlockedIndexerSpecificationFixture : CoreTest<BlockedIndexerSpecification>
+    {
+        private RemoteEpisode _remoteEpisode;
+
+        [SetUp]
+        public void Setup()
+        {
+            _remoteEpisode = new RemoteEpisode
+            {
+                Release = new ReleaseInfo { IndexerId = 1 }
+            };
+
+            Mocker.GetMock<IIndexerStatusService>()
+                  .Setup(v => v.GetBlockedProviders())
+                  .Returns(new List<IndexerStatus>());
+        }
+
+        private void WithBlockedIndexer()
+        {
+            Mocker.GetMock<IIndexerStatusService>()
+                  .Setup(v => v.GetBlockedProviders())
+                  .Returns(new List<IndexerStatus> { new IndexerStatus { ProviderId = 1, DisabledTill = DateTime.UtcNow } });
+        }
+
+        [Test]
+        public void should_return_true_if_no_blocked_indexer()
+        {
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_return_false_if_blocked_indexer()
+        {
+            WithBlockedIndexer();
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeFalse();
+            Subject.Type.Should().Be(RejectionType.Temporary);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/DecisionEngine/Specifications/BlockedIndexerSpecification.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NLog;
+using NzbDrone.Common.Cache;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.IndexerSearch.Definitions;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.DecisionEngine.Specifications
+{
+    public class BlockedIndexerSpecification : IDecisionEngineSpecification
+    {
+        private readonly IIndexerStatusService _indexerStatusService;
+        private readonly Logger _logger;
+
+        private readonly ICachedDictionary<IndexerStatus> _blockedIndexerCache;
+
+        public BlockedIndexerSpecification(IIndexerStatusService indexerStatusService, ICacheManager cacheManager, Logger logger)
+        {
+            _indexerStatusService = indexerStatusService;
+            _logger = logger;
+
+            _blockedIndexerCache = cacheManager.GetCacheDictionary(GetType(), "blocked", FetchBlockedIndexer, TimeSpan.FromSeconds(15));
+        }
+
+        public SpecificationPriority Priority => SpecificationPriority.Database;
+        public RejectionType Type => RejectionType.Temporary;
+
+        public virtual Decision IsSatisfiedBy(RemoteEpisode subject, SearchCriteriaBase searchCriteria)
+        {
+            var status = _blockedIndexerCache.Find(subject.Release.IndexerId.ToString());
+            if (status != null)
+            {
+                return Decision.Reject($"Indexer {subject.Release.Indexer} is blocked till {status.DisabledTill} due to failures, cannot grab release.");
+            }
+
+            return Decision.Accept();
+        }
+
+        private IDictionary<string, IndexerStatus> FetchBlockedIndexer()
+        {
+            return _indexerStatusService.GetBlockedProviders().ToDictionary(v => v.ProviderId.ToString());
+        }
+    }
+}
