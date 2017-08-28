eac3b0c6178b8236b8ac938c3309f811ecf3ef76
Mark McDowall <mark@mcdowall.ca>
2017-08-27 22:58:52 -0700

    Upgrade signalR to 2.2.2

--- src/Microsoft.AspNet.SignalR.Core/Hubs/IJavaScriptProxyGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
-
-namespace Microsoft.AspNet.SignalR.Hubs
-{
-    public interface IJavaScriptProxyGenerator
-    {
-        string GenerateProxy(string serviceUrl);
-    }
-}
--- src/NzbDrone.Host/Owin/MiddleWare/NzbDroneVersionMiddleWare.cs
+++ src/NzbDrone.Host/Owin/MiddleWare/NzbDroneVersionMiddleWare.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using NLog;
 using NzbDrone.Common.EnvironmentInfo;
+using NzbDrone.Common.Instrumentation;
 using Owin;
 
 namespace NzbDrone.Host.Owin.MiddleWare
@@ -19,17 +22,25 @@ namespace NzbDrone.Host.Owin.MiddleWare
     public class AddApplicationVersionHeader : OwinMiddleware
     {
         private readonly KeyValuePair<string, string[]> _versionHeader;
+        private static readonly Logger Logger = NzbDroneLogger.GetLogger(typeof(AddApplicationVersionHeader));
 
         public AddApplicationVersionHeader(OwinMiddleware next)
             : base(next)
         {
-            _versionHeader = new KeyValuePair<string, string[]>("X-ApplicationVersion",
-               new[] { BuildInfo.Version.ToString() });
+            _versionHeader = new KeyValuePair<string, string[]>("X-Application-Version", new[] { BuildInfo.Version.ToString() });
         }
-        public override Task Invoke(IOwinContext context)
+
+        public override async Task Invoke(IOwinContext context)
         {
-            context.Response.Headers.Add(_versionHeader);
-            return Next.Invoke(context);
+            try
+            {
+                context.Response.Headers.Add(_versionHeader);
+                await Next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                Logger.Debug("Unable to set version header");
+            }
         }
     }
 }
