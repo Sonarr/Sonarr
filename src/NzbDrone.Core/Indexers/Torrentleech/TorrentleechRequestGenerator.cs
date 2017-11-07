178861fef07b397471c6237773d095fc3f512fed
Christophe Gondouin <christophe@gondouin.eu>
2017-11-07 01:39:23 +0100

    Adding Torrentleech Search capability

--- src/NzbDrone.Core/Indexers/Torrentleech/TorrentleechRequestGenerator.cs
+++ src/NzbDrone.Core/Indexers/Torrentleech/TorrentleechRequestGenerator.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Text;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.IndexerSearch.Definitions;
+using NzbDrone.Core.Tv;
 
 namespace NzbDrone.Core.Indexers.Torrentleech
 {
     public class TorrentleechRequestGenerator : IIndexerRequestGenerator
     {
         public TorrentleechSettings Settings { get; set; }
-        
+        public IHttpClient HttpClient { get; set; }
+
         public virtual IndexerPageableRequestChain GetRecentRequests()
         {
             var pageableRequests = new IndexerPageableRequestChain();
@@ -19,12 +22,20 @@ namespace NzbDrone.Core.Indexers.Torrentleech
 
         public virtual IndexerPageableRequestChain GetSearchRequests(SingleEpisodeSearchCriteria searchCriteria)
         {
-            return new IndexerPageableRequestChain();
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetSearchRequest(searchCriteria.Series, searchCriteria.SeasonNumber, searchCriteria.EpisodeNumber));
+
+            return pageableRequests;
         }
 
         public virtual IndexerPageableRequestChain GetSearchRequests(SeasonSearchCriteria searchCriteria)
         {
-            return new IndexerPageableRequestChain();
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetSearchRequest(searchCriteria.Series, searchCriteria.SeasonNumber));
+
+            return pageableRequests;
         }
 
         public virtual IndexerPageableRequestChain GetSearchRequests(DailyEpisodeSearchCriteria searchCriteria)
@@ -42,9 +53,41 @@ namespace NzbDrone.Core.Indexers.Torrentleech
             return new IndexerPageableRequestChain();
         }
 
+        private IEnumerable<IndexerRequest> GetSearchRequest(Series series, int seasonNumber, int? episodeNumber = null)
+        {
+            var requestList = new List<IndexerRequest>();
+
+            var loginRequest = new HttpRequest(Settings.LoginUrl, HttpAccept.Html)
+            {
+                Method = HttpMethod.POST,
+                StoreResponseCookie = true
+            };
+            loginRequest.Headers.ContentType = "application/x-www-form-urlencoded";
+            loginRequest.SetContent($"username={Settings.Username}&password={Settings.Password}");
+
+            //var loginIndexerRequest = new IndexerRequest(loginRequest);
+
+            //requestList.Add(loginIndexerRequest);
+
+            HttpClient.Execute(loginRequest);
+
+            var requestUrl = new StringBuilder();
+            requestUrl.Append(Settings.RequestUrl);
+            requestUrl.Append($"{SearchCriteriaBase.GetQueryTitle(series.Title)} S{seasonNumber:00}");
+
+            requestUrl.Append(episodeNumber.HasValue ? $"E{episodeNumber:00}" : "/categories/27");
+
+            requestList.Add(new IndexerRequest(requestUrl.ToString(), HttpAccept.Html));
+
+            return requestList;
+        }
+
         private IEnumerable<IndexerRequest> GetRssRequests(string searchParameters)
         {
-            yield return new IndexerRequest(string.Format("{0}/{1}{2}", Settings.BaseUrl.Trim().TrimEnd('/'), Settings.ApiKey, searchParameters), HttpAccept.Rss);
+            yield return new IndexerRequest(string.Format("https://rss.torrentleech.org/{1}{2}", Settings.ApiKey, searchParameters), HttpAccept.Rss);
         }
     }
 }
+
+
+
