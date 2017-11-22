ae2a97763d3b2541b3393cd33762fe08dbc3256d
Mark McDowall <markus.mcd5@gmail.com>
2017-11-21 19:00:25 -0800

    New: Validate NZBs before sending to download client
    Closes #263

---
+++ src/NzbDrone.Core/Download/InvalidNzbException.cs
@@ -0,0 +1,29 @@
+using System;
+using NzbDrone.Common.Exceptions;
+
+namespace NzbDrone.Core.Download
+{
+    public class InvalidNzbException : NzbDroneException
+    {
+        public InvalidNzbException(string message, params object[] args) : base(message, args)
+        {
+        }
+
+        public InvalidNzbException(string message) : base(message)
+        {
+        }
+
+        public InvalidNzbException(string message, Exception innerException, params object[] args) : base(message, innerException, args)
+        {
+        }
+
+        public InvalidNzbException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public InvalidNzbException(string message, string nzbName)
+            : base($"{message} [{0}]", nzbName)
+        {
+        }
+    }
+}
