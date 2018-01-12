d25e5fe3298e21f811edc2bacf5869807138fa3c
Marcelo Castagna <margaale@users.noreply.github.com>
2018-01-12 16:50:21 -0300

    New: Setting for absolute maximum size for a release
    Closes #2367 #1996

---
+++ src/NzbDrone.Core.Test/DecisionEngineTests/MaximumSizeSpecificationFixture.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.DecisionEngine.Specifications;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.DecisionEngineTests
+{
+    public class MaximumSizeSpecificationFixture : CoreTest<MaximumSizeSpecification>
+    {
+        private RemoteEpisode _remoteEpisode;
+
+        [SetUp]
+        public void Setup()
+        {
+            _remoteEpisode = new RemoteEpisode() { Release = new ReleaseInfo() };
+        }
+
+        private void WithMaximumSize(int size)
+        {
+            Mocker.GetMock<IConfigService>().SetupGet(c => c.MaximumSize).Returns(size);
+        }
+
+        private void WithSize(int size)
+        {
+            _remoteEpisode.Release.Size = size * 1024 * 1024;
+        }
+
+        [Test]
+        public void should_return_true_when_maximum_size_is_set_to_zero()
+        {
+            WithMaximumSize(0);
+            WithSize(1000);
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_return_true_when_size_is_smaller_than_maximum_size()
+        {
+            WithMaximumSize(2000);
+            WithSize(1999);
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_return_true_when_size_is_equals_to_maximum_size()
+        {
+            WithMaximumSize(2000);
+            WithSize(2000);
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_return_false_when_size_is_bigger_than_maximum_size()
+        {
+            WithMaximumSize(2000);
+            WithSize(2001);
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_return_true_when_size_is_zero()
+        {
+            WithMaximumSize(2000);
+            WithSize(0);
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+    }
+}
---
+++ src/NzbDrone.Core/DecisionEngine/Specifications/MaximumSizeSpecification.cs
@@ -0,0 +1,53 @@
+﻿using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.IndexerSearch.Definitions;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.DecisionEngine.Specifications
+{
+    public class MaximumSizeSpecification : IDecisionEngineSpecification
+    {
+        private readonly IConfigService _configService;
+        private readonly Logger _logger;
+
+        public MaximumSizeSpecification(IConfigService configService, Logger logger)
+        {
+            _configService = configService;
+            _logger = logger;
+        }
+
+        public SpecificationPriority Priority => SpecificationPriority.Default;
+        public RejectionType Type => RejectionType.Permanent;
+
+        public Decision IsSatisfiedBy(RemoteEpisode subject, SearchCriteriaBase searchCriteria)
+        {
+            var size = subject.Release.Size;
+            var maximumSize = _configService.MaximumSize.Megabytes();
+
+            if (maximumSize == 0)
+            {
+                _logger.Debug("Maximum size is not set.");
+                return Decision.Accept();
+            }
+
+            if (subject.Release.Size == 0)
+            {
+                _logger.Debug("Release has unknown size, skipping size check.");
+                return Decision.Accept();
+            }
+
+            _logger.Debug("Checking if release meets maximum size requirements. {0}", size.SizeSuffix());
+
+            if (size > maximumSize)
+            {
+                var message = $"{size.SizeSuffix()} is too big, maximum size is {maximumSize.SizeSuffix()}";
+
+                _logger.Debug(message);
+                return Decision.Reject(message);
+            }
+
+            return Decision.Accept();
+        }
+    }
+}
