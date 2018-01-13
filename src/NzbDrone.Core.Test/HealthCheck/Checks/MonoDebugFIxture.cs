d944190b370984471dfaa92da46b09e29167a13d
margaale <margaale@gmail.com>
2018-01-13 13:12:08 -0300

    check if mono is running with --debug arg

---
+++ src/NzbDrone.Core.Test/HealthCheck/Checks/MonoDebugFIxture.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using NzbDrone.Core.HealthCheck.Checks;
+using NzbDrone.Core.Test.Framework;
+using static NzbDrone.Core.HealthCheck.Checks.MonoDebugCheck;
+
+namespace NzbDrone.Core.Test.HealthCheck.Checks
+{
+    [TestFixture]
+    public class MonoDebugFixture : CoreTest<MonoDebugCheck>
+    {
+        private void GivenHasStackFrame(bool hasStackFrame)
+        {
+            MonoOnly();
+
+            Mocker.GetMock<StackFrameHelper>()
+                  .Setup(f => f.HasStackFrameInfo())
+                  .Returns(hasStackFrame);
+        }
+
+        [Test]
+        public void should_return_warning()
+        {
+            GivenHasStackFrame(false);
+
+            Subject.Check().ShouldBeWarning();
+        }
+
+        [Test]
+        public void should_return_ok()
+        {
+            GivenHasStackFrame(true);
+
+            Subject.Check().ShouldBeOk();
+        }
+    }
+}
