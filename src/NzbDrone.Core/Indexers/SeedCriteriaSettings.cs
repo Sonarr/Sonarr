98f7395d3d3b36cd4349e69f6ccb66e88fd362e3
Taloth Saldono <Taloth@users.noreply.github.com>
2018-05-03 21:40:57 +0200

    Added Seed Time and Season-Pack seed time.

--- src/NzbDrone.Core/Indexers/SeedCriteriaSettings.cs
+++ src/NzbDrone.Core/Indexers/SeedCriteriaSettings.cs
@@ -13,8 +13,8 @@ namespace NzbDrone.Core.Indexers
         public SeedCriteriaSettingsValidator()
         {
             RuleFor(c => c.SeedRatio).GreaterThan(0.0).When(c => c.SeedRatio.HasValue);
-            //RuleFor(c => c.SeedTime).GreaterThan(0).When(c => c.SeedTime.HasValue);
-            //RuleFor(c => c.SeasonPackSeedTime).GreaterThan(0).When(c => c.SeasonPackSeedTime.HasValue);
+            RuleFor(c => c.SeedTime).GreaterThan(0).When(c => c.SeedTime.HasValue);
+            RuleFor(c => c.SeasonPackSeedTime).GreaterThan(0).When(c => c.SeasonPackSeedTime.HasValue);
         }
     }
 
@@ -25,10 +25,10 @@ namespace NzbDrone.Core.Indexers
         [FieldDefinition(0, Type = FieldType.Textbox, Label = "Seed Ratio", HelpText = "The ratio a torrent should reach before stopping, empty is download client's default", Advanced = true)]
         public double? SeedRatio { get; set; }
 
-        //[FieldDefinition(1, Type = FieldType.Textbox, Label = "Seed Time", HelpText = "The time a torrent should be seeded before stopping, empty is download client's default", Advanced = true)]
-        //public int? SeedTime { get; set; }
+        [FieldDefinition(1, Type = FieldType.Textbox, Label = "Seed Time", HelpText = "The time a torrent should be seeded before stopping, empty is download client's default", Advanced = true)]
+        public int? SeedTime { get; set; }
 
-        //[FieldDefinition(2, Type = FieldType.Textbox, Label = "Season-Pack Seed Time", HelpText = "The time a torrent should be seeded before stopping, empty is download client's default", Advanced = true)]
-        //public int? SeasonPackSeedTime { get; set; }
+        [FieldDefinition(2, Type = FieldType.Textbox, Label = "Season-Pack Seed Time", HelpText = "The time a torrent should be seeded before stopping, empty is download client's default", Advanced = true)]
+        public int? SeasonPackSeedTime { get; set; }
     }
 }
