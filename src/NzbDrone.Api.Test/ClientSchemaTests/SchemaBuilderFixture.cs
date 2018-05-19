7388e8c2c2ef06da36161aa8d855d40f7f14cb1f
Taloth Saldono <Taloth@users.noreply.github.com>
2018-05-19 22:15:19 +0200

    Fixed broken test for nested settings.

--- src/NzbDrone.Api.Test/ClientSchemaTests/SchemaBuilderFixture.cs
+++ src/NzbDrone.Api.Test/ClientSchemaTests/SchemaBuilderFixture.cs
@@ -42,8 +42,8 @@ namespace NzbDrone.Api.Test.ClientSchemaTests
 
             var schema = SchemaBuilder.ToSchema(model);
 
-            schema.Should().Contain(c => c.Order == 0 && c.Name == "FirstName" && c.Label == "First Name" && c.HelpText == "Your First Name" && (string)c.Value == "Bob");
-            schema.Should().Contain(c => c.Order == 1 && c.Name == "LastName" && c.Label == "Last Name" && c.HelpText == "Your Last Name" && (string)c.Value == "Poop");
+            schema.Should().Contain(c => c.Order == 0 && c.Name == "Name.FirstName" && c.Label == "First Name" && c.HelpText == "Your First Name" && (string)c.Value == "Bob");
+            schema.Should().Contain(c => c.Order == 1 && c.Name == "Name.LastName" && c.Label == "Last Name" && c.HelpText == "Your Last Name" && (string)c.Value == "Poop");
             schema.Should().Contain(c => c.Order == 2 && c.Name == "Quote" && c.Label == "Quote" && c.HelpText == "Your Favorite Quote");
         }
     }
