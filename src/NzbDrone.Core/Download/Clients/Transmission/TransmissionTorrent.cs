3c50c3e5a8c1ea3aa244f7a9c52c901e0c69afc4
Mark McDowall <mark@mcdowall.ca>
2018-05-20 22:57:53 -0700

    Fixed: Multi-file torrents in Vuze with different folder and file names
    Fixes #2571

--- src/NzbDrone.Core/Download/Clients/Transmission/TransmissionTorrent.cs
+++ src/NzbDrone.Core/Download/Clients/Transmission/TransmissionTorrent.cs
@@ -3,31 +3,19 @@
     public class TransmissionTorrent
     {
         public int Id { get; set; }
-
         public string HashString { get; set; }
-
         public string Name { get; set; }
-
         public string DownloadDir { get; set; }
-
         public long TotalSize { get; set; }
-
         public long LeftUntilDone { get; set; }
-
         public bool IsFinished { get; set; }
-
         public int Eta { get; set; }
-
         public TransmissionTorrentStatus Status { get; set; }
-
         public int SecondsDownloading { get; set; }
-
         public string ErrorString { get; set; }
-
         public long DownloadedEver { get; set; }
-
         public long UploadedEver { get; set; }
-
         public long SeedRatioLimit { get; set; }
+        public int FileCount { get; set; }
     }
 }
