62b03adb53f2888c3a04065243c1a36c38b48191
Mark McDowall <mark@mcdowall.ca>
2018-08-31 19:36:38 -0700

    Removed NotifyMyAndroid and Pushalot
    Fixed: Removed NotifyMyAndroid notifications
    Fixed: Remove Pushalot notifications

--- src/NzbDrone.Core/Notifications/Pushalot/PushalotResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NzbDrone.Core.Notifications.Pushalot
-{
-    public class PushalotResponse
-    {
-        public bool Success { get; set; }
-        public int Status { get; set; }
-        public string Description { get; set; }
-    }
-}
