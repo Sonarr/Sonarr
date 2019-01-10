6a489a0b8fe4f7c8c77cd3420d43c6ef88cfebbc
Mark McDowall <mark@mcdowall.ca>
2019-01-09 17:51:54 -0800

    Fixed: Importing of completed download when not a child of the download client output path

--- src/NzbDrone.Core.Test/MediaFiles/ImportApprovedEpisodesFixture.cs
+++ src/NzbDrone.Core.Test/MediaFiles/ImportApprovedEpisodesFixture.cs
@@ -375,6 +375,22 @@ namespace NzbDrone.Core.Test.MediaFiles
         }
 
         [Test]
+        public void should_use_folder_info_release_title_to_find_relative_path_when_file_is_not_in_download_client_item_output_directory()
+        {
+            var name = "Series.Title.S01E01.720p.HDTV.x264-Sonarr";
+            var outputPath = Path.Combine(@"C:\Test\Unsorted\TV\".AsOsAgnostic(), name);
+            var localEpisode = _approvedDecisions.First().LocalEpisode;
+
+            _downloadClientItem.OutputPath = new OsPath(Path.Combine(@"C:\Test\Unsorted\TV-Other\".AsOsAgnostic(), name));
+            localEpisode.FolderEpisodeInfo = new ParsedEpisodeInfo { ReleaseTitle = name };
+            localEpisode.Path = Path.Combine(outputPath, "subfolder", name + ".mkv");
+
+            Subject.Import(new List<ImportDecision> { _approvedDecisions.First() }, true, _downloadClientItem);
+
+            Mocker.GetMock<IMediaFileService>().Verify(v => v.Add(It.Is<EpisodeFile>(c => c.OriginalFilePath == $"{name}\\subfolder\\{name}.mkv".AsOsAgnostic())));
+        }
+
+        [Test]
         public void should_delete_existing_metadata_files_with_the_same_path()
         {
             Mocker.GetMock<IMediaFileService>()
