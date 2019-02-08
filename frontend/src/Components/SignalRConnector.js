29f905b9421de9cbd25135bcac3a66dce02b8739
Mark McDowall <mark@mcdowall.ca>
2019-02-07 18:43:56 -0800

    Root folder handler for signalR

--- frontend/src/Components/SignalRConnector.js
+++ frontend/src/Components/SignalRConnector.js
@@ -11,6 +11,7 @@ import { setAppValue, setVersion } from 'Store/Actions/appActions';
 import { update, updateItem, removeItem } from 'Store/Actions/baseActions';
 import { fetchHealth } from 'Store/Actions/systemActions';
 import { fetchQueue, fetchQueueDetails } from 'Store/Actions/queueActions';
+import { fetchRootFolders } from 'Store/Actions/rootFolderActions';
 import { fetchTags, fetchTagDetails } from 'Store/Actions/tagActions';
 
 function getState(status) {
@@ -70,6 +71,7 @@ const mapDispatchToProps = {
   dispatchFetchHealth: fetchHealth,
   dispatchFetchQueue: fetchQueue,
   dispatchFetchQueueDetails: fetchQueueDetails,
+  dispatchFetchRootFolders: fetchRootFolders,
   dispatchFetchTags: fetchTags,
   dispatchFetchTagDetails: fetchTagDetails
 };
@@ -259,6 +261,10 @@ class SignalRConnector extends Component {
     // No-op for now, we may want this later
   }
 
+  handleRootfolder = () => {
+    this.props.dispatchFetchRootFolders();
+  }
+
   handleTag = (body) => {
     if (body.action === 'sync') {
       this.props.dispatchFetchTags();
@@ -367,6 +373,7 @@ SignalRConnector.propTypes = {
   dispatchFetchHealth: PropTypes.func.isRequired,
   dispatchFetchQueue: PropTypes.func.isRequired,
   dispatchFetchQueueDetails: PropTypes.func.isRequired,
+  dispatchFetchRootFolders: PropTypes.func.isRequired,
   dispatchFetchTags: PropTypes.func.isRequired,
   dispatchFetchTagDetails: PropTypes.func.isRequired
 };
