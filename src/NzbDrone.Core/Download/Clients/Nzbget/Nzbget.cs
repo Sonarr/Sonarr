0c2331f638d1babfddd65891e9f0473a596d3ef1
Mark McDowall <mark@mcdowall.ca>
2019-05-08 22:25:10 -0700

    Fixed: Ignore deleted duplicates from Nzbget
    Fixes #1721

--- src/NzbDrone.Core/Download/Clients/Nzbget/Nzbget.cs
+++ src/NzbDrone.Core/Download/Clients/Nzbget/Nzbget.cs
@@ -9,6 +9,7 @@ using NzbDrone.Common.Disk;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Exceptions;
 using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.RemotePathMappings;
 using NzbDrone.Core.Validation;
@@ -36,16 +37,13 @@ namespace NzbDrone.Core.Download.Clients.Nzbget
         protected override string AddFromNzbFile(RemoteEpisode remoteEpisode, string filename, byte[] fileContent)
         {
             var category = Settings.TvCategory;
-
             var priority = remoteEpisode.IsRecentEpisode() ? Settings.RecentTvPriority : Settings.OlderTvPriority;
-
             var addpaused = Settings.AddPaused;
-
             var response = _proxy.DownloadNzb(fileContent, filename, category, priority, addpaused, Settings);
 
             if (response == null)
             {
-                throw new DownloadClientException("Failed to add nzb {0}", filename);
+                throw new DownloadClientRejectedReleaseException(remoteEpisode.Release, "NZBGet rejected the NZB for an unknown reason");
             }
 
             return response;
@@ -132,7 +130,7 @@ namespace NzbDrone.Core.Download.Clients.Nzbget
                 historyItem.CanMoveFiles = true;
                 historyItem.CanBeRemoved = true;
 
-                if (item.DeleteStatus == "MANUAL")
+                if (item.DeleteStatus == "MANUAL" || item.DeleteStatus == "COPY")
                 {
                     continue;
                 }
