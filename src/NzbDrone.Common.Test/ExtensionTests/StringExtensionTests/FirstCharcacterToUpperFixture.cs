0416fc7caeca771ea09fafd0a88dcffe3aae2e88
Mark McDowall <mark@mcdowall.ca>
2019-06-10 21:36:56 -0700

    fixup! New: Limit Custom Scripts to specified scripts folder

--- src/NzbDrone.Common.Test/ExtensionTests/StringExtensionTests/FirstCharcacterToUpperFixture.cs
+++ src/NzbDrone.Common.Test/ExtensionTests/StringExtensionTests/FirstCharcacterToUpperFixture.cs
@@ -10,11 +10,8 @@ namespace NzbDrone.Common.Test.ExtensionTests.StringExtensionTests
         [TestCase("hello", "Hello")]
         [TestCase("camelCase", "CamelCase")]
         [TestCase("a full sentence", "A full sentence")]
-<<<<<<< HEAD
         [TestCase("", "")]
         [TestCase(null, "")]
-=======
->>>>>>> b6072c2d3... fixup! New: Limit Custom Scripts to specified scripts folder
         public void should_capitalize_first_character(string input, string expected)
         {
             input.FirstCharToUpper().Should().Be(expected);
