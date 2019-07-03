a9c6d8b122466ab327a70f1a0491196bc2d2a62d
datahodor <wecouldreallyquit@protonmail.com>
2019-07-03 11:56:01 +0200

    Treat MaxdomeHD as Web-DL

--- src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
@@ -186,6 +186,7 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Community.6x11.Modern.Espionage.ITA.ENG.720p.WEB.DLMux.H.264-GiuseppeTnT", false)]
         [TestCase("The Walking Dead 2010 S09E13 [MKV / H.264 / AC3/AAC / WEB / Dual Áudio / Inglês / 720p]", false)]
         [TestCase("into.the.badlands.s03e16.h264.720p-web-handbrake.mkv", false)]
+        [TestCase("Jerks.S03E05.Griebnitzsee.German.720p.MaxdomeHD.AVC-TVS", false)]
         public void should_parse_webdl720p_quality(string title, bool proper)
         {
             ParseAndVerifyQuality(title, Quality.WEBDL720p, proper);
@@ -235,7 +236,7 @@ namespace NzbDrone.Core.Test.ParserTests
             ParseAndVerifyQuality(title, Quality.WEBRip1080p, proper);
         }
 
-        
+
         [TestCase("The.Nightly.Show.2016.03.14.2160p.WEB.x264-spamTV", false)]
         [TestCase("The.Nightly.Show.2016.03.14.2160p.WEB.h264-spamTV", false)]
         [TestCase("The.Nightly.Show.2016.03.14.2160p.WEB.PROPER.h264-spamTV", true)]
