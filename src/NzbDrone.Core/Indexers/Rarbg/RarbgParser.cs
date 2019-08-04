292620169445d58f73fba0822c26e82c6164611a
Mark McDowall <mark@mcdowall.ca>
2019-08-03 19:27:11 -0700

    Fixed: RARBG links in Interactive Search
    Fixes #3239

--- src/NzbDrone.Core/Indexers/Rarbg/RarbgParser.cs
+++ src/NzbDrone.Core/Indexers/Rarbg/RarbgParser.cs
@@ -51,7 +51,7 @@ namespace NzbDrone.Core.Indexers.Rarbg
                 torrentInfo.Title = torrent.title;
                 torrentInfo.Size = torrent.size;
                 torrentInfo.DownloadUrl = torrent.download;
-                torrentInfo.InfoUrl = torrent.info_page;
+                torrentInfo.InfoUrl = torrent.info_page + "&app_id=Sonarr";
                 torrentInfo.PublishDate = torrent.pubdate.ToUniversalTime();
                 torrentInfo.Seeders = torrent.seeders;
                 torrentInfo.Peers = torrent.leechers + torrent.seeders;
