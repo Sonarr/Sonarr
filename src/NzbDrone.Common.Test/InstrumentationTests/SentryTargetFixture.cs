63b7f4e1e0061f8583be42b9088b7a7e34cd93b5
Qstick <qstick@gmail.com>
2019-08-07 22:24:03 -0400

    New: Replace SharpRaven with new Sentry SDK
    Co-Authored-By: ta264 <ta264@users.noreply.github.com>

---
+++ src/NzbDrone.Common.Test/InstrumentationTests/SentryTargetFixture.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using NzbDrone.Common.Instrumentation.Sentry;
+using System;
+using NLog;
+using NzbDrone.Test.Common;
+using System.Globalization;
+using System.Linq;
+
+namespace NzbDrone.Common.Test.InstrumentationTests
+{
+    [TestFixture]
+    public class SentryTargetFixture : TestBase
+    {
+        private SentryTarget Subject;
+
+        private static LogLevel[] AllLevels = LogLevel.AllLevels.ToArray();
+        private static LogLevel[] SentryLevels = LogLevel.AllLevels.Where(x => x >= LogLevel.Error).ToArray();
+        private static LogLevel[] OtherLevels = AllLevels.Except(SentryLevels).ToArray();
+
+        private static Exception[] FilteredExceptions = new Exception[] {
+            new UnauthorizedAccessException(),
+            new TinyIoC.TinyIoCResolutionException(typeof(string))
+        };
+
+        [SetUp]
+        public void Setup()
+        {
+            Subject = new SentryTarget("https://aaaaaaaaaaaaaaaaaaaaaaaaaa@sentry.io/111111");
+        }
+
+        private LogEventInfo GivenLogEvent(LogLevel level, Exception ex, string message)
+        {
+            return LogEventInfo.Create(level, "SentryTest", ex, CultureInfo.InvariantCulture, message);
+        }
+
+        [Test, TestCaseSource("AllLevels")]
+        public void log_without_error_is_not_sentry_event(LogLevel level)
+        {
+            Subject.IsSentryMessage(GivenLogEvent(level, null, "test")).Should().BeFalse();
+        }
+
+        [Test, TestCaseSource("SentryLevels")]
+        public void error_or_worse_with_exception_is_sentry_event(LogLevel level)
+        {
+            Subject.IsSentryMessage(GivenLogEvent(level, new Exception(), "test")).Should().BeTrue();
+        }
+
+        [Test, TestCaseSource("OtherLevels")]
+        public void less_than_error_with_exception_is_not_sentry_event(LogLevel level)
+        {
+            Subject.IsSentryMessage(GivenLogEvent(level, new Exception(), "test")).Should().BeFalse();
+        }
+
+        [Test, TestCaseSource("FilteredExceptions")]
+        public void should_filter_event_for_filtered_exception_types(Exception ex)
+        {
+            var log = GivenLogEvent(LogLevel.Error, ex, "test");
+            Subject.IsSentryMessage(log).Should().BeFalse();
+        }
+
+        [Test, TestCaseSource("FilteredExceptions")]
+        public void should_not_filter_event_for_filtered_exception_types_if_filtering_disabled(Exception ex)
+        {
+            Subject.FilterEvents = false;
+            var log = GivenLogEvent(LogLevel.Error, ex, "test");
+            Subject.IsSentryMessage(log).Should().BeTrue();
+        }
+
+        [Test, TestCaseSource(typeof(SentryTarget), "FilteredExceptionMessages")]
+        public void should_filter_event_for_filtered_exception_messages(string message)
+        {
+            var log = GivenLogEvent(LogLevel.Error, new Exception("aaaaaaa" + message + "bbbbbbb"), "test");
+            Subject.IsSentryMessage(log).Should().BeFalse();
+        }
+
+        [TestCase("A message that isn't filtered")]
+        [TestCase("Error")]
+        public void should_not_filter_event_for_exception_messages_that_are_not_filtered(string message)
+        {
+            var log = GivenLogEvent(LogLevel.Error, new Exception(message), "test");
+            Subject.IsSentryMessage(log).Should().BeTrue();
+        }
+    }
+}
---
+++ src/NzbDrone.Common/Instrumentation/Sentry/SentryCleanser.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Linq;
+using Sentry;
+using Sentry.Protocol;
+
+namespace NzbDrone.Common.Instrumentation.Sentry
+{
+    public static class SentryCleanser
+    {
+        public static SentryEvent CleanseEvent(SentryEvent sentryEvent)
+        {
+            try
+            {
+                sentryEvent.Message = CleanseLogMessage.Cleanse(sentryEvent.Message);
+
+                if (sentryEvent.Fingerprint != null)
+                {
+                    var fingerprint = sentryEvent.Fingerprint.Select(x => CleanseLogMessage.Cleanse(x)).ToList();
+                    sentryEvent.SetFingerprint(fingerprint);
+                }
+
+                if (sentryEvent.Extra != null)
+                {
+                    var extras = sentryEvent.Extra.ToDictionary(x => x.Key, y => (object)CleanseLogMessage.Cleanse((string)y.Value));
+                    sentryEvent.SetExtras(extras);
+                }
+
+                foreach (var exception in sentryEvent.SentryExceptions)
+                {
+                    exception.Value = CleanseLogMessage.Cleanse(exception.Value);
+                    foreach (var frame in exception.Stacktrace.Frames)
+                    {
+                        frame.FileName = ShortenPath(frame.FileName);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return sentryEvent;
+        }
+
+        public static Breadcrumb CleanseBreadcrumb(Breadcrumb b)
+        {
+            try
+            {
+                var message = CleanseLogMessage.Cleanse(b.Message);
+                var data = b.Data?.ToDictionary(x => x.Key, y => CleanseLogMessage.Cleanse(y.Value));
+                return new Breadcrumb(message, b.Type, data, b.Category, b.Level);
+            }
+            catch(Exception)
+            {
+
+            }
+
+            return b;
+        }
+
+        private static string ShortenPath(string path)
+        {
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return null;
+            }
+
+            // the paths in the stacktrace depend on where it was compiled,
+            // not the current OS
+            var rootDirs = new [] { "\\src\\", "/src/" };
+            foreach (var rootDir in rootDirs)
+            {
+                var index = path.IndexOf(rootDir, StringComparison.Ordinal);
+
+                if (index > 0)
+                {
+                    return path.Substring(index + rootDir.Length - 1);
+                }
+            }
+
+            return path;
+        }
+    }
+}
