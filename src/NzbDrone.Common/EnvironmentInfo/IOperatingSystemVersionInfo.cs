b453d48fee9126c50a33f377e630c9edda5ffc27
Taloth Saldono <Taloth@users.noreply.github.com>
2019-08-16 20:07:32 +0200

    Removed excluded source files.

--- src/NzbDrone.Common/EnvironmentInfo/IOperatingSystemVersionInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NzbDrone.Common.EnvironmentInfo
-{
-    public interface IOperatingSystemVersionInfo
-    {
-        string Version { get; }
-        string Name { get; }
-        string FullName { get; }
-    }
-}
