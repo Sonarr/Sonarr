72902c8984bd19b31c3930d4ce031488a04ad3bd
Taloth Saldono <Taloth@users.noreply.github.com>
2019-08-24 01:39:16 +0200

    Test framework version

--- src/NzbDrone.Windows.Test/EnvironmentInfo/DotNetPlatformInfoFixture.cs
+++ src/NzbDrone.Windows.Test/EnvironmentInfo/DotNetPlatformInfoFixture.cs
@@ -13,7 +13,7 @@ namespace NzbDrone.Windows.Test.EnvironmentInfo
         public void should_get_framework_version()
         {
             Subject.Version.Major.Should().Be(4);
-            Subject.Version.Minor.Should().BeOneOf(0, 5, 6);
+            Subject.Version.Minor.Should().BeOneOf(0, 5, 6, 7, 8);
         }
     }
 }
