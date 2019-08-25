c209c1c034b042d41a82b662a1ebd42a07c6be69
Taloth Saldono <Taloth@users.noreply.github.com>
2019-08-25 14:49:17 +0200

    Typo

--- src/NzbDrone.Core/Download/Clients/Deluge/Deluge.cs
+++ src/NzbDrone.Core/Download/Clients/Deluge/Deluge.cs
@@ -235,7 +235,7 @@ namespace NzbDrone.Core.Download.Clients.Deluge
             }
             catch (WebException ex)
             {
-                _logger.Error(ex, "Unble to test connection");
+                _logger.Error(ex, "Unable to test connection");
                 switch (ex.Status)
                 {
                     case WebExceptionStatus.ConnectFailure:
