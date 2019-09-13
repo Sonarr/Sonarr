e8ce7898c1446b04e71aec2481e840055314ac4b
Taloth Saldono <Taloth@users.noreply.github.com>
2019-09-13 23:41:42 +0200

    use TestContext.Progress rather than Console.WriteLine for NzbDroneRunner output

--- src/NzbDrone.Test.Common/NzbDroneRunner.cs
+++ src/NzbDrone.Test.Common/NzbDroneRunner.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using System.Xml;
 using System.Xml.Linq;
-using System.Xml.XPath;
 using NLog;
 using NUnit.Framework;
 using NzbDrone.Common.EnvironmentInfo;
-using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Processes;
 using NzbDrone.Core.Configuration;
 using RestSharp;
@@ -21,7 +17,7 @@ namespace NzbDrone.Test.Common
         private readonly IProcessProvider _processProvider;
         private readonly IRestClient _restClient;
         private Process _nzbDroneProcess;
-        private List<string> _startupLog;
+        private TextWriter _progressWriter;
 
         public string AppData { get; private set; }
         public string ApiKey { get; private set; }
@@ -41,7 +37,8 @@ namespace NzbDrone.Test.Common
             
             var sonarrConsoleExe = OsInfo.IsWindows ? "Sonarr.Console.exe" : "Sonarr.exe";
 
-            _startupLog = new List<string>();
+            _progressWriter = TestContext.Progress;
+
             if (BuildInfo.IsDebug)
             {
                 Start(Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "_output", "Sonarr.Console.exe"));
@@ -57,10 +54,9 @@ namespace NzbDrone.Test.Common
 
                 if (_nzbDroneProcess.HasExited)
                 {
-                    Console.WriteLine("NzbDrone has exited unexpectedly");
+                    _progressWriter.WriteLine("NzbDrone has exited unexpectedly");
                     Thread.Sleep(2000);
-                    var output = _startupLog.Join(Environment.NewLine);
-                    Assert.Fail("Process has exited: ExitCode={0} Output={1}", _nzbDroneProcess.ExitCode, output);
+                    Assert.Fail("Process has exited: ExitCode={0}", _nzbDroneProcess.ExitCode);
                 }
 
                 var request = new RestRequest("system/status");
@@ -71,12 +67,11 @@ namespace NzbDrone.Test.Common
 
                 if (statusCall.ResponseStatus == ResponseStatus.Completed)
                 {
-                    _startupLog = null;
-                    Console.WriteLine("NzbDrone is started. Running Tests");
+                    _progressWriter.WriteLine("NzbDrone is started. Running Tests");
                     return;
                 }
 
-                Console.WriteLine("Waiting for NzbDrone to start. Response Status : {0}  [{1}] {2}", statusCall.ResponseStatus, statusCall.StatusDescription, statusCall.ErrorException.Message);
+                _progressWriter.WriteLine("Waiting for NzbDrone to start. Response Status : {0}  [{1}] {2}", statusCall.ResponseStatus, statusCall.StatusDescription, statusCall.ErrorException.Message);
 
                 Thread.Sleep(500);
             }
@@ -105,7 +100,7 @@ namespace NzbDrone.Test.Common
 
         private void Start(string outputNzbdroneConsoleExe)
         {
-            Console.WriteLine("Starting instance from {0}", outputNzbdroneConsoleExe);
+            _progressWriter.WriteLine("Starting instance from {0}", outputNzbdroneConsoleExe);
 
             var args = "-nobrowser -data=\"" + AppData + "\"";
             _nzbDroneProcess = _processProvider.Start(outputNzbdroneConsoleExe, args, null, OnOutputDataReceived, OnOutputDataReceived);
@@ -114,12 +109,7 @@ namespace NzbDrone.Test.Common
 
         private void OnOutputDataReceived(string data)
         {
-            Console.WriteLine(data);
-
-            if (_startupLog != null)
-            {
-                _startupLog.Add(data);
-            }
+            _progressWriter.WriteLine(" : " + data);
 
             if (data.Contains("Press enter to exit"))
             {
