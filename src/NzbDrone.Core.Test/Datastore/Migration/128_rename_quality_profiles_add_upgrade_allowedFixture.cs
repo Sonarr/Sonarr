3cbdd6bfd3a8a7314457de9e598c021b2de16847
Taloth Saldono <Taloth@users.noreply.github.com>
2019-12-05 23:59:36 +0100

    Fixed: Rare scenario where early Radarr version messes up Sonarr database

---
+++ src/NzbDrone.Core.Test/Datastore/Migration/128_rename_quality_profiles_add_upgrade_allowedFixture.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Datastore.Migration;
+using NzbDrone.Core.Datastore.Migration.Framework;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.Datastore.Migration
+{
+    [TestFixture]
+    public class rename_quality_profiles_add_upgrade_allowedFixture : MigrationTest<rename_quality_profiles_add_upgrade_allowed>
+    {
+        [Test]
+        public void should_handle_injected_radarr_migration()
+        {
+            var dbBefore = WithTestDb(new MigrationContext(MigrationType, 110));
+
+            // Ensure 111 isn't applied
+            dbBefore.GetDirectDataMapper().Query("INSERT INTO VersionInfo (Version, AppliedOn, Description) VALUES (111, '2018-12-24T18:21:07', 'remove_bitmetv')");
+
+            var dbAfter = WithMigrationTestDb();
+
+            var result = dbAfter.QueryScalar<int>("SELECT COUNT(*) FROM VersionInfo WHERE Description = 'remove_bitmetv'");
+
+            result.Should().Be(0);
+        }
+    }
+}
