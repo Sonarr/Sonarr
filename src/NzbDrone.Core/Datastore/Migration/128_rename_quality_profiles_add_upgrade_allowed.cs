bece53e3c5b43b4a32867a47c214cd7fe74d3df2
Taloth Saldono <Taloth@users.noreply.github.com>
2019-12-05 23:59:36 +0100

    Fixed: Rare scenario where early Radarr version messes up Sonarr database

--- src/NzbDrone.Core/Datastore/Migration/128_rename_quality_profiles_add_upgrade_allowed.cs
+++ src/NzbDrone.Core/Datastore/Migration/128_rename_quality_profiles_add_upgrade_allowed.cs
@@ -1,13 +1,20 @@
 using FluentMigrator;
+using FluentMigrator.Infrastructure;
 using NzbDrone.Core.Datastore.Migration.Framework;
+using System.Data;
+using System.Data.SQLite;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
     [Migration(128)]
     public class rename_quality_profiles_add_upgrade_allowed : NzbDroneMigrationBase
     {
+        private IMigrationContext _capturedContext;
+
         protected override void MainDbUpgrade()
         {
+            FixupMigration111();
+
             Rename.Table("Profiles").To("QualityProfiles");
 
             Alter.Table("QualityProfiles").AddColumn("UpgradeAllowed").AsInt32().Nullable();
@@ -18,6 +25,39 @@ namespace NzbDrone.Core.Datastore.Migration
             Update.Table("LanguageProfiles").Set(new { UpgradeAllowed = true }).AllRows();
 
             Rename.Column("ProfileId").OnTable("Series").To("QualityProfileId");
+        }
+
+        public override void GetUpExpressions(IMigrationContext context)
+        {
+            _capturedContext = context;
+
+            base.GetUpExpressions(context);
+
+            _capturedContext = null;
+        }
+
+        // Early Radarr versions can mess up Sonarr's database if they point to the same config. Fixup the damage.        
+        private void FixupMigration111()
+        {
+            // In order to get the expressions we need to check the database directly
+            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText = "SELECT Description FROM VersionInfo WHERE Version = 111";
+
+                var description = command.ExecuteScalar() as string;
+                connection.Close();
+
+                if (description == "remove_bitmetv")
+                {
+                    // Get the migration expressions from the 111 migration
+                    var migration111 = new create_language_profiles();
+                    migration111.GetUpExpressions(_capturedContext);
+
+                    Execute.Sql("UPDATE VersionInfo SET Description = 'create_language_profiles fixup' WHERE Version = 111");
+                }
+            }
         }
     }
 }
