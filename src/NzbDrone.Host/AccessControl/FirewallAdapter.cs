7bcfa3d7b2054e1ad891a763ec998a4c0ec73de9
Taloth Saldono <Taloth@users.noreply.github.com>
2019-12-25 00:41:06 +0100

    Updated Interop.NetFwTypeLib to x64

--- src/NzbDrone.Host/AccessControl/FirewallAdapter.cs
+++ src/NzbDrone.Host/AccessControl/FirewallAdapter.cs
@@ -31,13 +31,13 @@ namespace NzbDrone.Host.AccessControl
             {
                 if (!IsNzbDronePortOpen(_configFileProvider.Port))
                 {
-                    _logger.Debug("Opening Port for NzbDrone: {0}", _configFileProvider.Port);
+                    _logger.Debug("Opening Port for Sonarr: {0}", _configFileProvider.Port);
                     OpenFirewallPort(_configFileProvider.Port);
                 }
 
                 if (_configFileProvider.EnableSsl && !IsNzbDronePortOpen(_configFileProvider.SslPort))
                 {
-                    _logger.Debug("Opening SSL Port for NzbDrone: {0}", _configFileProvider.SslPort);
+                    _logger.Debug("Opening SSL Port for Sonarr: {0}", _configFileProvider.SslPort);
                     OpenFirewallPort(_configFileProvider.SslPort);
                 }
             }
