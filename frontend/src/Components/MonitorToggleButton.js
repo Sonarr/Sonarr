c615ef476aad3a4fecfcfcf21928e07e84636003
Mark McDowall <mark@mcdowall.ca>
2020-01-31 17:24:59 -0800

    Fixed: Typo in unmonitored series tooltip
    Fixes #3538

--- frontend/src/Components/MonitorToggleButton.js
+++ frontend/src/Components/MonitorToggleButton.js
@@ -7,7 +7,7 @@ import styles from './MonitorToggleButton.css';
 
 function getTooltip(monitored, isDisabled) {
   if (isDisabled) {
-    return 'Cannot toogle monitored state when series is unmonitored';
+    return 'Cannot toggle monitored state when series is unmonitored';
   }
 
   if (monitored) {
