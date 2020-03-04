5b0fe1a190318eefdf25b5cbd812ce3ff994a587
Mark McDowall <mark@mcdowall.ca>
2020-03-03 20:00:01 -0800

    Don't re-trigger completed event

--- src/NzbDrone.Core/Download/DownloadProcessingService.cs
+++ src/NzbDrone.Core/Download/DownloadProcessingService.cs
@@ -33,14 +33,6 @@ namespace NzbDrone.Core.Download
             _logger = logger;
         }
 
-        private void RemoveCompletedDownloads(List<TrackedDownload> trackedDownloads)
-        {
-            foreach (var trackedDownload in trackedDownloads.Where(c => c.DownloadItem.CanBeRemoved && c.State == TrackedDownloadState.Imported))
-            {
-                _eventAggregator.PublishEvent(new DownloadCompletedEvent(trackedDownload));
-            }
-        }
-
         public void Execute(ProcessMonitoredDownloadsCommand message)
         {
             var enableCompletedDownloadHandling = _configService.EnableCompletedDownloadHandling;
@@ -64,13 +56,6 @@ namespace NzbDrone.Core.Download
                 {
                     _logger.Debug(e, "Failed to process download: {0}", trackedDownload.DownloadItem.Title);
                 }
-
-            }
-
-            if (enableCompletedDownloadHandling && _configService.RemoveCompletedDownloads)
-            {
-                // Remove tracked downloads that are now complete
-                RemoveCompletedDownloads(trackedDownloads);
             }
 
             _eventAggregator.PublishEvent(new DownloadsProcessedEvent());
