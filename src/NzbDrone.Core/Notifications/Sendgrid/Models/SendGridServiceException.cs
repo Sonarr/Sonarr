5fe74083b49f3b85d8872f17f29f8b42bc7c838c
Skyler Mäntysaari <samip5@users.noreply.github.com>
2020-04-03 23:18:34 +0300

    Added dependency on Sendgrid and implemented it.
    However actual authenication to SendGrid does not yet work so WIP.

---
+++ src/NzbDrone.Core/Notifications/Sendgrid/Models/SendGridServiceException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NzbDrone.Core.Notifications.Sendgrid.Models
+{
+    public class SendGridServiceException : Exception
+    {
+        public string Body { get; private set; }
+
+        public SendGridServiceException(string message, string body) : base (message)
+        {
+            Body = body;
+        }
+
+        public SendGridServiceException(string message, string body, Exception innerException) : base(message,
+            innerException)
+        {
+            Body = body;
+        }
+    }
+
+    public class SendGridResponse
+    {
+        public DateTime DateSent { get; internal set; }
+        public string UniqueMessageId { get; internal set; }
+    }
+}
---
+++ src/NzbDrone.Core/Notifications/Sendgrid/SendgridSettings.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.Notifications.Sendgrid
+{
+
+    public class SendgridSettingsValidator : AbstractValidator<SendgridSettings>
+    {
+        public SendgridSettingsValidator()
+        {
+            RuleFor(c => SendgridSettings.ApiKey).NotEmpty();
+            RuleFor(c => c.From).NotEmpty();
+            RuleFor(c => c.To).NotEmpty();
+        }
+    }
+    
+    public class SendgridSettings : IProviderConfig {
+        
+        private static SendgridSettingsValidator Validator = new SendgridSettingsValidator();
+
+
+        public SendgridSettings() { }
+        
+        [FieldDefinition(0, Label = "Api Key", HelpText = "The API key for SendGrid")]
+        public static string ApiKey { get; set; }
+        
+        [FieldDefinition(1, Label = "From Address")]
+        public string From { get; set; }
+        
+        [FieldDefinition(2, Label= "Recipient Address")]
+        public string To {get; set; }
+        
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
