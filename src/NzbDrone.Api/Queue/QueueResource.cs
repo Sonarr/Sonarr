37969b3a2d63b037e743afe87e380d46c63d6c62
Devin Buhl <onedr0p@users.noreply.github.com>
2020-04-06 10:53:50 -0400

    reorder objects

--- src/NzbDrone.Api/Queue/QueueResource.cs
+++ src/NzbDrone.Api/Queue/QueueResource.cs
@@ -20,8 +20,8 @@ namespace NzbDrone.Api.Queue
         public decimal Sizeleft { get; set; }
         public TimeSpan? Timeleft { get; set; }
         public DateTime? EstimatedCompletionTime { get; set; }
-        public string Status { get; set; }
         public string TrackedDownloadState { get; set; }
+        public string Status { get; set; }
         public string TrackedDownloadStatus { get; set; }
         public List<TrackedDownloadStatusMessage> StatusMessages { get; set; }
         public string DownloadId { get; set; }
@@ -46,8 +46,8 @@ namespace NzbDrone.Api.Queue
                 Sizeleft = model.Sizeleft,
                 Timeleft = model.Timeleft,
                 EstimatedCompletionTime = model.EstimatedCompletionTime,
-                Status = model.Status,
                 TrackedDownloadState = model.TrackedDownloadState.ToString(),
+                Status = model.Status,
                 TrackedDownloadStatus = model.TrackedDownloadStatus.ToString(),
                 StatusMessages = model.StatusMessages,
                 DownloadId = model.DownloadId,
