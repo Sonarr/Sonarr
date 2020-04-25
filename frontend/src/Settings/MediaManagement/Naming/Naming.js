d25ac2e1f2acaa8eaed2f6b0e28c382efd80a125
rg9400 <39887349+rg9400@users.noreply.github.com>
2020-04-25 11:49:27 -0500

    Remove simply

--- frontend/src/Settings/MediaManagement/Naming/Naming.js
+++ frontend/src/Settings/MediaManagement/Naming/Naming.js
@@ -226,7 +226,7 @@ class Naming extends Component {
                 <FormInputGroup
                   type={inputTypes.CHECK}
                   name="replaceIllegalCharacters"
-                  helpText="Replace illegal characters. If unchecked, Sonarr will simply remove them instead"
+                  helpText="Replace illegal characters. If unchecked, Sonarr will remove them instead"
                   onChange={onInputChange}
                   {...settings.replaceIllegalCharacters}
                 />
