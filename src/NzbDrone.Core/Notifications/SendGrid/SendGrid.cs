93e3e92bba23910c8aff47913b5176a38bb26bfb
Skyler Mäntysaari <samip5@users.noreply.github.com>
2020-05-20 21:22:05 +0300

    New: SendGrid Notifications
    Closes #3341

---
+++ src/NzbDrone.Core/Notifications/SendGrid/SendGrid.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NLog;
+
+namespace NzbDrone.Core.Notifications.SendGrid
+{
+    public class SendGrid : NotificationBase<SendGridSettings>
+    {
+        private readonly ISendGridProxy _proxy;
+        private readonly Logger _logger;
+
+        public SendGrid(ISendGridProxy proxy, Logger logger)
+        {
+            _proxy = proxy;
+            _logger = logger;
+        }
+
+        public override string Name => "SendGrid";
+        public override string Link => "https://sendgrid.com/";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(EPISODE_GRABBED_TITLE, grabMessage.Message, Settings);
+        }
+
+        public override void OnDownload(DownloadMessage message)
+        {
+            _proxy.SendNotification(EPISODE_DOWNLOADED_TITLE, message.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheck.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            try
+            {
+                const string title = "Test Notification";
+                const string body = "This is a test message from Sonarr";
+
+                _proxy.SendNotification(title, body, Settings);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to send test message");
+                failures.Add(new ValidationFailure("", "Unable to send test message"));
+            }
+
+            return new ValidationResult(failures);
+        }
+    }
+}
