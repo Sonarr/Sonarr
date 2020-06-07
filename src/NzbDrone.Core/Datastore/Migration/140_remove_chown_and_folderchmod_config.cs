683bf823270838521523c432c9801272350c6623
Arthur Bols <arthur@bols.dev>
2020-06-07 13:41:36 +0200

    Update migration to remove chown config

--- src/NzbDrone.Core/Datastore/Migration/140_remove_chown_and_folderchmod_config.cs
+++ src/NzbDrone.Core/Datastore/Migration/140_remove_chown_and_folderchmod_config.cs
@@ -5,11 +5,13 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 namespace NzbDrone.Core.Datastore.Migration
 {
     [Migration(140)]
-    public class remove_folderchmod_config : NzbDroneMigrationBase
+    public class remove_chown_and_folderchmod_config : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
         {
             Execute.Sql("DELETE FROM Config WHERE Key = 'folderchmod'");
+            Execute.Sql("DELETE FROM Config WHERE Key = 'chownuser'");
+            Execute.Sql("DELETE FROM Config WHERE KEY = 'chowngroup'");
         }
     }
 }
