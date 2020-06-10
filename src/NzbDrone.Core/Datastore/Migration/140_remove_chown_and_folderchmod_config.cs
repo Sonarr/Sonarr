200e263f1f615cec47f337b7f5d3f5bb3fe821f7
Taloth Saldono <Taloth@users.noreply.github.com>
2020-06-10 17:40:26 +0200

    Prevent deletion of chowngroup we may need it later

--- src/NzbDrone.Core/Datastore/Migration/140_remove_chown_and_folderchmod_config.cs
+++ src/NzbDrone.Core/Datastore/Migration/140_remove_chown_and_folderchmod_config.cs
@@ -5,11 +5,12 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 namespace NzbDrone.Core.Datastore.Migration
 {
     [Migration(140)]
-    public class remove_chown_and_folderchmod_config : NzbDroneMigrationBase
+    public class remove_chown_and_folderchmod_config_v2 : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
         {
-            Execute.Sql("DELETE FROM config WHERE Key IN ('folderchmod', 'chownuser', 'chowngroup')");
+            Execute.Sql("DELETE FROM config WHERE Key IN ('folderchmod', 'chownuser')");
+            // Note: v1 version of migration removed 'chowngroup'
         }
     }
 }
