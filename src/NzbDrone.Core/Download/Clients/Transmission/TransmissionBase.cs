66caec31c9b14d029c205f4d855760f985b52bd7
ta264 <ta264@users.noreply.github.com>
2020-06-28 22:09:13 +0100

    Fixed: False positive in remote path check with transmission
    Correctly use the download directory when it's set
    
    (cherry picked from commit e09ca145d125a12016a6bf41a4971148bd2de870)

--- src/NzbDrone.Core/Download/Clients/Transmission/TransmissionBase.cs
+++ src/NzbDrone.Core/Download/Clients/Transmission/TransmissionBase.cs
@@ -171,12 +171,21 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 
         public override DownloadClientInfo GetStatus()
         {
-            var config = _proxy.GetConfig(Settings);
-            var destDir = config.DownloadDir;
+            string destDir;
 
-            if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+            if (Settings.TvDirectory.IsNotNullOrWhiteSpace())
             {
-                destDir = string.Format("{0}/{1}", destDir, Settings.TvCategory);
+                destDir = Settings.TvDirectory;
+            }
+            else
+            {
+                var config = _proxy.GetConfig(Settings);
+                destDir = config.DownloadDir;
+
+                if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+                {
+                    destDir = $"{destDir}/{Settings.TvCategory}";
+                }
             }
 
             return new DownloadClientInfo
