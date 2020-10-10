164f46e4c0f7a8a6002028da8e7482100e8c7039
Mark McDowall <mark@mcdowall.ca>
2020-10-10 10:15:48 -0700

    Fixed: Webhooks using lower case event types (in the future this could change)
    Closes #4010

--- src/NzbDrone.Core/Notifications/Webhook/WebhookEventType.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookEventType.cs
@@ -1,5 +1,11 @@
-﻿namespace NzbDrone.Core.Notifications.Webhook
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace NzbDrone.Core.Notifications.Webhook
 {
+    // TODO: In v4 this will likely be changed to the default camel case.
+    [JsonConverter(typeof(StringEnumConverter), converterParameters: typeof(DefaultNamingStrategy))]
     public enum WebhookEventType
     {
         Test,
