5fced70948c1e8d3d5e3c4747516507f87506697
Taloth Saldono <Taloth@users.noreply.github.com>
2020-11-16 21:24:55 +0100

    Give systemd a bit more time to restart sonarr after update

--- src/NzbDrone.Update/UpdateEngine/InstallUpdateService.cs
+++ src/NzbDrone.Update/UpdateEngine/InstallUpdateService.cs
@@ -157,7 +157,7 @@ namespace NzbDrone.Update.UpdateEngine
                     _terminateNzbDrone.Terminate(processId);
 
                     _logger.Info("Waiting for external auto-restart.");
-                    for (int i = 0; i < 5; i++)
+                    for (int i = 0; i < 10; i++)
                     {
                         System.Threading.Thread.Sleep(1000);
 
