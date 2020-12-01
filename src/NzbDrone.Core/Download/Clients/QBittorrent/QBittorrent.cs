68791ea98a3da1d8efaeb6a4442493a8944f2e97
ta264 <ta264@users.noreply.github.com>
2020-12-01 21:20:21 +0000

    Fixed: Use QBittorrent category savepath for healthcheck
    (cherry picked from commit 4f281669fcd460efa4191590de31da12c7e4271d)

--- src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
+++ src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -360,9 +360,29 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
         public override DownloadClientInfo GetStatus()
         {
+            var version = Proxy.GetApiVersion(Settings);
             var config = Proxy.GetConfig(Settings);
 
             var destDir = new OsPath(config.SavePath);
+
+            if (Settings.TvCategory.IsNotNullOrWhiteSpace() && version >= Version.Parse("2.0"))
+            {
+                var label = Proxy.GetLabels(Settings)[Settings.TvCategory];
+
+                if (label.SavePath.IsNotNullOrWhiteSpace())
+                {
+                    var labelDir = new OsPath(label.SavePath);
+
+                    if (labelDir.IsRooted)
+                    {
+                        destDir = labelDir;
+                    }
+                    else
+                    {
+                        destDir = destDir + labelDir;
+                    }
+                }
+            }
 
             return new DownloadClientInfo
             {
