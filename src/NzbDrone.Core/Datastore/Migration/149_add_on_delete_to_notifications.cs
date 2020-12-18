deb4c299e4980713289021d71153ae7dd707c45b
Robin Dadswell <robin@dadswell.email>
2020-12-18 23:13:54 +0000

    incremented database migration due to mediainfo PR

--- src/NzbDrone.Core/Datastore/Migration/149_add_on_delete_to_notifications.cs
+++ src/NzbDrone.Core/Datastore/Migration/149_add_on_delete_to_notifications.cs
@@ -3,7 +3,7 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
-    [Migration(148)]
+    [Migration(149)]
     public class add_on_delete_to_notifications : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
