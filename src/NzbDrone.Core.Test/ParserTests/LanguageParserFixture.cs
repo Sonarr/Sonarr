7b04e11c5487f6d981ab320246cdc79ba6023e0f
Nathaniel Peiffer <nathaniel@peiffer.com.au>
2020-12-18 11:55:07 +1100

    Fixed: Language parsing with space-delimited releases
    Closes #4056

--- src/NzbDrone.Core.Test/ParserTests/LanguageParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/LanguageParserFixture.cs
@@ -19,6 +19,9 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("2 Broke Girls - S01E01 - Pilot.eng.sub")]
         [TestCase("2 Broke Girls - S01E01 - Pilot.English.sub")]
         [TestCase("2 Broke Girls - S01E01 - Pilot.english.sub")]
+        [TestCase("The Spanish Princess S02E02 Flodden 720p AMZN WEB-DL DDP5 1 H 264-NTb")]
+        [TestCase("The.Spanish.Princess.S02E02.1080p.WEB.H264-CAKES")]
+        [TestCase("The.Spanish.Princess.S02E06.Field.of.Cloth.of.Gold.1080p.AMZN.WEBRip.DDP5.1.x264-NTb")]
         public void should_parse_language_english(string postTitle)
         {
             var result = LanguageParser.ParseLanguage(postTitle);
@@ -223,6 +226,8 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Russian.Puppets.S01E07.Cold.Action.HDTV.XviD-Droned")]
         [TestCase("Russian.Puppets.S01E07E08.Cold.Action.HDTV.XviD-Droned")]
         [TestCase("Russian.Puppets.S01.1080p.WEBRip.DDP5.1.x264-Drone")]
+        [TestCase("The.Spanish.Princess.S02E08.Peace.1080p.AMZN.WEBRip.DDP5.1.x264-NTb")]
+        [TestCase("The Spanish Princess S02E02 Flodden 720p AMZN WEB-DL DDP5 1 H 264-NTb")]
         public void should_not_parse_series_or_episode_title(string postTitle)
         {
             var result = Parser.Parser.ParseTitle(postTitle);
