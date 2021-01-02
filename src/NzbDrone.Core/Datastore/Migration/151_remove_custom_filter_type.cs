2c1fd7addbc66a860e50bbb678143878feb5d18a
Robin Dadswell <robin@dadswell.email>
2021-01-02 01:01:47 +0000

    filter migration and utilisation of filters across 3 pages

---
+++ src/NzbDrone.Core/Datastore/Migration/151_remove_custom_filter_type.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(151)]
+    public class remove_custom_filter_type : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Update.Table("CustomFilters").Set(new { Type = "series" }).Where(new { Type = "seriesIndex" });
+            Update.Table("CustomFilters").Set(new { Type = "series" }).Where(new { Type = "seriesEditor" });
+            Update.Table("CustomFilters").Set(new { Type = "series" }).Where(new { Type = "seasonPass" });
+        }
+    }
+}
