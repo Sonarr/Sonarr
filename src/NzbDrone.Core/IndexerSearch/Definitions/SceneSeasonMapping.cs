efb18223fed0d51843412f9d30969a654c81c186
Taloth Saldono <Taloth@users.noreply.github.com>
2021-01-16 01:32:08 +0100

    Fixed duplicate id searches due to missing Equals on SceneSeasonMapping

--- src/NzbDrone.Core/IndexerSearch/Definitions/SceneSeasonMapping.cs
+++ src/NzbDrone.Core/IndexerSearch/Definitions/SceneSeasonMapping.cs
@@ -11,5 +11,19 @@ namespace NzbDrone.Core.IndexerSearch.Definitions
         public SearchMode SearchMode { get; set; }
         public List<string> SceneTitles { get; set; }
         public int SeasonNumber { get; set; }
+
+        public override int GetHashCode()
+        {
+            return SearchMode.GetHashCode() ^ SeasonNumber.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as SceneSeasonMapping;
+
+            if (object.ReferenceEquals(other, null)) return false;
+
+            return SeasonNumber == other.SeasonNumber && SearchMode == other.SearchMode;
+        }
     }
 }
