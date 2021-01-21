28e0ad38b095c7db89ba4e18a678759c8d567d6f
Jesse Chan <jc@linux.com>
2021-01-21 15:43:52 +0800

    New: Flood Download Client

---
+++ src/NzbDrone.Core/Download/Clients/Flood/Models/AdditionalTags.cs
@@ -0,0 +1,28 @@
+using NzbDrone.Core.Annotations;
+
+namespace NzbDrone.Core.Download.Clients.Flood.Models
+{
+    public enum AdditionalTags
+    {
+        [FieldOption(Hint = "big-buck-bunny")]
+        TitleSlug = 0,
+
+        [FieldOption(Hint = "Bluray-2160p")]
+        Quality = 1,
+
+        [FieldOption(Hint = "English")]
+        Language = 2,
+
+        [FieldOption(Hint = "Example-Raws")]
+        ReleaseGroup = 3,
+
+        [FieldOption(Hint = "2020")]
+        Year = 4,
+
+        [FieldOption(Hint = "Torznab")]
+        Indexer = 5,
+
+        [FieldOption(Hint = "C-SPAN")]
+        Network = 6
+    }
+}
