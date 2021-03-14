402178bdb472759ef8729622655153828ed1225e
LLeny <LLeny@users.noreply.github.com>
2021-03-14 04:23:11 -0700

    Aria2 torrent client

---
+++ src/NzbDrone.Core/Download/Clients/Aria2/Aria2.cs
@@ -0,0 +1,222 @@
+﻿using CookComputing.XmlRpc;
+using FluentValidation.Results;
+using NLog;
+using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.MediaFiles.TorrentInfo;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.RemotePathMappings;
+using NzbDrone.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace NzbDrone.Core.Download.Clients.Aria2
+{
+    public class Aria2 : TorrentClientBase<Aria2Settings>
+    {
+        private readonly IAria2Proxy _proxy;
+
+        public override string Name => "Aria2";
+
+        public Aria2(IAria2Proxy proxy,
+                        ITorrentFileInfoReader torrentFileInfoReader,
+                        IHttpClient httpClient,
+                        IConfigService configService,
+                        IDiskProvider diskProvider,
+                        IRemotePathMappingService remotePathMappingService,
+                        Logger logger)
+            : base(torrentFileInfoReader, httpClient, configService, diskProvider, remotePathMappingService, logger)
+        {
+            _proxy = proxy; ;
+        }
+
+        protected override string AddFromMagnetLink(RemoteEpisode remoteEpisode, string hash, string magnetLink)
+        {
+            var gid = _proxy.AddMagnet(Settings, magnetLink);
+
+            var tries = 10;
+            var retryDelay = 500;
+
+            // Wait a bit for the magnet to be resolved.
+            if (!WaitForTorrent(gid, hash, tries, retryDelay))
+            {
+                _logger.Warn($"Aria2 could not add magnent within {tries * retryDelay / 1000} seconds, download may remain stuck: {magnetLink}.");
+                return hash;
+            }
+
+            _logger.Debug($"Äria2 AddFromMagnetLink '{hash}' -> '{gid}'");
+
+            return hash;
+        }
+
+        protected override string AddFromTorrentFile(RemoteEpisode remoteEpisode, string hash, string filename, byte[] fileContent)
+        {
+            var gid = _proxy.AddTorrent(Settings, fileContent);
+
+            var tries = 10;
+            var retryDelay = 500;
+
+            // Wait a bit for the magnet to be resolved.
+            if (!WaitForTorrent(gid, hash, tries, retryDelay))
+            {
+                _logger.Warn($"Aria2 could not add torrent within {tries * retryDelay / 1000} seconds, download may remain stuck: {filename}.");
+                return hash;
+            }
+
+            return hash;
+        }
+
+        public override IEnumerable<DownloadClientItem> GetItems()
+        {
+            var torrents = _proxy.GetTorrents(Settings);
+
+            foreach(var torrent in torrents)
+            {
+                var firstFile = torrent.Files?.FirstOrDefault();
+
+                if (firstFile?.Path?.Contains("[METADATA]") == true) //skip metadata download
+                {
+                    continue;
+                }
+
+                long completedLength = long.Parse(torrent.CompletedLength);
+                long totalLength = long.Parse(torrent.TotalLength);
+                long uploadedLength = long.Parse(torrent.UploadLength);
+                long downloadSpeed = long.Parse(torrent.DownloadSpeed);
+
+                var sta = DownloadItemStatus.Failed;
+                var title = "";
+
+                if(torrent.Bittorrent?.ContainsKey("info") == true && ((XmlRpcStruct)torrent.Bittorrent["info"]).ContainsKey("name"))
+                {
+                    title = ((XmlRpcStruct)torrent.Bittorrent["info"])["name"].ToString();
+                }
+
+                switch (torrent.Status)
+                {
+                    case "active":
+                        sta = DownloadItemStatus.Downloading;
+                        break;
+                    case "waiting":
+                        sta = DownloadItemStatus.Queued;
+                        break;
+                    case "paused":
+                        sta = DownloadItemStatus.Paused;
+                        break;
+                    case "error":
+                        sta = DownloadItemStatus.Failed;
+                        break;
+                    case "complete":
+                        sta = DownloadItemStatus.Completed;
+                        break;
+                    case "removed":
+                        sta = DownloadItemStatus.Failed;
+                        break;
+                }
+
+                _logger.Debug($"- aria2 getstatus hash:'{torrent.InfoHash}' gid:'{torrent.Gid}' sta:'{sta}' tot:{totalLength} comp:'{completedLength}'");
+
+                yield return new DownloadClientItem()
+                {
+                    CanMoveFiles = false,
+                    CanBeRemoved = true,
+                    Category = null,
+                    DownloadClientInfo = DownloadClientItemClientInfo.FromDownloadClient(this),
+                    DownloadId = torrent.InfoHash?.ToUpper(),
+                    IsEncrypted = false,
+                    Message = torrent.ErrorMessage,
+                    OutputPath = _remotePathMappingService.RemapRemoteToLocal(Settings.Host, new OsPath(torrent.Dir)),
+                    RemainingSize = totalLength - completedLength,
+                    RemainingTime = downloadSpeed == 0 ? (TimeSpan?)null : new TimeSpan(0,0, (int)((totalLength - completedLength) / downloadSpeed)),
+                    Removed = torrent.Status == "removed",
+                    SeedRatio = totalLength > 0 ? (double)uploadedLength / totalLength : 0,
+                    Status = sta,
+                    Title = title,
+                    TotalSize = totalLength,
+                };              
+            }
+        }
+
+        public override void RemoveItem(string hash, bool deleteData)
+        {
+            //Aria2 doesn't support file deletion at this point: https://github.com/aria2/aria2/issues/728
+
+            foreach(var torrent in _proxy.GetTorrents(Settings))
+            {
+                if(torrent.InfoHash?.ToLower() == hash.ToLower())
+                {
+                    _logger.Debug($"Aria2 removing hash:'{hash}' gid:'{torrent.Gid}'");
+                    _proxy.RemoveTorrent(Settings, torrent.Gid);
+                    return;
+                }
+            }
+
+            _logger.Error($"Aria2 could not find infoHash {hash} for deletion.");
+        }
+
+        public override DownloadClientInfo GetStatus()
+        {
+            var destDir = _proxy.GetGlobals(Settings);
+
+            return new DownloadClientInfo
+            {
+                IsLocalhost = Settings.Host.Contains("127.0.0.1") || Settings.Host.Contains("localhost"),
+                OutputRootFolders = new List<OsPath> { _remotePathMappingService.RemapRemoteToLocal(Settings.Host, new OsPath(destDir["dir"])) }
+            };
+        }
+
+        private bool WaitForTorrent(string gid, string hash, int tries, int retryDelay)
+        {
+            for (var i = 0; i < tries; i++)
+            {
+                var found = _proxy.GetFromGID(Settings, gid);
+
+                if (found?.InfoHash?.ToLower() == hash?.ToLower())
+                {
+                    return true;
+                }
+
+                Thread.Sleep(retryDelay);
+            }
+
+            _logger.Debug("Could not find hash {0} in {1} tries at {2} ms intervals.", hash, tries, retryDelay);
+
+            return false;
+        }
+
+        protected override void Test(List<ValidationFailure> failures)
+        {
+            failures.AddIfNotNull(TestConnection());
+
+            if (failures.HasErrors()) return;
+        }
+
+        private ValidationFailure TestConnection()
+        {
+            try
+            {
+                var version = _proxy.GetVersion(Settings);
+
+                if (new Version(version) < new Version("1.34.0"))
+                {
+                    return new ValidationFailure(string.Empty, "Aria2 version should be at least 1.34.0. Version reported is {0}", version);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Failed to test Aria2");
+
+                return new NzbDroneValidationFailure("Host", "Unable to connect to Aria2")
+                {
+                    DetailedDescription = ex.Message
+                };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Download/Clients/Aria2/Aria2Proxy.cs
@@ -0,0 +1,210 @@
+﻿using CookComputing.XmlRpc;
+using NLog;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+
+namespace NzbDrone.Core.Download.Clients.Aria2
+{
+    public interface IAria2Proxy
+    {
+        string GetVersion(Aria2Settings settings);
+        string AddMagnet(Aria2Settings settings, string magnet);
+        string AddTorrent(Aria2Settings settings, byte[] torrent);
+        bool RemoveTorrent(Aria2Settings settings, string gid);
+        Dictionary<string, string> GetGlobals(Aria2Settings settings);
+        Aria2Status[] GetTorrents(Aria2Settings settings);
+        Aria2Status GetFromGID(Aria2Settings settings, string gid);
+    }
+
+    public interface IAria2 : IXmlRpcProxy
+    {
+        [XmlRpcMethod("aria2.getVersion")]
+        Aria2Version GetVersion(string token);
+
+        [XmlRpcMethod("aria2.addUri")]
+        string AddUri(string token, string[] uri);
+
+        [XmlRpcMethod("aria2.addTorrent")]
+        string AddTorrent(string token, string base64Torrent);
+
+        [XmlRpcMethod("aria2.forceRemove")]
+        string Remove(string token, string gid);
+
+        [XmlRpcMethod("aria2.tellStatus")]
+        Aria2Status GetFromGid(string token, string gid);
+
+        [XmlRpcMethod("aria2.getGlobalOption")]
+        XmlRpcStruct GetGlobalOption(string token);
+
+        [XmlRpcMethod("aria2.tellActive")]
+        Aria2Status[] GetActives(string token);
+
+        [XmlRpcMethod("aria2.tellWaiting")]
+        Aria2Status[] GetWaitings(string token, int offset, int num);
+
+        [XmlRpcMethod("aria2.tellStopped")]
+        Aria2Status[] GetStoppeds(string token, int offset, int num); 
+    }
+
+    public class Aria2Proxy : IAria2Proxy
+    {
+        private readonly Logger _logger;
+
+        public Aria2Proxy(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        private string GetToken(Aria2Settings settings)
+        {
+            return $"token:{settings?.SecretToken}";
+        }
+
+        private string GetURL(Aria2Settings settings)
+        {
+            return $"http{(settings.UseSsl ? "s" : "")}://{settings.Host}:{settings.Port}{settings.RpcPath}";
+        }
+
+        public string GetVersion(Aria2Settings settings)
+        {
+            _logger.Debug("> aria2.getVersion");
+
+            var client = BuildClient(settings);
+            var version = ExecuteRequest(() => client.GetVersion(GetToken(settings)));
+
+            _logger.Debug("< aria2.getVersion");
+
+            return version.Version;
+        }
+
+        public Aria2Status GetFromGID(Aria2Settings settings, string gid)
+        {
+            _logger.Debug("> aria2.tellStatus");
+
+            var client = BuildClient(settings);
+            var found = ExecuteRequest(() => client.GetFromGid(GetToken(settings), gid));
+
+            _logger.Debug("< aria2.tellStatus");
+
+            return found;
+        }
+
+
+        public Aria2Status[] GetTorrents(Aria2Settings settings)
+        {
+            _logger.Debug("> aria2.tellActive");
+
+            var client = BuildClient(settings);
+
+            var actives = ExecuteRequest(() => client.GetActives(GetToken(settings)));
+
+            _logger.Debug("< aria2.tellActive");
+
+            _logger.Debug("> aria2.tellWaiting");
+
+            var waitings = ExecuteRequest(() => client.GetWaitings(GetToken(settings), 1, 10*1024));
+
+            _logger.Debug("< aria2.tellWaiting");
+
+            _logger.Debug("> aria2.tellStopped");
+
+            var stoppeds = ExecuteRequest(() => client.GetStoppeds(GetToken(settings), 1, 10*1024));
+
+            _logger.Debug("< aria2.tellStopped");
+
+            var ret = new List<Aria2Status>();
+
+            ret.AddRange(actives);
+            ret.AddRange(waitings);
+            ret.AddRange(stoppeds);
+            
+            return ret.ToArray();
+        }
+
+        public Dictionary<string, string> GetGlobals(Aria2Settings settings)
+        {
+            _logger.Debug("> aria2.getGlobalOption");
+
+            var client = BuildClient(settings);
+            var options = ExecuteRequest(() => client.GetGlobalOption(GetToken(settings)));
+
+            _logger.Debug("< aria2.getGlobalOption");
+
+            var ret = new Dictionary<string, string>();
+
+            foreach(DictionaryEntry option in options)
+            {                
+                ret.Add(option.Key.ToString(), option.Value?.ToString());
+            }
+
+            return ret;
+        }
+
+        public string AddMagnet(Aria2Settings settings, string magnet)
+        {
+            _logger.Debug("> aria2.addUri");
+
+            var client = BuildClient(settings);
+            var gid = ExecuteRequest(() => client.AddUri(GetToken(settings), new[] { magnet }));
+
+            _logger.Debug("< aria2.addUri");
+
+            return gid;
+        }
+
+        public string AddTorrent(Aria2Settings settings, byte[] torrent)
+        {
+            _logger.Debug("> aria2.addTorrent");
+
+            var client = BuildClient(settings);
+            var gid = ExecuteRequest(() => client.AddTorrent(GetToken(settings), Convert.ToBase64String(torrent)));
+
+            _logger.Debug("< aria2.addTorrent");
+
+            return gid;
+        }
+
+        public bool RemoveTorrent(Aria2Settings settings, string gid)
+        {
+            _logger.Debug("> aria2.forceRemove");
+
+            var client = BuildClient(settings);
+            var gidres = ExecuteRequest(() => client.Remove(GetToken(settings), gid));
+
+            _logger.Debug("< aria2.forceRemove");
+
+            return gid == gidres;
+        }
+
+        private IAria2 BuildClient(Aria2Settings settings)
+        {
+            var client = XmlRpcProxyGen.Create<IAria2>();
+            client.Url = GetURL(settings);
+
+            return client;
+        }
+
+        private T ExecuteRequest<T>(Func<T> task)
+        {
+            try
+            {
+                return task();
+            }
+            catch (XmlRpcServerException ex)
+            {
+                throw new DownloadClientException("Unable to connect to aria2, please check your settings", ex);
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.TrustFailure)
+                {
+                    throw new DownloadClientUnavailableException("Unable to connect to aria2, certificate validation failed.", ex);
+                }
+
+                throw new DownloadClientUnavailableException("Unable to connect to aria2, please check your settings", ex);
+            }
+        }
+    }
+}
