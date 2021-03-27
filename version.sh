a200dd5f6d927388feea5766757d86eed1738908
Taloth Saldono <Taloth@users.noreply.github.com>
2021-03-27 22:14:03 +0100

    Bump package version

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Increment packageVersion when package scripts change
-packageVersion='3.0.5'
+packageVersion='3.0.6'
 
 # For now we keep the build version and package version the same
 buildVersion=$packageVersion
