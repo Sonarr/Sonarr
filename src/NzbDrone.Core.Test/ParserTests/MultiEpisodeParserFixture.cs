f2422f814df50233d2836c2f5dbec77ea8841827
Mark McDowall <mark@mcdowall.ca>
2021-04-07 08:59:46 -0700

    Fixed: Parsing of some 360p multi-episode files

--- src/NzbDrone.Core.Test/ParserTests/MultiEpisodeParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/MultiEpisodeParserFixture.cs
@@ -70,6 +70,7 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Series Title - [02x01x02] - Episode 1", "Series Title", 2, new [] { 1, 2})]
         [TestCase("Series Title - [02x01-x02] - Episode 1", "Series Title", 2, new [] { 1, 2})]
         [TestCase("Series Title - [02x01-02] - Episode 1", "Series Title", 2, new [] { 1, 2})]
+        [TestCase("Series Title (2011) - S01E23-E24 - ...E i nuovi orizzonti [HDTV 360p] [ITA].mkv", "Series Title (2011)", 1, new [] { 23, 24 })]
         //[TestCase("", "", , new [] {  })]
         public void should_parse_multiple_episodes(string postTitle, string title, int season, int[] episodes)
         {
