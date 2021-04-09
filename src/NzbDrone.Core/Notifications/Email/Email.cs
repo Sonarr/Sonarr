5449389ca5295f80fe7d3e727a8f5c96abe146ae
Mark McDowall <mark@mcdowall.ca>
2021-04-09 10:38:16 -0700

    Set timeout for sending email
    Closes #4348

--- src/NzbDrone.Core/Notifications/Email/Email.cs
+++ src/NzbDrone.Core/Notifications/Email/Email.cs
@@ -103,6 +103,8 @@ namespace NzbDrone.Core.Notifications.Email
         {
             using (var client = new SmtpClient())
             {
+                client.Timeout = 10000;
+
                 var serverOption = SecureSocketOptions.Auto;
 
                 if (settings.RequireEncryption)
