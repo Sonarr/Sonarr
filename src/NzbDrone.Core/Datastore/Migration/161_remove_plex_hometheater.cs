91c9250d9c314889d720bb2cc636d4b64fad6a5b
Qstick <qstick@gmail.com>
2021-07-28 22:58:26 -0400

    Fixed: Remove obsolete Plex HomeTheater/Client notifcations
    Co-Authored-By: ta264 <ta264@users.noreply.github.com>

---
+++ src/NzbDrone.Core/Datastore/Migration/161_remove_plex_hometheater.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(161)]
+    public class remove_plex_hometheatre : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Delete.FromTable("Notifications").Row(new { Implementation = "PlexHomeTheater" });
+            Delete.FromTable("Notifications").Row(new { Implementation = "PlexClient" });
+        }
+    }
+}
