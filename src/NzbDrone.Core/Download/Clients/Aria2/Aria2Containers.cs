6fd31613c261148b7fcc46130ec6cbbc88a8b182
LLeny <LLeny@users.noreply.github.com>
2021-08-01 15:49:36 -0400

    New: Aria2 Torrent Client
    Closes #1374

---
+++ src/NzbDrone.Core/Download/Clients/Aria2/Aria2Containers.cs
@@ -0,0 +1,112 @@
+﻿using CookComputing.XmlRpc;
+
+namespace NzbDrone.Core.Download.Clients.Aria2
+{
+    public class Aria2Version
+    {
+        [XmlRpcMember("version")]
+        public string Version;
+
+        [XmlRpcMember("enabledFeatures")]
+        public string[] EnabledFeatures;
+    }
+
+    public class Aria2Uri
+    {
+        [XmlRpcMember("status")]
+        public string Status;
+
+        [XmlRpcMember("uri")]
+        public string Uri;
+    }
+
+    public class Aria2File
+    {
+        [XmlRpcMember("index")]
+        public string Index;
+
+        [XmlRpcMember("length")]
+        public string Length;
+
+        [XmlRpcMember("completedLength")]
+        public string CompletedLength;
+
+        [XmlRpcMember("path")]
+        public string Path;
+
+        [XmlRpcMember("selected")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string Selected;
+
+        [XmlRpcMember("uris")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public Aria2Uri[] Uris;
+    }
+
+    public class Aria2Status
+    {
+        [XmlRpcMember("bittorrent")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public XmlRpcStruct Bittorrent;
+
+        [XmlRpcMember("bitfield")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string Bitfield;
+
+        [XmlRpcMember("infoHash")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string InfoHash;
+
+        [XmlRpcMember("completedLength")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string CompletedLength;
+
+        [XmlRpcMember("connections")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string Connections;
+
+        [XmlRpcMember("dir")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string Dir;
+
+        [XmlRpcMember("downloadSpeed")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string DownloadSpeed;
+
+        [XmlRpcMember("files")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public Aria2File[] Files;
+
+        [XmlRpcMember("gid")]
+        public string Gid;
+
+        [XmlRpcMember("numPieces")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string NumPieces;
+
+        [XmlRpcMember("pieceLength")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string PieceLength;
+
+        [XmlRpcMember("status")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string Status;
+
+        [XmlRpcMember("totalLength")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string TotalLength;
+
+        [XmlRpcMember("uploadLength")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string UploadLength;
+
+        [XmlRpcMember("uploadSpeed")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string UploadSpeed;
+
+        [XmlRpcMember("errorMessage")]
+        [XmlRpcMissingMapping(MappingAction.Ignore)]
+        public string ErrorMessage;
+    }
+}
+
---
+++ src/NzbDrone.Core/Download/Clients/Aria2/Aria2Settings.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.Download.Clients.Aria2
+{
+    public class Aria2SettingsValidator : AbstractValidator<Aria2Settings>
+    {
+        public Aria2SettingsValidator()
+        {
+            RuleFor(c => c.Host).ValidHost();
+        }
+    }
+
+    public class Aria2Settings : IProviderConfig
+    {
+        private static readonly Aria2SettingsValidator Validator = new Aria2SettingsValidator();
+
+        public Aria2Settings()
+        {
+            Host = "localhost";
+            Port = 6800;
+            RpcPath = "/rpc";
+            UseSsl = false;
+            SecretToken = "MySecretToken";
+        }
+
+        [FieldDefinition(0, Label = "Host", Type = FieldType.Textbox)]
+        public string Host { get; set; }
+
+        [FieldDefinition(1, Label = "Port", Type = FieldType.Number)]
+        public int Port { get; set; }
+
+        [FieldDefinition(2, Label = "RPC Path", Type = FieldType.Textbox)]
+        public string RpcPath { get; set; }
+
+        [FieldDefinition(3, Label = "Use SSL", Type = FieldType.Checkbox)]
+        public bool UseSsl { get; set; }
+
+        [FieldDefinition(4, Label = "Secret token", Type = FieldType.Password, Privacy = PrivacyLevel.Password)]
+        public string SecretToken { get; set; }
+
+
+        public NzbDroneValidationResult Validate()
+        {
+                return new NzbDroneValidationResult(Validator.Validate(this));
+            }
+    }
+}
