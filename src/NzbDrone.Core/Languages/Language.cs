57e3bd8b4da16bea54a23e11d614ebd6809e2f93
siankatabg <siankata91@gmail.com>
2021-08-01 23:32:49 +0300

    New: Bulgarian Language

--- src/NzbDrone.Core/Languages/Language.cs
+++ src/NzbDrone.Core/Languages/Language.cs
@@ -83,6 +83,7 @@ namespace NzbDrone.Core.Languages
         public static Language Czech        { get { return new Language(25, "Czech"); } }
         public static Language Arabic       { get { return new Language(26, "Arabic"); } }
         public static Language Hindi        { get { return new Language(27, "Hindi"); } }
+        public static Language Bulgarian    { get { return new Language(28, "Bulgarian"); } }
 
 
         public static List<Language> All
@@ -118,7 +119,8 @@ namespace NzbDrone.Core.Languages
                     Lithuanian,
                     Czech,
                     Arabic,
-                    Hindi
+                    Hindi,
+                    Bulgarian
                 };
             }
         }
