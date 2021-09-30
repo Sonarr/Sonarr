d9e9b72a894a238e58d40cc0a6aa83c416db1a44
Kevin Lau <689163+kevinwlau@users.noreply.github.com>
2021-09-30 13:11:14 -0500

    New: Change Today color in calendar for better visibility

--- frontend/src/Styles/Variables/colors.js
+++ frontend/src/Styles/Variables/colors.js
@@ -178,7 +178,7 @@ module.exports = {
   //
   // Calendar
 
-  calendarTodayBackgroundColor: '#ddd',
+  calendarTodayBackgroundColor: '#c5c5c5',
   calendarBorderColor: '#cecece',
   calendarTextDim: '#666',
   calendarTextDimAlternate: '#eee',
