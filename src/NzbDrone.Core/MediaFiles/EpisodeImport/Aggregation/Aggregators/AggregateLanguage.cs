9e1dd4fa8c7fed03439f6af10a43dcf6d08e0812
Nicolas Ferec <nicolasferec@outlook.com>
2021-11-05 17:00:58 +0100

    Cleaning up AggregateLanguage.cs

--- src/NzbDrone.Core/MediaFiles/EpisodeImport/Aggregation/Aggregators/AggregateLanguage.cs
+++ src/NzbDrone.Core/MediaFiles/EpisodeImport/Aggregation/Aggregators/AggregateLanguage.cs
@@ -24,17 +24,6 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation.Aggregators
 
         public LocalEpisode Aggregate(LocalEpisode localEpisode, DownloadClientItem downloadClientItem)
         {
-            // Get languages in preferred order, download client item, folder and finally file.
-            // Non-English languages will be preferred later, in the event there is a conflict
-            // between parsed languages the more preferred item will be used.
-
-            /* var languages = new List<Language>
-                             {
-                                 GetLanguage(localEpisode.DownloadClientEpisodeInfo, localEpisode.Episodes),
-                                 GetLanguage(localEpisode.FolderEpisodeInfo, localEpisode.Episodes),
-                                 GetLanguage(localEpisode.FileEpisodeInfo, localEpisode.Episodes)
-                             };*/
-
             var languages = new List<Language> { };
             var languagesConfidence = Confidence.Default;
 
@@ -64,32 +53,5 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation.Aggregators
 
             return localEpisode;
         }
-
-        /*private Language GetLanguage(ParsedEpisodeInfo parsedEpisodeInfo, List<Episode> episodes)
-        {
-            if (parsedEpisodeInfo == null)
-            {
-                return Language.English;
-            }
-
-            var normalizedReleaseTitle = Parser.Parser.NormalizeEpisodeTitle(parsedEpisodeInfo.ReleaseTitle);
-
-            foreach (var episode in episodes)
-            {
-                var episodeTitleLanguage = LanguageParser.ParseLanguage(episode.Title, false);
-
-                if (episodeTitleLanguage != Language.Unknown && episodeTitleLanguage == parsedEpisodeInfo.Language)
-                {
-                    // Release title contains the episode title, return english instead of the parsed language.
-
-                    if (normalizedReleaseTitle.ContainsIgnoreCase(Parser.Parser.NormalizeEpisodeTitle(episode.Title)))
-                    {
-                        return Language.English;
-                    }
-                }
-            }
-
-            return parsedEpisodeInfo.Language;
-        }*/
     }
 }
