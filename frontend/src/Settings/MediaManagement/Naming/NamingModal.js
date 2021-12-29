250ac9d751d9ba2eaeddac1b8620fb821ea1bd49
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-12-29 10:30:05 -0600

    Fixed: Add Missing MediaInfo VideoDynamicRangeType Token

--- frontend/src/Settings/MediaManagement/Naming/NamingModal.js
+++ frontend/src/Settings/MediaManagement/Naming/NamingModal.js
@@ -101,7 +101,8 @@ const mediaInfoTokens = [
 
   { token: '{MediaInfo VideoCodec}', example: 'x264' },
   { token: '{MediaInfo VideoBitDepth}', example: '10' },
-  { token: '{MediaInfo VideoDynamicRange}', example: 'HDR' }
+  { token: '{MediaInfo VideoDynamicRange}', example: 'HDR' },
+  { token: '{MediaInfo VideoDynamicRangeType}', example: 'DV HDR10' }
 ];
 
 const otherTokens = [
