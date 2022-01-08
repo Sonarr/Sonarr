f4dbda1318564463b23056b866cb87a7c0e50393
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-01-08 11:29:24 -0600

    Fixed: Clarify Indexer Priority Helptext

--- frontend/src/Settings/Indexers/Indexers/EditIndexerModalContent.js
+++ frontend/src/Settings/Indexers/Indexers/EditIndexerModalContent.js
@@ -143,7 +143,7 @@ function EditIndexerModalContent(props) {
                 <FormInputGroup
                   type={inputTypes.NUMBER}
                   name="priority"
-                  helpText="Indexer Priority from 1 (Highest) to 50 (Lowest). Default: 25."
+                  helpText="Indexer Priority from 1 (Highest) to 50 (Lowest). Default: 25. Used when grabbing releases as a tiebreaker for otherwise equal releases, Sonarr will still use all enabled indexers for RSS Sync and Searching."
                   min={1}
                   max={50}
                   {...priority}
