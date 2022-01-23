35f1e530f870e4723c7bcbacffaf080c794d9f8f
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-01-23 14:33:28 -0600

    bump migration to 165

--- src/NzbDrone.Core/Datastore/Migration/165_add_on_update_to_notifications.cs
+++ src/NzbDrone.Core/Datastore/Migration/165_add_on_update_to_notifications.cs
@@ -3,7 +3,7 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
-    [Migration(163)]
+    [Migration(165)]
     public class add_on_update_to_notifications : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
