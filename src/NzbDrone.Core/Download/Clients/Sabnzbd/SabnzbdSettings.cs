d18751eff2c684a72e4c698a1e0f6c282c8da0da
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-02-21 14:14:44 -0600

    Fixed: Improve help text for download client Category

--- src/NzbDrone.Core/Download/Clients/Sabnzbd/SabnzbdSettings.cs
+++ src/NzbDrone.Core/Download/Clients/Sabnzbd/SabnzbdSettings.cs
@@ -66,7 +66,7 @@ namespace NzbDrone.Core.Download.Clients.Sabnzbd
         [FieldDefinition(6, Label = "Password", Type = FieldType.Password, Privacy = PrivacyLevel.Password)]
         public string Password { get; set; }
 
-        [FieldDefinition(7, Label = "Category", Type = FieldType.Textbox, HelpText = "Adding a category specific to Sonarr avoids conflicts with unrelated downloads, but it's optional")]
+        [FieldDefinition(7, Label = "Category", Type = FieldType.Textbox, HelpText = "Adding a category specific to Sonarr avoids conflicts with unrelated non-Sonarr downloads. Using a category is optional, but strongly recommended.")]
         public string TvCategory { get; set; }
 
         [FieldDefinition(8, Label = "Recent Priority", Type = FieldType.Select, SelectOptions = typeof(SabnzbdPriority), HelpText = "Priority to use when grabbing episodes that aired within the last 14 days")]
