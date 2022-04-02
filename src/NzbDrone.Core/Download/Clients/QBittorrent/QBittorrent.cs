41be937370ad9af4be95319a8772b2a40d605541
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-04-02 18:33:37 -0500

    fixup! Fixed: Clarify Qbit Content Path Error

--- src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
+++ src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -308,7 +308,7 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
                     else if (item.Status == DownloadItemStatus.Completed)
                     {
                         item.Status = DownloadItemStatus.Warning;
-                        item.Message = "Unable to Import. Path matches client download, perhaps 'Keep top-level folder' is disabled for this torrent or 'Torrent Content Layout' is NOT set to Original or Create Subfolder";
+                        item.Message = "Unable to Import. Path matches client download, perhaps 'Keep top-level folder' is disabled for this torrent or 'Torrent Content Layout' is NOT set to Original or Create Subfolder?";
                     }
                 }
 
