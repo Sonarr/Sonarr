6e271e9272d6a109bc4b8f3debe784d767ee7a63
Mark McDowall <mark@mcdowall.ca>
2022-04-05 17:08:58 -0700

    Fixed: Error when trying to import an empty Plex Watchlist

--- src/NzbDrone.Core/Notifications/Plex/Server/PlexSectionItem.cs
+++ src/NzbDrone.Core/Notifications/Plex/Server/PlexSectionItem.cs
@@ -30,6 +30,11 @@ namespace NzbDrone.Core.Notifications.Plex.Server
     {
         [JsonProperty("Metadata")]
         public List<PlexSectionItem> Items { get; set; }
+
+        public PlexSectionResponse()
+        {
+            Items = new List<PlexSectionItem>();
+        }
     }
 
     public class PlexSectionResponseLegacy
