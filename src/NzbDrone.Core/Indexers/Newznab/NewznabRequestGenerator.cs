f678775e5c3deb0c520f88a137198331fee2831f
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-04-09 23:16:20 -0500

    Fixed: Escape Characters as needed for *znab queries
    Closes #4788

--- src/NzbDrone.Core/Indexers/Newznab/NewznabRequestGenerator.cs
+++ src/NzbDrone.Core/Indexers/Newznab/NewznabRequestGenerator.cs
@@ -470,7 +470,8 @@ namespace NzbDrone.Core.Indexers.Newznab
 
         private static string NewsnabifyTitle(string title)
         {
-            return title.Replace("+", "%20");
+            title = title.Replace("+", " ");
+            return Uri.EscapeDataString(title);
         }
 
 
