68dfc500714a94aac7fafe2f4a3741b8262b288b
Qstick <qstick@gmail.com>
2022-05-12 19:06:14 -0500

    New: Instance name for Page Title

--- frontend/src/Components/Page/PageContent.js
+++ frontend/src/Components/Page/PageContent.js
@@ -14,7 +14,7 @@ function PageContent(props) {
 
   return (
     <ErrorBoundary errorComponent={PageContentError}>
-      <DocumentTitle title={title ? `${title} - Sonarr` : 'Sonarr'}>
+      <DocumentTitle title={title ? `${title} - ${window.Sonarr.instanceName}` : window.Sonarr.instanceName}>
         <div className={className}>
           {children}
         </div>
