be31f0e97a0c1766dac460f884518a5ef63fabed
Dominik Krivohlavek <github@xkrivo.net>
2022-06-03 22:44:38 +0200

    Update db migration number

--- src/NzbDrone.Core/Datastore/Migration/170_add_fields_to_subtitle_file.cs
+++ src/NzbDrone.Core/Datastore/Migration/170_add_fields_to_subtitle_file.cs
@@ -3,7 +3,7 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
-    [Migration(169)]
+    [Migration(170)]
     public class add_fields_to_subtitle_file : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
