e4c0e80e3e12688a2712078b65d9693c9e3d5c8f
Mark McDowall <mark@mcdowall.ca>
2022-08-06 15:36:38 -0700

    Bumped package version to 3.0.9

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Increment packageVersion when package scripts change
-packageVersion='3.0.8'
+packageVersion='3.0.9'
 
 # For now we keep the build version and package version the same
 buildVersion=$packageVersion
