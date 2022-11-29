f2b2eb69a3e8b271535bd92dc2f5cbfd45664daf
Mark McDowall <mark@mcdowall.ca>
2022-11-28 19:04:14 -0800

    Fixed: Handle Flood reporting errors for completed and stopped downloads
    Closes #5224

--- src/NzbDrone.Core/Download/Clients/Flood/Flood.cs
+++ src/NzbDrone.Core/Download/Clients/Flood/Flood.cs
@@ -132,21 +132,21 @@ namespace NzbDrone.Core.Download.Clients.Flood
                     item.RemainingTime = TimeSpan.FromSeconds(properties.Eta);
                 }
 
-                if (properties.Status.Contains("error"))
-                {
-                    item.Status = DownloadItemStatus.Warning;
-                }
-                else if (properties.Status.Contains("seeding") || properties.Status.Contains("complete"))
+                if (properties.Status.Contains("seeding") || properties.Status.Contains("complete"))
                 {
                     item.Status = DownloadItemStatus.Completed;
-                }
-                else if (properties.Status.Contains("downloading"))
-                {
-                    item.Status = DownloadItemStatus.Downloading;
                 }
                 else if (properties.Status.Contains("stopped"))
                 {
                     item.Status = DownloadItemStatus.Paused;
+                }
+                else if (properties.Status.Contains("error"))
+                {
+                    item.Status = DownloadItemStatus.Warning;
+                }
+                else if (properties.Status.Contains("downloading"))
+                {
+                    item.Status = DownloadItemStatus.Downloading;
                 }
 
                 if (item.Status == DownloadItemStatus.Completed)
