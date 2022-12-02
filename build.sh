e04bce4e18711b3681a460a2b0579234dd4a183b
Mark McDowall <mark@mcdowall.ca>
2022-12-01 21:15:30 -0800

    Get SDK_PATH from TeamCity

--- build.sh
+++ build.sh
@@ -31,7 +31,6 @@ UpdateVersionNumber()
 
 EnableExtraPlatformsInSDK()
 {
-    SDK_PATH=$(dotnet --list-sdks | grep -P '6\.\d\.\d+' | head -1 | sed 's/\(6\.[0-9]*\.[0-9]*\).*\[\(.*\)\]/\2\/\1/g')
     BUNDLEDVERSIONS="${SDK_PATH}/Microsoft.NETCoreSdk.BundledVersions.props"
     if grep -q freebsd-x64 $BUNDLEDVERSIONS; then
         echo "Extra platforms already enabled"
