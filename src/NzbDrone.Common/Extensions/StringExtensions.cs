1b90fbcf7df2c1086da4791c6491771924b1b7aa
Mark McDowall <mark@mcdowall.ca>
2022-12-05 00:57:21 -0800

    New: IPv6 support for connections/indexers/download clients
    Closes #4149

--- src/NzbDrone.Common/Extensions/StringExtensions.cs
+++ src/NzbDrone.Common/Extensions/StringExtensions.cs
@@ -237,5 +237,10 @@ namespace NzbDrone.Common.Extensions
 
             return parsedAddress.AddressFamily == AddressFamily.InterNetwork || parsedAddress.AddressFamily == AddressFamily.InterNetworkV6;
         }
+
+        public static string ToUrlHost(this string input)
+        {
+            return input.Contains(":") ? $"[{input}]" : input;
+        }
     }
 }
