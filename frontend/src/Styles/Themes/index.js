9a97eaf3e2913a7f563193ea74ed325352e6746c
Zak Saunders <thezak48@users.noreply.github.com>
2022-12-06 06:05:55 +0000

    New Auto theme
    Update Make Auto Theme Default
    
    Update Theme Help Text
    
    Co-Authored-By: Qstick <qstick@gmail.com>

--- frontend/src/Styles/Themes/index.js
+++ frontend/src/Styles/Themes/index.js
@@ -1,7 +1,11 @@
 import * as dark from './dark';
 import * as light from './light';
 
+const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const auto = defaultDark ? { ...dark } : { ...light };
+
 export default {
+  auto,
   light,
   dark
 };
