1b599c7e765b10ce043cbf6b127143cc737b378a
PearsonFlyer <john@theediguy.com>
2023-01-19 15:18:58 -0500

    Fix notifiation in Ntfy on test from Radarr to Sonarr

--- src/NzbDrone.Core/Notifications/Ntfy/NtfyProxy.cs
+++ src/NzbDrone.Core/Notifications/Ntfy/NtfyProxy.cs
@@ -70,9 +70,9 @@ namespace NzbDrone.Core.Notifications.Ntfy
         {
             try
             {
-                const string title = "Radarr - Test Notification";
+                const string title = "Sonarr - Test Notification";
 
-                const string body = "This is a test message from Radarr";
+                const string body = "This is a test message from Sonarr";
 
                 SendNotification(title, body, settings);
             }
