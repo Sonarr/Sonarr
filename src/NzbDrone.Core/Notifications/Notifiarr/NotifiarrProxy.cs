Radarr/Radarr

Convert Notifiarr Payload to JSON, Standardize with Webhook

src/NzbDrone.Core/Notifications/Notifiarr/NotifiarrProxy.cs
@@ -1,117 +1,69 @@
-using System;
-using System.Collections.Specialized;
-using FluentValidation.Results;
+using System.Net.Http;
 using NLog;
 using NzbDrone.Common.Http;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.Notifications.Webhook;
 
 namespace NzbDrone.Core.Notifications.Notifiarr
 {
     public interface INotifiarrProxy
     {
-        void SendNotification(StringDictionary message, NotifiarrSettings settings);
-        ValidationFailure Test(NotifiarrSettings settings);
+        void SendNotification(WebhookPayload payload, NotifiarrSettings settings);
     }
 
     public class NotifiarrProxy : INotifiarrProxy
     {
+        private const string URL = "https://notifiarr.com";
         private readonly IHttpClient _httpClient;
-        private readonly Logger _logger;
 
-        public NotifiarrProxy(IHttpClient httpClient, Logger logger)
+        public NotifiarrProxy(IHttpClient httpClient)
         {
             _httpClient = httpClient;
-            _logger = logger;
         }
 
-        public void SendNotification(StringDictionary message, NotifiarrSettings settings)
+        public void SendNotification(WebhookPayload payload, NotifiarrSettings settings)
+        {
+            ProcessNotification(payload, settings);
+        }
+
+        private void ProcessNotification(WebhookPayload payload, NotifiarrSettings settings)
         {
             try
             {
-                ProcessNotification(message, settings);
-            }
-            catch (NotifiarrException ex)
-            {
-                _logger.Error(ex, "Unable to send notification");
-                throw new NotifiarrException("Unable to send notification");
-            }
-        }
+                var request = new HttpRequestBuilder(URL + "/api/v1/notification/radarr")
+                    .Accept(HttpAccept.Json)
+                    .SetHeader("X-API-Key", settings.APIKey)
+                    .Build();
 
-        public ValidationFailure Test(NotifiarrSettings settings)
-        {
-            try
-            {
-                var variables = new StringDictionary();
-                variables.Add("Radarr_EventType", "Test");
+                request.Method = HttpMethod.Post;
 
-                SendNotification(variables, settings);
-                return null;
-            }
-            catch (HttpException ex)
-            {
-                switch ((int)ex.Response.StatusCode)
-                {
-                    case 401:
-                        _logger.Error(ex, "API key is invalid: " + ex.Message);
-                        return new ValidationFailure("APIKey", "API key is invalid");
-                    case 400:
-                        _logger.Error(ex, "Unable to send test message. Ensure Radarr Integration is enabled & assigned a channel on Notifiarr");
-                        return new ValidationFailure("", "Unable to send test message. Ensure Radarr Integration is enabled & assigned a channel on Notifiarr");
-                    case 520:
-                    case 521:
-                    case 522:
-                    case 523:
-                    case 524:
-                        _logger.Error(ex, "Cloudflare Related HTTP Error - Unable to send test message: " + ex.Message);
-                        return new ValidationFailure("", "Cloudflare Related HTTP Error - Unable to send test message");
-                }
-
-                _logger.Error(ex, "Unknown HTTP Error - Unable to send test message: " + ex.Message);
-                return new ValidationFailure("", "Unknown HTTP Error - Unable to send test message");
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, "Unable to send test notification: " + ex.Message);
-                return new ValidationFailure("", "Unable to send test notification");
-            }
-        }
-
-        private void ProcessNotification(StringDictionary message, NotifiarrSettings settings)
-        {
-            try
-            {
-                var url = "https://notifiarr.com";
-                var requestBuilder = new HttpRequestBuilder(url + "/api/v1/notification/radarr/" + settings.APIKey).Post();
-                requestBuilder.AddFormParameter("instanceName", settings.InstanceName).Build();
-
-                foreach (string key in message.Keys)
-                {
-                    requestBuilder.AddFormParameter(key, message[key]);
-                }
-
-                var request = requestBuilder.Build();
+                request.Headers.ContentType = "application/json";
+                request.SetContent(payload.ToJson());
 
                 _httpClient.Post(request);
             }
             catch (HttpException ex)
             {
-                switch ((int)ex.Response.StatusCode)
+                var responseCode = ex.Response.StatusCode;
+                switch ((int)responseCode)
                 {
                     case 401:
-                        _logger.Error("", "API key is invalid");
-                        throw new NotifiarrException("API key is invalid", ex);
+                        throw new NotifiarrException("API key is invalid");
                     case 400:
-                        _logger.Error(ex, "Unable to send notification. Ensure Radarr Integration is enabled & assigned a channel on Notifiarr");
-                        throw new NotifiarrException("Unable to send notification. Ensure Radarr Integration is enabled & assigned a channel on Notifiarr", ex);
+                        throw new NotifiarrException("Unable to send notification. Ensure Radarr Integration is enabled & assigned a channel on Notifiarr");
+                    case 502:
+                    case 503:
+                    case 504:
+                        throw new NotifiarrException("Unable to send notification. Service Unavailable", ex);
                     case 520:
                     case 521:
                     case 522:
                     case 523:
                     case 524:
-                        _logger.Error(ex, "Cloudflare Related HTTP Error - Unable to send notification");
                         throw new NotifiarrException("Cloudflare Related HTTP Error - Unable to send notification", ex);
+                    default:
+                        throw new NotifiarrException("Unknown HTTP Error - Unable to send notification", ex);
                 }
-
-                throw new NotifiarrException("Unknown HTTP Error - Unable to send notification", ex);
             }
         }
     }

src/NzbDrone.Core/Notifications/Webhook/Webhook.cs
@@ -1,19 +1,19 @@
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using FluentValidation.Results;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Configuration;
 using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.Movies;
 using NzbDrone.Core.Validation;
 
 namespace NzbDrone.Core.Notifications.Webhook
 {
-    public class Webhook : NotificationBase<WebhookSettings>
+    public class Webhook : WebhookBase<WebhookSettings>
     {
         private readonly IWebhookProxy _proxy;
 
-        public Webhook(IWebhookProxy proxy)
+        public Webhook(IWebhookProxy proxy, IConfigFileProvider configFileProvider)
+            : base(configFileProvider)
         {
             _proxy = proxy;
         }
@@ -22,124 +22,42 @@ namespace NzbDrone.Core.Notifications.Webhook
 
         public override void OnGrab(GrabMessage message)
         {
-            var remoteMovie = message.RemoteMovie;
-            var quality = message.Quality;
-
-            var payload = new WebhookGrabPayload
-            {
-                EventType = WebhookEventType.Grab,
-                Movie = new WebhookMovie(message.Movie),
-                RemoteMovie = new WebhookRemoteMovie(remoteMovie),
-                Release = new WebhookRelease(quality, remoteMovie),
-                DownloadClient = message.DownloadClientName,
-                DownloadClientType = message.DownloadClientType,
-                DownloadId = message.DownloadId
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnGrabPayload(message), Settings);
         }
 
         public override void OnDownload(DownloadMessage message)
         {
-            var movieFile = message.MovieFile;
-
-            var payload = new WebhookImportPayload
-            {
-                EventType = WebhookEventType.Download,
-                Movie = new WebhookMovie(message.Movie),
-                RemoteMovie = new WebhookRemoteMovie(message.Movie),
-                MovieFile = new WebhookMovieFile(movieFile),
-                IsUpgrade = message.OldMovieFiles.Any(),
-                DownloadClient = message.DownloadClientInfo?.Name,
-                DownloadClientType = message.DownloadClientInfo?.Type,
-                DownloadId = message.DownloadId
-            };
-
-            if (message.OldMovieFiles.Any())
-            {
-                payload.DeletedFiles = message.OldMovieFiles.ConvertAll(x =>
-                    new WebhookMovieFile(x)
-                    {
-                        Path = Path.Combine(message.Movie.Path, x.RelativePath)
-                    });
-            }
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnDownloadPayload(message), Settings);
         }
 
         public override void OnMovieRename(Movie movie, List<RenamedMovieFile> renamedFiles)
         {
-            var payload = new WebhookRenamePayload
-            {
-                EventType = WebhookEventType.Rename,
-                Movie = new WebhookMovie(movie),
-                RenamedMovieFiles = renamedFiles.ConvertAll(x => new WebhookRenamedMovieFile(x))
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnRenamePayload(movie, renamedFiles), Settings);
         }
 
         public override void OnMovieAdded(Movie movie)
         {
-            var payload = new WebhookRenamePayload
-            {
-                EventType = WebhookEventType.MovieAdded,
-                Movie = new WebhookMovie(movie)
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnMovieAdded(movie), Settings);
         }
 
         public override void OnMovieFileDelete(MovieFileDeleteMessage deleteMessage)
         {
-            var payload = new WebhookMovieFileDeletePayload
-            {
-                EventType = WebhookEventType.MovieFileDelete,
-                Movie = new WebhookMovie(deleteMessage.Movie),
-                MovieFile = new WebhookMovieFile(deleteMessage.MovieFile),
-                DeleteReason = deleteMessage.Reason
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnMovieFileDelete(deleteMessage), Settings);
         }
 
         public override void OnMovieDelete(MovieDeleteMessage deleteMessage)
         {
-            var payload = new WebhookMovieDeletePayload
-            {
-                EventType = WebhookEventType.MovieDelete,
-                Movie = new WebhookMovie(deleteMessage.Movie),
-                DeletedFiles = deleteMessage.DeletedFiles
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnMovieDelete(deleteMessage), Settings);
         }
 
         public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
         {
-            var payload = new WebhookHealthPayload
-                          {
-                              EventType = WebhookEventType.Health,
-                              Level = healthCheck.Type,
-                              Message = healthCheck.Message,
-                              Type = healthCheck.Source.Name,
-                              WikiUrl = healthCheck.WikiUrl?.ToString()
-                          };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildHealthPayload(healthCheck), Settings);
         }
 
         public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
         {
-            var payload = new WebhookApplicationUpdatePayload
-            {
-                EventType = WebhookEventType.ApplicationUpdate,
-                Message = updateMessage.Message,
-                PreviousVersion = updateMessage.PreviousVersion.ToString(),
-                NewVersion = updateMessage.NewVersion.ToString()
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildApplicationUpdatePayload(updateMessage), Settings);
         }
 
         public override string Name => "Webhook";
@@ -157,36 +75,7 @@ namespace NzbDrone.Core.Notifications.Webhook
         {
             try
             {
-                var payload = new WebhookGrabPayload
-                {
-                    EventType = WebhookEventType.Test,
-                    Movie = new WebhookMovie
-                    {
-                        Id = 1,
-                        Title = "Test Title",
-                        Year = 1970,
-                        FolderPath = "C:\\testpath",
-                        ReleaseDate = "1970-01-01"
-                    },
-                    RemoteMovie = new WebhookRemoteMovie
-                    {
-                        TmdbId = 1234,
-                        ImdbId = "5678",
-                        Title = "Test title",
-                        Year = 1970
-                    },
-                    Release = new WebhookRelease
-                    {
-                        Indexer = "Test Indexer",
-                        Quality = "Test Quality",
-                        QualityVersion = 1,
-                        ReleaseGroup = "Test Group",
-                        ReleaseTitle = "Test Title",
-                        Size = 9999999
-                    }
-                };
-
-                _proxy.SendWebhook(payload, Settings);
+                _proxy.SendWebhook(BuildTestPayload(), Settings);
             }
             catch (WebhookException ex)
             {

Sonarr/Sonarr

src/NzbDrone.Core/Notifications/Notifiarr/NotifiarrProxy.cs
@@ -1,81 +1,46 @@
-using System;
-using System.Collections.Specialized;
-using FluentValidation.Results;
+using System.Net.Http;
 using NLog;
-using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
-using NzbDrone.Core.Configuration;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.Notifications.Webhook;
 
 namespace NzbDrone.Core.Notifications.Notifiarr
 {
     public interface INotifiarrProxy
     {
-        void SendNotification(StringDictionary message, NotifiarrSettings settings);
-        ValidationFailure Test(NotifiarrSettings settings);
+        void SendNotification(WebhookPayload payload, NotifiarrSettings settings);
     }
 
     public class NotifiarrProxy : INotifiarrProxy
     {
         private const string URL = "https://notifiarr.com";
         private readonly IHttpClient _httpClient;
-        private readonly IConfigFileProvider _configFileProvider;
         private readonly Logger _logger;
 
-        public NotifiarrProxy(IHttpClient httpClient, IConfigFileProvider configFileProvider, Logger logger)
+        public NotifiarrProxy(IHttpClient httpClient, Logger logger)
         {
             _httpClient = httpClient;
-            _configFileProvider = configFileProvider;
             _logger = logger;
         }
 
-        public void SendNotification(StringDictionary message, NotifiarrSettings settings)
+        public void SendNotification(WebhookPayload payload, NotifiarrSettings settings)
         {
-            try
-            {
-                ProcessNotification(message, settings);
-            }
-            catch (NotifiarrException ex)
-            {
-                throw ex;
-            }
+            ProcessNotification(payload, settings);
         }
 
-        public ValidationFailure Test(NotifiarrSettings settings)
+        private void ProcessNotification(WebhookPayload payload, NotifiarrSettings settings)
         {
             try
             {
-                var variables = new StringDictionary();
-                variables.Add("Sonarr_EventType", "Test");
+                var request = new HttpRequestBuilder(URL + "/api/v1/notification/sonarr")
+                    .Accept(HttpAccept.Json)
+                    .SetHeader("X-API-Key", settings.ApiKey)
+                    .Build();
 
-                SendNotification(variables, settings);
-                return null;
-            }
-            catch (NotifiarrException ex)
-            {
-                return new ValidationFailure("ApiKey", ex.Message);
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, ex.Message);
-                return new ValidationFailure(string.Empty, "$Unable to send test notification: {ex.Message}. Check the log surrounding this error for details");
-            }
-        }
+                request.Method = HttpMethod.Post;
 
-        private void ProcessNotification(StringDictionary message, NotifiarrSettings settings)
-        {
-            try
-            {
-                var instanceName = _configFileProvider.InstanceName;
-                var requestBuilder = new HttpRequestBuilder(URL + "/api/v1/notification/sonarr").Post();
-                requestBuilder.AddFormParameter("instanceName", instanceName).Build();
-                requestBuilder.SetHeader("X-API-Key", settings.ApiKey);
-
-                foreach (string key in message.Keys)
-                {
-                    requestBuilder.AddFormParameter(key, message[key]);
-                }
-
-                var request = requestBuilder.Build();
+                request.Headers.ContentType = "application/json";
+                request.SetContent(payload.ToJson());
 
                 _httpClient.Post(request);
             }
@@ -85,22 +50,21 @@ namespace NzbDrone.Core.Notifications.Notifiarr
                 switch ((int)responseCode)
                 {
                     case 401:
-                        _logger.Error("Unauthorized", "HTTP 401 - API key is invalid");
+                        _logger.Error("HTTP 401 - API key is invalid");
                         throw new NotifiarrException("API key is invalid");
                     case 400:
-                        _logger.Error("Invalid Request", "HTTP 400 - Unable to send notification. Ensure Sonarr Integration is enabled & assigned a channel on Notifiarr");
+                        _logger.Error("HTTP 400 - Unable to send notification. Ensure Sonarr Integration is enabled & assigned a channel on Notifiarr");
                         throw new NotifiarrException("Unable to send notification. Ensure Sonarr Integration is enabled & assigned a channel on Notifiarr");
                     case 502:
                     case 503:
                     case 504:
-                        _logger.Error("Service Unavailable", "Unable to send notification. Service Unavailable");
+                        _logger.Error("Unable to send notification. Service Unavailable");
                         throw new NotifiarrException("Unable to send notification. Service Unavailable", ex);
                     case 520:
                     case 521:
                     case 522:
                     case 523:
                     case 524:
-                        _logger.Error(ex, "Cloudflare Related HTTP Error - Unable to send notification");
                         throw new NotifiarrException("Cloudflare Related HTTP Error - Unable to send notification", ex);
                     default:
                         _logger.Error(ex, "Unknown HTTP Error - Unable to send notification");

src/NzbDrone.Core/Notifications/Webhook/Webhook.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using FluentValidation.Results;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Configuration;
@@ -10,18 +8,13 @@ using NzbDrone.Core.Validation;
 
 namespace NzbDrone.Core.Notifications.Webhook
 {
-    public class Webhook : NotificationBase<WebhookSettings>
+    public class Webhook : WebhookBase<WebhookSettings>
     {
-        private readonly IConfigFileProvider _configFileProvider;
-        private readonly IConfigService _configService;
         private readonly IWebhookProxy _proxy;
 
-        public Webhook(IConfigFileProvider configFileProvider,
-            IConfigService configService,
-            IWebhookProxy proxy)
+        public Webhook(IWebhookProxy proxy, IConfigFileProvider configFileProvider, IConfigService configService)
+            : base(configFileProvider, configService)
         {
-            _configFileProvider = configFileProvider;
-            _configService = configService;
             _proxy = proxy;
         }
 
@@ -29,129 +22,37 @@ namespace NzbDrone.Core.Notifications.Webhook
 
         public override void OnGrab(GrabMessage message)
         {
-            var remoteEpisode = message.Episode;
-            var quality = message.Quality;
-
-            var payload = new WebhookGrabPayload
-            {
-                EventType = WebhookEventType.Grab,
-                InstanceName = _configFileProvider.InstanceName,
-                ApplicationUrl = _configService.ApplicationUrl,
-                Series = new WebhookSeries(message.Series),
-                Episodes = remoteEpisode.Episodes.ConvertAll(x => new WebhookEpisode(x)),
-                Release = new WebhookRelease(quality, remoteEpisode),
-                DownloadClient = message.DownloadClientName,
-                DownloadClientType = message.DownloadClientType,
-                DownloadId = message.DownloadId,
-                CustomFormatInfo = new WebhookCustomFormatInfo(remoteEpisode.CustomFormats, remoteEpisode.CustomFormatScore)
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnGrabPayload(message), Settings);
         }
 
         public override void OnDownload(DownloadMessage message)
         {
-            var episodeFile = message.EpisodeFile;
-
-            var payload = new WebhookImportPayload
-            {
-                EventType = WebhookEventType.Download,
-                InstanceName = _configFileProvider.InstanceName,
-                ApplicationUrl = _configService.ApplicationUrl,
-                Series = new WebhookSeries(message.Series),
-                Episodes = episodeFile.Episodes.Value.ConvertAll(x => new WebhookEpisode(x)),
-                EpisodeFile = new WebhookEpisodeFile(episodeFile),
-                IsUpgrade = message.OldFiles.Any(),
-                DownloadClient = message.DownloadClientInfo?.Name,
-                DownloadClientType = message.DownloadClientInfo?.Type,
-                DownloadId = message.DownloadId,
-                CustomFormatInfo = new WebhookCustomFormatInfo(message.EpisodeInfo.CustomFormats, message.EpisodeInfo.CustomFormatScore)
-            };
-
-            if (message.OldFiles.Any())
-            {
-                payload.DeletedFiles = message.OldFiles.ConvertAll(x => new WebhookEpisodeFile(x)
-                {
-                    Path = Path.Combine(message.Series.Path, x.RelativePath)
-                });
-            }
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnDownloadPayload(message), Settings);
         }
 
         public override void OnRename(Series series, List<RenamedEpisodeFile> renamedFiles)
         {
-            var payload = new WebhookRenamePayload
-            {
-                EventType = WebhookEventType.Rename,
-                InstanceName = _configFileProvider.InstanceName,
-                ApplicationUrl = _configService.ApplicationUrl,
-                Series = new WebhookSeries(series),
-                RenamedEpisodeFiles = renamedFiles.ConvertAll(x => new WebhookRenamedEpisodeFile(x))
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnRenamePayload(series, renamedFiles), Settings);
         }
 
         public override void OnEpisodeFileDelete(EpisodeDeleteMessage deleteMessage)
         {
-            var payload = new WebhookEpisodeDeletePayload
-            {
-                EventType = WebhookEventType.EpisodeFileDelete,
-                InstanceName = _configFileProvider.InstanceName,
-                ApplicationUrl = _configService.ApplicationUrl,
-                Series = new WebhookSeries(deleteMessage.Series),
-                Episodes = deleteMessage.EpisodeFile.Episodes.Value.ConvertAll(x => new WebhookEpisode(x)),
-                EpisodeFile = deleteMessage.EpisodeFile,
-                DeleteReason = deleteMessage.Reason
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnEpisodeFileDelete(deleteMessage), Settings);
         }
 
         public override void OnSeriesDelete(SeriesDeleteMessage deleteMessage)
         {
-            var payload = new WebhookSeriesDeletePayload
-            {
-                EventType = WebhookEventType.SeriesDelete,
-                InstanceName = _configFileProvider.InstanceName,
-                ApplicationUrl = _configService.ApplicationUrl,
-                Series = new WebhookSeries(deleteMessage.Series),
-                DeletedFiles = deleteMessage.DeletedFiles
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildOnSeriesDelete(deleteMessage), Settings);
         }
 
         public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
         {
-            var payload = new WebhookHealthPayload
-            {
-                EventType = WebhookEventType.Health,
-                InstanceName = _configFileProvider.InstanceName,
-                ApplicationUrl = _configService.ApplicationUrl,
-                Level = healthCheck.Type,
-                Message = healthCheck.Message,
-                Type = healthCheck.Source.Name,
-                WikiUrl = healthCheck.WikiUrl?.ToString()
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildHealthPayload(healthCheck), Settings);
         }
 
         public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
         {
-            var payload = new WebhookApplicationUpdatePayload
-            {
-                EventType = WebhookEventType.ApplicationUpdate,
-                InstanceName = _configFileProvider.InstanceName,
-                ApplicationUrl = _configService.ApplicationUrl,
-                Message = updateMessage.Message,
-                PreviousVersion = updateMessage.PreviousVersion.ToString(),
-                NewVersion = updateMessage.NewVersion.ToString()
-            };
-
-            _proxy.SendWebhook(payload, Settings);
+            _proxy.SendWebhook(BuildApplicationUpdatePayload(updateMessage), Settings);
         }
 
         public override string Name => "Webhook";
@@ -169,31 +70,7 @@ namespace NzbDrone.Core.Notifications.Webhook
         {
             try
             {
-                var payload = new WebhookGrabPayload
-                {
-                    EventType = WebhookEventType.Test,
-                    InstanceName = _configFileProvider.InstanceName,
-                    ApplicationUrl = _configService.ApplicationUrl,
-                    Series = new WebhookSeries()
-                    {
-                        Id = 1,
-                        Title = "Test Title",
-                        Path = "C:\\testpath",
-                        TvdbId = 1234
-                    },
-                    Episodes = new List<WebhookEpisode>()
-                    {
-                        new WebhookEpisode()
-                        {
-                            Id = 123,
-                            EpisodeNumber = 1,
-                            SeasonNumber = 1,
-                            Title = "Test title"
-                        }
-                    }
-                };
-
-                _proxy.SendWebhook(payload, Settings);
+                _proxy.SendWebhook(BuildTestPayload(), Settings);
             }
             catch (WebhookException ex)
             {
f59276881a89735ea8a2a34aa3fae587bef113e6
Qstick <qstick@gmail.com>
2022-11-22 21:17:25 -0600

    Convert Notifiarr Payload to JSON, Standardize with Webhook

--- src/NzbDrone.Core/Notifications/Notifiarr/NotifiarrProxy.cs
+++ src/NzbDrone.Core/Notifications/Notifiarr/NotifiarrProxy.cs
@@ -1,81 +1,46 @@
-using System;
-using System.Collections.Specialized;
-using FluentValidation.Results;
+using System.Net.Http;
 using NLog;
-using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
-using NzbDrone.Core.Configuration;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.Notifications.Webhook;
 
 namespace NzbDrone.Core.Notifications.Notifiarr
 {
     public interface INotifiarrProxy
     {
-        void SendNotification(StringDictionary message, NotifiarrSettings settings);
-        ValidationFailure Test(NotifiarrSettings settings);
+        void SendNotification(WebhookPayload payload, NotifiarrSettings settings);
     }
 
     public class NotifiarrProxy : INotifiarrProxy
     {
         private const string URL = "https://notifiarr.com";
         private readonly IHttpClient _httpClient;
-        private readonly IConfigFileProvider _configFileProvider;
         private readonly Logger _logger;
 
-        public NotifiarrProxy(IHttpClient httpClient, IConfigFileProvider configFileProvider, Logger logger)
+        public NotifiarrProxy(IHttpClient httpClient, Logger logger)
         {
             _httpClient = httpClient;
-            _configFileProvider = configFileProvider;
             _logger = logger;
         }
 
-        public void SendNotification(StringDictionary message, NotifiarrSettings settings)
+        public void SendNotification(WebhookPayload payload, NotifiarrSettings settings)
         {
-            try
-            {
-                ProcessNotification(message, settings);
-            }
-            catch (NotifiarrException ex)
-            {
-                throw ex;
-            }
+            ProcessNotification(payload, settings);
         }
 
-        public ValidationFailure Test(NotifiarrSettings settings)
+        private void ProcessNotification(WebhookPayload payload, NotifiarrSettings settings)
         {
             try
             {
-                var variables = new StringDictionary();
-                variables.Add("Sonarr_EventType", "Test");
+                var request = new HttpRequestBuilder(URL + "/api/v1/notification/sonarr")
+                    .Accept(HttpAccept.Json)
+                    .SetHeader("X-API-Key", settings.ApiKey)
+                    .Build();
 
-                SendNotification(variables, settings);
-                return null;
-            }
-            catch (NotifiarrException ex)
-            {
-                return new ValidationFailure("ApiKey", ex.Message);
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, ex.Message);
-                return new ValidationFailure(string.Empty, "$Unable to send test notification: {ex.Message}. Check the log surrounding this error for details");
-            }
-        }
+                request.Method = HttpMethod.Post;
 
-        private void ProcessNotification(StringDictionary message, NotifiarrSettings settings)
-        {
-            try
-            {
-                var instanceName = _configFileProvider.InstanceName;
-                var requestBuilder = new HttpRequestBuilder(URL + "/api/v1/notification/sonarr").Post();
-                requestBuilder.AddFormParameter("instanceName", instanceName).Build();
-                requestBuilder.SetHeader("X-API-Key", settings.ApiKey);
-
-                foreach (string key in message.Keys)
-                {
-                    requestBuilder.AddFormParameter(key, message[key]);
-                }
-
-                var request = requestBuilder.Build();
+                request.Headers.ContentType = "application/json";
+                request.SetContent(payload.ToJson());
 
                 _httpClient.Post(request);
             }
@@ -85,22 +50,21 @@ namespace NzbDrone.Core.Notifications.Notifiarr
                 switch ((int)responseCode)
                 {
                     case 401:
-                        _logger.Error("Unauthorized", "HTTP 401 - API key is invalid");
+                        _logger.Error("HTTP 401 - API key is invalid");
                         throw new NotifiarrException("API key is invalid");
                     case 400:
-                        _logger.Error("Invalid Request", "HTTP 400 - Unable to send notification. Ensure Sonarr Integration is enabled & assigned a channel on Notifiarr");
+                        _logger.Error("HTTP 400 - Unable to send notification. Ensure Sonarr Integration is enabled & assigned a channel on Notifiarr");
                         throw new NotifiarrException("Unable to send notification. Ensure Sonarr Integration is enabled & assigned a channel on Notifiarr");
                     case 502:
                     case 503:
                     case 504:
-                        _logger.Error("Service Unavailable", "Unable to send notification. Service Unavailable");
+                        _logger.Error("Unable to send notification. Service Unavailable");
                         throw new NotifiarrException("Unable to send notification. Service Unavailable", ex);
                     case 520:
                     case 521:
                     case 522:
                     case 523:
                     case 524:
-                        _logger.Error(ex, "Cloudflare Related HTTP Error - Unable to send notification");
                         throw new NotifiarrException("Cloudflare Related HTTP Error - Unable to send notification", ex);
                     default:
                         _logger.Error(ex, "Unknown HTTP Error - Unable to send notification");
---
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookBase.cs
@@ -0,0 +1,163 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.MediaFiles;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Notifications.Webhook
+{
+    public abstract class WebhookBase<TSettings> : NotificationBase<TSettings>
+        where TSettings : IProviderConfig, new()
+    {
+        private readonly IConfigFileProvider _configFileProvider;
+        private readonly IConfigService _configService;
+
+        protected WebhookBase(IConfigFileProvider configFileProvider, IConfigService configService)
+        {
+            _configFileProvider = configFileProvider;
+            _configService = configService;
+        }
+
+        protected WebhookGrabPayload BuildOnGrabPayload(GrabMessage message)
+        {
+            var remoteEpisode = message.Episode;
+            var quality = message.Quality;
+
+            return new WebhookGrabPayload
+            {
+                EventType = WebhookEventType.Grab,
+                InstanceName = _configFileProvider.InstanceName,
+                ApplicationUrl = _configService.ApplicationUrl,
+                Series = new WebhookSeries(message.Series),
+                Episodes = remoteEpisode.Episodes.ConvertAll(x => new WebhookEpisode(x)),
+                Release = new WebhookRelease(quality, remoteEpisode),
+                DownloadClient = message.DownloadClientName,
+                DownloadClientType = message.DownloadClientType,
+                DownloadId = message.DownloadId,
+                CustomFormatInfo = new WebhookCustomFormatInfo(remoteEpisode.CustomFormats, remoteEpisode.CustomFormatScore)
+            };
+        }
+
+        protected WebhookImportPayload BuildOnDownloadPayload(DownloadMessage message)
+        {
+            var episodeFile = message.EpisodeFile;
+
+            var payload = new WebhookImportPayload
+            {
+                EventType = WebhookEventType.Download,
+                InstanceName = _configFileProvider.InstanceName,
+                ApplicationUrl = _configService.ApplicationUrl,
+                Series = new WebhookSeries(message.Series),
+                Episodes = episodeFile.Episodes.Value.ConvertAll(x => new WebhookEpisode(x)),
+                EpisodeFile = new WebhookEpisodeFile(episodeFile),
+                IsUpgrade = message.OldFiles.Any(),
+                DownloadClient = message.DownloadClientInfo?.Name,
+                DownloadClientType = message.DownloadClientInfo?.Type,
+                DownloadId = message.DownloadId,
+                CustomFormatInfo = new WebhookCustomFormatInfo(message.EpisodeInfo.CustomFormats, message.EpisodeInfo.CustomFormatScore)
+            };
+
+            if (message.OldFiles.Any())
+            {
+                payload.DeletedFiles = message.OldFiles.ConvertAll(x => new WebhookEpisodeFile(x)
+                {
+                    Path = Path.Combine(message.Series.Path, x.RelativePath)
+                });
+            }
+
+            return payload;
+        }
+
+        protected WebhookEpisodeDeletePayload BuildOnEpisodeFileDelete(EpisodeDeleteMessage deleteMessage)
+        {
+            return new WebhookEpisodeDeletePayload
+            {
+                EventType = WebhookEventType.EpisodeFileDelete,
+                InstanceName = _configFileProvider.InstanceName,
+                ApplicationUrl = _configService.ApplicationUrl,
+                Series = new WebhookSeries(deleteMessage.Series),
+                Episodes = deleteMessage.EpisodeFile.Episodes.Value.ConvertAll(x => new WebhookEpisode(x)),
+                EpisodeFile = deleteMessage.EpisodeFile,
+                DeleteReason = deleteMessage.Reason
+            };
+        }
+
+        protected WebhookSeriesDeletePayload BuildOnSeriesDelete(SeriesDeleteMessage deleteMessage)
+        {
+            return new WebhookSeriesDeletePayload
+            {
+                EventType = WebhookEventType.SeriesDelete,
+                InstanceName = _configFileProvider.InstanceName,
+                ApplicationUrl = _configService.ApplicationUrl,
+                Series = new WebhookSeries(deleteMessage.Series),
+                DeletedFiles = deleteMessage.DeletedFiles
+            };
+        }
+
+        protected WebhookRenamePayload BuildOnRenamePayload(Series series, List<RenamedEpisodeFile> renamedFiles)
+        {
+            return new WebhookRenamePayload
+            {
+                EventType = WebhookEventType.Rename,
+                InstanceName = _configFileProvider.InstanceName,
+                ApplicationUrl = _configService.ApplicationUrl,
+                Series = new WebhookSeries(series),
+                RenamedEpisodeFiles = renamedFiles.ConvertAll(x => new WebhookRenamedEpisodeFile(x))
+            };
+        }
+
+        protected WebhookHealthPayload BuildHealthPayload(HealthCheck.HealthCheck healthCheck)
+        {
+            return new WebhookHealthPayload
+            {
+                EventType = WebhookEventType.Health,
+                InstanceName = _configFileProvider.InstanceName,
+                Level = healthCheck.Type,
+                Message = healthCheck.Message,
+                Type = healthCheck.Source.Name,
+                WikiUrl = healthCheck.WikiUrl?.ToString()
+            };
+        }
+
+        protected WebhookApplicationUpdatePayload BuildApplicationUpdatePayload(ApplicationUpdateMessage updateMessage)
+        {
+            return new WebhookApplicationUpdatePayload
+            {
+                EventType = WebhookEventType.ApplicationUpdate,
+                InstanceName = _configFileProvider.InstanceName,
+                Message = updateMessage.Message,
+                PreviousVersion = updateMessage.PreviousVersion.ToString(),
+                NewVersion = updateMessage.NewVersion.ToString()
+            };
+        }
+
+        protected WebhookPayload BuildTestPayload()
+        {
+            return new WebhookGrabPayload
+            {
+                EventType = WebhookEventType.Test,
+                InstanceName = _configFileProvider.InstanceName,
+                ApplicationUrl = _configService.ApplicationUrl,
+                Series = new WebhookSeries()
+                {
+                    Id = 1,
+                    Title = "Test Title",
+                    Path = "C:\\testpath",
+                    TvdbId = 1234
+                },
+                Episodes = new List<WebhookEpisode>()
+                {
+                    new WebhookEpisode()
+                    {
+                        Id = 123,
+                        EpisodeNumber = 1,
+                        SeasonNumber = 1,
+                        Title = "Test title"
+                    }
+                }
+            };
+        }
+    }
+}
