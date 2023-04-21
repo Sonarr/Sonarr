2b9e576b25a2cc06463312257e2108d0b63f8dd3
LarsHelo <lars.erik.heloe@gmail.com>
2023-04-21 23:28:21 +0200

    Move Root Folder field to the bottom of the import list modal

--- src/NzbDrone.Core/ImportLists/Sonarr/SonarrSettings.cs
+++ src/NzbDrone.Core/ImportLists/Sonarr/SonarrSettings.cs
@@ -41,11 +41,11 @@ namespace NzbDrone.Core.ImportLists.Sonarr
         [FieldDefinition(3, Type = FieldType.Select, SelectOptionsProviderAction = "getLanguageProfiles", Label = "Language Profiles", HelpText = "Language Profiles from the source instance to import from")]
         public IEnumerable<int> LanguageProfileIds { get; set; }
 
-        [FieldDefinition(3, Type = FieldType.Select, SelectOptionsProviderAction = "getRootFolders", Label = "Root Folders", HelpText = "Root Folders from the source instance to import from")]
-        public IEnumerable<string> RootFolderPaths { get; set; }
-
         [FieldDefinition(4, Type = FieldType.Select, SelectOptionsProviderAction = "getTags", Label = "Tags", HelpText = "Tags from the source instance to import from")]
         public IEnumerable<int> TagIds { get; set; }
+
+        [FieldDefinition(5, Type = FieldType.Select, SelectOptionsProviderAction = "getRootFolders", Label = "Root Folders", HelpText = "Root Folders from the source instance to import from")]
+        public IEnumerable<string> RootFolderPaths { get; set; }
 
         public NzbDroneValidationResult Validate()
         {
