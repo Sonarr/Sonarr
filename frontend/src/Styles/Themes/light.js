2c302b3a65b68e8c5030af1ac15f801f59624f34
Benjamin Staneck <staneck@gmail.com>
2023-04-27 10:36:10 +0200

    Fix: A typo in a CSS variable name

--- frontend/src/Styles/Themes/light.js
+++ frontend/src/Styles/Themes/light.js
@@ -210,7 +210,7 @@ module.exports = {
   // Calendar
 
   calendarTodayBackgroundColor: '#c5c5c5',
-  calendarBackgroudColor: '#e4eaec',
+  calendarBackgroundColor: '#e4eaec',
   calendarBorderColor: '#cecece',
   calendarTextDim: '#666',
   calendarTextDimAlternate: '#242424',
