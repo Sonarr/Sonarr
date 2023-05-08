993c69530ed34460800f40ecf8a0b7bc9a2f7d48
Mark McDowall <mark@mcdowall.ca>
2023-05-07 18:57:41 -0700

    GracePeriod not Graceperiod

--- src/NzbDrone.Core/HealthCheck/HealthCheckFailedEvent.cs
+++ src/NzbDrone.Core/HealthCheck/HealthCheckFailedEvent.cs
@@ -1,16 +1,16 @@
-﻿using NzbDrone.Common.Messaging;
+using NzbDrone.Common.Messaging;
 
 namespace NzbDrone.Core.HealthCheck
 {
     public class HealthCheckFailedEvent : IEvent
     {
         public HealthCheck HealthCheck { get; private set; }
-        public bool IsInStartupGraceperiod { get; private set; }
+        public bool IsInStartupGracePeriod { get; private set; }
 
-        public HealthCheckFailedEvent(HealthCheck healthCheck, bool isInStartupGraceperiod)
+        public HealthCheckFailedEvent(HealthCheck healthCheck, bool isInStartupGracePeriod)
         {
             HealthCheck = healthCheck;
-            IsInStartupGraceperiod = isInStartupGraceperiod;
+            IsInStartupGracePeriod = isInStartupGracePeriod;
         }
     }
 }
