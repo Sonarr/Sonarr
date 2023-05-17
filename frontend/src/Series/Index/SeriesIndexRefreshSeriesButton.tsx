c8e23f9d2ce7af8cba2ff2295ac31eb4a185fe87
Stevie Robinson <stevie.robinson@gmail.com>
2023-05-17 21:52:56 +0200

    Add functionalilty to update only selected or filtered series

---
+++ frontend/src/Series/Index/SeriesIndexRefreshSeriesButton.tsx
@@ -0,0 +1,71 @@
+import React, { useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSelect } from 'App/SelectContext';
+import ClientSideCollectionAppState from 'App/State/ClientSideCollectionAppState';
+import SeriesAppState, { SeriesIndexAppState } from 'App/State/SeriesAppState';
+import { REFRESH_SERIES } from 'Commands/commandNames';
+import PageToolbarButton from 'Components/Page/Toolbar/PageToolbarButton';
+import { icons } from 'Helpers/Props';
+import { executeCommand } from 'Store/Actions/commandActions';
+import createCommandExecutingSelector from 'Store/Selectors/createCommandExecutingSelector';
+import createSeriesClientSideCollectionItemsSelector from 'Store/Selectors/createSeriesClientSideCollectionItemsSelector';
+import getSelectedIds from 'Utilities/Table/getSelectedIds';
+
+interface SeriesIndexRefreshSeriesButtonProps {
+  isSelectMode: boolean;
+  selectedFilterKey: string;
+}
+
+function SeriesIndexRefreshSeriesButton(
+  props: SeriesIndexRefreshSeriesButtonProps
+) {
+  const isRefreshing = useSelector(
+    createCommandExecutingSelector(REFRESH_SERIES)
+  );
+  const {
+    items,
+    totalItems,
+  }: SeriesAppState & SeriesIndexAppState & ClientSideCollectionAppState =
+    useSelector(createSeriesClientSideCollectionItemsSelector('seriesIndex'));
+
+  const dispatch = useDispatch();
+  const { isSelectMode, selectedFilterKey } = props;
+  const [selectState] = useSelect();
+  const { selectedState } = selectState;
+
+  const selectedSeriesIds = useMemo(() => {
+    return getSelectedIds(selectedState);
+  }, [selectedState]);
+
+  const seriesToRefresh =
+    isSelectMode && selectedSeriesIds.length > 0
+      ? selectedSeriesIds
+      : items.map((m) => m.id);
+
+  const refreshIndexLabel =
+    selectedFilterKey === 'all' ? 'Update All' : 'Update Filtered';
+
+  const refreshSelectLabel =
+    selectedSeriesIds.length > 0 ? 'Update Selected' : 'Update All';
+
+  const onPress = useCallback(() => {
+    dispatch(
+      executeCommand({
+        name: REFRESH_SERIES,
+        seriesIds: seriesToRefresh,
+      })
+    );
+  }, [dispatch, seriesToRefresh]);
+
+  return (
+    <PageToolbarButton
+      label={isSelectMode ? refreshSelectLabel : refreshIndexLabel}
+      isSpinning={isRefreshing}
+      isDisabled={!totalItems}
+      iconName={icons.REFRESH}
+      onPress={onPress}
+    />
+  );
+}
+
+export default SeriesIndexRefreshSeriesButton;
--- src/NzbDrone.Core/Tv/Commands/RefreshSeriesCommand.cs
+++ src/NzbDrone.Core/Tv/Commands/RefreshSeriesCommand.cs
@@ -1,25 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
 using NzbDrone.Core.Messaging.Commands;
 
 namespace NzbDrone.Core.Tv.Commands
 {
     public class RefreshSeriesCommand : Command
     {
-        public int? SeriesId { get; set; }
+        public List<int> SeriesIds { get; set; }
         public bool IsNewSeries { get; set; }
 
         public RefreshSeriesCommand()
         {
+            SeriesIds = new List<int>();
         }
 
-        public RefreshSeriesCommand(int? seriesId, bool isNewSeries = false)
+        public RefreshSeriesCommand(List<int> seriesIds, bool isNewSeries = false)
         {
-            SeriesId = seriesId;
+            SeriesIds = seriesIds;
             IsNewSeries = isNewSeries;
         }
 
         public override bool SendUpdatesToClient => true;
 
-        public override bool UpdateScheduledTask => !SeriesId.HasValue;
+        public override bool UpdateScheduledTask => !SeriesIds.Any();
 
         public override bool IsLongRunning => true;
     }
