d0471d4811b1a0a1660df00e1586cfc878929a65
Stevie Robinson <stevie.robinson@gmail.com>
2023-05-18 09:40:18 +0200

    add option for single int seriesId to avoid breaking change

--- src/NzbDrone.Core/Tv/Commands/RefreshSeriesCommand.cs
+++ src/NzbDrone.Core/Tv/Commands/RefreshSeriesCommand.cs
@@ -6,12 +6,18 @@ namespace NzbDrone.Core.Tv.Commands
 {
     public class RefreshSeriesCommand : Command
     {
+        public int? SeriesId { get; set; }
         public List<int> SeriesIds { get; set; }
         public bool IsNewSeries { get; set; }
 
         public RefreshSeriesCommand()
         {
             SeriesIds = new List<int>();
+        }
+
+        public RefreshSeriesCommand(int seriesId, bool isNewSeries = false)
+        {
+            SeriesIds = new List<int> { seriesId };
         }
 
         public RefreshSeriesCommand(List<int> seriesIds, bool isNewSeries = false)
