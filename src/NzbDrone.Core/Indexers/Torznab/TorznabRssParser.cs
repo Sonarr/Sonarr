9bdc6183663a3510e53433a30ad701065e7ee9d9
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-05-18 16:02:14 +0300

    Fixed: Use indexer errors from response if Content-Type is XML before processing response

--- src/NzbDrone.Core/Indexers/Torznab/TorznabRssParser.cs
+++ src/NzbDrone.Core/Indexers/Torznab/TorznabRssParser.cs
@@ -19,7 +19,8 @@ namespace NzbDrone.Core.Indexers.Torznab
 
         protected override bool PreProcess(IndexerResponse indexerResponse)
         {
-            if (indexerResponse.HttpResponse.HasHttpError)
+            if (indexerResponse.HttpResponse.HasHttpError &&
+                (indexerResponse.HttpResponse.Headers.ContentType == null || !indexerResponse.HttpResponse.Headers.ContentType.Contains("xml")))
             {
                 base.PreProcess(indexerResponse);
             }
