1ca954469b5125e84a6f7624d9000790d32491a0
Your Name <yohoho@testsmsd.com>
2023-05-21 17:43:16 +0200

    deletion of useless newlines (markus101 comment)

--- src/NzbDrone.Core/Notifications/Signal/SignalPayload.cs
+++ src/NzbDrone.Core/Notifications/Signal/SignalPayload.cs
@@ -3,9 +3,7 @@ namespace NzbDrone.Core.Notifications.Signal
     public class SignalPayload
     {
         public string Message { get; set; }
-
         public string Number { get; set; }
-
         public string[] Recipients { get; set; }
     }
 }
--- src/NzbDrone.Core/Notifications/Signal/SignalProxy.cs
+++ src/NzbDrone.Core/Notifications/Signal/SignalProxy.cs
@@ -65,7 +65,7 @@ namespace NzbDrone.Core.Notifications.Signal
             try
             {
                 const string title = "Test Notification";
-                const string body = "This is a test message from Radarr";
+                const string body = "This is a test message from Sonarr";
 
                 SendNotification(title, body, settings);
             }
