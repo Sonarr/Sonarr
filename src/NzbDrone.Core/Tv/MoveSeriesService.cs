88cd72e8eace18b9491d83a2123e69ba2b9a32ed
Qstick <qstick@gmail.com>
2023-07-09 23:08:05 -0500

    Fixed: Skip move when source and destination are the same ignore-downstream
    Co-Authored-By: Colin Hebert <makkhdyn@gmail.com>
    (cherry picked from commit 7a5ae56a96700f401726ac80b3031a25207d8f75)

--- src/NzbDrone.Core/Tv/MoveSeriesService.cs
+++ src/NzbDrone.Core/Tv/MoveSeriesService.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using NLog;
 using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Instrumentation.Extensions;
 using NzbDrone.Core.Messaging.Commands;
 using NzbDrone.Core.Messaging.Events;
@@ -49,6 +50,12 @@ namespace NzbDrone.Core.Tv
             else
             {
                 _logger.ProgressInfo("Moving {0} from '{1}' to '{2}'", series.Title, sourcePath, destinationPath);
+            }
+
+            if (sourcePath.PathEquals(destinationPath))
+            {
+                _logger.ProgressInfo("{0} is already in the specified location '{1}'.", series, destinationPath);
+                return;
             }
 
             try
