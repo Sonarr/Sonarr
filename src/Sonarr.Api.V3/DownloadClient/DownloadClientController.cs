a2193d96ee3746e87e5530169dfbddebad566685
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-11 10:32:41 +0300

    Fixed: (API) Mark empty bulk endpoints as not implemented

--- src/Sonarr.Api.V3/DownloadClient/DownloadClientController.cs
+++ src/Sonarr.Api.V3/DownloadClient/DownloadClientController.cs
@@ -6,8 +6,8 @@ namespace Sonarr.Api.V3.DownloadClient
     [V3ApiController]
     public class DownloadClientController : ProviderControllerBase<DownloadClientResource, DownloadClientBulkResource, IDownloadClient, DownloadClientDefinition>
     {
-        public static readonly DownloadClientResourceMapper ResourceMapper = new DownloadClientResourceMapper();
-        public static readonly DownloadClientBulkResourceMapper BulkResourceMapper = new DownloadClientBulkResourceMapper();
+        public static readonly DownloadClientResourceMapper ResourceMapper = new ();
+        public static readonly DownloadClientBulkResourceMapper BulkResourceMapper = new ();
 
         public DownloadClientController(IDownloadClientFactory downloadClientFactory)
             : base(downloadClientFactory, "downloadclient", ResourceMapper, BulkResourceMapper)
