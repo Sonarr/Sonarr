b779c92200b8e14398061199535bb3eb0841f626
Jessica Nguyen <122261093+jtnguyen45@users.noreply.github.com>
2023-07-22 17:11:22 -0700

    New: Simkl Anime List integration
    Co-authored-by: iceypotato <nickyjedi@gmail.com>
    Closes #5635

--- src/NzbDrone.Core/ImportLists/Simkl/User/SimklUserSettings.cs
+++ src/NzbDrone.Core/ImportLists/Simkl/User/SimklUserSettings.cs
@@ -19,9 +19,13 @@ namespace NzbDrone.Core.ImportLists.Simkl.User
         public SimklUserSettings()
         {
             ListType = (int)SimklUserListType.Watching;
+            ShowType = (int)SimklUserShowType.Shows;
         }
 
         [FieldDefinition(1, Label = "List Type", Type = FieldType.Select, SelectOptions = typeof(SimklUserListType), HelpText = "Type of list you're seeking to import from")]
         public int ListType { get; set; }
+
+        [FieldDefinition(1, Label = "Show Type", Type = FieldType.Select, SelectOptions = typeof(SimklUserShowType), HelpText = "Type of show you're seeking to import from")]
+        public int ShowType { get; set; }
     }
 }
---
+++ src/NzbDrone.Core/ImportLists/Simkl/User/SimklUserShowType.cs
@@ -0,0 +1,8 @@
+namespace NzbDrone.Core.ImportLists.Simkl.User
+{
+    public enum SimklUserShowType
+    {
+        Shows = 0,
+        Anime = 1
+    }
+}
