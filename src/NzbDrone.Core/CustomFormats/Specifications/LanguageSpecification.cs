65323d5e872cb87b1f3d16c520aef373f4447915
Mark McDowall <mark@mcdowall.ca>
2023-08-07 18:07:03 -0700

    Fixed: Allow Unknown Language in Custom Format
    Closes #5886

--- src/NzbDrone.Core/CustomFormats/Specifications/LanguageSpecification.cs
+++ src/NzbDrone.Core/CustomFormats/Specifications/LanguageSpecification.cs
@@ -10,7 +10,7 @@ namespace NzbDrone.Core.CustomFormats
     {
         public LanguageSpecificationValidator()
         {
-            RuleFor(c => c.Value).NotEmpty();
+            RuleFor(c => c.Value).GreaterThanOrEqualTo(0);
             RuleFor(c => c.Value).Custom((value, context) =>
             {
                 if (!Language.All.Any(o => o.Id == value))
