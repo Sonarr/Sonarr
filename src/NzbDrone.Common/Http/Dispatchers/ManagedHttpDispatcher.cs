e9f0ac2165f867c4dab1ffe649e53ebb6fc87a7b
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-08-30 22:11:27 +0300

    Use await on reading the response content

--- src/NzbDrone.Common/Http/Dispatchers/ManagedHttpDispatcher.cs
+++ src/NzbDrone.Common/Http/Dispatchers/ManagedHttpDispatcher.cs
@@ -114,7 +114,7 @@ namespace NzbDrone.Common.Http.Dispatchers
                     }
                     else
                     {
-                        data = responseMessage.Content.ReadAsByteArrayAsync(cts.Token).GetAwaiter().GetResult();
+                        data = await responseMessage.Content.ReadAsByteArrayAsync(cts.Token);
                     }
                 }
                 catch (Exception ex)
