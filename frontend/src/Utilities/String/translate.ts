a5539a0a9842e9f25de4a2f42ff1b34f50cf654a
Qstick <qstick@gmail.com>
2023-09-10 01:06:46 -0500

    Use variable for App name in translations

--- frontend/src/Utilities/String/translate.ts
+++ frontend/src/Utilities/String/translate.ts
@@ -25,7 +25,7 @@ export async function fetchTranslations(): Promise<boolean> {
 
 export default function translate(
   key: string,
-  tokens?: Record<string, string | number | boolean>
+  tokens: Record<string, string | number | boolean> = { appName: 'Sonarr' }
 ) {
   const translation = translations[key] || key;
 
