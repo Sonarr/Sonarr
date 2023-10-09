d9bd9e11a7002ed46ce6e083f2b34713a6f4ea15
Mark McDowall <markus.mcd5@gmail.com>
2023-10-08 23:26:16 -0400

    Update src/NzbDrone.Core/Organizer/FileNameBuilder.cs

--- src/NzbDrone.Core/Organizer/FileNameBuilder.cs
+++ src/NzbDrone.Core/Organizer/FileNameBuilder.cs
@@ -384,7 +384,7 @@ namespace NzbDrone.Core.Organizer
                 return title.Substring(0, 1).ToUpper().RemoveAccent();
             }
 
-            // try the second character if the first was non alphanumeric
+            // Try the second character if the first was non alphanumeric
             if (char.IsLetterOrDigit(title[1]))
             {
                 return title.Substring(1, 1).ToUpper().RemoveAccent();
