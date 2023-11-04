10d0312b5cffa0433bdd9a805a97db51c21c977b
Stevie Robinson <stevie.robinson@gmail.com>
2023-11-04 11:03:17 +0100

    remove unused using

--- src/NzbDrone.Core.Test/Download/DownloadClientTests/HadoukenTests/HadoukenFixture.cs
+++ src/NzbDrone.Core.Test/Download/DownloadClientTests/HadoukenTests/HadoukenFixture.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
-using FluentValidation.Results;
 using Moq;
 using NUnit.Framework;
 using NzbDrone.Common.Http;
