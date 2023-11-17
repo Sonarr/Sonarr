5328fb4ab31b643449a38adb4abcd2ea57371df4
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-11-17 17:25:38 +0200

    New: Additional options for HDBits

--- src/NzbDrone.Core/Indexers/HDBits/HDBits.cs
+++ src/NzbDrone.Core/Indexers/HDBits/HDBits.cs
@@ -12,7 +12,7 @@ namespace NzbDrone.Core.Indexers.HDBits
         public override DownloadProtocol Protocol => DownloadProtocol.Torrent;
         public override bool SupportsRss => true;
         public override bool SupportsSearch => true;
-        public override int PageSize => 30;
+        public override int PageSize => 100;
 
         public HDBits(IHttpClient httpClient, IIndexerStatusService indexerStatusService, IConfigService configService, IParsingService parsingService, Logger logger, ILocalizationService localizationService)
             : base(httpClient, indexerStatusService, configService, parsingService, logger, localizationService)
@@ -21,7 +21,7 @@ namespace NzbDrone.Core.Indexers.HDBits
 
         public override IIndexerRequestGenerator GetRequestGenerator()
         {
-            return new HDBitsRequestGenerator() { Settings = Settings };
+            return new HDBitsRequestGenerator { Settings = Settings };
         }
 
         public override IParseIndexerResponse GetParser()
