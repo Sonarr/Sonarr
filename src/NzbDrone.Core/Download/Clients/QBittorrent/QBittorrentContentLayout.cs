4b22200708ca120cfdcf9cb796be92183adb95d1
Mark McDowall <mark@mcdowall.ca>
2023-11-25 15:52:01 -0800

    New: Add qBittorrent option for Content Layout
    Closes #6213

---
+++ src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrentContentLayout.cs
@@ -0,0 +1,9 @@
+namespace NzbDrone.Core.Download.Clients.QBittorrent
+{
+    public enum QBittorrentContentLayout
+    {
+        Default = 0,
+        Original = 1,
+        Subfolder = 2
+    }
+}
