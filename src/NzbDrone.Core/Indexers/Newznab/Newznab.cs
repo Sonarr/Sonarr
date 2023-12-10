f5ffcfd7a02529573db45c37e9cf3b1507b7de46
Fossil <Fossil01@users.noreply.github.com>
2023-12-10 14:05:03 +0100

    Updated NZB Finder categories and removed OZnzb
    NZB Finder will consolidate WEBDL & X265 into SD,HD,UHD so removed 5010 and 5090 categories.
    
    OZnzb is dead so removed it from the presets list.

--- src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
+++ src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
@@ -52,7 +52,7 @@ namespace NzbDrone.Core.Indexers.Newznab
                 yield return GetDefinition("DrunkenSlug", GetSettings("https://drunkenslug.com"));
                 yield return GetDefinition("Nzb.su", GetSettings("https://api.nzb.su"));
                 yield return GetDefinition("NZBCat", GetSettings("https://nzb.cat"));
-                yield return GetDefinition("NZBFinder.ws", GetSettings("https://nzbfinder.ws", categories: new[] { 5010, 5030, 5040, 5045, 5090 }));
+                yield return GetDefinition("NZBFinder.ws", GetSettings("https://nzbfinder.ws", categories: new[] { 5030, 5040, 5045 }));
                 yield return GetDefinition("NZBgeek", GetSettings("https://api.nzbgeek.info"));
                 yield return GetDefinition("nzbplanet.net", GetSettings("https://api.nzbplanet.net"));
                 yield return GetDefinition("OZnzb.com", GetSettings("https://api.oznzb.com"));
