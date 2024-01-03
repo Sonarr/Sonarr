d172f2e6d919b07248635160a5f90185ffe48b32
Mark McDowall <mark@mcdowall.ca>
2024-01-03 11:42:31 -0800

    Remove version.sh

--- version.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# Increment packageVersion when package scripts change
-packageVersion='3.0.9'
-
-# For now we keep the build version and package version the same
-buildVersion=$packageVersion
-
