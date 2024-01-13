53cf5308931069638c23925596a3fd8aaccc5d98
Mark McDowall <mark@mcdowall.ca>
2024-01-12 23:17:29 -0800

    Fixed: Series posters flickering when width changes repeatedly
    Closes #6311

--- frontend/src/Series/Index/Posters/SeriesIndexPosters.tsx
+++ frontend/src/Series/Index/Posters/SeriesIndexPosters.tsx
@@ -202,13 +202,18 @@ export default function SeriesIndexPosters(props: SeriesIndexPostersProps) {
     if (current) {
       const width = current.clientWidth;
       const padding = bodyPadding - 5;
+      const finalWidth = width - padding * 2;
+
+      if (Math.abs(size.width - finalWidth) < 20 || size.width === finalWidth) {
+        return;
+      }
 
       setSize({
-        width: width - padding * 2,
+        width: finalWidth,
         height: window.innerHeight,
       });
     }
-  }, [isSmallScreen, scrollerRef, bounds]);
+  }, [isSmallScreen, size, scrollerRef, bounds]);
 
   useEffect(() => {
     const currentScrollerRef = scrollerRef.current as HTMLElement;
