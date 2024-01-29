2199e09e687097cc356f64bc1e2e9ea759a19ed5
Mark McDowall <mark@mcdowall.ca>
2024-01-28 20:49:46 -0800

    Fixed: Grouped calendar events not correctly showing as downloading
    Closes #6441

--- frontend/src/Calendar/Events/CalendarEventGroupConnector.js
+++ frontend/src/Calendar/Events/CalendarEventGroupConnector.js
@@ -10,7 +10,7 @@ function createIsDownloadingSelector() {
     (state) => state.queue.details,
     (episodeIds, details) => {
       return details.items.some((item) => {
-        return item.episode && episodeIds.includes(item.episode.id);
+        return !!(item.episodeId && episodeIds.includes(item.episodeId));
       });
     }
   );
