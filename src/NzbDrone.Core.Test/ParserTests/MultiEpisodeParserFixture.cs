372e2facddc4de8ad6d9b44f4666fc38382f0656
Stas Panasiuk <temnyip@gmail.com>
2024-01-29 22:56:15 -0500

    New: Parsing for titles with multiple translated titles separated by '/' and the season at the end

--- src/NzbDrone.Core.Test/ParserTests/MultiEpisodeParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/MultiEpisodeParserFixture.cs
@@ -76,6 +76,7 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Series T Se.3 afl.3 en 4", "Series T", 3, new[] { 3, 4 })]
         [TestCase("Series Title (S15E06-08) City Sushi", "Series Title", 15, new[] { 6, 7, 8 })]
         [TestCase("Босх: Спадок (S2E1-4) / Series: Legacy (S2E1-4) (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Series: Legacy", 2, new[] { 1, 2, 3, 4 })]
+        [TestCase("Босх: Спадок / Series: Legacy / S2E1-4 of 10 (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Series: Legacy", 2, new[] { 1, 2, 3, 4 })]
 
         // [TestCase("", "", , new [] {  })]
         public void should_parse_multiple_episodes(string postTitle, string title, int season, int[] episodes)
--- src/NzbDrone.Core.Test/ParserTests/SingleEpisodeParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/SingleEpisodeParserFixture.cs
@@ -165,6 +165,7 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Series Title [HDTV][Cap.402](website.com).avi", "Series Title", 4, 2)]
         [TestCase("Series Title [HDTV 720p][Cap.101](website.com).mkv", "Series Title", 1, 1)]
         [TestCase("Босх: Спадок (S2E1) / Series: Legacy (S2E1) (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Series: Legacy", 2, 1)]
+        [TestCase("Босх: Спадок / Series: Legacy / S2E1 of 10 (2023) WEB-DL 1080p Ukr/Eng | sub Eng", "Series: Legacy", 2, 1)]
 
         // [TestCase("", "", 0, 0)]
         public void should_parse_single_episode(string postTitle, string title, int seasonNumber, int episodeNumber)
