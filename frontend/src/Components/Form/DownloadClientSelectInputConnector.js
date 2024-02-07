52a688d1d57b1980ea64b8fc0460483e65451edf
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-02-07 10:05:01 +0200

    Show download client ID as hint in select options

--- frontend/src/Components/Form/DownloadClientSelectInputConnector.js
+++ frontend/src/Components/Form/DownloadClientSelectInputConnector.js
@@ -26,7 +26,8 @@ function createMapStateToProps() {
       const values = _.map(filteredItems.sort(sortByName), (downloadClient) => {
         return {
           key: downloadClient.id,
-          value: downloadClient.name
+          value: downloadClient.name,
+          hint: `(${downloadClient.id})`
         };
       });
 
