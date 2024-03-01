16293f4c969164f99054a7cfdeaa588f4ba4a560
Uruk <uruknarb20@gmail.com>
2024-03-02 00:39:34 +0100

    Update to .NET 8  - Update project to .NET 8  - Update .NETFramework to 4.8.1  - Update NuGet packages

--- .github/actions/package/package.sh
+++ .github/actions/package/package.sh
@@ -3,7 +3,7 @@
 outputFolder=_output
 artifactsFolder=_artifacts
 uiFolder="$outputFolder/UI"
-framework="${FRAMEWORK:=net6.0}"
+framework="${FRAMEWORK:=net8.0}"
 
 rm -rf $artifactsFolder
 mkdir $artifactsFolder
