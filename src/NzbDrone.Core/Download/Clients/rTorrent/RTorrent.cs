1a575a7b6bb5e3c7826c41afc04c4d09f3c8e8a4
Stevie Robinson <stevie.robinson@gmail.com>
2024-03-24 19:42:33 +0100

    Adjust reporting of torrents with relative path in rTorrent

--- src/NzbDrone.Core/Download/Clients/rTorrent/RTorrent.cs
+++ src/NzbDrone.Core/Download/Clients/rTorrent/RTorrent.cs
@@ -139,12 +139,14 @@ namespace NzbDrone.Core.Download.Clients.RTorrent
                 // Ignore torrents with an empty path
                 if (torrent.Path.IsNullOrWhiteSpace())
                 {
+                    _logger.Warn($"Torrent '{torrent.Name}' has an empty download path and will not be processed. Adjust this to an absolute path in rTorrent");
                     continue;
                 }
 
                 if (torrent.Path.StartsWith("."))
                 {
-                    throw new DownloadClientException("Download paths must be absolute. Please specify variable \"directory\" in rTorrent.");
+                    _logger.Warn($"Torrent '{torrent.Name}' has a download path starting with '.' and will not be processed. Adjust this to an absolute path in rTorrent");
+                    continue;
                 }
 
                 var item = new DownloadClientItem();
