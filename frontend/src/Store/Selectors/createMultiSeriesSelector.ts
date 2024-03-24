fc6494c569324c839debdb1d08dde23b8f1b8d76
Mark McDowall <mark@mcdowall.ca>
2024-03-23 21:42:54 -0700

    Fixed: Task with removed series causing error

--- frontend/src/Store/Selectors/createMultiSeriesSelector.ts
+++ frontend/src/Store/Selectors/createMultiSeriesSelector.ts
@@ -1,12 +1,21 @@
 import { createSelector } from 'reselect';
 import AppState from 'App/State/AppState';
+import Series from 'Series/Series';
 
 function createMultiSeriesSelector(seriesIds: number[]) {
   return createSelector(
     (state: AppState) => state.series.itemMap,
     (state: AppState) => state.series.items,
     (itemMap, allSeries) => {
-      return seriesIds.map((seriesId) => allSeries[itemMap[seriesId]]);
+      return seriesIds.reduce((acc: Series[], seriesId) => {
+        const series = allSeries[itemMap[seriesId]];
+
+        if (series) {
+          acc.push(series);
+        }
+
+        return acc;
+      }, []);
     }
   );
 }
