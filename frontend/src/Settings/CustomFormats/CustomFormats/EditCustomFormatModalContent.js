27dcb9a975b67db047d4cd2b19e9df965d9358e9
Stevie Robinson <stevie.robinson@gmail.com>
2024-04-01 12:00:28 +0200

    add info box to custom formats modal

--- frontend/src/Settings/CustomFormats/CustomFormats/EditCustomFormatModalContent.js
+++ frontend/src/Settings/CustomFormats/CustomFormats/EditCustomFormatModalContent.js
@@ -151,6 +151,11 @@ class EditCustomFormatModalContent extends Component {
                   </Form>
 
                   <FieldSet legend={translate('Conditions')}>
+                    <Alert kind={kinds.INFO}>
+                      <div>
+                        {translate('CustomFormatsSettingsTriggerInfo')}
+                      </div>
+                    </Alert>
                     <div className={styles.customFormats}>
                       {
                         specifications.map((tag) => {
