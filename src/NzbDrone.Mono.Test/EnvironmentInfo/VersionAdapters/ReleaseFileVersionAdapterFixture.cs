9dee7a3a73c1e185cc524684ac0155cac101868f
Uruk <uruknarb20@gmail.com>
2024-04-18 19:05:20 +0200

    Update to .NET 8
    Update discord webhook
    
    update ubuntu to 22, minimum required by net8
    
    remove serialization

--- src/NzbDrone.Mono.Test/EnvironmentInfo/VersionAdapters/ReleaseFileVersionAdapterFixture.cs
+++ src/NzbDrone.Mono.Test/EnvironmentInfo/VersionAdapters/ReleaseFileVersionAdapterFixture.cs
@@ -76,8 +76,8 @@ namespace NzbDrone.Mono.Test.EnvironmentInfo.VersionAdapters
             var version = Subject.Read();
             version.Should().NotBeNull();
             version.Name.Should().Be("ubuntu");
-            version.Version.Should().Be("14.04");
-            version.FullName.Should().Be("Ubuntu 14.04.5 LTS");
+            version.Version.Should().Be("22.04");
+            version.FullName.Should().Be("Ubuntu 22.04.4 LTS");
         }
     }
 }
