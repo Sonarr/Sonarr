699acf0ed27b1273c7230830062e5a9e71f8782c
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-04-25 11:06:10 +0300

    Treat CorruptDatabaseException as a startup failure
    ignore-downstream

--- src/NzbDrone.Core/Datastore/CorruptDatabaseException.cs
+++ src/NzbDrone.Core/Datastore/CorruptDatabaseException.cs
@@ -3,7 +3,7 @@ using NzbDrone.Common.Exceptions;
 
 namespace NzbDrone.Core.Datastore
 {
-    public class CorruptDatabaseException : NzbDroneException
+    public class CorruptDatabaseException : SonarrStartupException
     {
         public CorruptDatabaseException(string message, params object[] args)
             : base(message, args)
@@ -16,12 +16,12 @@ namespace NzbDrone.Core.Datastore
         }
 
         public CorruptDatabaseException(string message, Exception innerException, params object[] args)
-            : base(message, innerException, args)
+            : base(innerException, message, args)
         {
         }
 
         public CorruptDatabaseException(string message, Exception innerException)
-            : base(message, innerException)
+            : base(innerException, message)
         {
         }
     }
