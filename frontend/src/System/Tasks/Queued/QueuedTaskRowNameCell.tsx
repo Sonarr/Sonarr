da8b9ba1ad506249bb2db4ff2d92e51354dbf50a
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-04-25 22:59:00 +0300

    Fixed: Limit titles in task name to 10 series

--- frontend/src/System/Tasks/Queued/QueuedTaskRowNameCell.tsx
+++ frontend/src/System/Tasks/Queued/QueuedTaskRowNameCell.tsx
@@ -6,6 +6,22 @@ import createMultiSeriesSelector from 'Store/Selectors/createMultiSeriesSelector
 import translate from 'Utilities/String/translate';
 import styles from './QueuedTaskRowNameCell.css';
 
+function formatTitles(titles: string[]) {
+  if (!titles) {
+    return null;
+  }
+
+  if (titles.length > 11) {
+    return (
+      <span title={titles.join(', ')}>
+        {titles.slice(0, 10).join(', ')}, {titles.length - 10} more
+      </span>
+    );
+  }
+
+  return <span>{titles.join(', ')}</span>;
+}
+
 export interface QueuedTaskRowNameCellProps {
   commandName: string;
   body: CommandBody;
@@ -32,7 +48,7 @@ export default function QueuedTaskRowNameCell(
       <span className={styles.commandName}>
         {commandName}
         {sortedSeries.length ? (
-          <span> - {sortedSeries.map((s) => s.title).join(', ')}</span>
+          <span> - {formatTitles(sortedSeries.map((s) => s.title))}</span>
         ) : null}
         {body.seasonNumber ? (
           <span>
