4440aa3cac545e48549b30256c2d193b23feb55f
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-05-09 20:09:45 +0300

    New: Root folder exists validation for import lists

--- src/Sonarr.Api.V3/ImportLists/ImportListController.cs
+++ src/Sonarr.Api.V3/ImportLists/ImportListController.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using NzbDrone.Core.ImportLists;
 using NzbDrone.Core.Validation;
 using NzbDrone.Core.Validation.Paths;
@@ -11,13 +12,16 @@ namespace Sonarr.Api.V3.ImportLists
         public static readonly ImportListResourceMapper ResourceMapper = new ();
         public static readonly ImportListBulkResourceMapper BulkResourceMapper = new ();
 
-        public ImportListController(IImportListFactory importListFactory, QualityProfileExistsValidator qualityProfileExistsValidator)
+        public ImportListController(IImportListFactory importListFactory, RootFolderExistsValidator rootFolderExistsValidator, QualityProfileExistsValidator qualityProfileExistsValidator)
             : base(importListFactory, "importlist", ResourceMapper, BulkResourceMapper)
         {
-            Http.Validation.RuleBuilderExtensions.ValidId(SharedValidator.RuleFor(s => s.QualityProfileId));
+            SharedValidator.RuleFor(c => c.RootFolderPath).Cascade(CascadeMode.Stop)
+                .IsValidPath()
+                .SetValidator(rootFolderExistsValidator);
 
-            SharedValidator.RuleFor(c => c.RootFolderPath).IsValidPath();
-            SharedValidator.RuleFor(c => c.QualityProfileId).SetValidator(qualityProfileExistsValidator);
+            SharedValidator.RuleFor(c => c.QualityProfileId).Cascade(CascadeMode.Stop)
+                .ValidId()
+                .SetValidator(qualityProfileExistsValidator);
         }
     }
 }
