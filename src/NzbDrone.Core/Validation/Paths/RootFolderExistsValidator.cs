bb7c81f773428a799b2b7762b1c95cb1b2ad777d
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-05-09 20:09:45 +0300

    New: Root folder exists validation for import lists

---
+++ src/NzbDrone.Core/Validation/Paths/RootFolderExistsValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Validators;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.RootFolders;
+
+namespace NzbDrone.Core.Validation.Paths
+{
+    public class RootFolderExistsValidator : PropertyValidator
+    {
+        private readonly IRootFolderService _rootFolderService;
+
+        public RootFolderExistsValidator(IRootFolderService rootFolderService)
+        {
+            _rootFolderService = rootFolderService;
+        }
+
+        protected override string GetDefaultMessageTemplate() => "Root folder '{path}' does not exist";
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            context.MessageFormatter.AppendArgument("path", context.PropertyValue?.ToString());
+
+            return context.PropertyValue == null || _rootFolderService.All().Exists(r => r.Path.PathEquals(context.PropertyValue.ToString()));
+        }
+    }
+}
