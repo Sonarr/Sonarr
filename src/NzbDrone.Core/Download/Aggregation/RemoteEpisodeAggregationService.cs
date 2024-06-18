7fccf590a815179cbb6512c1de33e9cff1962f7d
Mark McDowall <mark@mcdowall.ca>
2024-06-17 20:39:13 -0700

    Fixed: Adding series with unknown items in queue

--- src/NzbDrone.Core/Download/Aggregation/RemoteEpisodeAggregationService.cs
+++ src/NzbDrone.Core/Download/Aggregation/RemoteEpisodeAggregationService.cs
@@ -25,6 +25,11 @@ namespace NzbDrone.Core.Download.Aggregation
 
         public RemoteEpisode Augment(RemoteEpisode remoteEpisode)
         {
+            if (remoteEpisode == null)
+            {
+                return null;
+            }
+
             foreach (var augmenter in _augmenters)
             {
                 try
