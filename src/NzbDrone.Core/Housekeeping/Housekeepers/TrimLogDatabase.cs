84bbc2f4fc54dfc8fe672ae75b95822c002f8aa1
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-06-30 10:03:17 +0300

    Disable trim log database housekeeping task

--- src/NzbDrone.Core/Housekeeping/Housekeepers/TrimLogDatabase.cs
+++ src/NzbDrone.Core/Housekeeping/Housekeepers/TrimLogDatabase.cs
@@ -1,18 +1,26 @@
-﻿using NzbDrone.Core.Instrumentation;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Instrumentation;
 
 namespace NzbDrone.Core.Housekeeping.Housekeepers
 {
     public class TrimLogDatabase : IHousekeepingTask
     {
         private readonly ILogRepository _logRepo;
+        private readonly IConfigFileProvider _configFileProvider;
 
-        public TrimLogDatabase(ILogRepository logRepo)
+        public TrimLogDatabase(ILogRepository logRepo, IConfigFileProvider configFileProvider)
         {
             _logRepo = logRepo;
+            _configFileProvider = configFileProvider;
         }
 
         public void Clean()
         {
+            if (!_configFileProvider.LogDbEnabled)
+            {
+                return;
+            }
+
             _logRepo.Trim();
         }
     }
