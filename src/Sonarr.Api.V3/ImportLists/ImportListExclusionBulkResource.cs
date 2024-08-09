9e7c74c2acce114f94d8daeb94fa1c813245c402
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-08-09 20:05:07 +0300

    New: Bulk import list exclusions removal

---
+++ src/Sonarr.Api.V3/ImportLists/ImportListExclusionBulkResource.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Sonarr.Api.V3.ImportLists
+{
+    public class ImportListExclusionBulkResource
+    {
+        public HashSet<int> Ids { get; set; }
+    }
+}
