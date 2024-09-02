ee99c3895de497bb1c99193ba16c56393b8ff593
Mark McDowall <mark@mcdowall.ca>
2024-09-02 16:05:19 -0700

    Convert series images to TypeScript

--- frontend/src/Series/Series.ts
+++ frontend/src/Series/Series.ts
@@ -19,8 +19,10 @@ export type SeriesStatus = 'continuing' | 'ended' | 'upcoming' | 'deleted';
 
 export type MonitorNewItems = 'all' | 'none';
 
+export type CoverType = 'poster' | 'banner' | 'fanart' | 'season';
+
 export interface Image {
-  coverType: string;
+  coverType: CoverType;
   url: string;
   remoteUrl: string;
 }
