afa609dc38e5795f105391dfc0870a91d529c336
Mark McDowall <mark@mcdowall.ca>
2024-09-02 16:04:56 -0700

    Convert NoSeries to TypeScript

--- frontend/src/Series/NoSeries.tsx
+++ frontend/src/Series/NoSeries.tsx
@@ -1,11 +1,14 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import Button from 'Components/Link/Button';
 import { kinds } from 'Helpers/Props';
 import translate from 'Utilities/String/translate';
 import styles from './NoSeries.css';
 
-function NoSeries(props) {
+interface NoSeriesProps {
+  totalItems: number;
+}
+
+function NoSeries(props: NoSeriesProps) {
   const { totalItems } = props;
 
   if (totalItems > 0) {
@@ -25,28 +28,18 @@ function NoSeries(props) {
       </div>
 
       <div className={styles.buttonContainer}>
-        <Button
-          to="/add/import"
-          kind={kinds.PRIMARY}
-        >
+        <Button to="/add/import" kind={kinds.PRIMARY}>
           {translate('ImportExistingSeries')}
         </Button>
       </div>
 
       <div className={styles.buttonContainer}>
-        <Button
-          to="/add/new"
-          kind={kinds.PRIMARY}
-        >
+        <Button to="/add/new" kind={kinds.PRIMARY}>
           {translate('AddNewSeries')}
         </Button>
       </div>
     </div>
   );
 }
-
-NoSeries.propTypes = {
-  totalItems: PropTypes.number.isRequired
-};
 
 export default NoSeries;
