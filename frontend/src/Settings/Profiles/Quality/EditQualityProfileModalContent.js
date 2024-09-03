cde1ec6776d749c065023258e89d6cb15be9b194
somniumV <179984073+somniumV@users.noreply.github.com>
2024-09-03 21:35:29 +0200

    Improvement: Better Helptext and Naming

--- frontend/src/Settings/Profiles/Quality/EditQualityProfileModalContent.js
+++ frontend/src/Settings/Profiles/Quality/EditQualityProfileModalContent.js
@@ -249,7 +249,7 @@ class EditQualityProfileModalContent extends Component {
                           upgradeAllowed.value && formatItems.value.length > 0 &&
                             <FormGroup size={sizes.EXTRA_SMALL}>
                               <FormLabel size={sizes.SMALL}>
-                                {translate('MinimumCustomFormatScoreIncrease')}
+                                {translate('MinimumCustomFormatScoreIncrement')}
                               </FormLabel>
 
                               <FormInputGroup
@@ -257,7 +257,7 @@ class EditQualityProfileModalContent extends Component {
                                 name="minUpgradeFormatScore"
                                 min={1}
                                 {...minUpgradeFormatScore}
-                                helpText={translate('MinimumCustomFormatScoreIncreaseHelpText')}
+                                helpText={translate('MinimumCustomFormatScoreIncrementHelpText')}
                                 onChange={onInputChange}
                               />
                             </FormGroup>
