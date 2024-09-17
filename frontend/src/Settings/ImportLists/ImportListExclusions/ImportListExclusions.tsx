Radarr/Radarr

New: Allowed sort keys for paginated resources

frontend/src/Settings/ImportLists/ImportListExclusions/ImportListExclusions.tsx
@@ -42,7 +42,7 @@ import styles from './ImportListExclusions.css';
 
 const COLUMNS: Column[] = [
   {
-    name: 'tmdbid',
+    name: 'tmdbId',
     label: () => translate('TMDBId'),
     isVisible: true,
     isSortable: true,

src/Radarr.Api.V3/History/HistoryController.cs
@@ -64,7 +64,16 @@ namespace Radarr.Api.V3.History
         public PagingResource<HistoryResource> GetHistory([FromQuery] PagingRequestResource paging, bool includeMovie, [FromQuery(Name = "eventType")] int[] eventTypes, string downloadId, [FromQuery] int[] movieIds = null, [FromQuery] int[] languages = null, [FromQuery] int[] quality = null)
         {
             var pagingResource = new PagingResource<HistoryResource>(paging);
-            var pagingSpec = pagingResource.MapToPagingSpec<HistoryResource, MovieHistory>("date", SortDirection.Descending);
+            var pagingSpec = pagingResource.MapToPagingSpec<HistoryResource, MovieHistory>(
+                "date",
+                SortDirection.Descending,
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "movieMetadata.sortTitle",
+                    "languages",
+                    "quality",
+                    "date"
+                });
 
             if (eventTypes != null && eventTypes.Any())
             {

src/Radarr.Api.V3/Wanted/CutoffController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.CustomFormats;
@@ -45,13 +46,18 @@ namespace Radarr.Api.V3.Wanted
         public PagingResource<MovieResource> GetCutoffUnmetMovies([FromQuery] PagingRequestResource paging, bool monitored = true)
         {
             var pagingResource = new PagingResource<MovieResource>(paging);
-            var pagingSpec = new PagingSpec<Movie>
-            {
-                Page = pagingResource.Page,
-                PageSize = pagingResource.PageSize,
-                SortKey = pagingResource.SortKey,
-                SortDirection = pagingResource.SortDirection
-            };
+            var pagingSpec = pagingResource.MapToPagingSpec<MovieResource, Movie>(
+                "movieMetadata.sortTitle",
+                SortDirection.Ascending,
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "movieMetadata.sortTitle",
+                    "movieMetadata.year",
+                    "movieMetadata.inCinemas",
+                    "movieMetadata.digitalRelease",
+                    "movieMetadata.physicalRelease",
+                    "movies.lastSearchTime"
+                });
 
             pagingSpec.FilterExpressions.Add(v => v.Monitored == monitored);
 

src/Radarr.Api.V3/Wanted/MissingController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.CustomFormats;
@@ -41,13 +42,18 @@ namespace Radarr.Api.V3.Wanted
         public PagingResource<MovieResource> GetMissingMovies([FromQuery] PagingRequestResource paging, bool monitored = true)
         {
             var pagingResource = new PagingResource<MovieResource>(paging);
-            var pagingSpec = new PagingSpec<Movie>
-            {
-                Page = pagingResource.Page,
-                PageSize = pagingResource.PageSize,
-                SortKey = pagingResource.SortKey,
-                SortDirection = pagingResource.SortDirection
-            };
+            var pagingSpec = pagingResource.MapToPagingSpec<MovieResource, Movie>(
+                "movieMetadata.sortTitle",
+                SortDirection.Ascending,
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "movieMetadata.sortTitle",
+                    "movieMetadata.year",
+                    "movieMetadata.inCinemas",
+                    "movieMetadata.digitalRelease",
+                    "movieMetadata.physicalRelease",
+                    "movies.lastSearchTime"
+                });
 
             pagingSpec.FilterExpressions.Add(v => v.Monitored == monitored);
 

Sonarr/Sonarr

frontend/src/Settings/ImportLists/ImportListExclusions/ImportListExclusions.tsx
@@ -48,7 +48,7 @@ const COLUMNS: Column[] = [
     isSortable: true,
   },
   {
-    name: 'tvdbid',
+    name: 'tvdbId',
     label: () => translate('TvdbId'),
     isVisible: true,
     isSortable: true,

src/Sonarr.Api.V3/History/HistoryController.cs
@@ -65,7 +65,14 @@ namespace Sonarr.Api.V3.History
         public PagingResource<HistoryResource> GetHistory([FromQuery] PagingRequestResource paging, bool includeSeries, bool includeEpisode, [FromQuery(Name = "eventType")] int[] eventTypes, int? episodeId, string downloadId, [FromQuery] int[] seriesIds = null, [FromQuery] int[] languages = null, [FromQuery] int[] quality = null)
         {
             var pagingResource = new PagingResource<HistoryResource>(paging);
-            var pagingSpec = pagingResource.MapToPagingSpec<HistoryResource, EpisodeHistory>("date", SortDirection.Descending);
+            var pagingSpec = pagingResource.MapToPagingSpec<HistoryResource, EpisodeHistory>(
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "series.sortTitle",
+                    "date"
+                },
+                "date",
+                SortDirection.Descending);
 
             if (eventTypes != null && eventTypes.Any())
             {

src/Sonarr.Api.V3/Wanted/CutoffController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Datastore;
@@ -31,13 +33,15 @@ namespace Sonarr.Api.V3.Wanted
         public PagingResource<EpisodeResource> GetCutoffUnmetEpisodes([FromQuery] PagingRequestResource paging, bool includeSeries = false, bool includeEpisodeFile = false, bool includeImages = false, bool monitored = true)
         {
             var pagingResource = new PagingResource<EpisodeResource>(paging);
-            var pagingSpec = new PagingSpec<Episode>
-            {
-                Page = pagingResource.Page,
-                PageSize = pagingResource.PageSize,
-                SortKey = pagingResource.SortKey,
-                SortDirection = pagingResource.SortDirection
-            };
+            var pagingSpec = pagingResource.MapToPagingSpec<EpisodeResource, Episode>(
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "series.sortTitle",
+                    "episodes.airDateUtc",
+                    "episodes.lastSearchTime"
+                },
+                "episodes.airDateUtc",
+                SortDirection.Ascending);
 
             if (monitored)
             {

src/Sonarr.Api.V3/Wanted/MissingController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Datastore;
@@ -27,13 +29,15 @@ namespace Sonarr.Api.V3.Wanted
         public PagingResource<EpisodeResource> GetMissingEpisodes([FromQuery] PagingRequestResource paging, bool includeSeries = false, bool includeImages = false, bool monitored = true)
         {
             var pagingResource = new PagingResource<EpisodeResource>(paging);
-            var pagingSpec = new PagingSpec<Episode>
-            {
-                Page = pagingResource.Page,
-                PageSize = pagingResource.PageSize,
-                SortKey = pagingResource.SortKey,
-                SortDirection = pagingResource.SortDirection
-            };
+            var pagingSpec = pagingResource.MapToPagingSpec<EpisodeResource, Episode>(
+                new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "series.sortTitle",
+                    "episodes.airDateUtc",
+                    "episodes.lastSearchTime"
+                },
+                "episodes.airDateUtc",
+                SortDirection.Ascending);
 
             if (monitored)
             {