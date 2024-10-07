Radarr/Radarr

Convert Naming options to TypeScript

frontend/src/typings/Settings/NamingExample.ts
@@ -1,0 +1,4 @@
+export default interface NamingExample {
+  movieExample: string;
+  movieFolderExample: string;
+}

Sonarr/Sonarr

frontend/src/typings/Settings/NamingExample.ts
@@ -1,0 +1,10 @@
+export default interface NamingExample {
+  singleEpisodeExample: string;
+  multiEpisodeExample: string;
+  dailyEpisodeExample: string;
+  animeEpisodeExample: string;
+  animeMultiEpisodeExample: string;
+  seriesFolderExample: string;
+  seasonFolderExample: string;
+  specialsFolderExample: string;
+}