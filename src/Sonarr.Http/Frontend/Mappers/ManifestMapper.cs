1fcfb88d2aa0126c4b3c878c8e310311ea57d04d
Mark McDowall <mark@mcdowall.ca>
2024-10-26 20:48:00 -0700

    New: Use instance name in PWA manifest
    Closes #7315

--- src/Sonarr.Http/Frontend/Mappers/ManifestMapper.cs
+++ src/Sonarr.Http/Frontend/Mappers/ManifestMapper.cs
@@ -8,9 +8,14 @@ namespace Sonarr.Http.Frontend.Mappers
 {
     public class ManifestMapper : UrlBaseReplacementResourceMapperBase
     {
+        private readonly IConfigFileProvider _configFileProvider;
+
+        private string _generatedContent;
+
         public ManifestMapper(IAppFolderInfo appFolderInfo, IDiskProvider diskProvider, IConfigFileProvider configFileProvider, Logger logger)
             : base(diskProvider, configFileProvider, logger)
         {
+            _configFileProvider = configFileProvider;
             FilePath = Path.Combine(appFolderInfo.StartUpFolder, configFileProvider.UiFolder, "Content", "manifest.json");
         }
 
@@ -22,6 +27,22 @@ namespace Sonarr.Http.Frontend.Mappers
         public override bool CanHandle(string resourceUrl)
         {
             return resourceUrl.StartsWith("/Content/manifest");
+        }
+
+        protected override string GetFileText()
+        {
+            if (RuntimeInfo.IsProduction && _generatedContent != null)
+            {
+                return _generatedContent;
+            }
+
+            var text = base.GetFileText();
+
+            text = text.Replace("__INSTANCE_NAME__", _configFileProvider.InstanceName);
+
+            _generatedContent = text;
+
+            return _generatedContent;
         }
     }
 }
