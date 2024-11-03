78cf13d341e6690bf6079dd1819d060d002155a7
Mark McDowall <mark@mcdowall.ca>
2024-11-03 14:46:31 -0800

    Increase retries for DebouncerFixture

--- src/NzbDrone.Common.Test/TPLTests/DebouncerFixture.cs
+++ src/NzbDrone.Common.Test/TPLTests/DebouncerFixture.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using FluentAssertions;
 using NUnit.Framework;
@@ -20,7 +20,7 @@ namespace NzbDrone.Common.Test.TPLTests
         }
 
         [Test]
-        [Retry(3)]
+        [Retry(10)]
         public void should_hold_the_call_for_debounce_duration()
         {
             var counter = new Counter();
@@ -38,7 +38,7 @@ namespace NzbDrone.Common.Test.TPLTests
         }
 
         [Test]
-        [Retry(3)]
+        [Retry(10)]
         public void should_throttle_calls()
         {
             var counter = new Counter();
@@ -62,7 +62,7 @@ namespace NzbDrone.Common.Test.TPLTests
         }
 
         [Test]
-        [Retry(3)]
+        [Retry(10)]
         public void should_hold_the_call_while_paused()
         {
             var counter = new Counter();
@@ -96,7 +96,7 @@ namespace NzbDrone.Common.Test.TPLTests
         }
 
         [Test]
-        [Retry(3)]
+        [Retry(10)]
         public void should_handle_pause_reentrancy()
         {
             var counter = new Counter();
