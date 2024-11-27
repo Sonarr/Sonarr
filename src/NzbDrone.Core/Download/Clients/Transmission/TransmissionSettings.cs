a0d09a30f9732af19e9bb532f0eeee4f31b8e606
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-11-27 23:55:50 +0200

    Fixed: Avoid default category on existing Transmission configurations
    Co-authored-by: Mark McDowall <mark@mcdowall.ca>

--- src/NzbDrone.Core/Download/Clients/Transmission/TransmissionSettings.cs
+++ src/NzbDrone.Core/Download/Clients/Transmission/TransmissionSettings.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using FluentValidation;
 using NzbDrone.Common.Extensions;
@@ -27,12 +28,25 @@ namespace NzbDrone.Core.Download.Clients.Transmission
     {
         private static readonly TransmissionSettingsValidator Validator = new ();
 
+        // This constructor is used when creating a new instance, such as the user adding a new Transmission client.
         public TransmissionSettings()
         {
             Host = "localhost";
             Port = 9091;
             UrlBase = "/transmission/";
             TvCategory = "tv-sonarr";
+        }
+
+        // TODO: Remove this in v5
+        // This constructor is used when deserializing from JSON, it will set the
+        // category to the deserialized value, defaulting to null.
+        [JsonConstructor]
+        public TransmissionSettings(string tvCategory = null)
+        {
+            Host = "localhost";
+            Port = 9091;
+            UrlBase = "/transmission/";
+            TvCategory = tvCategory;
         }
 
         [FieldDefinition(0, Label = "Host", Type = FieldType.Textbox)]
