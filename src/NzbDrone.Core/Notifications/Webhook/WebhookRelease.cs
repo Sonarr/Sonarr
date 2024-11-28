cfb99782f8d6f87d2f897781802094dfef38bba7
Stevie Robinson <stevie.robinson@gmail.com>
2024-11-28 20:08:37 +0100

    added languages string to WebhookRelease

--- src/NzbDrone.Core/Notifications/Webhook/WebhookRelease.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookRelease.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Qualities;
 
@@ -21,6 +22,7 @@ namespace NzbDrone.Core.Notifications.Webhook
             Size = remoteEpisode.Release.Size;
             CustomFormats = remoteEpisode.CustomFormats?.Select(x => x.Name).ToList();
             CustomFormatScore = remoteEpisode.CustomFormatScore;
+            Languages = remoteEpisode.Languages.SelectList(language => language.Name).ConcatToString("/");
         }
 
         public string Quality { get; set; }
@@ -31,5 +33,6 @@ namespace NzbDrone.Core.Notifications.Webhook
         public long Size { get; set; }
         public int CustomFormatScore { get; set; }
         public List<string> CustomFormats { get; set; }
+        public string Languages { get; set; }
     }
 }
