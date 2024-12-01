8b35271f5bfc4336a1ee6cbf6f0129681ab11b3d
Stevie Robinson <stevie.robinson@gmail.com>
2024-12-01 15:17:50 +0100

    pass OriginalLanguage as Language type instead of string

--- src/NzbDrone.Core/Notifications/Webhook/WebhookSeries.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookSeries.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using NzbDrone.Core.Languages;
 using NzbDrone.Core.Tv;
 
 namespace NzbDrone.Core.Notifications.Webhook
@@ -19,7 +20,7 @@ namespace NzbDrone.Core.Notifications.Webhook
         public List<string> Genres { get; set; }
         public List<WebhookImage> Images { get; set; }
         public List<string> Tags { get; set; }
-        public string OriginalLanguage { get; set; }
+        public Language OriginalLanguage { get; set; }
 
         public WebhookSeries()
         {
@@ -40,7 +41,7 @@ namespace NzbDrone.Core.Notifications.Webhook
             Genres = series.Genres;
             Images = series.Images.Select(i => new WebhookImage(i)).ToList();
             Tags = tags;
-            OriginalLanguage = series.OriginalLanguage.ToString();
+            OriginalLanguage = series.OriginalLanguage;
         }
     }
 }
