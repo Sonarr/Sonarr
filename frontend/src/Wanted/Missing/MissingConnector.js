bcc36ef04297508cb36ecef5128d0859d3f4a2cd
Stevie Robinson <stevie.robinson@gmail.com>
2024-12-03 20:34:50 +0100

    make wanted search buttons reactive

--- frontend/src/Wanted/Missing/MissingConnector.js
+++ frontend/src/Wanted/Missing/MissingConnector.js
@@ -17,9 +17,10 @@ function createMapStateToProps() {
   return createSelector(
     (state) => state.wanted.missing,
     createCommandExecutingSelector(commandNames.MISSING_EPISODE_SEARCH),
-    (missing, isSearchingForMissingEpisodes) => {
+    createCommandExecutingSelector(commandNames.EPISODE_SEARCH),
+    (missing, isSearchingForAllMissingEpisodes, isSearchingForSelectedMissingEpisodes) => {
       return {
-        isSearchingForMissingEpisodes,
+        isSearchingForMissingEpisodes: isSearchingForAllMissingEpisodes || isSearchingForSelectedMissingEpisodes,
         isSaving: missing.items.filter((m) => m.isSaving).length > 1,
         ...missing
       };
