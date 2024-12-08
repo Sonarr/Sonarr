71764ade7c36b9ad0f7973c505b3f6b80b348fbd
Mark McDowall <mark@mcdowall.ca>
2024-12-07 19:52:26 -0800

    Convert EpisodeHistory to TypeScript

---
+++ frontend/src/Episode/History/EpisodeHistory.tsx
@@ -0,0 +1,138 @@
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import AppState from 'App/State/AppState';
+import Alert from 'Components/Alert';
+import Icon from 'Components/Icon';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import Table from 'Components/Table/Table';
+import TableBody from 'Components/Table/TableBody';
+import { icons, kinds } from 'Helpers/Props';
+import {
+  clearEpisodeHistory,
+  episodeHistoryMarkAsFailed,
+  fetchEpisodeHistory,
+} from 'Store/Actions/episodeHistoryActions';
+import translate from 'Utilities/String/translate';
+import EpisodeHistoryRow from './EpisodeHistoryRow';
+
+const columns = [
+  {
+    name: 'eventType',
+    isVisible: true,
+  },
+  {
+    name: 'sourceTitle',
+    label: () => translate('SourceTitle'),
+    isVisible: true,
+  },
+  {
+    name: 'languages',
+    label: () => translate('Languages'),
+    isVisible: true,
+  },
+  {
+    name: 'quality',
+    label: () => translate('Quality'),
+    isVisible: true,
+  },
+  {
+    name: 'customFormats',
+    label: () => translate('CustomFormats'),
+    isSortable: false,
+    isVisible: true,
+  },
+  {
+    name: 'customFormatScore',
+    label: React.createElement(Icon, {
+      name: icons.SCORE,
+      title: () => translate('CustomFormatScore'),
+    }),
+    isSortable: true,
+    isVisible: true,
+  },
+  {
+    name: 'date',
+    label: () => translate('Date'),
+    isVisible: true,
+  },
+  {
+    name: 'actions',
+    isVisible: true,
+  },
+];
+
+function createMapStateToProps() {
+  return createSelector(
+    (state: AppState) => state.episodeHistory,
+    (episodeHistory) => episodeHistory
+  );
+}
+
+interface EpisodeHistoryProps {
+  episodeId: number;
+}
+
+function EpisodeHistory({ episodeId }: EpisodeHistoryProps) {
+  const dispatch = useDispatch();
+  const { items, isFetching, isPopulated, error } = useSelector(
+    createMapStateToProps()
+  );
+
+  const handleMarkAsFailedPress = useCallback(
+    (historyId: number) => {
+      dispatch(episodeHistoryMarkAsFailed({ historyId, episodeId }));
+    },
+    [episodeId, dispatch]
+  );
+
+  const hasItems = !!items.length;
+
+  useEffect(() => {
+    dispatch(fetchEpisodeHistory({ episodeId }));
+
+    return () => {
+      dispatch(clearEpisodeHistory());
+    };
+  }, [episodeId, dispatch]);
+
+  if (isFetching) {
+    return <LoadingIndicator />;
+  }
+
+  if (!isFetching && !!error) {
+    return (
+      <Alert kind={kinds.DANGER}>{translate('EpisodeHistoryLoadError')}</Alert>
+    );
+  }
+
+  if (isPopulated && !hasItems && !error) {
+    return <Alert kind={kinds.INFO}>{translate('NoEpisodeHistory')}</Alert>;
+  }
+
+  if (isPopulated && hasItems && !error) {
+    return (
+      <Table columns={columns}>
+        <TableBody>
+          {items.map((item) => {
+            return (
+              <EpisodeHistoryRow
+                key={item.id}
+                {...item}
+                onMarkAsFailedPress={handleMarkAsFailedPress}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    );
+  }
+
+  return null;
+}
+
+EpisodeHistory.defaultProps = {
+  selectedTab: 'details',
+};
+
+export default EpisodeHistory;
