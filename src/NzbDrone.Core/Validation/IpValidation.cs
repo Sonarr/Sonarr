424f95f8adfa318032f39c6bb6734f0e21255878
Stevie Robinson <stevie.robinson@gmail.com>
2024-12-31 13:33:50 +0100

    remove `0.0.0.0` validation for bind host

--- src/NzbDrone.Core/Validation/IpValidation.cs
+++ src/NzbDrone.Core/Validation/IpValidation.cs
@@ -1,5 +1,4 @@
 using FluentValidation;
-using FluentValidation.Validators;
 using NzbDrone.Common.Extensions;
 
 namespace NzbDrone.Core.Validation
@@ -9,11 +8,6 @@ namespace NzbDrone.Core.Validation
         public static IRuleBuilderOptions<T, string> ValidIpAddress<T>(this IRuleBuilder<T, string> ruleBuilder)
         {
             return ruleBuilder.Must(x => x.IsValidIpAddress()).WithMessage("Must contain wildcard (*) or a valid IP Address");
-        }
-
-        public static IRuleBuilderOptions<T, string> NotListenAllIp4Address<T>(this IRuleBuilder<T, string> ruleBuilder)
-        {
-            return ruleBuilder.SetValidator(new RegularExpressionValidator(@"^(?!0\.0\.0\.0)")).WithMessage("Use * instead of 0.0.0.0");
         }
     }
 }
