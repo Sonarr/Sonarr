f10b0e51b5cb34f26fec8752f7ae374140edbbf3
Mark McDowall <mark@mcdowall.ca>
2025-01-29 20:14:22 -0800

    Fix builds with v5 API

--- docs.sh
+++ docs.sh
@@ -3,7 +3,7 @@ set -e
 
 FRAMEWORK="net8.0"
 PLATFORM=$1
-ARCHITECTURE="${2:-'default value'}"
+ARCHITECTURE="${2:-x64}"
 
 if [ "$PLATFORM" = "Windows" ]; then
   RUNTIME="win-$ARCHITECTURE"
@@ -37,7 +37,7 @@ dotnet clean $slnFile -c Release
 
 dotnet msbuild -restore $slnFile -p:Configuration=Debug -p:Platform=$platform -p:RuntimeIdentifiers=$RUNTIME -t:PublishAllRids
 
-# dotnet new tool-manifests
+dotnet new tool-manifest
 dotnet tool install --version 6.6.2 Swashbuckle.AspNetCore.Cli
 
 dotnet tool run swagger tofile --output ./src/Sonarr.Api.V5/openapi.json "$outputFolder/$FRAMEWORK/$RUNTIME/$application" v5 &
