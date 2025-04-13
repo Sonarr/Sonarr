9683b0af35220bb0af801779a06d73feaeba809a
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-04-13 13:10:06 +0300

    Pass messages with arguments to NLog in LoggerExtensions

--- src/NzbDrone.Common/Instrumentation/Extensions/LoggerExtensions.cs
+++ src/NzbDrone.Common/Instrumentation/Extensions/LoggerExtensions.cs
@@ -4,27 +4,27 @@ namespace NzbDrone.Common.Instrumentation.Extensions
 {
     public static class LoggerExtensions
     {
+        [MessageTemplateFormatMethod("message")]
         public static void ProgressInfo(this Logger logger, string message, params object[] args)
         {
-            var formattedMessage = string.Format(message, args);
-            LogProgressMessage(logger, LogLevel.Info, formattedMessage);
+            LogProgressMessage(logger, LogLevel.Info, message, args);
         }
 
+        [MessageTemplateFormatMethod("message")]
         public static void ProgressDebug(this Logger logger, string message, params object[] args)
         {
-            var formattedMessage = string.Format(message, args);
-            LogProgressMessage(logger, LogLevel.Debug, formattedMessage);
+            LogProgressMessage(logger, LogLevel.Debug, message, args);
         }
 
+        [MessageTemplateFormatMethod("message")]
         public static void ProgressTrace(this Logger logger, string message, params object[] args)
         {
-            var formattedMessage = string.Format(message, args);
-            LogProgressMessage(logger, LogLevel.Trace, formattedMessage);
+            LogProgressMessage(logger, LogLevel.Trace, message, args);
         }
 
-        private static void LogProgressMessage(Logger logger, LogLevel level, string message)
+        private static void LogProgressMessage(Logger logger, LogLevel level, string message, object[] parameters)
         {
-            var logEvent = new LogEventInfo(level, logger.Name, message);
+            var logEvent = new LogEventInfo(level, logger.Name, null, message, parameters);
             logEvent.Properties.Add("Status", "");
 
             logger.Log(logEvent);
