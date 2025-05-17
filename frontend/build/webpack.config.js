7d252b23b10517dd7b283fccce6ef9a7f591286c
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-05-17 18:02:51 +0300

    Bump core-js to 3.42

--- frontend/build/webpack.config.js
+++ frontend/build/webpack.config.js
@@ -176,7 +176,7 @@ module.exports = (env) => {
                       loose: true,
                       debug: false,
                       useBuiltIns: 'entry',
-                      corejs: '3.41'
+                      corejs: '3.42'
                     }
                   ]
                 ]
