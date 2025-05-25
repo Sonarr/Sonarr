1cdca8ef3e47e19c9264db6b322161b615b20294
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-05-25 20:04:55 +0300

    Follow redirects for usenet grabs on non-prod builds

--- src/NzbDrone.Core/Download/UsenetClientBase.cs
+++ src/NzbDrone.Core/Download/UsenetClientBase.cs
@@ -48,6 +48,7 @@ namespace NzbDrone.Core.Download
             {
                 var request = indexer?.GetDownloadRequest(url) ?? new HttpRequest(url);
                 request.RateLimitKey = remoteEpisode?.Release?.IndexerId.ToString();
+                request.AllowAutoRedirect = true;
 
                 var response = await RetryStrategy
                     .ExecuteAsync(static async (state, _) => await state._httpClient.GetAsync(state.request), (_httpClient, request))
