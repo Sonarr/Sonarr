bf44936adc7ae2b9a1c278f287da0d63c8c75632
Alexander Westberg-Bladh <alexander.westberg@hygglo.se>
2025-09-01 10:37:08 +0200

    Fix spacing issue

--- src/NzbDrone.Core/Download/Clients/RQBit/RQBit.cs
+++ src/NzbDrone.Core/Download/Clients/RQBit/RQBit.cs
@@ -143,7 +143,7 @@ namespace NzbDrone.Core.Download.Clients.RQBit
         protected override void Test(List<ValidationFailure> failures)
         {
             failures.AddIfNotNull(TestConnection());
-            
+
             if (failures.HasErrors())
             {
                 return;
@@ -163,7 +163,7 @@ namespace NzbDrone.Core.Download.Clients.RQBit
             try
             {
                 var versionString = _proxy.GetVersion(Settings);
-                
+
                 if (string.IsNullOrWhiteSpace(versionString))
                 {
                     return new ValidationFailure("", "Unable to determine RQBit version. Please check that RQBit is running and accessible.");
