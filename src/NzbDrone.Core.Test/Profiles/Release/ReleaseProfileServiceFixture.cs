b9f598d72110d1e7adf71eb1ae4212e7ea0eb629
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2025-09-16 18:20:56 +0000

    Improving test readability

--- src/NzbDrone.Core.Test/Profiles/Release/ReleaseProfileServiceFixture.cs
+++ src/NzbDrone.Core.Test/Profiles/Release/ReleaseProfileServiceFixture.cs
@@ -15,29 +15,48 @@ namespace NzbDrone.Core.Test.Profiles
         private ReleaseProfile _defaultReleaseProfile;
         private ReleaseProfile _includedReleaseProfile;
         private ReleaseProfile _excludedReleaseProfile;
+        private ReleaseProfile _includedAndExcludedReleaseProfile;
+        private int _providedTag;
+        private int _providedTagToExclude;
+        private int _notUsedTag;
+        private List<ReleaseProfile> _releaseProfilesWithoutTags;
+        private List<ReleaseProfile> _releaseProfilesWithProvidedTag;
+        private List<ReleaseProfile> _releaseProfilesWithProvidedTagOrWithoutTags;
 
         [SetUp]
         public void Setup()
         {
-            _releaseProfiles = Builder<ReleaseProfile>.CreateListOfSize(4)
+            _providedTag = 1;
+            _providedTagToExclude = 2;
+            _notUsedTag = 3;
+
+            _releaseProfiles = Builder<ReleaseProfile>.CreateListOfSize(5)
                 .TheFirst(1)
                 .With(r => r.Required = ["required_one"])
                 .TheNext(1)
                 .With(r => r.Required = ["required_two"])
-                .With(r => r.Tags = [1])
+                .With(r => r.Tags = [_providedTag])
                 .TheNext(1)
                 .With(r => r.Required = ["required_three"])
-                .With(r => r.ExcludedTags = [2])
+                .With(r => r.ExcludedTags = [_providedTagToExclude])
                 .TheNext(1)
                 .With(r => r.Required = ["required_four"])
-                .With(r => r.Tags = [3])
-                .With(r => r.ExcludedTags = [4])
+                .With(r => r.Tags = [_providedTag])
+                .With(r => r.ExcludedTags = [_providedTagToExclude])
+                .TheNext(1)
+                .With(r => r.Required = ["required_five"])
+                .With(r => r.Tags = [_notUsedTag])
                 .Build()
                 .ToList();
 
             _defaultReleaseProfile = _releaseProfiles[0];
             _includedReleaseProfile = _releaseProfiles[1];
             _excludedReleaseProfile = _releaseProfiles[2];
+            _includedAndExcludedReleaseProfile = _releaseProfiles[3];
+
+            _releaseProfilesWithoutTags = [_defaultReleaseProfile, _excludedReleaseProfile];
+            _releaseProfilesWithProvidedTag = [_includedReleaseProfile, _includedAndExcludedReleaseProfile];
+            _releaseProfilesWithProvidedTagOrWithoutTags = [_defaultReleaseProfile, _includedReleaseProfile, _excludedReleaseProfile, _includedAndExcludedReleaseProfile];
 
             Mocker.GetMock<IRestrictionRepository>()
                   .Setup(s => s.All())
@@ -47,34 +66,30 @@ namespace NzbDrone.Core.Test.Profiles
         [Test]
         public void all_for_tags_should_return_release_profiles_without_tags_by_default()
         {
-            List<ReleaseProfile> releaseProfilesWithoutTags = [_defaultReleaseProfile, _excludedReleaseProfile];
             var releaseProfiles = Subject.AllForTags([]);
-            releaseProfiles.Should().Equal(releaseProfilesWithoutTags);
+            releaseProfiles.Should().Equal(_releaseProfilesWithoutTags);
         }
 
         [Test]
         public void all_for_tags_should_return_release_profiles_with_provided_tag_or_without_tags()
         {
-            var providedTag = 1;
-            List<ReleaseProfile> releaseProfilesWithProvidedTagOrWithoutTags = [_defaultReleaseProfile, _includedReleaseProfile, _excludedReleaseProfile];
-            var releaseProfiles = Subject.AllForTags([providedTag]);
-            releaseProfiles.Should().Equal(releaseProfilesWithProvidedTagOrWithoutTags);
+            var releaseProfiles = Subject.AllForTags([_providedTag]);
+            releaseProfiles.Should().Equal(_releaseProfilesWithProvidedTagOrWithoutTags);
         }
 
         [Test]
         public void all_for_tags_should_not_return_release_profiles_with_provided_tag_excluded()
         {
-            var providedTag = 2;
-            var releaseProfiles = Subject.AllForTags([providedTag]);
-            releaseProfiles.Should().Equal([_defaultReleaseProfile]);
+            var releaseProfiles = Subject.AllForTags([_providedTagToExclude]);
+            releaseProfiles.Should().NotContain(_excludedReleaseProfile);
+            releaseProfiles.Should().NotContain(_includedAndExcludedReleaseProfile);
         }
 
         [Test]
         public void all_for_tag_should_return_release_profiles_with_provided_tag()
         {
-            var providedTag = 1;
-            var releaseProfiles = Subject.AllForTag(providedTag);
-            releaseProfiles.Should().Equal([_includedReleaseProfile]);
+            var releaseProfiles = Subject.AllForTag(_providedTag);
+            releaseProfiles.Should().Equal(_releaseProfilesWithProvidedTag);
         }
 
         [Test]
@@ -87,9 +102,8 @@ namespace NzbDrone.Core.Test.Profiles
         [Test]
         public void all_for_tags_should_not_return_release_profiles_with_a_provided_tag_both_included_and_excluded()
         {
-            HashSet<int> providedTags = [3, 4];
-            var releaseProfiles = Subject.AllForTags(providedTags);
-            releaseProfiles.Should().Equal([_defaultReleaseProfile, _excludedReleaseProfile]);
+            var releaseProfiles = Subject.AllForTags([_providedTag, _providedTagToExclude]);
+            releaseProfiles.Should().Equal([_defaultReleaseProfile, _includedReleaseProfile]);
         }
     }
 }
