b4315fb2811732f0dba19027e99e768a167d5a87
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2025-09-16 17:54:19 +0000

    Changing tests to address feedback

--- src/NzbDrone.Core.Test/Profiles/Release/ReleaseProfileServiceFixture.cs
+++ src/NzbDrone.Core.Test/Profiles/Release/ReleaseProfileServiceFixture.cs
@@ -15,23 +15,29 @@ namespace NzbDrone.Core.Test.Profiles
         private ReleaseProfile _defaultReleaseProfile;
         private ReleaseProfile _includedReleaseProfile;
         private ReleaseProfile _excludedReleaseProfile;
+
         [SetUp]
         public void Setup()
         {
-            _releaseProfiles = Builder<ReleaseProfile>.CreateListOfSize(3).TheFirst(1)
+            _releaseProfiles = Builder<ReleaseProfile>.CreateListOfSize(4)
+                .TheFirst(1)
                 .With(r => r.Required = ["required_one"])
                 .TheNext(1)
                 .With(r => r.Required = ["required_two"])
+                .With(r => r.Tags = [1])
                 .TheNext(1)
                 .With(r => r.Required = ["required_three"])
+                .With(r => r.ExcludedTags = [2])
+                .TheNext(1)
+                .With(r => r.Required = ["required_four"])
+                .With(r => r.Tags = [3])
+                .With(r => r.ExcludedTags = [4])
                 .Build()
                 .ToList();
 
             _defaultReleaseProfile = _releaseProfiles[0];
             _includedReleaseProfile = _releaseProfiles[1];
-            _includedReleaseProfile.Tags.Add(1);
             _excludedReleaseProfile = _releaseProfiles[2];
-            _excludedReleaseProfile.ExcludedTags.Add(2);
 
             Mocker.GetMock<IRestrictionRepository>()
                   .Setup(s => s.All())
@@ -39,38 +45,51 @@ namespace NzbDrone.Core.Test.Profiles
         }
 
         [Test]
-        public void all_for_tags_should_match_default_and_excluded_release_profiles_for_untagged_series()
+        public void all_for_tags_should_return_release_profiles_without_tags_by_default()
         {
+            List<ReleaseProfile> releaseProfilesWithoutTags = [_defaultReleaseProfile, _excludedReleaseProfile];
             var releaseProfiles = Subject.AllForTags([]);
-            releaseProfiles.Should().Equal([_defaultReleaseProfile, _excludedReleaseProfile]);
+            releaseProfiles.Should().Equal(releaseProfilesWithoutTags);
         }
 
         [Test]
-        public void all_for_tags_should_match_default_and_included_and_not_excluded_release_profiles_for_tagged_series()
+        public void all_for_tags_should_return_release_profiles_with_provided_tag_or_without_tags()
         {
-            var releaseProfiles = Subject.AllForTags([1]);
-            releaseProfiles.Should().Equal([_defaultReleaseProfile, _includedReleaseProfile, _excludedReleaseProfile]);
+            var providedTag = 1;
+            List<ReleaseProfile> releaseProfilesWithProvidedTagOrWithoutTags = [_defaultReleaseProfile, _includedReleaseProfile, _excludedReleaseProfile];
+            var releaseProfiles = Subject.AllForTags([providedTag]);
+            releaseProfiles.Should().Equal(releaseProfilesWithProvidedTagOrWithoutTags);
         }
 
         [Test]
-        public void all_for_tags_should_not_match_excluded_release_profiles_for_tagged_series()
+        public void all_for_tags_should_not_return_release_profiles_with_provided_tag_excluded()
         {
-            var releaseProfiles = Subject.AllForTags([2]);
+            var providedTag = 2;
+            var releaseProfiles = Subject.AllForTags([providedTag]);
             releaseProfiles.Should().Equal([_defaultReleaseProfile]);
         }
 
         [Test]
-        public void all_for_tag_should_match_included_release_profiles_for_tagged_series()
+        public void all_for_tag_should_return_release_profiles_with_provided_tag()
         {
-            var releaseProfiles = Subject.AllForTag(1);
+            var providedTag = 1;
+            var releaseProfiles = Subject.AllForTag(providedTag);
             releaseProfiles.Should().Equal([_includedReleaseProfile]);
         }
 
         [Test]
-        public void all_should_match_all_release_profiles()
+        public void all_should_return_all_release_profiles()
         {
             var releaseProfiles = Subject.All();
             releaseProfiles.Should().Equal(_releaseProfiles);
+        }
+
+        [Test]
+        public void all_for_tags_should_not_return_release_profiles_with_a_provided_tag_both_included_and_excluded()
+        {
+            HashSet<int> providedTags = [3, 4];
+            var releaseProfiles = Subject.AllForTags(providedTags);
+            releaseProfiles.Should().Equal([_defaultReleaseProfile, _excludedReleaseProfile]);
         }
     }
 }
