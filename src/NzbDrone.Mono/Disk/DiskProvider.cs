3c47f71c6bed59d7ecbc3dfff5401de2ea6fdbd2
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-09-30 19:23:00 +0300

    Bump Mono.Unix to 7.1.0

--- src/NzbDrone.Mono/Disk/DiskProvider.cs
+++ src/NzbDrone.Mono/Disk/DiskProvider.cs
@@ -8,15 +8,13 @@ using NLog;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.EnsureThat;
 using NzbDrone.Common.EnvironmentInfo;
-using NzbDrone.Common.Extensions;
 
 namespace NzbDrone.Mono.Disk
 {
     public class DiskProvider : DiskProviderBase
     {
-        // Mono supports sending -1 for a uint to indicate that the owner or group should not be set
-        // `unchecked((uint)-1)` and `uint.MaxValue` are the same thing.
-        private const uint UNCHANGED_ID = uint.MaxValue;
+        // Mono supports sending -1 for an int to indicate that the owner or group should not be set
+        private const int UnchangedId = -1;
 
         private readonly Logger _logger;
         private readonly IProcMountProvider _procMountProvider;
@@ -105,7 +103,7 @@ namespace NzbDrone.Mono.Disk
 
             var groupId = GetGroupId(group);
 
-            if (Syscall.chown(path, unchecked((uint)-1), groupId) < 0)
+            if (Syscall.chown(path, -1, groupId) < 0)
             {
                 var error = Stdlib.GetLastError();
 
@@ -321,8 +319,8 @@ namespace NzbDrone.Mono.Disk
             // Mono 6.x till 6.10 doesn't properly try use rename first.
             if (move)
             {
-                if (Syscall.lstat(source, out var sourcestat) == 0 &&
-                    Syscall.lstat(destination, out var deststat) != 0 &&
+                if (Syscall.lstat(source, out _) == 0 &&
+                    Syscall.lstat(destination, out _) != 0 &&
                     Syscall.rename(source, destination) == 0)
                 {
                     _logger.Trace("Moved '{0}' -> '{1}' using Syscall.rename", source, destination);
@@ -449,14 +447,14 @@ namespace NzbDrone.Mono.Disk
             }
         }
 
-        private uint GetUserId(string user)
+        private static int GetUserId(string user)
         {
-            if (user.IsNullOrWhiteSpace())
+            if (string.IsNullOrWhiteSpace(user))
             {
-                return UNCHANGED_ID;
+                return UnchangedId;
             }
 
-            if (uint.TryParse(user, out var userId))
+            if (int.TryParse(user, out var userId))
             {
                 return userId;
             }
@@ -468,17 +466,17 @@ namespace NzbDrone.Mono.Disk
                 throw new LinuxPermissionsException("Unknown user: {0}", user);
             }
 
-            return u.pw_uid;
+            return checked((int)u.pw_uid);
         }
 
-        private uint GetGroupId(string group)
+        private static int GetGroupId(string group)
         {
-            if (group.IsNullOrWhiteSpace())
+            if (string.IsNullOrWhiteSpace(group))
             {
-                return UNCHANGED_ID;
+                return UnchangedId;
             }
 
-            if (uint.TryParse(group, out var groupId))
+            if (int.TryParse(group, out var groupId))
             {
                 return groupId;
             }
@@ -490,7 +488,7 @@ namespace NzbDrone.Mono.Disk
                 throw new LinuxPermissionsException("Unknown group: {0}", group);
             }
 
-            return g.gr_gid;
+            return checked((int)g.gr_gid);
         }
     }
 }
