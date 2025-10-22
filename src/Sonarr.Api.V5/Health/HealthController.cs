52f5bd7ecc10a8c1c15c4b0cbc8539a2fe096c54
Mark McDowall <mark@mcdowall.ca>
2025-10-21 20:30:52 -0700

    Add v5 health endpoints

---
+++ src/Sonarr.Api.V5/Health/HealthController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Datastore.Events;
+using NzbDrone.Core.HealthCheck;
+using NzbDrone.Core.Messaging.Events;
+using NzbDrone.SignalR;
+using Sonarr.Http;
+using Sonarr.Http.REST;
+
+namespace Sonarr.Api.V5.Health;
+
+[V5ApiController]
+public class HealthController : RestControllerWithSignalR<HealthResource, HealthCheck>,
+                            IHandle<HealthCheckCompleteEvent>
+{
+    private readonly IHealthCheckService _healthCheckService;
+
+    public HealthController(IBroadcastSignalRMessage signalRBroadcaster, IHealthCheckService healthCheckService)
+        : base(signalRBroadcaster)
+    {
+        _healthCheckService = healthCheckService;
+    }
+
+    [NonAction]
+    public override ActionResult<HealthResource> GetResourceByIdWithErrorHandler(int id)
+    {
+        return base.GetResourceByIdWithErrorHandler(id);
+    }
+
+    protected override HealthResource GetResourceById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    [Produces("application/json")]
+    public List<HealthResource> GetHealth()
+    {
+        return _healthCheckService.Results().ToResource();
+    }
+
+    [NonAction]
+    public void Handle(HealthCheckCompleteEvent message)
+    {
+        BroadcastResourceChange(ModelAction.Sync);
+    }
+}
---
+++ src/Sonarr.Api.V5/Health/HealthResource.cs
@@ -0,0 +1,34 @@
+﻿using NzbDrone.Core.HealthCheck;
+using Sonarr.Http.REST;
+
+namespace Sonarr.Api.V5.Health;
+
+public class HealthResource : RestResource
+{
+    public string? Source { get; set; }
+    public HealthCheckResult Type { get; set; }
+    public HealthCheckReason Reason { get; set; }
+    public string? Message { get; set; }
+    public string? WikiUrl { get; set; }
+}
+
+public static class HealthResourceMapper
+{
+    public static HealthResource ToResource(this HealthCheck model)
+    {
+        return new HealthResource
+        {
+            Id = model.Id,
+            Source = model.Source.Name,
+            Type = model.Type,
+            Reason = model.Reason,
+            Message = model.Message,
+            WikiUrl = model.WikiUrl.FullUri
+        };
+    }
+
+    public static List<HealthResource> ToResource(this IEnumerable<HealthCheck> models)
+    {
+        return models.Select(ToResource).ToList();
+    }
+}
