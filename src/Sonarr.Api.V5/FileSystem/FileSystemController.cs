27e22b869365e347bcf2113ffeb14a78dcd51358
Mark McDowall <mark@mcdowall.ca>
2025-11-26 21:15:54 -0800

    Add additional v5 filesystem endpoints

---
+++ src/Sonarr.Api.V5/FileSystem/FileSystemController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.MediaFiles;
+using Sonarr.Http;
+
+namespace Sonarr.Api.V5.FileSystem;
+
+[V5ApiController]
+public class FileSystemController : Controller
+{
+    private readonly IFileSystemLookupService _fileSystemLookupService;
+    private readonly IDiskProvider _diskProvider;
+    private readonly IDiskScanService _diskScanService;
+
+    public FileSystemController(IFileSystemLookupService fileSystemLookupService,
+                            IDiskProvider diskProvider,
+                            IDiskScanService diskScanService)
+    {
+        _fileSystemLookupService = fileSystemLookupService;
+        _diskProvider = diskProvider;
+        _diskScanService = diskScanService;
+    }
+
+    [HttpGet]
+    [Produces("application/json")]
+    public IActionResult GetContents(string path, bool includeFiles = false, bool allowFoldersWithoutTrailingSlashes = false)
+    {
+        return Ok(_fileSystemLookupService.LookupContents(path, includeFiles, allowFoldersWithoutTrailingSlashes));
+    }
+
+    [HttpGet("type")]
+    [Produces("application/json")]
+    public object GetEntityType(string path)
+    {
+        if (_diskProvider.FileExists(path))
+        {
+            return new { type = "file" };
+        }
+
+        // Return folder even if it doesn't exist on disk to avoid leaking anything from the UI about the underlying system
+        return new { type = "folder" };
+    }
+
+    [HttpGet("mediafiles")]
+    [Produces("application/json")]
+    public object GetMediaFiles(string path)
+    {
+        if (!_diskProvider.FolderExists(path))
+        {
+            return Array.Empty<string>();
+        }
+
+        return _diskScanService.GetVideoFiles(path).Select(f => new
+        {
+            Path = f,
+            RelativePath = path.GetRelativePath(f),
+            Name = Path.GetFileName(f)
+        });
+    }
+}
