3cad2c792f054ce7a682d1017438293d8f839a9c
Mark McDowall <mark@mcdowall.ca>
2025-12-01 20:28:36 -0800

    Add v5 localization endpoints

---
+++ src/Sonarr.Api.V5/Localization/LocalizationController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Localization;
+using Sonarr.Http;
+using Sonarr.Http.REST;
+
+namespace Sonarr.Api.V5.Localization;
+
+[V5ApiController]
+public class LocalizationController : RestController<LocalizationResource>
+{
+    private readonly ILocalizationService _localizationService;
+
+    public LocalizationController(ILocalizationService localizationService)
+    {
+        _localizationService = localizationService;
+    }
+
+    protected override LocalizationResource GetResourceById(int id)
+    {
+        return GetLocalization();
+    }
+
+    [HttpGet]
+    [Produces("application/json")]
+    public LocalizationResource GetLocalization()
+    {
+        return _localizationService.GetLocalizationDictionary().ToResource();
+    }
+
+    [HttpGet("language")]
+    [Produces("application/json")]
+    public LocalizationLanguageResource GetLanguage()
+    {
+        var identifier = _localizationService.GetLanguageIdentifier();
+
+        return new LocalizationLanguageResource
+        {
+            Identifier = identifier
+        };
+    }
+}
---
+++ src/Sonarr.Api.V5/Localization/LocalizationLanguageResource.cs
@@ -0,0 +1,6 @@
+namespace Sonarr.Api.V5.Localization;
+
+public class LocalizationLanguageResource
+{
+    public string? Identifier { get; set; }
+}
