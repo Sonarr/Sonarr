034acb35fc09ea819c4c0ebb84b3c767324d9b3d
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-12-23 18:34:02 +0200

    Bump Npgsql and Swashbuckle

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -38,7 +38,7 @@ dotnet clean $slnFile -c Release
 dotnet msbuild -restore $slnFile -p:Configuration=Debug -p:Platform=$platform -p:RuntimeIdentifiers=$RUNTIME -t:PublishAllRids
 
 dotnet new tool-manifest
-dotnet tool install --version 10.0.1 Swashbuckle.AspNetCore.Cli
+dotnet tool install --version 10.1.0 Swashbuckle.AspNetCore.Cli
 
 # Remove the openapi.json file so we can check if it was created
 rm $outputFile
