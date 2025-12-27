c0a53fc1b8f3bb4e3069fbd7c519a6c739a75a10
Mark McDowall <mark@mcdowall.ca>
2025-12-26 19:20:21 -0800

    Clean up AppState

--- frontend/src/App/State/AppState.ts
+++ frontend/src/App/State/AppState.ts
@@ -1,10 +1,8 @@
-import BlocklistAppState from './BlocklistAppState';
 import CaptchaAppState from './CaptchaAppState';
 import ImportSeriesAppState from './ImportSeriesAppState';
 import SettingsAppState from './SettingsAppState';
 
 interface AppState {
-  blocklist: BlocklistAppState;
   captcha: CaptchaAppState;
   importSeries: ImportSeriesAppState;
   settings: SettingsAppState;
--- frontend/src/App/State/BackupAppState.ts
+++
@@ -1,9 +0,0 @@
-import Backup from 'typings/Backup';
-import AppSectionState, { Error } from './AppSectionState';
-
-interface BackupAppState extends AppSectionState<Backup> {
-  isRestoring: boolean;
-  restoreError?: Error;
-}
-
-export default BackupAppState;
