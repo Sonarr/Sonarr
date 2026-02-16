bcceb22512b2a94bf8456425a80279d0a1aa45aa
Mark McDowall <mark@mcdowall.ca>
2026-02-16 14:53:14 -0800

    Add v5 Indexer endpoints

---
+++ src/Sonarr.Api.V5/Indexers/IndexerController.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Validation;
+using NzbDrone.SignalR;
+using Sonarr.Api.V5.Provider;
+using Sonarr.Http;
+
+namespace Sonarr.Api.V5.Indexers;
+
+[V5ApiController]
+public class IndexerController : ProviderControllerBase<IndexerResource, IndexerBulkResource, IIndexer, IndexerDefinition>
+{
+    public static readonly IndexerResourceMapper ResourceMapper = new();
+    public static readonly IndexerBulkResourceMapper BulkResourceMapper = new();
+
+    public IndexerController(IBroadcastSignalRMessage signalRBroadcaster,
+        IndexerFactory indexerFactory,
+        DownloadClientExistsValidator downloadClientExistsValidator)
+        : base(signalRBroadcaster, indexerFactory, "indexer", ResourceMapper, BulkResourceMapper)
+    {
+        SharedValidator.RuleFor(c => c.Priority).InclusiveBetween(1, 50);
+        SharedValidator.RuleFor(c => c.SeasonSearchMaximumSingleEpisodeAge).GreaterThanOrEqualTo(0);
+        SharedValidator.RuleFor(c => c.DownloadClientId).SetValidator(downloadClientExistsValidator);
+    }
+}
---
+++ src/Sonarr.Api.V5/Indexers/IndexerResource.cs
@@ -0,0 +1,51 @@
+using NzbDrone.Core.Indexers;
+using Sonarr.Api.V5.Provider;
+
+namespace Sonarr.Api.V5.Indexers;
+
+public class IndexerResource : ProviderResource<IndexerResource>
+{
+    public bool EnableRss { get; set; }
+    public bool EnableAutomaticSearch { get; set; }
+    public bool EnableInteractiveSearch { get; set; }
+    public bool SupportsRss { get; set; }
+    public bool SupportsSearch { get; set; }
+    public DownloadProtocol Protocol { get; set; }
+    public int Priority { get; set; }
+    public int SeasonSearchMaximumSingleEpisodeAge { get; set; }
+    public int DownloadClientId { get; set; }
+}
+
+public class IndexerResourceMapper : ProviderResourceMapper<IndexerResource, IndexerDefinition>
+{
+    public override IndexerResource ToResource(IndexerDefinition definition)
+    {
+        var resource = base.ToResource(definition);
+
+        resource.EnableRss = definition.EnableRss;
+        resource.EnableAutomaticSearch = definition.EnableAutomaticSearch;
+        resource.EnableInteractiveSearch = definition.EnableInteractiveSearch;
+        resource.SupportsRss = definition.SupportsRss;
+        resource.SupportsSearch = definition.SupportsSearch;
+        resource.Protocol = definition.Protocol;
+        resource.Priority = definition.Priority;
+        resource.SeasonSearchMaximumSingleEpisodeAge = definition.SeasonSearchMaximumSingleEpisodeAge;
+        resource.DownloadClientId = definition.DownloadClientId;
+
+        return resource;
+    }
+
+    public override IndexerDefinition ToModel(IndexerResource resource, IndexerDefinition? existingDefinition)
+    {
+        var definition = base.ToModel(resource, existingDefinition);
+
+        definition.EnableRss = resource.EnableRss;
+        definition.EnableAutomaticSearch = resource.EnableAutomaticSearch;
+        definition.EnableInteractiveSearch = resource.EnableInteractiveSearch;
+        definition.Priority = resource.Priority;
+        definition.SeasonSearchMaximumSingleEpisodeAge = resource.SeasonSearchMaximumSingleEpisodeAge;
+        definition.DownloadClientId = resource.DownloadClientId;
+
+        return definition;
+    }
+}
