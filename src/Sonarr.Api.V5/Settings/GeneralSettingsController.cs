dd6533c18a2aca1d7af48599b43f2d45462dbdb2
Mark McDowall <mark@mcdowall.ca>
2026-02-16 14:24:08 -0800

    Add v5 General settings endpoints

---
+++ src/Sonarr.Api.V5/Settings/GeneralSettingsController.cs
@@ -0,0 +1,114 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Authentication;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Update;
+using NzbDrone.Core.Validation;
+using NzbDrone.Core.Validation.Paths;
+using Sonarr.Http;
+
+namespace Sonarr.Api.V5.Settings;
+
+[V5ApiController("settings/general")]
+public class GeneralSettingsController : SettingsController<GeneralSettingsResource>
+{
+    private readonly IUserService _userService;
+
+    public GeneralSettingsController(IConfigFileProvider configFileProvider,
+                                IConfigService configService,
+                                IUserService userService,
+                                IDiskProvider diskProvider)
+        : base(configFileProvider, configService)
+    {
+        _userService = userService;
+
+        SharedValidator.RuleFor(c => c.BindAddress)
+                       .ValidIpAddress()
+                       .When(c => c.BindAddress != "*" && c.BindAddress != "localhost");
+
+        SharedValidator.RuleFor(c => c.Port).ValidPort();
+
+        SharedValidator.RuleFor(c => c.UrlBase).ValidUrlBase();
+        SharedValidator.RuleFor(c => c.InstanceName).StartsOrEndsWithSonarr();
+
+        SharedValidator.RuleFor(c => c.Username).NotEmpty().When(c => c.AuthenticationMethod == AuthenticationType.Forms);
+        SharedValidator.RuleFor(c => c.Password).NotEmpty().When(c => c.AuthenticationMethod == AuthenticationType.Forms);
+
+        SharedValidator.RuleFor(c => c.AuthenticationMethod)
+#pragma warning disable CS0618 // Type or member is obsolete
+            .NotEqual(AuthenticationType.Basic)
+#pragma warning restore CS0618 // Type or member is obsolete
+            .WithMessage("'Basic' is no longer supported, switch to 'Forms' instead.");
+
+        SharedValidator.RuleFor(c => c.PasswordConfirmation)
+            .Must((resource, p) => IsMatchingPassword(resource)).WithMessage("Must match Password");
+
+        SharedValidator.RuleFor(c => c.SslPort).ValidPort().When(c => c.EnableSsl);
+        SharedValidator.RuleFor(c => c.SslPort).NotEqual(c => c.Port).When(c => c.EnableSsl);
+
+        SharedValidator.RuleFor(c => c.SslCertPath)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .IsValidPath()
+            .SetValidator(new FileExistsValidator(diskProvider))
+            .IsValidCertificate()
+            .When(c => c.EnableSsl);
+
+        SharedValidator.RuleFor(c => c.SslKeyPath)
+            .NotEmpty()
+            .IsValidPath()
+            .SetValidator(new FileExistsValidator(diskProvider))
+            .When(c => c.SslKeyPath.IsNotNullOrWhiteSpace());
+
+        SharedValidator.RuleFor(c => c.LogSizeLimit).InclusiveBetween(1, 10);
+
+        SharedValidator.RuleFor(c => c.Branch).NotEmpty().WithMessage("Branch name is required, 'main' is the default");
+        SharedValidator.RuleFor(c => c.UpdateScriptPath).IsValidPath().When(c => c.UpdateMechanism == UpdateMechanism.Script);
+
+        SharedValidator.RuleFor(c => c.BackupFolder).IsValidPath().When(c => Path.IsPathRooted(c.BackupFolder));
+        SharedValidator.RuleFor(c => c.BackupInterval).InclusiveBetween(1, 7);
+        SharedValidator.RuleFor(c => c.BackupRetention).InclusiveBetween(1, 90);
+    }
+
+    private bool IsMatchingPassword(GeneralSettingsResource resource)
+    {
+        var user = _userService.FindUser();
+
+        if (user != null && user.Password == resource.Password)
+        {
+            return true;
+        }
+
+        if (resource.Password == resource.PasswordConfirmation)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected override GeneralSettingsResource ToResource(IConfigFileProvider configFile, IConfigService model)
+    {
+        var resource = GeneralSettingsResourceMapper.ToResource(configFile, model);
+
+        var user = _userService.FindUser();
+
+        resource.Username = user?.Username ?? string.Empty;
+        resource.Password = user?.Password ?? string.Empty;
+        resource.PasswordConfirmation = string.Empty;
+
+        return resource;
+    }
+
+    public override ActionResult<GeneralSettingsResource> SaveSettings(GeneralSettingsResource resource)
+    {
+        if (resource.Username.IsNotNullOrWhiteSpace() && resource.Password.IsNotNullOrWhiteSpace())
+        {
+            _userService.Upsert(resource.Username, resource.Password);
+        }
+
+        return base.SaveSettings(resource);
+    }
+}
