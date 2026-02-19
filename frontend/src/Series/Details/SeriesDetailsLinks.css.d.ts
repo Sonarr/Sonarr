1af3b1f90df3ee86c5e5a14f932d93f4b13aa5de
yashizzle <16638776+Yashizzle@users.noreply.github.com>
2026-02-19 20:49:00 +1100

    New: Display the series link IDs
    This includes,
    - Add a `label` field to the `ClipboardButton`
    - Display the link's external ID for `SeriesDetailsLinks`
    - Update the MDBList link ot use https

--- frontend/src/Series/Details/SeriesDetailsLinks.css.d.ts
+++ frontend/src/Series/Details/SeriesDetailsLinks.css.d.ts
@@ -2,6 +2,7 @@
 // Please do not change this file!
 interface CssExports {
   'link': string;
+  'linkBlock': string;
   'linkLabel': string;
   'links': string;
 }
--- frontend/src/Series/Details/SeriesDetailsLinks.tsx
+++ frontend/src/Series/Details/SeriesDetailsLinks.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Label from 'Components/Label';
+import ClipboardButton from 'Components/Link/ClipboardButton';
 import Link from 'Components/Link/Link';
 import { kinds, sizes } from 'Helpers/Props';
 import Series from 'Series/Series';
+import translate from 'Utilities/String/translate';
 import styles from './SeriesDetailsLinks.css';
 
 type SeriesDetailsLinksProps = Pick<
@@ -10,96 +12,90 @@ type SeriesDetailsLinksProps = Pick<
   'tvdbId' | 'tvMazeId' | 'imdbId' | 'tmdbId'
 >;
 
+interface SeriesDetailsLink {
+  externalId: string | number;
+  name: string;
+  url: string;
+}
+
 function SeriesDetailsLinks(props: SeriesDetailsLinksProps) {
   const { tvdbId, tvMazeId, imdbId, tmdbId } = props;
 
+  const links = useMemo(() => {
+    const validLinks: SeriesDetailsLink[] = [];
+
+    if (tvdbId) {
+      validLinks.push(
+        {
+          externalId: tvdbId,
+          name: 'The TVDB',
+          url: `https://www.thetvdb.com/?tab=series&id=${tvdbId}`,
+        },
+        {
+          externalId: tvdbId,
+          name: 'Trakt',
+          url: `https://trakt.tv/search/tvdb/${tvdbId}?id_type=show`,
+        }
+      );
+    }
+
+    if (tvMazeId) {
+      validLinks.push({
+        externalId: tvMazeId,
+        name: 'TV Maze',
+        url: `https://www.tvmaze.com/shows/${tvMazeId}/_`,
+      });
+    }
+
+    if (imdbId) {
+      validLinks.push(
+        {
+          externalId: imdbId,
+          name: 'IMDB',
+          url: `https://imdb.com/title/${imdbId}/`,
+        },
+        {
+          externalId: imdbId,
+          name: 'MDBList',
+          url: `https://mdblist.com/show/${imdbId}`,
+        }
+      );
+    }
+
+    if (tmdbId) {
+      validLinks.push({
+        externalId: tmdbId,
+        name: 'TMDB',
+        url: `https://www.themoviedb.org/tv/${tmdbId}`,
+      });
+    }
+
+    return validLinks;
+  }, [tvdbId, tvMazeId, imdbId, tmdbId]);
+
   return (
     <div className={styles.links}>
-      <Link
-        className={styles.link}
-        to={`https://www.thetvdb.com/?tab=series&id=${tvdbId}`}
-      >
-        <Label
-          className={styles.linkLabel}
-          kind={kinds.INFO}
-          size={sizes.LARGE}
-        >
-          The TVDB
-        </Label>
-      </Link>
-
-      <Link
-        className={styles.link}
-        to={`https://trakt.tv/search/tvdb/${tvdbId}?id_type=show`}
-      >
-        <Label
-          className={styles.linkLabel}
-          kind={kinds.INFO}
-          size={sizes.LARGE}
-        >
-          Trakt
-        </Label>
-      </Link>
-
-      {tvMazeId ? (
-        <Link
-          className={styles.link}
-          to={`https://www.tvmaze.com/shows/${tvMazeId}/_`}
-        >
-          <Label
-            className={styles.linkLabel}
-            kind={kinds.INFO}
-            size={sizes.LARGE}
-          >
-            TV Maze
-          </Label>
-        </Link>
-      ) : null}
-
-      {imdbId ? (
-        <>
-          <Link
-            className={styles.link}
-            to={`https://imdb.com/title/${imdbId}/`}
-          >
+      {links.map((link) => (
+        <div key={link.name} className={styles.linkBlock}>
+          <Link className={styles.link} to={link.url}>
             <Label
               className={styles.linkLabel}
               kind={kinds.INFO}
               size={sizes.LARGE}
             >
-              IMDB
+              {link.name}
             </Label>
           </Link>
 
-          <Link
-            className={styles.link}
-            to={`http://mdblist.com/show/${imdbId}`}
-          >
-            <Label
-              className={styles.linkLabel}
-              kind={kinds.INFO}
-              size={sizes.LARGE}
-            >
-              MDBList
-            </Label>
-          </Link>
-        </>
-      ) : null}
-
-      {tmdbId ? (
-        <Link
-          className={styles.link}
-          to={`https://www.themoviedb.org/tv/${tmdbId}`}
-        >
-          <Label
-            className={styles.linkLabel}
-            kind={kinds.INFO}
-            size={sizes.LARGE}
-          >
-            TMDB
-          </Label>
-        </Link>
-      ) : null}
+          <ClipboardButton
+            value={`${link.externalId}`}
+            title={translate('CopyToClipboard')}
+            kind={kinds.DEFAULT}
+            size={sizes.SMALL}
+            label={link.externalId}
+          />
+        </div>
+      ))}
     </div>
   );
 }
