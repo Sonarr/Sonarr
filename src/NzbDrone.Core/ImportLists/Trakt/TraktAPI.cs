734b4e75b0b3d366c0f137b64530e735155ea1e3
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-03-08 14:12:28 +0200

    Fix import lists with null Trakt IDs

--- src/NzbDrone.Core/ImportLists/Trakt/TraktAPI.cs
+++ src/NzbDrone.Core/ImportLists/Trakt/TraktAPI.cs
@@ -5,7 +5,7 @@ namespace NzbDrone.Core.ImportLists.Trakt
 {
     public class TraktSeriesIdsResource
     {
-        public int Trakt { get; set; }
+        public int? Trakt { get; set; }
         public string Slug { get; set; }
         public string Imdb { get; set; }
         public int? Tmdb { get; set; }
