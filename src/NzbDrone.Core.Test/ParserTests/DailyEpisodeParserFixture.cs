5e5f1835f510ff4509da0b300e69247564fb41f7
Mark McDowall <mark@mcdowall.ca>
2026-03-16 10:19:03 -0700

    Fixed: Parsing date-based releases with 3 digit number in episode title

--- src/NzbDrone.Core.Test/ParserTests/DailyEpisodeParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/DailyEpisodeParserFixture.cs
@@ -36,6 +36,8 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Series Title - 30-04-2024 HDTV 1080p H264 AAC", "Series Title", 2024, 4, 30)]
         [TestCase("Series On TitleClub E76 2024 08 08 1080p WEB H264-RnB96 [TJET]", "Series On TitleClub", 2024, 8, 8)]
         [TestCase("Series.Title.13.02.2025.1080i.HDTV.MPA2.0.H.264", "Series Title", 2025, 2, 13)]
+        [TestCase("Series.2025.09.01.The.170.Million.Pound.Diamond.Scam.1080p.HDTV.H264-DEADPOOL'", "Series", 2025, 9, 1)]
+        [TestCase("Series.2025.09.01.The.Million.Pound.Diamond.Scam.1080p.HDTV.H264-DEADPOOL'", "Series", 2025, 9, 1)]
         public void should_parse_daily_episode(string postTitle, string title, int year, int month, int day)
         {
             var result = Parser.Parser.ParseTitle(postTitle);
