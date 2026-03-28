3c087f0a80738b24d15f321183807d1f64deef87
Mark McDowall <mark@mcdowall.ca>
2026-03-28 14:10:11 -0700

    Prevent duplicating providers when adding a new provider

--- frontend/src/Settings/useProviderSettings.ts
+++ frontend/src/Settings/useProviderSettings.ts
@@ -122,13 +122,17 @@ export const useSaveProviderSettings = <T extends ModelBase>(
     },
     onSuccess: (updatedSettings: T) => {
       queryClient.setQueryData<T[]>([path], (oldData = []) => {
-        if (id) {
-          return oldData.map((item) =>
-            item.id === updatedSettings.id ? updatedSettings : item
-          );
+        const existingIndex = oldData.findIndex(
+          (item) => item.id === updatedSettings.id
+        );
+
+        if (existingIndex === -1) {
+          return [...oldData, updatedSettings];
         }
 
-        return [...oldData, updatedSettings];
+        return oldData.map((item) =>
+          item.id === updatedSettings.id ? updatedSettings : item
+        );
       });
       onSuccess?.(updatedSettings);
     },
