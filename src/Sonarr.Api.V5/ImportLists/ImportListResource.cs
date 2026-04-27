16bcb757cf3574acd31e1f8f50234c756c573d11
Mark McDowall <mark@mcdowall.ca>
2026-04-26 21:57:22 -0700

    Add v5 Import List endpoints

---
+++ src/Sonarr.Api.V5/ImportLists/ImportListResource.cs
@@ -0,0 +1,60 @@
+using NzbDrone.Core.ImportLists;
+using NzbDrone.Core.Tv;
+using Sonarr.Api.V5.Provider;
+
+namespace Sonarr.Api.V5.ImportLists;
+
+public class ImportListResource : ProviderResource<ImportListResource>
+{
+    public bool EnableAutomaticAdd { get; set; }
+    public bool SearchForMissingEpisodes { get; set; }
+    public MonitorTypes ShouldMonitor { get; set; }
+    public NewItemMonitorTypes MonitorNewItems { get; set; }
+    public string? RootFolderPath { get; set; }
+    public int QualityProfileId { get; set; }
+    public SeriesTypes SeriesType { get; set; }
+    public bool SeasonFolder { get; set; }
+    public ImportListType ListType { get; set; }
+    public int ListOrder { get; set; }
+    public TimeSpan MinRefreshInterval { get; set; }
+}
+
+public class ImportListResourceMapper : ProviderResourceMapper<ImportListResource, ImportListDefinition>
+{
+    public override ImportListResource ToResource(ImportListDefinition definition)
+    {
+        var resource = base.ToResource(definition);
+
+        resource.EnableAutomaticAdd = definition.EnableAutomaticAdd;
+        resource.SearchForMissingEpisodes = definition.SearchForMissingEpisodes;
+        resource.ShouldMonitor = definition.ShouldMonitor;
+        resource.MonitorNewItems = definition.MonitorNewItems;
+        resource.RootFolderPath = definition.RootFolderPath;
+        resource.QualityProfileId = definition.QualityProfileId;
+        resource.SeriesType = definition.SeriesType;
+        resource.SeasonFolder = definition.SeasonFolder;
+        resource.ListType = definition.ListType;
+        resource.ListOrder = (int)definition.ListType;
+        resource.MinRefreshInterval = definition.MinRefreshInterval;
+
+        return resource;
+    }
+
+    public override ImportListDefinition ToModel(ImportListResource resource, ImportListDefinition? existingDefinition)
+    {
+        var definition = base.ToModel(resource, existingDefinition);
+
+        definition.EnableAutomaticAdd = resource.EnableAutomaticAdd;
+        definition.SearchForMissingEpisodes = resource.SearchForMissingEpisodes;
+        definition.ShouldMonitor = resource.ShouldMonitor;
+        definition.MonitorNewItems = resource.MonitorNewItems;
+        definition.RootFolderPath = resource.RootFolderPath;
+        definition.QualityProfileId = resource.QualityProfileId;
+        definition.SeriesType = resource.SeriesType;
+        definition.SeasonFolder = resource.SeasonFolder;
+        definition.ListType = resource.ListType;
+        definition.MinRefreshInterval = resource.MinRefreshInterval;
+
+        return definition;
+    }
+}
