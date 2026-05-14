e79077d5fd33d2ee7cb025f13858785f4473e8b3
Mark McDowall <mark@mcdowall.ca>
2026-05-13 19:16:44 -0700

    New: Optional end-to-end encryption setting for Pushover
    Closes #8588

--- src/NzbDrone.Core/Notifications/Pushover/PushoverProxy.cs
+++ src/NzbDrone.Core/Notifications/Pushover/PushoverProxy.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Extensions;
@@ -33,12 +37,26 @@ namespace NzbDrone.Core.Notifications.Pushover
         {
             var requestBuilder = new HttpRequestBuilder(URL).Post();
 
+            var encrypted = settings.EncryptionKey.IsNotNullOrWhiteSpace();
+
+            if (encrypted)
+            {
+                var key = HexStringToBytes(settings.EncryptionKey);
+                title = EncryptField(title, key);
+                message = EncryptField(message, key);
+            }
+
             requestBuilder.AddFormParameter("token", settings.ApiKey)
                           .AddFormParameter("user", settings.UserKey)
                           .AddFormParameter("device", string.Join(",", settings.Devices))
                           .AddFormParameter("title", title)
                           .AddFormParameter("message", message)
                           .AddFormParameter("priority", settings.Priority);
+
+            if (encrypted)
+            {
+                requestBuilder.AddFormParameter("encrypted", 1);
+            }
 
             if ((PushoverPriority)settings.Priority == PushoverPriority.Emergency)
             {
@@ -77,6 +95,62 @@ namespace NzbDrone.Core.Notifications.Pushover
             }
 
             return null;
+        }
+
+        private static string EncryptField(string plaintext, byte[] key)
+        {
+            var compressed = GzipCompress(Encoding.UTF8.GetBytes(plaintext ?? string.Empty));
+
+            using var aes = Aes.Create();
+            aes.KeySize = 256;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = key;
+            aes.GenerateIV();
+
+            var iv = aes.IV;
+            byte[] ciphertext;
+
+            using (var encryptor = aes.CreateEncryptor())
+            {
+                ciphertext = encryptor.TransformFinalBlock(compressed, 0, compressed.Length);
+            }
+
+            var ivAndCiphertext = new byte[iv.Length + ciphertext.Length];
+            Buffer.BlockCopy(iv, 0, ivAndCiphertext, 0, iv.Length);
+            Buffer.BlockCopy(ciphertext, 0, ivAndCiphertext, iv.Length, ciphertext.Length);
+
+            using var hmac = new HMACSHA256(key);
+            var mac = hmac.ComputeHash(ivAndCiphertext);
+
+            var payload = new byte[ivAndCiphertext.Length + mac.Length];
+            Buffer.BlockCopy(ivAndCiphertext, 0, payload, 0, ivAndCiphertext.Length);
+            Buffer.BlockCopy(mac, 0, payload, ivAndCiphertext.Length, mac.Length);
+
+            return Convert.ToBase64String(payload);
+        }
+
+        private static byte[] GzipCompress(byte[] data)
+        {
+            using var output = new MemoryStream();
+            using (var gzip = new GZipStream(output, CompressionLevel.Optimal, leaveOpen: true))
+            {
+                gzip.Write(data, 0, data.Length);
+            }
+
+            return output.ToArray();
+        }
+
+        private static byte[] HexStringToBytes(string hex)
+        {
+            var bytes = new byte[hex.Length / 2];
+
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+
+            return bytes;
         }
     }
 }
