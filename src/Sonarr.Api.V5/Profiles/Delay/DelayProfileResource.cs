8bced0351305e367e549963c784888e81fcdedfc
Mark McDowall <mark@mcdowall.ca>
2026-05-16 15:58:49 -0700

    Add v5 Delay Profile endpoints

---
+++ src/Sonarr.Api.V5/Profiles/Delay/DelayProfileResource.cs
@@ -0,0 +1,63 @@
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Profiles.Delay;
+using Sonarr.Http.REST;
+
+namespace Sonarr.Api.V5.Profiles.Delay;
+
+public class DelayProfileResource : RestResource
+{
+    public bool EnableUsenet { get; set; }
+    public bool EnableTorrent { get; set; }
+    public DownloadProtocol PreferredProtocol { get; set; }
+    public int UsenetDelay { get; set; }
+    public int TorrentDelay { get; set; }
+    public bool BypassIfHighestQuality { get; set; }
+    public bool BypassIfAboveCustomFormatScore { get; set; }
+    public int MinimumCustomFormatScore { get; set; }
+    public int Order { get; set; }
+    public HashSet<int> Tags { get; set; } = [];
+}
+
+public static class DelayProfileResourceMapper
+{
+    public static DelayProfileResource ToResource(this DelayProfile model)
+    {
+        return new DelayProfileResource
+        {
+            Id = model.Id,
+            EnableUsenet = model.EnableUsenet,
+            EnableTorrent = model.EnableTorrent,
+            PreferredProtocol = model.PreferredProtocol,
+            UsenetDelay = model.UsenetDelay,
+            TorrentDelay = model.TorrentDelay,
+            BypassIfHighestQuality = model.BypassIfHighestQuality,
+            BypassIfAboveCustomFormatScore = model.BypassIfAboveCustomFormatScore,
+            MinimumCustomFormatScore = model.MinimumCustomFormatScore,
+            Order = model.Order,
+            Tags = new HashSet<int>(model.Tags)
+        };
+    }
+
+    public static DelayProfile ToModel(this DelayProfileResource resource)
+    {
+        return new DelayProfile
+        {
+            Id = resource.Id,
+            EnableUsenet = resource.EnableUsenet,
+            EnableTorrent = resource.EnableTorrent,
+            PreferredProtocol = resource.PreferredProtocol,
+            UsenetDelay = resource.UsenetDelay,
+            TorrentDelay = resource.TorrentDelay,
+            BypassIfHighestQuality = resource.BypassIfHighestQuality,
+            BypassIfAboveCustomFormatScore = resource.BypassIfAboveCustomFormatScore,
+            MinimumCustomFormatScore = resource.MinimumCustomFormatScore,
+            Order = resource.Order,
+            Tags = new HashSet<int>(resource.Tags)
+        };
+    }
+
+    public static List<DelayProfileResource> ToResource(this IEnumerable<DelayProfile> models)
+    {
+        return models.Select(m => m.ToResource()).ToList();
+    }
+}
