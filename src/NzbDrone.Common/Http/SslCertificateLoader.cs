3bd01858d193b66a4749930f5ceabf693a0e01b0
Tamer Wahba <tamwahba@hotmail.com>
2026-05-27 11:59:33 -0400

    move ssl load exception to separate file

--- src/NzbDrone.Common/Http/SslCertificateLoader.cs
+++ src/NzbDrone.Common/Http/SslCertificateLoader.cs
@@ -1,60 +1,49 @@
 using System.Linq;
 using System.Net.Security;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using NzbDrone.Common.Extensions;
 
-namespace NzbDrone.Common.Http
+namespace NzbDrone.Common.Http;
+public static class SslCertificateLoader
 {
-    public class SslCertificateLoadException : CryptographicException
+    public static SslStreamCertificateContext LoadCertificateContext(string certPath, string keyPath, string certPassword)
     {
-        public SslCertificateLoadException(string message)
-            : base(message)
+        X509Certificate2Collection certificateCollection;
+        X509Certificate2 leafCert;
+
+        var type = X509Certificate2.GetCertContentType(certPath);
+        if (type == X509ContentType.Cert)
         {
-        }
-    }
+            leafCert = X509Certificate2.CreateFromPemFile(certPath, keyPath.IsNullOrWhiteSpace() ? null : keyPath);
 
-    public static class SslCertificateLoader
-    {
-        public static SslStreamCertificateContext LoadCertificateContext(string certPath, string keyPath, string certPassword)
+            certificateCollection = new X509Certificate2Collection();
+            certificateCollection.ImportFromPemFile(certPath);
+
+            var duplicate = certificateCollection.FirstOrDefault(c => c.SerialNumber == leafCert.SerialNumber);
+
+            if (duplicate != null)
+            {
+                certificateCollection.Remove(duplicate);
+            }
+
+            certificateCollection.Insert(0, leafCert);
+        }
+        else if (type == X509ContentType.Pkcs12)
         {
-            X509Certificate2Collection certificateCollection;
-            X509Certificate2 leafCert;
-
-            var type = X509Certificate2.GetCertContentType(certPath);
-            if (type == X509ContentType.Cert)
-            {
-                leafCert = X509Certificate2.CreateFromPemFile(certPath, keyPath.IsNullOrWhiteSpace() ? null : keyPath);
-
-                certificateCollection = new X509Certificate2Collection();
-                certificateCollection.ImportFromPemFile(certPath);
-
-                var duplicate = certificateCollection.FirstOrDefault(c => c.SerialNumber == leafCert.SerialNumber);
-
-                if (duplicate != null)
-                {
-                    certificateCollection.Remove(duplicate);
-                }
-
-                certificateCollection.Insert(0, leafCert);
-            }
-            else if (type == X509ContentType.Pkcs12)
-            {
-                certificateCollection = X509CertificateLoader.LoadPkcs12CollectionFromFile(certPath, certPassword, X509KeyStorageFlags.DefaultKeySet);
-                leafCert = certificateCollection.FirstOrDefault(c => c.HasPrivateKey);
-            }
-            else
-            {
-                throw new SslCertificateLoadException($"Invalid certificate type: {type}");
-            }
-
-            if (leafCert == null)
-            {
-                throw new SslCertificateLoadException(
-                    $"The SSL certificate file {certPath} does not contain a certificate with an associated private key");
-            }
-
-            return SslStreamCertificateContext.Create(leafCert, certificateCollection, offline: true);
+            certificateCollection = X509CertificateLoader.LoadPkcs12CollectionFromFile(certPath, certPassword, X509KeyStorageFlags.DefaultKeySet);
+            leafCert = certificateCollection.FirstOrDefault(c => c.HasPrivateKey);
         }
+        else
+        {
+            throw new SslCertificateLoadException($"Invalid certificate type: {type}");
+        }
+
+        if (leafCert == null)
+        {
+            throw new SslCertificateLoadException(
+                $"The SSL certificate file {certPath} does not contain a certificate with an associated private key");
+        }
+
+        return SslStreamCertificateContext.Create(leafCert, certificateCollection, offline: true);
     }
 }
