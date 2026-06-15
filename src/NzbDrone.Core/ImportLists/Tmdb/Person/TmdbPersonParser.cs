667012382d859011d0f51914dadee6f65a79a5a1
Adolfo Huitron <adolfo@huitron.dev>
2026-06-15 12:37:53 -0700

    refactor: evaluate department names when parsing response

--- src/NzbDrone.Core/ImportLists/Tmdb/Person/TmdbPersonParser.cs
+++ src/NzbDrone.Core/ImportLists/Tmdb/Person/TmdbPersonParser.cs
@@ -6,36 +6,18 @@ namespace NzbDrone.Core.ImportLists.Tmdb.Person;
 
 public class TmdbPersonParser : TmdbParserBase<TmdbCreditsResource>
 {
-    private readonly bool _isIncludingCastCredits;
-    private readonly HashSet<string> _departments;
+    private readonly TmdbPersonSettings _settings;
 
     public TmdbPersonParser(TmdbPersonSettings settings)
     {
-        _isIncludingCastCredits = settings.IncludingCastCredits;
-        _departments = GetDepartmentNames(settings.IncludeDepartmentTypes);
+        _settings = settings;
     }
 
     protected override IEnumerable<ImportListItemInfo> ParseResponse(TmdbCreditsResource resource)
     {
         var items = Enumerable.Empty<ImportListItemInfo>();
-        if (_isIncludingCastCredits && resource.Cast.Count > 0)
-        {
-            items = resource.Cast.Select(AsImportable);
-        }
 
-        if (_departments.Count > 0 && resource.Crew.Count > 0)
-        {
-            items = items.Concat(resource.Crew
-                .Where(c => _departments.Contains(c.Department))
-                .Select(AsImportable));
-        }
-
-        return items;
-    }
-
-    private static HashSet<string> GetDepartmentNames(IEnumerable<int> departmentTypes)
-    {
-        return departmentTypes
+        var departments = _settings.IncludeDepartmentTypes
             .Cast<TmdbDepartmentType>()
             .Select(department => department switch
             {
@@ -43,5 +25,19 @@ public class TmdbPersonParser : TmdbParserBase<TmdbCreditsResource>
                 TmdbDepartmentType.VisualEffects => "Visual Effects",
                 _ => department.ToString()
             }).ToHashSet();
+
+        if (_settings.IncludingCastCredits && resource.Cast.Count > 0)
+        {
+            items = resource.Cast.Select(AsImportable);
+        }
+
+        if (departments.Count > 0 && resource.Crew.Count > 0)
+        {
+            items = items.Concat(resource.Crew
+                .Where(c => departments.Contains(c.Department))
+                .Select(AsImportable));
+        }
+
+        return items;
     }
 }
