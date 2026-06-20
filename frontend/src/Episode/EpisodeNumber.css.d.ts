958c148ee75be9da303f74ac8ddf63f737dcb0f4
Bert Vanhaeke <github@inorigc.top>
2026-06-20 06:37:56 +0200

    v5 Series Details

--- frontend/src/Episode/EpisodeNumber.css.d.ts
+++ frontend/src/Episode/EpisodeNumber.css.d.ts
@@ -2,6 +2,7 @@
 // Please do not change this file!
 interface CssExports {
   'absoluteEpisodeNumber': string;
+  'episodeNumber': string;
   'warning': string;
 }
 export const cssExports: CssExports;
