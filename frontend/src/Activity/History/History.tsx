041d3fac066f60bb1316ea6f1e0cfeeac46b64f8
Bert Vanhaeke <github@inorigc.top>
2026-06-25 18:26:17 +0200

    Redesign history

--- frontend/src/Activity/History/History.tsx
+++ frontend/src/Activity/History/History.tsx
@@ -4,6 +4,8 @@ import LoadingIndicator from 'Components/Loading/LoadingIndicator';
 import FilterMenu from 'Components/Menu/FilterMenu';
 import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
+import PageHeading from 'Components/Page/PageHeading';
+import PageMessage from 'Components/Page/PageMessage';
 import PageToolbar from 'Components/Page/Toolbar/PageToolbar';
 import PageToolbarSpacer from 'Components/Page/Toolbar/PageToolbarSpacer';
 import ToolbarItem from 'Components/Page/Toolbar/ToolbarItem';
@@ -160,6 +162,16 @@ function History() {
       </PageToolbar>
 
       <PageContentBody>
+        <PageHeading
+          scope={`${translate('Activity')} · ${translate('History')}`}
+          title={translate('History')}
+          meta={
+            totalRecords > 0
+              ? [translate('HistoryEventsCount', { count: totalRecords })]
+              : [translate('HistoryNoEvents')]
+          }
+        />
+
         {isFetchingAny && !isAllPopulated ? <LoadingIndicator /> : null}
 
         {!isFetchingAny && hasError ? (
@@ -171,7 +183,7 @@ function History() {
           // wait for the episodes to populate because they are never coming.
 
           isFetched && !hasError && !records.length ? (
-            <Alert kind={kinds.INFO}>{translate('NoHistoryFound')}</Alert>
+            <PageMessage>{translate('NoHistoryFound')}</PageMessage>
           ) : null
         }
 
@@ -198,6 +210,7 @@ function History() {
               page={page}
               totalPages={totalPages}
               totalRecords={totalRecords}
+              showTotalRecords={false}
               isFetching={isFetching}
               onPageSelect={goToPage}
             />
