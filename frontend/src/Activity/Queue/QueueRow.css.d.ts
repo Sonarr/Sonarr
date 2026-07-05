7c13cded6b05005b0ecbf0bedaa400eb62f341f2
Bert Vanhaeke <github@inorigc.top>
2026-07-05 18:14:26 +0200

    v5 Activity and Wanted

--- frontend/src/Activity/Queue/QueueRow.css.d.ts
+++ frontend/src/Activity/Queue/QueueRow.css.d.ts
@@ -6,6 +6,7 @@ interface CssExports {
   'progress': string;
   'protocol': string;
   'quality': string;
+  'unmatchedTitle': string;
 }
 export const cssExports: CssExports;
 export default cssExports;
