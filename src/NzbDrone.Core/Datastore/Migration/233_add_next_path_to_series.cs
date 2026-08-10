df93ff428b5909e97a43bf6c27c637baf9e28011
kushiemoon-dev <122986913+kushiemoon-dev@users.noreply.github.com>
2026-08-10 13:10:11 +0200

    feat: add NextPath migration for deferred series move tracking

---
+++ src/NzbDrone.Core/Datastore/Migration/233_add_next_path_to_series.cs
@@ -0,0 +1,13 @@
+using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration;
+
+[Migration(233)]
+public class add_next_path_to_series : NzbDroneMigrationBase
+{
+    protected override void MainDbUpgrade()
+    {
+        Alter.Table("Series").AddColumn("NextPath").AsString().Nullable();
+    }
+}
