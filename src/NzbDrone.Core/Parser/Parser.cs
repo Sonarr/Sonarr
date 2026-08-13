6b1a9651d34f3704eef0b45faece6369e399b920
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2026-08-14 02:10:42 +0900

    Fixed: Parse spaced season episode file names

--- src/NzbDrone.Core/Parser/Parser.cs
+++ src/NzbDrone.Core/Parser/Parser.cs
@@ -515,6 +515,7 @@ namespace NzbDrone.Core.Parser
 
         // Regex to detect whether the title was reversed.
         private static readonly Regex ReversedTitleRegex = new Regex(@"(?:^|[-._ ])(p027|p0801|\d{2,3}E-?\d{2}S)[-._ ]", RegexOptions.Compiled);
+        private static readonly Regex SpacedSeasonEpisodeFileRegex = new Regex(@"(?<season>S\d{1,4}) +(?<episode>\d{1,4})$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         private static readonly RegexReplace NormalizeRegex = new RegexReplace(@"((?:\b|_)(?<!^)([aà](?!$)|an|the|and|or|of)(?!$)(?:\b|_))|\W|_",
                                                                 string.Empty,
@@ -693,6 +694,7 @@ namespace NzbDrone.Core.Parser
                 }
 
                 var releaseTitle = FileExtensions.RemoveFileExtension(title);
+                var isFileName = releaseTitle != title;
 
                 releaseTitle = releaseTitle.Replace("【", "[").Replace("】", "]");
 
@@ -706,6 +708,11 @@ namespace NzbDrone.Core.Parser
                 }
 
                 var simpleTitle = SimpleTitleRegex.Replace(releaseTitle);
+
+                if (isFileName)
+                {
+                    simpleTitle = SpacedSeasonEpisodeFileRegex.Replace(simpleTitle, "${season}E${episode}");
+                }
 
                 // TODO: Quick fix stripping [url] - prefixes and postfixes.
                 simpleTitle = ParserCommon.WebsitePrefixRegex.Replace(simpleTitle);
