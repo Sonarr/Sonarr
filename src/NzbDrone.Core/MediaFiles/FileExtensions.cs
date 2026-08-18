cd64d0c5a0565a807153d811d71857ccc69be3f1
Adrian Leon <7547280+nairdaleo@users.noreply.github.com>
2026-08-17 18:18:02 -0700

    Fixed: Missing using for IsNullOrWhiteSpace in ParseExtensions

--- src/NzbDrone.Core/MediaFiles/FileExtensions.cs
+++ src/NzbDrone.Core/MediaFiles/FileExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using NzbDrone.Common.Extensions;
 
 namespace NzbDrone.Core.MediaFiles
 {
