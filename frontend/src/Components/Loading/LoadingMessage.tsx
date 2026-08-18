0d74bebb7065d6e6fcacbc617f17068ed4bd02ce
Corgo <94418843+TeamCorgo@users.noreply.github.com>
2026-08-18 16:29:22 -0600

    Remove offensive joke about a sensitive topic

--- frontend/src/Components/Loading/LoadingMessage.tsx
+++ frontend/src/Components/Loading/LoadingMessage.tsx
@@ -14,7 +14,6 @@ const messages = [
   "I could've been faster in Python",
   "Don't forget to rewind your episodes",
   'Congratulations! You are the 1000th visitor.',
-  "HELP! I'm being held hostage and forced to write these stupid lines!",
   'Re-calibrating the internet...',
   "I'll be here all week",
   "Don't forget to tip your waitress",
